27cfadf45a2c7df727d0cf16aa8bcefab342da1b
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2019-06-18 10:43:23 +0200

    Add test assertions
    Add ability to seed random number generator for RABrandes. We ignore
    this test for now, pending future work on RNG seeding. For now, test
    corner cases including all nodes and including no nodes.
    
    Co-Authored-By: Mats Rydberg <mats@neotechnology.com>

--- algo/src/test/java/org/neo4j/graphalgo/impl/betweenness/BetweennessCentralityTest.java
+++ algo/src/test/java/org/neo4j/graphalgo/impl/betweenness/BetweennessCentralityTest.java
@@ -21,7 +21,12 @@ package org.neo4j.graphalgo.impl.betweenness;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 import org.neo4j.graphalgo.HeavyHugeTester;
 import org.neo4j.graphalgo.TestDatabaseCreator;
 import org.neo4j.graphalgo.api.Graph;
@@ -31,6 +36,10 @@ import org.neo4j.graphalgo.core.utils.Pools;
 import org.neo4j.graphdb.Transaction;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
 /**
  *  (A)-->(B)-->(C)-->(D)-->(E)
  *  0.0   3.0   4.0   3.0   0.0
@@ -39,8 +48,18 @@ import org.neo4j.kernel.internal.GraphDatabaseAPI;
  */
 public class BetweennessCentralityTest extends HeavyHugeTester {
 
+    @Rule
+    public MockitoRule rule = MockitoJUnit.rule();
+
     private static GraphDatabaseAPI db;
     private static Graph graph;
+
+    interface TestConsumer {
+        void accept(String name, double centrality);
+    }
+
+    @Mock
+    private TestConsumer testConsumer;
 
     @BeforeClass
     public static void setupGraph() {
@@ -81,6 +100,14 @@ public class BetweennessCentralityTest extends HeavyHugeTester {
                 .load(graphImpl);
     }
 
+    public void verifyMock(TestConsumer mock) {
+        verify(mock, times(1)).accept(eq("a"), eq(0.0));
+        verify(mock, times(1)).accept(eq("b"), eq(3.0));
+        verify(mock, times(1)).accept(eq("c"), eq(4.0));
+        verify(mock, times(1)).accept(eq("d"), eq(3.0));
+        verify(mock, times(1)).accept(eq("e"), eq(0.0));
+    }
+
     private String name(long id) {
         String[] name = {""};
         db.execute("MATCH (n:Node) WHERE id(n) = " + id + " RETURN n.name as name")
@@ -93,28 +120,50 @@ public class BetweennessCentralityTest extends HeavyHugeTester {
 
     @Test
     public void testBC() {
-
         new BetweennessCentrality(graph)
                 .compute()
                 .resultStream()
-                .forEach(r -> System.out.println(name(r.nodeId) + " -> " + r.centrality));
+                .forEach(r -> testConsumer.accept(name(r.nodeId), r.centrality));
+        verifyMock(testConsumer);
     }
 
     @Test
-    public void testRABrandes() {
-
-        new RABrandesBetweennessCentrality(graph, Pools.DEFAULT, 3, new RandomSelectionStrategy(graph, 0.3))
+    public void testRABrandesForceCompleteSampling() {
+        new RABrandesBetweennessCentrality(graph, Pools.DEFAULT, 3, new RandomSelectionStrategy(graph, 1.0))
                 .compute()
                 .resultStream()
-                .forEach(r -> System.out.println(name(r.nodeId) + " -> " + r.centrality));
+                .forEach(r -> testConsumer.accept(name(r.nodeId), r.centrality));
+        verifyMock(testConsumer);
+    }
+
+    @Test
+    public void testRABrandesForceEmptySampling() {
+        new RABrandesBetweennessCentrality(graph, Pools.DEFAULT, 3, new RandomSelectionStrategy(graph, 0.0))
+                .compute()
+                .resultStream()
+                .forEach(r -> testConsumer.accept(name(r.nodeId), r.centrality));
+        verify(testConsumer, times(1)).accept(eq("a"), eq(0.0));
+        verify(testConsumer, times(1)).accept(eq("b"), eq(0.0));
+        verify(testConsumer, times(1)).accept(eq("c"), eq(0.0));
+        verify(testConsumer, times(1)).accept(eq("d"), eq(0.0));
+        verify(testConsumer, times(1)).accept(eq("e"), eq(0.0));
+    }
+
+    @Ignore
+    public void testRABrandes() {
+        new RABrandesBetweennessCentrality(graph, Pools.DEFAULT, 3, new RandomSelectionStrategy(graph, 0.3, 5))
+                .compute()
+                .resultStream()
+                .forEach(r -> testConsumer.accept(name(r.nodeId), r.centrality));
+        verifyMock(testConsumer);
     }
 
     @Test
     public void testPBC() {
-
         new ParallelBetweennessCentrality(graph, Pools.DEFAULT, 4)
                 .compute()
                 .resultStream()
-                .forEach(r -> System.out.println(name(r.nodeId) + " -> " + r.centrality));
+                .forEach(r -> testConsumer.accept(name(r.nodeId), r.centrality));
+        verifyMock(testConsumer);
     }
 }
