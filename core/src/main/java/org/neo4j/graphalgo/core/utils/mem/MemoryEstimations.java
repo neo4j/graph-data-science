66dfb37335777de597a29a29605df172c1d442bd
Martin Junghanns <martin.junghanns@neo4j.com>
2019-06-20 15:09:07 +0200

    Add documentation for MemoryEstimations and Builder
    Co-authored-by: Paul Horn <phorn@avantgarde-labs.de>

--- core/src/main/java/org/neo4j/graphalgo/core/utils/mem/MemoryEstimations.java
+++ core/src/main/java/org/neo4j/graphalgo/core/utils/mem/MemoryEstimations.java
@@ -50,50 +50,140 @@ public final class MemoryEstimations {
         return NULL_ESTIMATION;
     }
 
+    /**
+     * Creates a {@link MemoryEstimation} for the fields of the given class.
+     *
+     * @param instanceType class
+     * @return memory estimation
+     */
     public static MemoryEstimation of(final Class<?> instanceType) {
         return new LeafEstimation("instance", MemoryResidents.fixed(MemoryRange.of(sizeOfInstance(instanceType))));
     }
 
+    /**
+     * Creates a {@link MemoryEstimation} using the given {@link MemoryResident} function.
+     *
+     * Inputs to that function are the {@link GraphDimensions} and the number of available threads.
+     *
+     * @param description description of the sub-component
+     * @param resident function to compute a {@link MemoryRange}
+     * @return memory estimation
+     */
     public static MemoryEstimation of(final String description, final MemoryResident resident) {
         return new LeafEstimation(description, resident);
     }
 
+    /**
+     * Creates a {@link MemoryEstimation} for a given collection of sub-components.
+     *
+     * @param description description of the sub-component
+     * @param components sub-components to add
+     * @return memory estimation
+     */
     private static MemoryEstimation of(final String description, final Collection<MemoryEstimation> components) {
         return new CompositeEstimation(description, components);
     }
 
+    /**
+     * Creates a {@link MemoryEstimation} using the given {@link MemoryEstimationSetup} function.
+     *
+     * Inputs to that function are the {@link GraphDimensions} and the number of available threads.
+     *
+     * @param description description of the sub-component
+     * @param setup function to compute a {@link MemoryEstimation}
+     * @return memory estimation
+     */
     public static MemoryEstimation setup(final String description, MemoryEstimationSetup setup) {
         return new SetupEstimation(description, setup);
     }
 
+    /**
+     * Creates a {@link MemoryEstimation} using the given {@link MemoryEstimationSetup} function.
+     *
+     * Inputs to that function are the {@link GraphDimensions}.
+     *
+     * @param description description of the sub-component
+     * @param fn function to compute a {@link MemoryEstimation}
+     * @return memory estimation
+     */
     public static MemoryEstimation setup(final String description, Function<GraphDimensions, MemoryEstimation> fn) {
         return new SetupEstimation(description, (dimensions, concurrency) -> fn.apply(dimensions));
     }
 
+    /**
+     * Updates a given {@link MemoryEstimation} by applying a {@link MemoryRangeModifier}.
+     *
+     * The description is inherited from {@code delegate.description()}.
+     *
+     * @param delegate memory estimation to modify
+     * @param andThen function to update the memory estimation
+     * @return updated memory estimation
+     */
     public static MemoryEstimation andThen(MemoryEstimation delegate, MemoryRangeModifier andThen) {
         return andThen(delegate.description(), delegate, andThen);
     }
 
+    /**
+     * Updates the memory range of given {@link MemoryEstimation} by applying a given function.
+     *
+     * The description is inherited from {@code delegate.description()}.
+     *
+     * @param delegate memory estimation to modify
+     * @param andThen function to update the memory range of the delegate
+     * @return updated memory estimation
+     */
     public static MemoryEstimation andThen(MemoryEstimation delegate, UnaryOperator<MemoryRange> andThen) {
         return andThen(delegate.description(), delegate, (range, dimensions, concurrency) -> andThen.apply(range));
     }
 
+    /**
+     * Updates a given {@link MemoryEstimation} by applying a {@link MemoryRangeModifier}.
+     *
+     * @param description new description of the memory estimation
+     * @param delegate memory estimation to modify
+     * @param andThen function to update the memory estimation
+     * @return updated memory estimation
+     */
     public static MemoryEstimation andThen(final String description, MemoryEstimation delegate, MemoryRangeModifier andThen) {
         return new AndThenEstimation(description, delegate, andThen);
     }
 
+    /**
+     * Creates a new {@link Builder} with an empty description.
+     *
+     * @return builder
+     */
     public static Builder builder() {
         return new Builder("");
     }
 
+    /**
+     * Creates a new {@link Builder} using the given description.
+     *
+     * @return builder
+     */
     public static Builder builder(final String description) {
         return new Builder(description);
     }
 
+    /**
+     * Creates a new {@link Builder} from the given class type.
+     *
+     * The description is inferred from the class type by {@code type.getSimpleName()}.
+     *
+     * @return builder
+     */
     public static Builder builder(final Class<?> type) {
         return new Builder(type.getSimpleName(), type);
     }
 
+    /**
+     * Creates a new {@link Builder} using the given description and class type.
+     *
+     * @param description description of the component
+     * @param type class type
+     * @return builder
+     */
     public static Builder builder(final String description, final Class<?> type) {
         return new Builder(description, type);
     }
@@ -118,10 +208,26 @@ public final class MemoryEstimations {
             this.parent = parent;
         }
 
+        /**
+         * Starts the construction of new sub-components.
+         *
+         * The returned builder is used to construct the sub-component.
+         * After construction, {@link #endField()} must be called to return to the parent builder.
+         *
+         * @param name description of the sub-component
+         * @param type type to estimate fields for
+         * @return a builder for the sub-component
+         */
         public Builder startField(final String name, final Class<?> type) {
             return new Builder(name, type, this);
         }
 
+        /**
+         * Ends the construction of a sub-component and returns the parent builder.
+         *
+         * @return parent builder
+         * @throws IllegalArgumentException if this is the root builder
+         */
         public Builder endField() {
             if (parent == null) {
                 throw new IllegalArgumentException("Cannot end field from root builder");
@@ -130,63 +236,181 @@ public final class MemoryEstimations {
             return parent;
         }
 
+        /**
+         * Adds a {@link MemoryEstimation} as a sub-component to the builder.
+         * Uses {@link MemoryEstimation#description()} as sub-component name.
+         *
+         * @param estimation new sub-component
+         * @return this builder
+         */
         public Builder add(final MemoryEstimation estimation) {
             components.add(estimation);
             return this;
         }
 
-        public Builder add(final String name, final MemoryEstimation estimation) {
-            components.add(new DelegateEstimation(estimation, name));
+        /**
+         * Adds a {@link MemoryEstimation} as a sub-component to the builder.
+         * Uses the given {@code description} as sub-component name.
+         *
+         * @param description description of the sub-component
+         * @param estimation  new sub-component
+         * @return this builder
+         */
+        public Builder add(final String description, final MemoryEstimation estimation) {
+            components.add(new DelegateEstimation(estimation, description));
             return this;
         }
 
-        public Builder field(final String name, final Class<?> type) {
-            components.add(new LeafEstimation(name, MemoryResidents.fixed(MemoryRange.of(sizeOfInstance(type)))));
+        /**
+         * Adds the memory consumption of the given class as a sub-component to this builder.
+         *
+         * This is shorthand syntax for:
+         * <pre>
+         * {@code
+         * builder.startField(name, type);
+         * builder.endField();
+         * }
+         * </pre>
+         * or
+         * <pre>
+         * {@code
+         * builder.add(name, MemoryEstimations.of(type);
+         * }
+         * </pre>
+         *
+         * @param description description of the sub-component
+         * @param type class type
+         * @return this builder
+         */
+        public Builder field(final String description, final Class<?> type) {
+            components.add(new LeafEstimation(description, MemoryResidents.fixed(MemoryRange.of(sizeOfInstance(type)))));
             return this;
         }
 
+        /**
+         * Adds a fixed amount of memory consumption in bytes to the builder.
+         *
+         * @param description description of the sub-component
+         * @param bytes memory consumption in bytes
+         * @return this builder
+         */
         public Builder fixed(final String description, final long bytes) {
             components.add(new LeafEstimation(description, MemoryResidents.fixed(MemoryRange.of(bytes))));
             return this;
         }
 
+        /**
+         * Adds a fixed memory range to the builder.
+         *
+         * @param description description of the sub-component
+         * @param range memory range
+         * @return this builder
+         */
         public Builder fixed(final String description, final MemoryRange range) {
             components.add(new LeafEstimation(description, MemoryResidents.fixed(range)));
             return this;
         }
 
+        /**
+         * Adds a {@link MemoryEstimation} as a sub-component to the builder.
+         *
+         * The given memory estimation is multiplied by the number of nodes in the graph.
+         *
+         * @param description description of the sub-component
+         * @param estimation new sub-component
+         * @return this builder
+         */
+        public Builder perNode(final String description, final MemoryEstimation estimation) {
+            components.add(new AndThenEstimation(description, estimation, (mem, dim, concurrency) -> mem.times(dim.hugeNodeCount())));
+            return this;
+        }
+
+        /**
+         * Adds a new sub-component to the builder.
+         *
+         * The memory consumption (in bytes) is being computed by the supplied function.
+         * The input to that function is the number of nodes within the graph.
+         *
+         * @param description description of the sub-component
+         * @param fn function to compute memory consumption in bytes
+         * @return this builder
+         */
         public Builder perNode(final String description, final LongUnaryOperator fn) {
             components.add(new LeafEstimation(description, MemoryResidents.perNode(fn)));
             return this;
         }
 
-        public Builder perNode(final String description, final MemoryEstimation subComponent) {
-            components.add(new AndThenEstimation(description, subComponent, (mem, dim, concurrency) -> mem.times(dim.nodeCount())));
-            return this;
-        }
-
-        public Builder perGraphDimension(final String description, final ToLongFunction<GraphDimensions> fn) {
-            components.add(new LeafEstimation(description, MemoryResidents.perDim(fn)));
-            return this;
-        }
-
-        public Builder rangePerGraphDimension(final String description, final Function<GraphDimensions, MemoryRange> fn) {
-            components.add(new LeafEstimation(description, MemoryResidents.perDim(fn)));
-            return this;
-        }
-
+        /**
+         * Adds a new sub-component to the builder.
+         *
+         * The memory consumption is being computed by the supplied function.
+         * The input to that function is the number of nodes within the graph.
+         *
+         * @param description description of the sub-component
+         * @param fn function to compute memory consumption
+         * @return this builder
+         */
         public Builder rangePerNode(final String description, final LongFunction<MemoryRange> fn) {
             components.add(new LeafEstimation(description, MemoryResidents.perNode(fn)));
             return this;
         }
 
+        /**
+         * Adds a new sub-component to the builder.
+         *
+         * The memory consumption (in bytes) is being computed by the supplied function.
+         * The input to that function are the {@link GraphDimensions} of the graph.
+         *
+         * @param description description of the sub-component
+         * @param fn function to compute memory consumption in bytes
+         * @return this builder
+         */
+        public Builder perGraphDimension(final String description, final ToLongFunction<GraphDimensions> fn) {
+            components.add(new LeafEstimation(description, MemoryResidents.perDim(fn)));
+            return this;
+        }
+
+        /**
+         * Adds a new sub-component to the builder.
+         *
+         * The memory consumption is being computed by the supplied function.
+         * The input to that function are the {@link GraphDimensions} of the graph.
+         *
+         * @param description description of the sub-component
+         * @param fn function to compute memory consumption
+         * @return this builder
+         */
+        public Builder rangePerGraphDimension(final String description, final Function<GraphDimensions, MemoryRange> fn) {
+            components.add(new LeafEstimation(description, MemoryResidents.perDim(fn)));
+            return this;
+        }
+
+        /**
+         * Adds a new sub-component to the builder.
+         *
+         * The memory consumption (in bytes) is being computed by the supplied function.
+         * The input to that function is the number of available threads.
+         *
+         * @param description description of the sub-component
+         * @param fn function to compute memory consumption in bytes
+         * @return this builder
+         */
         public Builder perThread(final String description, final IntToLongFunction fn) {
             components.add(new LeafEstimation(description, MemoryResidents.perThread(fn)));
             return this;
         }
 
-        public Builder perThread(final String description, final MemoryEstimation subComponent) {
-            components.add(new AndThenEstimation(description, subComponent, (mem, dim, concurrency) -> mem.times(concurrency)));
+        /**
+         * Adds a {@link MemoryEstimation} as a sub-component to the builder.
+         *
+         * The given memory estimation is multiplied by the number of available threads.
+         *
+         * @param description description of the sub-component
+         * @param estimation new sub-component
+         * @return this builder
+         */
+        public Builder perThread(final String description, final MemoryEstimation estimation) {
+            components.add(new AndThenEstimation(description, estimation, (mem, dim, concurrency) -> mem.times(concurrency)));
             return this;
         }
 
