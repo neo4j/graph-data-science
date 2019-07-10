036b785a29f9e9b54746d4c3cb55d7c32c7864cd
Soeren Reichardt <soren.reichardt@neotechnology.com>
2019-07-10 09:11:55 +0200

    Add missing license header

--- algo/src/test/java/org/neo4j/graphalgo/impl/labelprop/NonStabilizingLabelPropagationTest.java
+++ algo/src/test/java/org/neo4j/graphalgo/impl/labelprop/NonStabilizingLabelPropagationTest.java
@@ -1,3 +1,22 @@
+/*
+ * Copyright (c) 2017-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.neo4j.graphalgo.impl.labelprop;
 
 import org.junit.Before;
@@ -24,7 +43,6 @@ import org.neo4j.test.rule.ImpermanentDatabaseRule;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.neo4j.graphalgo.impl.labelprop.LabelPropagation.LABEL_TYPE;
 import static org.neo4j.graphalgo.impl.labelprop.LabelPropagation.WEIGHT_TYPE;
