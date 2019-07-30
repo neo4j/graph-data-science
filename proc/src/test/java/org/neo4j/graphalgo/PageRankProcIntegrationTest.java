cc2172d788294e33e5450e3b1471e7d5962ba9e4
Soeren Reichardt <soren.reichardt@neotechnology.com>
2019-07-30 16:54:52 +0200

    Add test for PageRank with tolerance parameter
    Co-authored-by: Paul Horn <phorn@avantgarde-labs.de>
    Co-authored-by: Jonatan Jäderberg <jonatan.jaderberg@gmail.com>

--- proc/src/test/java/org/neo4j/graphalgo/PageRankProcIntegrationTest.java
+++ proc/src/test/java/org/neo4j/graphalgo/PageRankProcIntegrationTest.java
@@ -50,49 +50,49 @@ public class PageRankProcIntegrationTest {
     private static Map<Long, Double> weightedExpected = new HashMap<>();
 
     private static final String DB_CYPHER = "" +
-            "CREATE (a:Label1 {name:\"a\"})\n" +
-            "CREATE (b:Label1 {name:\"b\"})\n" +
-            "CREATE (c:Label1 {name:\"c\"})\n" +
-            "CREATE (d:Label1 {name:\"d\"})\n" +
-            "CREATE (e:Label1 {name:\"e\"})\n" +
-            "CREATE (f:Label1 {name:\"f\"})\n" +
-            "CREATE (g:Label1 {name:\"g\"})\n" +
-            "CREATE (h:Label1 {name:\"h\"})\n" +
-            "CREATE (i:Label1 {name:\"i\"})\n" +
-            "CREATE (j:Label1 {name:\"j\"})\n" +
-            "CREATE (k:Label2 {name:\"k\"})\n" +
-            "CREATE (l:Label2 {name:\"l\"})\n" +
-            "CREATE (m:Label2 {name:\"m\"})\n" +
-            "CREATE (n:Label2 {name:\"n\"})\n" +
-            "CREATE (o:Label2 {name:\"o\"})\n" +
-            "CREATE (p:Label2 {name:\"p\"})\n" +
-            "CREATE (q:Label2 {name:\"q\"})\n" +
-            "CREATE (r:Label2 {name:\"r\"})\n" +
-            "CREATE (s:Label2 {name:\"s\"})\n" +
-            "CREATE (t:Label2 {name:\"t\"})\n" +
-            "CREATE\n" +
-            "  (b)-[:TYPE1{foo:1.0, equalWeight: 1.0}]->(c),\n" +
+                                            "CREATE (a:Label1 {name:\"a\"})\n" +
+                                            "CREATE (b:Label1 {name:\"b\"})\n" +
+                                            "CREATE (c:Label1 {name:\"c\"})\n" +
+                                            "CREATE (d:Label1 {name:\"d\"})\n" +
+                                            "CREATE (e:Label1 {name:\"e\"})\n" +
+                                            "CREATE (f:Label1 {name:\"f\"})\n" +
+                                            "CREATE (g:Label1 {name:\"g\"})\n" +
+                                            "CREATE (h:Label1 {name:\"h\"})\n" +
+                                            "CREATE (i:Label1 {name:\"i\"})\n" +
+                                            "CREATE (j:Label1 {name:\"j\"})\n" +
+                                            "CREATE (k:Label2 {name:\"k\"})\n" +
+                                            "CREATE (l:Label2 {name:\"l\"})\n" +
+                                            "CREATE (m:Label2 {name:\"m\"})\n" +
+                                            "CREATE (n:Label2 {name:\"n\"})\n" +
+                                            "CREATE (o:Label2 {name:\"o\"})\n" +
+                                            "CREATE (p:Label2 {name:\"p\"})\n" +
+                                            "CREATE (q:Label2 {name:\"q\"})\n" +
+                                            "CREATE (r:Label2 {name:\"r\"})\n" +
+                                            "CREATE (s:Label2 {name:\"s\"})\n" +
+                                            "CREATE (t:Label2 {name:\"t\"})\n" +
+                                            "CREATE\n" +
+                                            "  (b)-[:TYPE1{foo:1.0, equalWeight: 1.0}]->(c),\n" +
 
-            "  (c)-[:TYPE1{foo:1.2, equalWeight: 1.0}]->(b),\n" +
+                                            "  (c)-[:TYPE1{foo:1.2, equalWeight: 1.0}]->(b),\n" +
 
-            "  (d)-[:TYPE1{foo:1.3, equalWeight: 1.0}]->(a),\n" +
-            "  (d)-[:TYPE1{foo:1.7, equalWeight: 1.0}]->(b),\n" +
+                                            "  (d)-[:TYPE1{foo:1.3, equalWeight: 1.0}]->(a),\n" +
+                                            "  (d)-[:TYPE1{foo:1.7, equalWeight: 1.0}]->(b),\n" +
 
-            "  (e)-[:TYPE1{foo:6.1, equalWeight: 1.0}]->(b),\n" +
-            "  (e)-[:TYPE1{foo:2.2, equalWeight: 1.0}]->(d),\n" +
-            "  (e)-[:TYPE1{foo:1.5, equalWeight: 1.0}]->(f),\n" +
+                                            "  (e)-[:TYPE1{foo:6.1, equalWeight: 1.0}]->(b),\n" +
+                                            "  (e)-[:TYPE1{foo:2.2, equalWeight: 1.0}]->(d),\n" +
+                                            "  (e)-[:TYPE1{foo:1.5, equalWeight: 1.0}]->(f),\n" +
 
-            "  (f)-[:TYPE1{foo:10.5, equalWeight: 1.0}]->(b),\n" +
-            "  (f)-[:TYPE1{foo:2.9, equalWeight: 1.0}]->(e),\n" +
+                                            "  (f)-[:TYPE1{foo:10.5, equalWeight: 1.0}]->(b),\n" +
+                                            "  (f)-[:TYPE1{foo:2.9, equalWeight: 1.0}]->(e),\n" +
 
-            "  (g)-[:TYPE2{foo:3.2, equalWeight: 1.0}]->(b),\n" +
-            "  (g)-[:TYPE2{foo:5.3, equalWeight: 1.0}]->(e),\n" +
-            "  (h)-[:TYPE2{foo:9.5, equalWeight: 1.0}]->(b),\n" +
-            "  (h)-[:TYPE2{foo:0.3, equalWeight: 1.0}]->(e),\n" +
-            "  (i)-[:TYPE2{foo:5.4, equalWeight: 1.0}]->(b),\n" +
-            "  (i)-[:TYPE2{foo:3.2, equalWeight: 1.0}]->(e),\n" +
-            "  (j)-[:TYPE2{foo:9.5, equalWeight: 1.0}]->(e),\n" +
-            "  (k)-[:TYPE2{foo:4.2, equalWeight: 1.0}]->(e)\n";
+                                            "  (g)-[:TYPE2{foo:3.2, equalWeight: 1.0}]->(b),\n" +
+                                            "  (g)-[:TYPE2{foo:5.3, equalWeight: 1.0}]->(e),\n" +
+                                            "  (h)-[:TYPE2{foo:9.5, equalWeight: 1.0}]->(b),\n" +
+                                            "  (h)-[:TYPE2{foo:0.3, equalWeight: 1.0}]->(e),\n" +
+                                            "  (i)-[:TYPE2{foo:5.4, equalWeight: 1.0}]->(b),\n" +
+                                            "  (i)-[:TYPE2{foo:3.2, equalWeight: 1.0}]->(e),\n" +
+                                            "  (j)-[:TYPE2{foo:9.5, equalWeight: 1.0}]->(e),\n" +
+                                            "  (k)-[:TYPE2{foo:4.2, equalWeight: 1.0}]->(e)\n";
 
     @AfterClass
     public static void tearDown() throws Exception {
@@ -156,9 +156,9 @@ public class PageRankProcIntegrationTest {
     public void testPageRankStream() throws Exception {
         final Map<Long, Double> actual = new HashMap<>();
         runQuery(
-                "CALL algo.pageRank.stream('Label1', 'TYPE1', {graph:'"+graphImpl+"'}) YIELD nodeId, score",
+                "CALL algo.pageRank.stream('Label1', 'TYPE1', {graph:'" + graphImpl + "'}) YIELD nodeId, score",
                 row -> actual.put(
-                        (Long)row.get("nodeId"),
+                        (Long) row.get("nodeId"),
                         (Double) row.get("score")));
 
         assertMapEquals(expected, actual);
@@ -168,9 +168,9 @@ public class PageRankProcIntegrationTest {
     public void testWeightedPageRankStream() throws Exception {
         final Map<Long, Double> actual = new HashMap<>();
         runQuery(
-                "CALL algo.pageRank.stream('Label1', 'TYPE1', {graph:'"+graphImpl+"', weightProperty: 'foo'}) YIELD nodeId, score",
+                "CALL algo.pageRank.stream('Label1', 'TYPE1', {graph:'" + graphImpl + "', weightProperty: 'foo'}) YIELD nodeId, score",
                 row -> actual.put(
-                        (Long)row.get("nodeId"),
+                        (Long) row.get("nodeId"),
                         (Double) row.get("score")));
 
         assertMapEquals(weightedExpected, actual);
@@ -180,9 +180,9 @@ public class PageRankProcIntegrationTest {
     public void testWeightedPageRankWithCachedWeightsStream() throws Exception {
         final Map<Long, Double> actual = new HashMap<>();
         runQuery(
-                "CALL algo.pageRank.stream('Label1', 'TYPE1', {graph:'"+graphImpl+"', weightProperty: 'foo', cacheWeights: true}) YIELD nodeId, score",
+                "CALL algo.pageRank.stream('Label1', 'TYPE1', {graph:'" + graphImpl + "', weightProperty: 'foo', cacheWeights: true}) YIELD nodeId, score",
                 row -> actual.put(
-                        (Long)row.get("nodeId"),
+                        (Long) row.get("nodeId"),
                         (Double) row.get("score")));
 
         assertMapEquals(weightedExpected, actual);
@@ -192,9 +192,9 @@ public class PageRankProcIntegrationTest {
     public void testWeightedPageRankWithAllRelationshipsEqualStream() throws Exception {
         final Map<Long, Double> actual = new HashMap<>();
         runQuery(
-                "CALL algo.pageRank.stream('Label1', 'TYPE1', {graph:'"+graphImpl+"', weightProperty: 'equalWeight'}) YIELD nodeId, score",
+                "CALL algo.pageRank.stream('Label1', 'TYPE1', {graph:'" + graphImpl + "', weightProperty: 'equalWeight'}) YIELD nodeId, score",
                 row -> actual.put(
-                        (Long)row.get("nodeId"),
+                        (Long) row.get("nodeId"),
                         (Double) row.get("score")));
 
         assertMapEquals(expected, actual);
@@ -204,7 +204,7 @@ public class PageRankProcIntegrationTest {
     @Test
     public void testPageRankWriteBack() throws Exception {
         runQuery(
-                "CALL algo.pageRank('Label1', 'TYPE1', {graph:'"+graphImpl+"'}) YIELD writeMillis, write, writeProperty",
+                "CALL algo.pageRank('Label1', 'TYPE1', {graph:'" + graphImpl + "'}) YIELD writeMillis, write, writeProperty",
                 row -> {
                     assertTrue(row.getBoolean("write"));
                     assertEquals("pagerank", row.getString("writeProperty"));
@@ -219,7 +219,7 @@ public class PageRankProcIntegrationTest {
     @Test
     public void testWeightedPageRankWriteBack() throws Exception {
         runQuery(
-                "CALL algo.pageRank('Label1', 'TYPE1', {graph:'"+graphImpl+"', weightProperty: 'foo'}) YIELD writeMillis, write, writeProperty",
+                "CALL algo.pageRank('Label1', 'TYPE1', {graph:'" + graphImpl + "', weightProperty: 'foo'}) YIELD writeMillis, write, writeProperty",
                 row -> {
                     assertTrue(row.getBoolean("write"));
                     assertEquals("pagerank", row.getString("writeProperty"));
@@ -234,7 +234,7 @@ public class PageRankProcIntegrationTest {
     @Test
     public void testPageRankWriteBackUnderDifferentProperty() throws Exception {
         runQuery(
-                "CALL algo.pageRank('Label1', 'TYPE1', {writeProperty:'foobar', graph:'"+graphImpl+"'}) YIELD writeMillis, write, writeProperty",
+                "CALL algo.pageRank('Label1', 'TYPE1', {writeProperty:'foobar', graph:'" + graphImpl + "'}) YIELD writeMillis, write, writeProperty",
                 row -> {
                     assertTrue(row.getBoolean("write"));
                     assertEquals("foobar", row.getString("writeProperty"));
@@ -249,7 +249,7 @@ public class PageRankProcIntegrationTest {
     @Test
     public void testPageRankParallelWriteBack() throws Exception {
         runQuery(
-                "CALL algo.pageRank('Label1', 'TYPE1', {batchSize:3, write:true, graph:'"+graphImpl+"'}) YIELD writeMillis, write, writeProperty",
+                "CALL algo.pageRank('Label1', 'TYPE1', {batchSize:3, write:true, graph:'" + graphImpl + "'}) YIELD writeMillis, write, writeProperty",
                 row -> assertTrue(
                         "write time not set",
                         row.getNumber("writeMillis").intValue() >= 0));
@@ -261,7 +261,7 @@ public class PageRankProcIntegrationTest {
     public void testPageRankParallelExecution() throws Exception {
         final Map<Long, Double> actual = new HashMap<>();
         runQuery(
-                "CALL algo.pageRank.stream('Label1', 'TYPE1', {batchSize:2, graph:'"+graphImpl+"'}) YIELD nodeId, score",
+                "CALL algo.pageRank.stream('Label1', 'TYPE1', {batchSize:2, graph:'" + graphImpl + "'}) YIELD nodeId, score",
                 row -> {
                     final long nodeId = row.getNumber("nodeId").longValue();
                     actual.put(nodeId, (Double) row.get("score"));
@@ -272,7 +272,19 @@ public class PageRankProcIntegrationTest {
     @Test
     public void testPageRankWithToleranceParam() throws Exception {
         runQuery("CALL algo.pageRank('Label1', 'TYPE1', {tolerance: 0.0001, batchSize:3, graph:'"+graphImpl+"'}) YIELD nodes, iterations",
-                row -> {});
+                row -> assertEquals((long)PageRankProc.DEFAULT_ITERATIONS, (long)row.getNumber("iterations")));
+
+        runQuery("CALL algo.pageRank('Label1', 'TYPE1', {tolerance: 100.0, batchSize:3, graph:'"+graphImpl+"'}) YIELD nodes, iterations",
+                row -> assertEquals(1L, (long)row.getNumber("iterations")));
+
+        runQuery(
+                "CALL algo.pageRank('Label1', 'TYPE1', {tolerance: 0.20010237991809848, batchSize:3, graph:'" + graphImpl + "'}) YIELD nodes, iterations",
+                row -> assertEquals(4L, (long) row.getNumber("iterations")));
+
+        runQuery(
+                "CALL algo.pageRank('Label1', 'TYPE1', {tolerance: 0.20010237991809843, batchSize:3, graph:'" + graphImpl + "'}) YIELD nodes, iterations",
+                row -> assertEquals(5L, (long) row.getNumber("iterations")));
+
     }
 
     private static void runQuery(
@@ -286,7 +298,6 @@ public class PageRankProcIntegrationTest {
             Map<String, Object> params,
             Consumer<Result.ResultRow> check) {
         try (Result result = db.execute(query, params)) {
-            System.out.println(result.resultAsString());
             result.accept(row -> {
                 check.accept(row);
                 return true;
