001b3f88dd7fbc2e32a9ed627605e72dc7b67c94
Martin Junghanns <martin.junghanns@neo4j.com>
2019-10-02 22:38:00 +0200

    Remove package move TODO

--- algo/src/main/java/org/neo4j/graphalgo/beta/pregel/Pregel.java
+++ algo/src/main/java/org/neo4j/graphalgo/beta/pregel/Pregel.java
@@ -17,7 +17,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-// TODO: move to `beta` package namespace
 package org.neo4j.graphalgo.beta.pregel;
 
 import com.carrotsearch.hppc.BitSet;
