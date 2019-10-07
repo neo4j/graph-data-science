3fae9d287c6e023becca975ae9289d0d2c9e742e
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2019-10-07 11:48:07 +0200

    Refactor centrality result test
    Configure mockito to mock final classes so we can enable some tests.
    Fix bug in labs test runner so that all tests run.
    
    Co-Authored-By: Martin Junghanns <martin.junghanns@neotechnology.com>

--- contrib/labs/src/test/java/org/neo4j/graphalgo/results/NormalizedCentralityResultTest.java
+++ contrib/labs/src/test/java/org/neo4j/graphalgo/results/NormalizedCentralityResultTest.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.graphalgo.impl.results;
+package org.neo4j.graphalgo.results;
 
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -25,28 +25,91 @@ import org.mockito.ArgumentCaptor;
 import org.mockito.ArgumentMatcher;
 import org.neo4j.graphalgo.core.utils.paged.HugeDoubleArray;
 import org.neo4j.graphalgo.core.write.Exporter;
+import org.neo4j.graphalgo.impl.results.CentralityResult;
+import org.neo4j.graphalgo.impl.results.HugeDoubleArrayResult;
+import org.neo4j.graphalgo.impl.results.PartitionedDoubleArrayResult;
 import org.neo4j.graphalgo.impl.utils.Normalization;
 
 import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.argThat;
-import static org.mockito.Mockito.eq;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
-class CentralityResultTest {
+public class NormalizedCentralityResultTest {
 
     @Test
-    void doubleArrayResult() {
-        HugeDoubleArrayResult result = new HugeDoubleArrayResult(HugeDoubleArray.of(1, 2, 3, 4));
+    public void maxNormalization() {
+        CentralityResult centralityResult = mock(CentralityResult.class);
+        when(centralityResult.score(0)).thenReturn(1.0);
+        when(centralityResult.score(1)).thenReturn(2.0);
+        when(centralityResult.score(2)).thenReturn(3.0);
+        when(centralityResult.score(3)).thenReturn(4.0);
+        when(centralityResult.computeMax()).thenReturn(4.0);
 
-        assertEquals(4.0, result.computeMax(), 0.01);
-        assertEquals(10.0, result.computeL1Norm(), 0.01);
-        assertEquals(5.477225575051661, result.computeL2Norm(), 0.01);
+        CentralityResult normalizedResult = Normalization.MAX.apply(centralityResult);
+
+        assertEquals(0.25, normalizedResult.score(0), 0.01);
+        assertEquals(0.5, normalizedResult.score(1), 0.01);
+        assertEquals(0.75, normalizedResult.score(2), 0.01);
+        assertEquals(1.0, normalizedResult.score(3), 0.01);
     }
 
-    @Disabled
+    @Test
+    public void noNormalization() {
+        CentralityResult centralityResult = mock(CentralityResult.class);
+        when(centralityResult.score(0)).thenReturn(1.0);
+        when(centralityResult.score(1)).thenReturn(2.0);
+        when(centralityResult.score(2)).thenReturn(3.0);
+        when(centralityResult.score(3)).thenReturn(4.0);
+        when(centralityResult.computeMax()).thenReturn(4.0);
+
+        CentralityResult normalizedResult = Normalization.NONE.apply(centralityResult);
+
+        assertEquals(1.0, normalizedResult.score(0), 0.01);
+        assertEquals(2.0, normalizedResult.score(1), 0.01);
+        assertEquals(3.0, normalizedResult.score(2), 0.01);
+        assertEquals(4.0, normalizedResult.score(3), 0.01);
+    }
+
+    @Test
+    public void l2Norm() {
+        CentralityResult centralityResult = mock(CentralityResult.class);
+        when(centralityResult.score(0)).thenReturn(1.0);
+        when(centralityResult.score(1)).thenReturn(2.0);
+        when(centralityResult.score(2)).thenReturn(3.0);
+        when(centralityResult.score(3)).thenReturn(4.0);
+        when(centralityResult.computeL2Norm()).thenReturn(4.0);
+
+        CentralityResult normalizedResult = Normalization.L2NORM.apply(centralityResult);
+
+        assertEquals(0.25, normalizedResult.score(0), 0.01);
+        assertEquals(0.5, normalizedResult.score(1), 0.01);
+        assertEquals(0.75, normalizedResult.score(2), 0.01);
+        assertEquals(1.0, normalizedResult.score(3), 0.01);
+    }
+
+    @Test
+    public void l1Norm() {
+        CentralityResult centralityResult = mock(CentralityResult.class);
+        when(centralityResult.score(0)).thenReturn(1.0);
+        when(centralityResult.score(1)).thenReturn(2.0);
+        when(centralityResult.score(2)).thenReturn(3.0);
+        when(centralityResult.score(3)).thenReturn(4.0);
+        when(centralityResult.computeL1Norm()).thenReturn(4.0);
+
+        CentralityResult normalizedResult = Normalization.L1NORM.apply(centralityResult);
+
+        assertEquals(0.25, normalizedResult.score(0), 0.01);
+        assertEquals(0.5, normalizedResult.score(1), 0.01);
+        assertEquals(0.75, normalizedResult.score(2), 0.01);
+        assertEquals(1.0, normalizedResult.score(3), 0.01);
+    }
+
+    @Test
     void doubleArrayResultExport() {
         String property = "eigenvector";
         final HugeDoubleArray given = HugeDoubleArray.of(1, 2, 3, 4);
@@ -68,17 +131,6 @@ class CentralityResultTest {
     }
 
     @Test
-    void partitionedPrimitiveDoubleArrayResult() {
-        double[][] partitions = new double[][]{{1.0, 2.0}, {3.0, 4.0}};
-        long[] starts = new long[]{0, 2};
-        PartitionedDoubleArrayResult result = new PartitionedDoubleArrayResult(partitions, starts);
-
-        assertEquals(4.0, result.computeMax(), 0.01);
-        assertEquals(10.0, result.computeL1Norm(), 0.01);
-        assertEquals(5.477225575051661, result.computeL2Norm(), 0.01);
-    }
-
-    @Disabled
     void partitionedPrimitiveDoubleArrayResultExport() {
         String property = "eigenvector";
         double[][] partitions = new double[][]{{1.0, 2.0}, {3.0, 4.0}};
@@ -92,17 +144,6 @@ class CentralityResultTest {
     }
 
     @Test
-    void partitionedDoubleArrayResult() {
-        double[][] partitions = new double[][]{{1.0, 2.0}, {3.0, 4.0}};
-        long[] starts = new long[]{0, 2};
-        PartitionedDoubleArrayResult result = new PartitionedDoubleArrayResult(partitions, starts);
-
-        assertEquals(4.0, result.computeMax(), 0.01);
-        assertEquals(10.0, result.computeL1Norm(), 0.01);
-        assertEquals(5.477225575051661, result.computeL2Norm(), 0.01);
-    }
-
-    @Disabled
     void partitionedDoubleArrayResultExport() {
         String property = "eigenvector";
         double[][] partitions = new double[][]{{1.0, 2.0}, {3.0, 4.0}};
