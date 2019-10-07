62688aa7ae20d5ea13d6399368744d9f999766fc
Martin Junghanns <martin.junghanns@neo4j.com>
2019-10-07 17:40:18 +0200

    Comment InfoMap benchmarks
    Co-Authored-By: Jonatan Jäderberg <jonatan.jaderberg@gmail.com>

--- benchmark/src/jmh/java/org/neo4j/graphalgo/bench/InfoMapYelp.java
+++ benchmark/src/jmh/java/org/neo4j/graphalgo/bench/InfoMapYelp.java
@@ -1,74 +1,74 @@
-/*
- * Copyright (c) 2017 "Neo4j, Inc." <http://neo4j.com>
- *
- * This file is part of Neo4j Graph Algorithms <http://github.com/neo4j-contrib/neo4j-graph-algorithms>.
- *
- * Neo4j Graph Algorithms is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
-package org.neo4j.graphalgo.bench;
-
-import org.neo4j.graphalgo.core.utils.Pools;
-import org.neo4j.graphalgo.core.utils.TerminationFlag;
-import org.neo4j.graphalgo.impl.infomap.InfoMap;
-import org.openjdk.jmh.annotations.Benchmark;
-import org.openjdk.jmh.annotations.BenchmarkMode;
-import org.openjdk.jmh.annotations.Fork;
-import org.openjdk.jmh.annotations.Measurement;
-import org.openjdk.jmh.annotations.Mode;
-import org.openjdk.jmh.annotations.OutputTimeUnit;
-import org.openjdk.jmh.annotations.Threads;
-import org.openjdk.jmh.annotations.Timeout;
-import org.openjdk.jmh.annotations.Warmup;
-import org.openjdk.jmh.results.format.ResultFormatType;
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
-
-import java.util.concurrent.TimeUnit;
-
-@Threads(1)
-@Fork(value = 1, jvmArgs = {"-ea", "-Xms4g", "-Xmx8g", "-XX:+UseG1GC"})
-@Warmup(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
-@Measurement(iterations = 10, time = 2, timeUnit = TimeUnit.SECONDS)
-@BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
-@Timeout(time = 2, timeUnit = TimeUnit.MINUTES)
-public class InfoMapYelp {
-
-    @Benchmark
-    public InfoMap infoMap(final InfoMapGraph infoMapGraph) {
-        InfoMap infoMap = InfoMap.unweighted(
-                infoMapGraph.graph,
-                infoMapGraph.pageRanks,
-                infoMapGraph.threshold,
-                infoMapGraph.tau,
-                Pools.FJ_POOL,
-                infoMapGraph.concurrency,
-                infoMapGraph.progressLogger,
-                TerminationFlag.RUNNING_TRUE
-        );
-        return infoMap.compute();
-    }
-
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(InfoMapYelp.class.getSimpleName())
-                .addProfiler("gc")
-                .result(InfoMapYelp.class.getSimpleName() + "-jmh.json")
-                .resultFormat(ResultFormatType.JSON)
-                .build();
-        new Runner(opt).run();
-    }
-}
+///*
+// * Copyright (c) 2017 "Neo4j, Inc." <http://neo4j.com>
+// *
+// * This file is part of Neo4j Graph Algorithms <http://github.com/neo4j-contrib/neo4j-graph-algorithms>.
+// *
+// * Neo4j Graph Algorithms is free software: you can redistribute it and/or modify
+// * it under the terms of the GNU General Public License as published by
+// * the Free Software Foundation, either version 3 of the License, or
+// * (at your option) any later version.
+// *
+// * This program is distributed in the hope that it will be useful,
+// * but WITHOUT ANY WARRANTY; without even the implied warranty of
+// * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// * GNU General Public License for more details.
+// *
+// * You should have received a copy of the GNU General Public License
+// * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// */
+//package org.neo4j.graphalgo.bench;
+//
+//import org.neo4j.graphalgo.core.utils.Pools;
+//import org.neo4j.graphalgo.core.utils.TerminationFlag;
+//import org.neo4j.graphalgo.impl.infomap.InfoMap;
+//import org.openjdk.jmh.annotations.Benchmark;
+//import org.openjdk.jmh.annotations.BenchmarkMode;
+//import org.openjdk.jmh.annotations.Fork;
+//import org.openjdk.jmh.annotations.Measurement;
+//import org.openjdk.jmh.annotations.Mode;
+//import org.openjdk.jmh.annotations.OutputTimeUnit;
+//import org.openjdk.jmh.annotations.Threads;
+//import org.openjdk.jmh.annotations.Timeout;
+//import org.openjdk.jmh.annotations.Warmup;
+//import org.openjdk.jmh.results.format.ResultFormatType;
+//import org.openjdk.jmh.runner.Runner;
+//import org.openjdk.jmh.runner.RunnerException;
+//import org.openjdk.jmh.runner.options.Options;
+//import org.openjdk.jmh.runner.options.OptionsBuilder;
+//
+//import java.util.concurrent.TimeUnit;
+//
+//@Threads(1)
+//@Fork(value = 1, jvmArgs = {"-ea", "-Xms4g", "-Xmx8g", "-XX:+UseG1GC"})
+//@Warmup(iterations = 5, time = 2, timeUnit = TimeUnit.SECONDS)
+//@Measurement(iterations = 10, time = 2, timeUnit = TimeUnit.SECONDS)
+//@BenchmarkMode(Mode.AverageTime)
+//@OutputTimeUnit(TimeUnit.MILLISECONDS)
+//@Timeout(time = 2, timeUnit = TimeUnit.MINUTES)
+//public class InfoMapYelp {
+//
+//    @Benchmark
+//    public InfoMap infoMap(final InfoMapGraph infoMapGraph) {
+//        InfoMap infoMap = InfoMap.unweighted(
+//                infoMapGraph.graph,
+//                infoMapGraph.pageRanks,
+//                infoMapGraph.threshold,
+//                infoMapGraph.tau,
+//                Pools.FJ_POOL,
+//                infoMapGraph.concurrency,
+//                infoMapGraph.progressLogger,
+//                TerminationFlag.RUNNING_TRUE
+//        );
+//        return infoMap.compute();
+//    }
+//
+//    public static void main(String[] args) throws RunnerException {
+//        Options opt = new OptionsBuilder()
+//                .include(InfoMapYelp.class.getSimpleName())
+//                .addProfiler("gc")
+//                .result(InfoMapYelp.class.getSimpleName() + "-jmh.json")
+//                .resultFormat(ResultFormatType.JSON)
+//                .build();
+//        new Runner(opt).run();
+//    }
+//}
