d87fdf37a559ed47698ac0c514e4f328fe31061d
Soeren Reichardt <soren.reichardt@neotechnology.com>
2019-11-07 13:13:54 +0100

    Add procedure test for modularity optimization
    Co-authored-by: Max Kießling <max.kiessling@neotechnology.com>

---
+++ proc/src/test/java/org/neo4j/graphalgo/ModularityOptimizationProcTest.java
@@ -0,0 +1,173 @@
+/*
+ * Copyright (c) 2017-2019 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package org.neo4j.graphalgo;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.core.loading.GraphCatalog;
+import org.neo4j.internal.kernel.api.exceptions.KernelException;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.neo4j.graphalgo.CommunityHelper.assertCommunities;
+
+class ModularityOptimizationProcTest extends ProcTestBase {
+
+    static final String DB_CYPHER =
+        "CREATE" +
+        "  (a:Node {name:'a', seed1: 0, seed2: 1})" +
+        ", (b:Node {name:'b', seed1: 0, seed2: 1})" +
+        ", (c:Node {name:'c', seed1: 2, seed2: 1})" +
+        ", (d:Node {name:'d', seed1: 2, seed2: 42})" +
+        ", (e:Node {name:'e', seed1: 2, seed2: 42})" +
+        ", (f:Node {name:'f', seed1: 2, seed2: 42})" +
+        ", (a)-[:TYPE {weight: 0.01}]->(b)" +
+        ", (a)-[:TYPE {weight: 5.0}]->(e)" +
+        ", (a)-[:TYPE {weight: 5.0}]->(f)" +
+        ", (b)-[:TYPE {weight: 5.0}]->(c)" +
+        ", (b)-[:TYPE {weight: 5.0}]->(d)" +
+        ", (c)-[:TYPE {weight: 0.01}]->(e)" +
+        ", (f)-[:TYPE {weight: 0.01}]->(d)";
+
+    @BeforeEach
+    void setup() throws KernelException {
+        db = TestDatabaseCreator.createTestDatabase();
+        registerProcedures(ModularityOptimizationProc.class, GraphLoadProc.class);
+        db.execute(DB_CYPHER);
+    }
+
+    @AfterEach
+    void tearDown() {
+        db.shutdown();
+        GraphCatalog.removeAllLoadedGraphs();
+    }
+
+    @Test
+    void writingOnImplicitlyLoadedGraph() {
+        String query = "CALL algo.beta.modularityOptimization.write(" +
+                       "    null, null, {" +
+                       "        write: false, direction: 'BOTH'" +
+                       "    }" +
+                       ")";
+
+        runQuery(query, row -> {
+            assertEquals(true, row.getBoolean("didConverge"));
+            assertEquals(0.12244, row.getNumber("modularity").doubleValue(),0.001);
+            assertEquals(2, row.getNumber("communityCount").longValue());
+            assertTrue(row.getNumber("ranIterations").longValue() <= 3);
+        });
+    }
+
+    @Test
+    void writingOnExplicitlyLoadedGraph() {
+        String loadQuery = "CALL algo.graph.load(" +
+                           "    'myGraph', null, null, {" +
+                           "        graph: 'huge', direction: 'BOTH'" +
+                           "    }" +
+                           ")";
+        runQuery(loadQuery);
+        String query = "CALL algo.beta.modularityOptimization.write(" +
+                       "    null, null, {" +
+                       "        graph: 'myGraph', write: false, direction: 'BOTH'" +
+                       "    }" +
+                       ")";
+
+        runQuery(query, row -> {
+            assertEquals(true, row.getBoolean("didConverge"));
+            assertEquals(0.12244, row.getNumber("modularity").doubleValue(),0.001);
+            assertEquals(2, row.getNumber("communityCount").longValue());
+            assertTrue(row.getNumber("ranIterations").longValue() <= 3);
+        });
+    }
+
+    @Test
+    void weightedWritingOnexplicitlyLoadedGraph() {
+        String loadQuery = "CALL algo.graph.load(" +
+                           "    'myGraph', null, null, {" +
+                           "        graph: 'huge', direction: 'BOTH', relationshipProperties: 'weight'" +
+                           "    }" +
+                           ")";
+        runQuery(loadQuery);
+        String query = "CALL algo.beta.modularityOptimization.write(" +
+                       "    null, null, {" +
+                       "        graph: 'myGraph', write: false, direction: 'BOTH'" +
+                       "    }" +
+                       ")";
+
+        runQuery(query, row -> {
+            assertEquals(true, row.getBoolean("didConverge"));
+            assertEquals(0.4985, row.getNumber("modularity").doubleValue(),0.001);
+            assertEquals(2, row.getNumber("communityCount").longValue());
+            assertTrue(row.getNumber("ranIterations").longValue() <= 3);
+        });
+    }
+
+    @Test
+    void streamingOnExplicitlyLoadedGraph() {
+        String loadQuery = "CALL algo.graph.load(" +
+                           "    'myGraph', null, null, {" +
+                           "        graph: 'huge', direction: 'BOTH'" +
+                           "    }" +
+                           ")";
+        runQuery(loadQuery);
+        String query = "CALL algo.beta.modularityOptimization.stream(" +
+                       "    null, null, {" +
+                       "        graph: 'myGraph', direction: 'BOTH'" +
+                       "    }" +
+                       ") YIELD nodeId, community";
+
+        long[] communities = new long[6];
+        runQuery(query, row -> {
+            long nodeId = row.getNumber("nodeId").longValue();
+            communities[(int)nodeId] = row.getNumber("community").longValue();
+        });
+
+        System.out.println(Arrays.toString(communities));
+        assertCommunities(communities, new long[]{0, 1, 2, 4}, new long[]{3, 5});
+    }
+
+    @Test
+    void weightedStreamingOnExplicitlyLoadedGraph() {
+        String loadQuery = "CALL algo.graph.load(" +
+                           "    'myGraph', null, null, {" +
+                           "        graph: 'huge', direction: 'BOTH', relationshipProperties: 'weight'" +
+                           "    }" +
+                           ")";
+        runQuery(loadQuery);
+        String query = "CALL algo.beta.modularityOptimization.stream(" +
+                       "    null, null, {" +
+                       "        graph: 'myGraph', direction: 'BOTH'" +
+                       "    }" +
+                       ") YIELD nodeId, community";
+
+        long[] communities = new long[6];
+        runQuery(query, row -> {
+            long nodeId = row.getNumber("nodeId").longValue();
+            communities[(int)nodeId] = row.getNumber("community").longValue();
+        });
+
+        System.out.println(Arrays.toString(communities));
+        assertCommunities(communities, new long[]{0, 4, 5} , new long[]{1, 2, 3});
+    }
+}
