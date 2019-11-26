39fedb8bb4585652653f524df7043c133f9db733
Paul Horn <paul.horn@neotechnology.com>
2019-11-26 10:34:26 +0100

    Use 4 spaces to indent the generated source files according to our style.
    Co-Authored-By: Mats Rydberg <mats@neotechnology.com>
    Co-Authored-By: Max Kießling <max.kiessling@neotechnology.com>

--- config-generator/src/main/java/org/neo4j/graphalgo/proc/GenerateConfiguration.java
+++ config-generator/src/main/java/org/neo4j/graphalgo/proc/GenerateConfiguration.java
@@ -90,7 +90,11 @@ final class GenerateConfiguration {
         PackageElement rootPackage = elementUtils.getPackageOf(config.root());
         String packageName = rootPackage.getQualifiedName().toString();
         TypeSpec typeSpec = process(config, packageName, className);
-        return JavaFile.builder(packageName, typeSpec).skipJavaLangImports(true).build();
+        return JavaFile
+            .builder(packageName, typeSpec)
+            .indent("    ")
+            .skipJavaLangImports(true)
+            .build();
     }
 
     private TypeSpec process(ConfigParser.Spec config, String packageName, String generatedClassName) {
