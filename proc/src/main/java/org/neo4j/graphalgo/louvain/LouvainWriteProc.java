2c9bb5168da5943a0daaa3b8e54741c9a20c52c2
Paul Horn <developer@knutwalker.de>
2019-12-10 14:15:10 +0100

    Remove redundant else
    Co-Authored-By: Jonatan Jäderberg <jonatan.jaderberg@gmail.com>

--- proc/src/main/java/org/neo4j/graphalgo/louvain/LouvainWriteProc.java
+++ proc/src/main/java/org/neo4j/graphalgo/louvain/LouvainWriteProc.java
@@ -188,29 +188,29 @@ public class LouvainWriteProc extends LouvainProcBase<LouvainWriteConfig> {
                     new double[0], Collections.emptyMap()
                 )
             );
-        } else {
-            LouvainWriteConfig config = computeResult.config();
-            Graph graph = computeResult.graph();
-            Louvain louvain = computeResult.algorithm();
-
-            WriteResultBuilder builder = new WriteResultBuilder(config, callContext, computeResult.tracker());
-
-            builder.setLoadMillis(computeResult.createMillis());
-            builder.setComputeMillis(computeResult.computeMillis());
-            builder.withNodeCount(graph.nodeCount());
-            builder
-                .withLevels(louvain.levels())
-                .withModularity(louvain.modularities()[louvain.levels() - 1])
-                .withModularities(louvain.modularities())
-                .withCommunityFunction(louvain::getCommunity);
-
-            if (write && !config.writeProperty().isEmpty()) {
-                writeNodeProperties(builder, computeResult);
-                graph.releaseProperties();
-            }
-
-            return Stream.of(builder.build());
         }
+
+        LouvainWriteConfig config = computeResult.config();
+        Graph graph = computeResult.graph();
+        Louvain louvain = computeResult.algorithm();
+
+        WriteResultBuilder builder = new WriteResultBuilder(config, callContext, computeResult.tracker());
+
+        builder.setLoadMillis(computeResult.createMillis());
+        builder.setComputeMillis(computeResult.computeMillis());
+        builder.withNodeCount(graph.nodeCount());
+        builder
+            .withLevels(louvain.levels())
+            .withModularity(louvain.modularities()[louvain.levels() - 1])
+            .withModularities(louvain.modularities())
+            .withCommunityFunction(louvain::getCommunity);
+
+        if (write && !config.writeProperty().isEmpty()) {
+            writeNodeProperties(builder, computeResult);
+            graph.releaseProperties();
+        }
+
+        return Stream.of(builder.build());
     }
 
     @Override
