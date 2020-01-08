5cd8dcf38194c659431caee35cbb4188c0ffd615
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2020-01-08 23:47:54 +0100

    Rename Pearson similarity test

--- labs/proc/src/test/java/org/neo4j/graphalgo/similarity/ModernPearsonSimilarityTest.java
+++ labs/proc/src/test/java/org/neo4j/graphalgo/similarity/ModernPearsonSimilarityTest.java
@@ -34,7 +34,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
-class PearsonSimilarityTest {
+class ModernPearsonSimilarityTest {
 
     @Test
     void identicalVectors() {
