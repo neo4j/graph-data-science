316a0f5193a8751bd8a4b3091d7518dc97ee54a1
Paul Horn <paul.horn@neotechnology.com>
2020-01-09 14:05:53 +0100

    Remove unnecessary import

--- annotations/src/main/java/org/neo4j/graphalgo/core/CypherMapWrapper.java
+++ annotations/src/main/java/org/neo4j/graphalgo/core/CypherMapWrapper.java
@@ -24,7 +24,6 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
