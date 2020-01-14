6fe8a91a60f172177af1c23e8b752922806a2a6f
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2020-01-14 06:53:49 +0000

    Use `GdsCypher` to build the queries
    - Replaced String queries with GdsCypher
    - Parametrized the test

--- proc/src/test/java/org/neo4j/graphalgo/louvain/WeightedLouvainStreamProcTest.java
+++ proc/src/test/java/org/neo4j/graphalgo/louvain/WeightedLouvainStreamProcTest.java
@@ -23,22 +23,35 @@ package org.neo4j.graphalgo.louvain;
 import org.intellij.lang.annotations.Language;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.neo4j.graphalgo.BaseProcTest;
+import org.neo4j.graphalgo.ElementIdentifier;
+import org.neo4j.graphalgo.GdsCypher;
 import org.neo4j.graphalgo.GetNodeFunc;
 import org.neo4j.graphalgo.GraphLoadProc;
+import org.neo4j.graphalgo.ImmutablePropertyMapping;
+import org.neo4j.graphalgo.NodeProjections;
+import org.neo4j.graphalgo.Projection;
 import org.neo4j.graphalgo.QueryRunner;
+import org.neo4j.graphalgo.RelationshipProjection;
+import org.neo4j.graphalgo.RelationshipProjections;
 import org.neo4j.graphalgo.TestDatabaseCreator;
+import org.neo4j.graphalgo.core.DeduplicationStrategy;
 import org.neo4j.graphalgo.core.loading.GraphCatalog;
 import org.neo4j.graphalgo.newapi.GraphCreateProc;
+import org.neo4j.graphalgo.newapi.ImmutableGraphCreateFromStoreConfig;
 import org.neo4j.graphdb.Result;
 import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.internal.kernel.api.exceptions.KernelException;
 
 import java.util.Map;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 import static org.neo4j.graphalgo.compat.MapUtil.map;
 
 class WeightedLouvainStreamProcTest extends BaseProcTest {
@@ -59,7 +72,10 @@ class WeightedLouvainStreamProcTest extends BaseProcTest {
             dbBuilder -> dbBuilder.setConfig(GraphDatabaseSettings.procedure_unrestricted, "algo.*")
         );
 
-        @Language("Cypher") String cypher =
+        registerProcedures(LouvainStreamProc.class, LouvainWriteProc.class, GraphLoadProc.class, GraphCreateProc.class);
+        registerFunctions(GetNodeFunc.class);
+
+        @Language("Cypher") String createDataQuery =
             "CREATE" +
             "  (nAlice:User {name: 'Alice', seed: 42})" +
             ", (nBridget:User {name: 'Bridget', seed: 42})" +
@@ -75,26 +91,24 @@ class WeightedLouvainStreamProcTest extends BaseProcTest {
             ", (nMark)-[:LINK {weight: 1}]->(nMichael)" +
             ", (nMichael)-[:LINK {weight: 1}]->(nMark)";
 
-        registerProcedures(LouvainStreamProc.class, LouvainWriteProc.class, GraphLoadProc.class, GraphCreateProc.class);
-        registerFunctions(GetNodeFunc.class);
-        runQuery(cypher);
-        runQuery("CALL gds.graph.create(" +
-                 "    'weightedGraph'," +
-                 "    {" +
-                 "      Node: {" +
-                 "        label: 'User'," +
-                 "        properties: ['seed']" +
-                 "      }" +
-                 "    }," +
-                 "    {" +
-                 "      LINK: {" +
-                 "        type: 'LINK'," +
-                 "        projection: 'UNDIRECTED'," +
-                 "        aggregation: 'NONE'," +
-                 "        properties: ['weight']" +
-                 "      }" +
-                 "    }" +
-                 ")");
+        runQuery(createDataQuery);
+
+        String graphCreateQuery = GdsCypher.call()
+            .withNodeLabel("User")
+            .withNodeProperty("seed")
+            .withRelationshipType(
+                "LINK",
+                RelationshipProjection.of(
+                    "LINK",
+                    Projection.UNDIRECTED,
+                    DeduplicationStrategy.NONE
+                )
+            )
+            .withRelationshipProperty("weight")
+            .graphCreate("weightedGraph")
+            .yields();
+
+        runQuery(graphCreateQuery);
     }
 
     @AfterEach
@@ -102,36 +116,54 @@ class WeightedLouvainStreamProcTest extends BaseProcTest {
         GraphCatalog.removeAllLoadedGraphs();
     }
 
-    @Test
-    void weightedLouvainTest() {
-        String query = "CALL gds.louvain.stream('weightedGraph') " +
-                       "YIELD nodeId, communityId, communityIds\n" +
-                       "RETURN algo.asNode(nodeId).name as name, communityId, communityIds\n" +
-                       "ORDER BY name ASC";
-
+    @ParameterizedTest(name = "{1}")
+    @MethodSource("graphQueries")
+    void weightedLouvainTest(String query, String graphMode) {
         QueryRunner.runQueryWithRowConsumer(db, query, this::assertLouvainResultRow);
     }
 
-    @Test
-    void weightedLouvainImplicitGraphTest() {
-        String query = "CALL gds.louvain.stream({\n" +
-                       "    nodeProjection: ['User'],\n" +
-                       "    relationshipProjection: {\n" +
-                       "        LINK: {\n" +
-                       "            type: 'LINK',\n" +
-                       "            projection: 'UNDIRECTED',\n" +
-                       "            aggregation: 'NONE',\n" +
-                       "            properties: ['weight']\n" +
-                       "        }\n" +
-                       "    },\n" +
-                       "    weightProperty: 'weight'\n" +
-                       "}) YIELD nodeId, communityId, communityIds\n" +
-                       "RETURN algo.asNode(nodeId).name as name, communityId, communityIds\n" +
-                       "ORDER BY name ASC";
-
-        QueryRunner.runQueryWithRowConsumer(db, query, this::assertLouvainResultRow);
+    static Stream<Arguments> graphQueries() {
+        return Stream.of(
+            arguments(
+                GdsCypher
+                    .call()
+                    .explicitCreation("weightedGraph")
+                    .algo("louvain")
+                    .streamMode()
+                    .yields("nodeId", "communityId", "communityIds")
+                    .concat(" RETURN algo.asNode(nodeId).name as name, communityId, communityIds")
+                    .concat(" ORDER BY name ASC"),
+                "explicit graph"
+            ),
+            arguments(
+                GdsCypher
+                    .call()
+                    .implicitCreation(
+                        ImmutableGraphCreateFromStoreConfig.builder()
+                            .graphName("implicitWeightedGraph")
+                            .nodeProjection(NodeProjections.fromString("User"))
+                            .relationshipProjection(RelationshipProjections.builder()
+                                .putProjection(
+                                    ElementIdentifier.of("LINK"),
+                                    RelationshipProjection.builder()
+                                        .type("LINK")
+                                        .projection(Projection.UNDIRECTED)
+                                        .aggregation(DeduplicationStrategy.NONE)
+                                        .addProperty(ImmutablePropertyMapping.builder().propertyKey("weight").build())
+                                        .build()
+                                ).build()
+                            ).build()
+                    )
+                    .algo("louvain")
+                    .streamMode()
+                    .yields("nodeId", "communityId", "communityIds")
+                    .concat(" RETURN algo.asNode(nodeId).name as name, communityId, communityIds")
+                    .concat(" ORDER BY name ASC"),
+                "implicit graph"
+            )
+        );
     }
-    
+
     private void assertLouvainResultRow(Result.ResultRow row) {
         String computedName = row.getString("name");
         Object computedCommunityId = row.get("communityId");
