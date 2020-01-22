158a5942c2455797ff6fdcf3834e7ecf31f18999
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2020-01-22 12:57:00 +0100

    Require suppression reason
    and help enforce closing results in benchmarks.
    
    Co-Authored-By: Paul Horn <paul.horn@neotechnology.com>

--- annotations/src/main/java/org/neo4j/graphalgo/annotation/SuppressForbidden.java
+++ annotations/src/main/java/org/neo4j/graphalgo/annotation/SuppressForbidden.java
@@ -27,4 +27,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.CLASS)
 @Target({ElementType.CONSTRUCTOR, ElementType.FIELD, ElementType.METHOD, ElementType.TYPE})
 public @interface SuppressForbidden {
+    String reason();
 }
