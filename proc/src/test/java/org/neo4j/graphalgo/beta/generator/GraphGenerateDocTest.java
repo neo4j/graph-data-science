f6fb6c42eb759e2023dfa3c684bf2b8d658a6372
Paul Horn <paul.horn@neotechnology.com>
2020-01-22 18:14:52 +0100

    Remove unnecessary setup code from generate doc test
    Co-Authored-By: Veselin Nikolov <veselin.nikolov@neotechnology.com>
    Signed-off-by: Paul Horn <paul.horn@neotechnology.com>

--- proc/src/test/java/org/neo4j/graphalgo/beta/generator/GraphGenerateDocTest.java
+++ proc/src/test/java/org/neo4j/graphalgo/beta/generator/GraphGenerateDocTest.java
@@ -25,11 +25,9 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.neo4j.graphalgo.BaseProcTest;
-import org.neo4j.graphalgo.GetNodeFunc;
 import org.neo4j.graphalgo.TestDatabaseCreator;
 import org.neo4j.graphalgo.core.loading.GraphCatalog;
 import org.neo4j.graphdb.Result;
-import org.neo4j.graphdb.factory.GraphDatabaseSettings;
 import org.neo4j.internal.kernel.api.exceptions.KernelException;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -38,32 +36,10 @@ final class GraphGenerateDocTest extends BaseProcTest {
 
     private static final String NL = System.lineSeparator();
 
-    public static final String DB_CYPHER =
-        "CREATE" +
-        "  (alice:User {name: 'Alice'})" +
-        ", (bridget:User {name: 'Bridget'})" +
-        ", (charles:User {name: 'Charles'})" +
-        ", (doug:User {name: 'Doug'})" +
-        ", (mark:User {name: 'Mark'})" +
-        ", (michael:User {name: 'Michael'})" +
-        ", (alice)-[:FOLLOWS {score: 1}]->(doug)" +
-        ", (alice)-[:FOLLOWS {score: 2}]->(bridget)" +
-        ", (alice)-[:FOLLOWS {score: 5}]->(charles)" +
-        ", (mark)-[:FOLLOWS {score: 1.5}]->(doug)" +
-        ", (mark)-[:FOLLOWS {score: 4.5}]->(michael)" +
-        ", (bridget)-[:FOLLOWS {score: 1.5}]->(doug)" +
-        ", (charles)-[:FOLLOWS {score: 2}]->(doug)" +
-        ", (michael)-[:FOLLOWS {score: 1.5}]->(doug)";
-
     @BeforeEach
     void setupGraph() throws KernelException {
-        db = TestDatabaseCreator.createTestDatabase(builder ->
-            builder.setConfig(GraphDatabaseSettings.procedure_unrestricted, "gds.*")
-        );
-
+        db = TestDatabaseCreator.createTestDatabase();
         registerProcedures(GraphGenerateProc.class);
-        registerFunctions(GetNodeFunc.class);
-        runQuery(DB_CYPHER);
     }
 
     @AfterEach
