3e613e3cb3378270d39315e9db78dec438b2d301
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2020-02-18 21:03:30 +0100

    Add neo4j-adapter to be used for 4.0

---
+++ compatibility/4.0/neo4j-adapter/src/main/java/org/neo4j/graphalgo/compat/MapUtil.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.compat;
+
+import java.util.Map;
+
+/**
+ * Compatibility class for {@link org.neo4j.helpers.collection.MapUtil}.
+ * By using this class we reduce the number of references to the Neo4j helper class,
+ * which means we break less when the Neo4j helper class moves between Neo4j versions.
+ */
+public final class MapUtil {
+
+    private MapUtil() {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Delegates to {@link org.neo4j.helpers.collection.MapUtil#map(Object...)} and allows static import.
+     */
+    public static Map<String, Object> map(Object... objects) {
+        return org.neo4j.helpers.collection.MapUtil.map(objects);
+    }
+
+    /**
+     * Delegates to {@link org.neo4j.helpers.collection.MapUtil#map(Map, Object...)} and allows static import.
+     */
+    public static Map<String, Object> map(Map<String, Object> targetMap, Object... objects) {
+        return org.neo4j.helpers.collection.MapUtil.map(targetMap, objects);
+    }
+
+    /**
+     * Delegates to {@link org.neo4j.helpers.collection.MapUtil#genericMap(Object...)} and allows static import.
+     */
+    public static <K, V> Map<K, V> genericMap(Object... objects) {
+        return org.neo4j.helpers.collection.MapUtil.genericMap(objects);
+    }
+
+    /**
+     * Delegates to {@link org.neo4j.helpers.collection.MapUtil#genericMap(Map, Object...)} and allows static import.
+     */
+    public static <K, V> Map<K, V> genericMap(Map<K, V> targetMap, Object... objects) {
+        return org.neo4j.helpers.collection.MapUtil.genericMap(targetMap, objects);
+    }
+}
---
+++ compatibility/4.0/neo4j-adapter/src/main/java/org/neo4j/graphalgo/compat/StatementConstantsProxy.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.compat;
+
+import org.neo4j.kernel.api.StatementConstants;
+
+public final class StatementConstantsProxy {
+
+    public static final int NO_SUCH_RELATIONSHIP_TYPE = StatementConstants.NO_SUCH_RELATIONSHIP_TYPE;
+    public static final int NO_SUCH_LABEL = StatementConstants.NO_SUCH_LABEL;
+    public static final int NO_SUCH_PROPERTY_KEY = StatementConstants.NO_SUCH_PROPERTY_KEY;
+    public static final int ANY_RELATIONSHIP_TYPE = StatementConstants.ANY_RELATIONSHIP_TYPE;
+
+    private StatementConstantsProxy() {
+        throw new UnsupportedOperationException();
+    }
+}
