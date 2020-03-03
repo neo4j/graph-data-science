5c1090a0e8cbd9ce6d7b925cfa22414c5b6dc9e9
Mats Rydberg <mats@neotechnology.com>
2020-03-03 15:23:24 +0100

    Rename GDS license configuration key
    Co-authored-by: Veselin Nikolov <veselin.nikolov@neotechnology.com>

--- core/src/main/java/org/neo4j/graphalgo/config/ConcurrencyValidation.java
+++ core/src/main/java/org/neo4j/graphalgo/config/ConcurrencyValidation.java
@@ -27,7 +27,7 @@ import org.neo4j.kernel.configuration.Settings;
 public interface ConcurrencyValidation {
 
     Setting<Boolean> CORE_LIMITATION_SETTING = Settings.setting(
-        "gds.concurrency.unlimited",
+        "gds.enterprise.licensed",
         Settings.BOOLEAN,
         "false"
     );
