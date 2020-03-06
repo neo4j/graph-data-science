0296fd93efebaf611b1e73d1b8c48e4ea483d33c
Jacob Sznajdman <breakanalysis@gmail.com>
2020-03-06 17:02:59 +0100

    OCD stream procedure

---
+++ alpha/alpha-proc/src/main/java/org/neo4j/graphalgo/ocd/OverlappingCommunityDetectionBaseProc.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.ocd;
+
+import org.neo4j.graphalgo.AlgoBaseProc;
+import org.neo4j.graphalgo.AlgorithmFactory;
+import org.neo4j.graphalgo.AlphaAlgorithmFactory;
+import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.graphalgo.core.concurrency.Pools;
+import org.neo4j.graphalgo.core.utils.ProgressLogger;
+import org.neo4j.graphalgo.core.utils.TerminationFlag;
+import org.neo4j.graphalgo.core.utils.paged.AllocationTracker;
+import org.neo4j.graphalgo.impl.ocd.CommunityAffiliations;
+import org.neo4j.graphalgo.impl.ocd.ConductanceAffiliationInitializer;
+import org.neo4j.graphalgo.impl.ocd.OverlappingCommunityDetection;
+import org.neo4j.graphalgo.impl.ocd.OverlappingCommunityDetectionBaseConfig;
+import org.neo4j.logging.Log;
+
+abstract class OverlappingCommunityDetectionBaseProc<CONFIG extends OverlappingCommunityDetectionBaseConfig> extends AlgoBaseProc<OverlappingCommunityDetection, CommunityAffiliations, CONFIG> {
+    protected static final String DESCRIPTION = "The BIGCLAM overlapping community detection algorithm can detect non-overlapping, overlapping and nested community structures at scale";
+
+    @Override
+    protected AlgorithmFactory<OverlappingCommunityDetection, CONFIG> algorithmFactory(CONFIG config) {
+        return new AlphaAlgorithmFactory<OverlappingCommunityDetection, CONFIG>() {
+            @Override
+            public OverlappingCommunityDetection build(
+                Graph graph, CONFIG configuration, AllocationTracker tracker, Log log
+            ) {
+                ConductanceAffiliationInitializer initializer = new ConductanceAffiliationInitializer(
+                    transaction,
+                    Pools.DEFAULT,
+                    tracker,
+                    log,
+                    config.concurrency()
+                );
+                return new OverlappingCommunityDetection(
+                    graph,
+                    initializer,
+                    transaction,
+                    Pools.DEFAULT,
+                    tracker,
+                    log
+                )
+                    .withProgressLogger(ProgressLogger.wrap(log, "OverlappingCommunityDetection"))
+                    .withTerminationFlag(TerminationFlag.wrap(transaction));
+            }
+        };
+    }
+}
---
+++ alpha/alpha-proc/src/test/java/org/neo4j/graphalgo/ocd/OverlappingCommunityDetectionStreamProcTest.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.ocd;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.BaseProcTest;
+import org.neo4j.graphalgo.GetNodeFunc;
+import org.neo4j.graphalgo.TestDatabaseCreator;
+import org.neo4j.graphdb.factory.GraphDatabaseSettings;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class OverlappingCommunityDetectionStreamProcTest extends BaseProcTest {
+    /**
+     * (a)-- (b)--(d)--(e)
+     * \T1/       \T2/
+     * (c)   (g)  (f)
+     * \  /T3\
+     * (h)--(i)
+     */
+    String dbCypher() {
+        return
+            "CREATE " +
+            "  (a:Node {name: 'a'})" +
+            ", (b:Node {name: 'b'})" +
+            ", (c:Node {name: 'c'})" +
+            ", (d:Node {name: 'd'})" +
+            ", (e:Node {name: 'e'})" +
+            ", (f:Node {name: 'f'})" +
+            ", (g:Node {name: 'g'})" +
+            ", (h:Node {name: 'h'})" +
+            ", (i:Node {name: 'i'})" +
+            ", (a)-[:TYPE]->(b)" +
+            ", (b)-[:TYPE]->(c)" +
+            ", (c)-[:TYPE]->(a)" +
+            ", (c)-[:TYPE]->(h)" +
+            ", (d)-[:TYPE]->(e)" +
+            ", (e)-[:TYPE]->(f)" +
+            ", (f)-[:TYPE]->(d)" +
+            ", (b)-[:TYPE]->(d)" +
+            ", (g)-[:TYPE]->(h)" +
+            ", (h)-[:TYPE]->(i)" +
+            ", (i)-[:TYPE]->(g)";
+    }
+
+    @BeforeEach
+    void setup() throws Exception {
+        db = TestDatabaseCreator.createTestDatabase((builder) ->
+            builder.setConfig(GraphDatabaseSettings.procedure_unrestricted, "gds.util.*")
+        );
+        registerProcedures(OverlappingCommunityDetectionStreamProc.class);
+        registerFunctions(GetNodeFunc.class);
+        runQuery(dbCypher());
+    }
+
+    @AfterEach
+    void shutdownGraph() {
+        db.shutdown();
+    }
+
+    @Test
+    void stream() {
+        String q = "CALL gds.alpha.ocd.stream({nodeProjection: \"*\", relationshipProjection: {`*`: {type: \"*\", orientation: \"UNDIRECTED\"}}})" +
+                   " YIELD nodeId, communityIds, scores";
+//                   " RETURN gds.util.asNode(nodeId).name as node, [gds.util.asNodes(communityIds)] as community, conductance";
+
+        Set<String> actualRowDescriptions = new HashSet<>();
+
+/*
+        runQueryWithRowConsumer(q, row -> {
+            System.out.println(row);
+        });
+*/
+
+        runQueryWithResultConsumer(q, result -> System.out.println(result.resultAsString()));
+    }
+}
