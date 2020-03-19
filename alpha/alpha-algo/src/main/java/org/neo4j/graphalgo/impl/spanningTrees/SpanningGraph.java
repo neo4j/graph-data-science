f996c1aa79af1e7b82cc95c394f780d50a5e0f12
Florentin Dörre <florentin.dorre@neotechnology.com>
2020-03-19 09:35:15 +0100

    Inline variable and rephrase change log entry
    Co-authored-by: Co-Authored-By: Martin Junghanns <martin.junghanns@neotechnology.com>

--- alpha/alpha-algo/src/main/java/org/neo4j/graphalgo/impl/spanningTrees/SpanningGraph.java
+++ alpha/alpha-algo/src/main/java/org/neo4j/graphalgo/impl/spanningTrees/SpanningGraph.java
@@ -57,8 +57,7 @@ public class SpanningGraph extends FilterGraph {
     public void forEachRelationship(long nodeId, double fallbackValue, RelationshipWithPropertyConsumer consumer) {
         int parent = spanningTree.parent[Math.toIntExact(nodeId)];
         if (parent != -1) {
-            double property = relationshipProperty(parent, nodeId, fallbackValue);
-            consumer.accept(parent, nodeId, property);
+            consumer.accept(parent, nodeId, relationshipProperty(parent, nodeId, fallbackValue));
         }
     }
 
