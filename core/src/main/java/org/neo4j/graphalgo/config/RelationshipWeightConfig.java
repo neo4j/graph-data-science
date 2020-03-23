28226fe6c559f6aff6d5b57cee6d25e1aecca9ef
Soeren Reichardt <soren.reichardt@neotechnology.com>
2020-03-23 15:35:23 +0100

    Add trim to null conversions for node and relationship weights

--- core/src/main/java/org/neo4j/graphalgo/config/RelationshipWeightConfig.java
+++ core/src/main/java/org/neo4j/graphalgo/config/RelationshipWeightConfig.java
@@ -21,10 +21,12 @@ package org.neo4j.graphalgo.config;
 
 import org.immutables.value.Value;
 import org.jetbrains.annotations.Nullable;
+import org.neo4j.graphalgo.annotation.Configuration;
 
 public interface RelationshipWeightConfig {
 
     @Value.Default
+    @Configuration.ConvertWith("org.apache.commons.lang3.StringUtils#trimToNull")
     default @Nullable String relationshipWeightProperty() {
         return null;
     }
