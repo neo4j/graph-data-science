623ce16bdf16f48a37700c32c61aff288c34b298
Max Kießling <max.kiessling@neotechnology.com>
2020-04-02 11:03:20 +0200

    Rename GraphStoreCatalogTest to GraphStoreTest
    Co-authored-by: Sören Reichardt <soren.reichardt@neotechnology.com>

--- core/src/test/java/org/neo4j/graphalgo/core/loading/GraphStoreTest.java
+++ core/src/test/java/org/neo4j/graphalgo/core/loading/GraphStoreTest.java
@@ -19,34 +19,233 @@
  */
 package org.neo4j.graphalgo.core.loading;
 
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.neo4j.graphalgo.AlgoTestBase;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.neo4j.graphalgo.ElementIdentifier;
+import org.neo4j.graphalgo.NodeProjection;
 import org.neo4j.graphalgo.Orientation;
 import org.neo4j.graphalgo.PropertyMapping;
+import org.neo4j.graphalgo.PropertyMappings;
+import org.neo4j.graphalgo.RelationshipProjection;
 import org.neo4j.graphalgo.StoreLoaderBuilder;
 import org.neo4j.graphalgo.TestDatabaseCreator;
+import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.graphalgo.compat.GraphDbApi;
+import org.neo4j.graphalgo.core.Aggregation;
+import org.neo4j.graphalgo.core.GraphLoader;
 import org.neo4j.graphalgo.core.huge.AdjacencyList;
 import org.neo4j.graphalgo.core.huge.AdjacencyOffsets;
 import org.neo4j.graphalgo.core.huge.HugeGraph;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
+import static java.util.Collections.singletonList;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.neo4j.graphalgo.AbstractProjections.PROJECT_ALL;
+import static org.neo4j.graphalgo.QueryRunner.runQuery;
+import static org.neo4j.graphalgo.TestGraph.Builder.fromGdl;
+import static org.neo4j.graphalgo.TestSupport.assertGraphEquals;
 
-class GraphStoreTest extends AlgoTestBase {
+class GraphStoreTest {
+
+    public static final ElementIdentifier LABEL_A = ElementIdentifier.of("A");
+    public static final ElementIdentifier LABEL_B = ElementIdentifier.of("B");
+    private GraphDbApi db;
 
     @BeforeEach
     void setup() {
         db = TestDatabaseCreator.createTestDatabase();
+        runQuery(db, " CREATE (a:A {nodeProperty: 33})" +
+                     " CREATE (b:B {nodeProperty: 42})" +
+                     " CREATE (c:Ignore)" +
+                     " CREATE (a)-[:T1 {property1: 42, property2: 1337}]->(b)" +
+                     " CREATE (a)-[:T2 {property1: 43}]->(b)" +
+                     " CREATE (a)-[:T3 {property2: 1338}]->(b)" +
+                     " CREATE (a)-[:T1 {property1: 33}]->(c)" +
+                     " CREATE (c)-[:T1 {property1: 33}]->(a)" +
+                     " CREATE (b)-[:T1 {property1: 33}]->(c)" +
+                     " CREATE (c)-[:T1 {property1: 33}]->(b)");
     }
 
     @AfterEach
-    void shutdown() {
+    void tearDown() {
         db.shutdown();
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("validFilterParameters")
+    void testFilteringGraphsByRelationships(String desc, List<String> relTypes, Optional<String> relProperty, String expectedGraph) {
+        GraphLoader graphLoader = new StoreLoaderBuilder()
+            .api(db)
+            .graphName("myGraph")
+            .nodeProjections(nodeProjections(false))
+            .relationshipProjections(relationshipProjections())
+            .build();
+
+        GraphStore graphStore = graphLoader.graphStore(NativeFactory.class);
+
+        Graph filteredGraph =graphStore.getGraph(relTypes, relProperty);
+
+        assertGraphEquals(fromGdl(expectedGraph), filteredGraph);
+    }
+
+    @ParameterizedTest(name = "{0}")
+    @MethodSource("validFilterParameters")
+    void testFilteringGraphsByNodeLabels(String desc, List<String> relTypes, Optional<String> relProperty, String expectedGraph) {
+        GraphLoader graphLoader = new StoreLoaderBuilder()
+            .api(db)
+            .graphName("myGraph")
+            .nodeProjections(nodeProjections(true))
+            .relationshipProjections(relationshipProjections())
+            .build();
+
+        GraphStore graphStore = graphLoader.graphStore(NativeFactory.class);
+
+        Graph filteredGraph = graphStore.getGraph(Arrays.asList(LABEL_A, LABEL_B), relTypes, relProperty, 1);
+
+        assertGraphEquals(fromGdl(expectedGraph), filteredGraph);
+    }
+
+    @Test
+    void testFilterNodesWithAllProjectionIncluded() {
+        GraphLoader graphLoader = new StoreLoaderBuilder()
+            .api(db)
+            .graphName("myGraph")
+            .putNodeProjectionsWithIdentifier("A", NodeProjection.of("A", PropertyMappings.of()))
+            .putNodeProjectionsWithIdentifier("All", NodeProjection.all())
+            .loadAnyRelationshipType()
+            .build();
+
+        GraphStore graphStore = graphLoader.graphStore(NativeFactory.class);
+
+        Graph filteredAGraph = graphStore.getGraph(
+            Collections.singletonList(LABEL_A),
+            Collections.singletonList("*"),
+            Optional.empty(),
+            1
+        );
+
+        assertGraphEquals(fromGdl("(a)"), filteredAGraph);
+
+        Graph filteredAllGraph = graphStore.getGraph(
+            Collections.singletonList(ElementIdentifier.of("All")),
+            Collections.singletonList("*"),
+            Optional.empty(),
+            1
+        );
+
+        Graph nonFilteredGraph = graphStore
+            .getGraph(Collections.singletonList(PROJECT_ALL), Collections.singletonList("*"), Optional.empty(), 1);
+
+        assertGraphEquals(filteredAllGraph, nonFilteredGraph);
+    }
+
+    @NotNull
+    private List<NodeProjection> nodeProjections(boolean includeIgnore) {
+        NodeProjection aMapping = NodeProjection.builder()
+            .label("A")
+            .properties(PropertyMappings.of(PropertyMapping.of("nodeProperty", -1D)))
+            .build();
+        NodeProjection bMapping = NodeProjection.builder()
+            .label("B")
+            .properties(PropertyMappings.of(PropertyMapping.of("nodeProperty", -1D)))
+            .build();
+        NodeProjection ignoreMapping = NodeProjection.of("Ignore");
+        return includeIgnore ? Arrays.asList(aMapping, bMapping, ignoreMapping) : Arrays.asList(aMapping, bMapping);
+    }
+
+    @NotNull
+    private List<RelationshipProjection> relationshipProjections() {
+        RelationshipProjection t1Mapping = RelationshipProjection.builder()
+            .type("T1")
+            .orientation(Orientation.NATURAL)
+            .aggregation(Aggregation.NONE)
+            .properties(
+                PropertyMappings.builder()
+                    .addMapping("property1", "property1", 42D, Aggregation.NONE)
+                    .addMapping("property2", "property2", 1337D, Aggregation.NONE)
+                    .build()
+            ).build();
+
+        RelationshipProjection t2Mapping = RelationshipProjection.builder()
+            .type("T2")
+            .orientation(Orientation.NATURAL)
+            .aggregation(Aggregation.NONE)
+            .properties(
+                PropertyMappings.builder()
+                    .addMapping("property1", "property1", 42D, Aggregation.NONE)
+                    .build()
+            ).build();
+
+        RelationshipProjection t3Mapping = RelationshipProjection.builder()
+            .type("T3")
+            .orientation(Orientation.NATURAL)
+            .aggregation(Aggregation.NONE)
+            .properties(
+                PropertyMappings.builder()
+                    .addMapping("property2", "property2", 42D, Aggregation.NONE)
+                    .build()
+            ).build();
+
+        return Arrays.asList(t1Mapping, t2Mapping, t3Mapping);
+    }
+
+    static Stream<Arguments> validFilterParameters() {
+        return Stream.of(
+            Arguments.of(
+                "filterByRelationshipType",
+                singletonList("T1"),
+                Optional.empty(),
+                "(a {nodeProperty: 33}), (b {nodeProperty: 42}), (a)-[T1]->(b)"
+            ),
+            Arguments.of(
+                "filterByMultipleRelationshipTypes",
+                Arrays.asList("T1", "T2"),
+                Optional.empty(),
+                "(a {nodeProperty: 33}), (b {nodeProperty: 42}), (a)-[T1]->(b), (a)-[T2]->(b)"
+            ),
+            Arguments.of(
+                "filterByAnyRelationshipType",
+                singletonList("*"),
+                Optional.empty(),
+                "(a {nodeProperty: 33}), (b {nodeProperty: 42}), (a)-[T1]->(b), (a)-[T2]->(b), (a)-[T3]->(b)"
+            ),
+            Arguments.of(
+                "filterByRelationshipProperty",
+                Arrays.asList("T1", "T2"),
+                Optional.of("property1"),
+                "(a {nodeProperty: 33}), (b {nodeProperty: 42}), (a)-[T1 {property1: 42}]->(b), (a)-[T2 {property1: 43}]->(b)"
+            ),
+            Arguments.of(
+                "filterByRelationshipTypeAndProperty",
+                singletonList("T1"),
+                Optional.of("property1"),
+                "(a {nodeProperty: 33}), (b {nodeProperty: 42}), (a)-[T1 {property1: 42}]->(b)"
+            ),
+            /*
+              As our graph loader is not capable of loading different relationship properties for different types
+              it will still load property1 for T3.
+              It seems that the default values it uses is taken from one of the other property mappings
+              This test should be adapted once the loader is capable of loading the correct projections.
+             */
+            Arguments.of(
+                "includeRelatiionshipTypesThatDoNotHaveTheProperty",
+                singletonList("*"),
+                Optional.of("property1"),
+                "(a {nodeProperty: 33}), (b {nodeProperty: 42}), (a)-[T1 {property1: 42}]->(b), (a)-[T2 {property1: 43}]->(b), (a)-[T3 {property1: 42.0}]->(b)"
+            )
+        );
     }
 
     @Test
