45c9f303f964e710356f5f933d56875b334ff317
Martin Junghanns <martin.junghanns@neo4j.com>
2020-04-15 23:02:12 +0200

    Add mutate + seeding + write proc test for Louvain

--- proc/community/src/test/java/org/neo4j/graphalgo/louvain/LouvainMutateProcTest.java
+++ proc/community/src/test/java/org/neo4j/graphalgo/louvain/LouvainMutateProcTest.java
@@ -23,8 +23,12 @@ import org.junit.jupiter.api.Test;
 import org.neo4j.graphalgo.AlgoBaseProc;
 import org.neo4j.graphalgo.GdsCypher;
 import org.neo4j.graphalgo.GraphMutationTest;
+import org.neo4j.graphalgo.Orientation;
+import org.neo4j.graphalgo.StoreLoaderBuilder;
 import org.neo4j.graphalgo.compat.MapUtil;
+import org.neo4j.graphalgo.core.Aggregation;
 import org.neo4j.graphalgo.core.CypherMapWrapper;
+import org.neo4j.graphalgo.core.loading.NativeFactory;
 
 import java.util.List;
 import java.util.Optional;
@@ -32,6 +36,8 @@ import java.util.Optional;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.lessThan;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.neo4j.graphalgo.TestGraph.Builder.fromGdl;
+import static org.neo4j.graphalgo.TestSupport.assertGraphEquals;
 
 public class LouvainMutateProcTest extends LouvainProcTest<LouvainMutateConfig> implements GraphMutationTest<Louvain, LouvainMutateConfig, Louvain> {
 
@@ -99,6 +105,43 @@ public class LouvainMutateProcTest extends LouvainProcTest<LouvainMutateConfig> 
     @Override
     public LouvainMutateConfig createConfig(CypherMapWrapper mapWrapper) {
         return LouvainMutateConfig.of(getUsername(), Optional.empty(), Optional.empty(), mapWrapper);
+    }
+
+    @Test
+    void testMutateAndWriteWithSeeding() {
+        var testGraphName = mutateGraphName().get();
+
+        var mutateQuery = GdsCypher
+            .call()
+            .explicitCreation(testGraphName)
+            .algo("louvain")
+            .mutateMode()
+            .addParameter("mutateProperty", mutateProperty())
+            .yields();
+
+        runQuery(mutateQuery);
+
+        var writeQuery = GdsCypher
+            .call()
+            .explicitCreation(testGraphName)
+            .algo("louvain")
+            .writeMode()
+            .addParameter("seedProperty", mutateProperty())
+            .addParameter("writeProperty", mutateProperty())
+            .yields();
+
+        runQuery(writeQuery);
+
+        var updatedGraph = new StoreLoaderBuilder().api(db)
+            .loadAnyLabel()
+            .loadAnyRelationshipType()
+            .globalOrientation(Orientation.UNDIRECTED)
+            .addNodeProperty(mutateProperty(), mutateProperty(), 42.0, Aggregation.NONE)
+            .addNodeProperty("seed", "seed", 42.0, Aggregation.NONE)
+            .build()
+            .graph(NativeFactory.class);
+
+        assertGraphEquals(fromGdl(expectedMutatedGraph()), updatedGraph);
     }
 
     @Test
