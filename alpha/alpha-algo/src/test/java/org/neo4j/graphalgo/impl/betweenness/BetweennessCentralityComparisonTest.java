c0c5486bc65b78563df86247eef77709cf72dd63
Martin Junghanns <martin.junghanns@neo4j.com>
2020-06-09 17:51:34 +0200

    Add LegacyRABrandesBetweennessCentrality for benchmarking purpose
    Co-authored-by: Mats Rydberg <mats@neotechnology.com>

--- alpha/alpha-algo/src/test/java/org/neo4j/graphalgo/impl/betweenness/BetweennessCentralityComparisonTest.java
+++ alpha/alpha-algo/src/test/java/org/neo4j/graphalgo/impl/betweenness/BetweennessCentralityComparisonTest.java
@@ -154,6 +154,22 @@ class BetweennessCentralityComparisonTest {
         );
         randomSampledBC.compute();
 
+        var legacyDegreeSampledBC = new LegacyRABrandesBetweennessCentrality(
+            graph,
+            executorService,
+            concurrency,
+            new RandomDegreeSelectionStrategy(graph, 1.0, executorService, concurrency, tracker)
+        );
+        legacyDegreeSampledBC.compute();
+
+        var legacyRandomSampledBC = new LegacyRABrandesBetweennessCentrality(
+            graph,
+            executorService,
+            concurrency,
+            new RandomSelectionStrategy(graph, 1.0, tracker)
+        );
+        legacyRandomSampledBC.compute();
+
         for (int i = 0; i < graph.nodeCount(); i++) {
             Assert.assertEquals(
                 String.format(Locale.ENGLISH, "BC vs MS-BC: node %d with wrong BC value", i),
@@ -173,6 +189,20 @@ class BetweennessCentralityComparisonTest {
                 String.format(Locale.ENGLISH, "BC vs Random-Sampled-BC: node %d with wrong BC value", i),
                 bc.getCentrality().get(i),
                 randomSampledBC.getCentrality().get(i),
+                1E-3
+            );
+
+            Assert.assertEquals(
+                String.format(Locale.ENGLISH, "BC vs Legacy-Degree-Sampled-BC: node %d with wrong BC value", i),
+                bc.getCentrality().get(i),
+                legacyDegreeSampledBC.getCentrality().get(i),
+                1E-3
+            );
+
+            Assert.assertEquals(
+                String.format(Locale.ENGLISH, "BC vs Legacy-Random-Sampled-BC: node %d with wrong BC value", i),
+                bc.getCentrality().get(i),
+                legacyRandomSampledBC.getCentrality().get(i),
                 1E-3
             );
         }
