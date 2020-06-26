28c1788837932600b64352ea2754acf79028be3b
Paul Horn <paul.horn@neotechnology.com>
2020-06-26 08:52:06 +0200

    Add embedding test failing for cosine similarity
    Co-Authored-By: Alicia Frame <alicia.frame@neotechnology.com>

---
+++ alpha/alpha-proc/src/test/java/org/neo4j/graphalgo/similarity/EmbeddingsAsInputTest.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.similarity;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.BaseProcTest;
+import org.neo4j.graphalgo.core.SecureTransaction;
+import org.neo4j.graphalgo.core.Settings;
+import org.neo4j.graphalgo.functions.AsNodeFunc;
+import org.neo4j.graphalgo.functions.IsFiniteFunc;
+import org.neo4j.test.TestDatabaseManagementServiceBuilder;
+import org.neo4j.test.extension.ExtensionCallback;
+import org.neo4j.values.storable.Values;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class EmbeddingsAsInputTest extends BaseProcTest {
+
+    @BeforeEach
+    void setup() throws Exception {
+        registerProcedures(CosineProc.class);
+        registerFunctions(IsFiniteFunc.class);
+        registerFunctions(AsNodeFunc.class);
+
+        // create graph with kernel API as we can't write a float[]
+        // as property using Cypher. It is equivalent to
+        //   CREATE
+        //     (a:Label { id:"1", embedding: [42F, 43F] })
+        //   , (b:Label { id:"2", embedding: [44F, 45F] })
+        SecureTransaction.of(db).accept((tx, ktx) -> {
+            var tokens = ktx.tokenWrite();
+            var label = tokens.labelGetOrCreateForName("Label");
+            var id = tokens.propertyKeyGetOrCreateForName("id");
+            var embedding = tokens.propertyKeyGetOrCreateForName("embedding");
+
+            var data = ktx.dataWrite();
+            var node1 = data.nodeCreate();
+            data.nodeAddLabel(node1, label);
+            data.nodeSetProperty(node1, id, Values.stringValue("1"));
+            data.nodeSetProperty(node1, embedding, Values.floatArray(new float[]{42, 43}));
+
+            var node2 = data.nodeCreate();
+            data.nodeAddLabel(node2, label);
+            data.nodeSetProperty(node2, id, Values.stringValue("2"));
+            data.nodeSetProperty(node2, embedding, Values.floatArray(new float[]{44, 45}));
+        });
+    }
+
+    @Override
+    @ExtensionCallback
+    protected void configuration(TestDatabaseManagementServiceBuilder builder) {
+        super.configuration(builder);
+        builder.setConfig(Settings.enterpriseLicensed(), true);
+    }
+
+    @Test
+    void cosineSimilarityWithEmbeddingResult() {
+        var query =
+            " MATCH (a:Label)" +
+            " WITH id(a) as id, a.embedding as weights" +
+            " WITH {item:id, weights: weights} as userData" +
+            " WITH collect(userData) as data" +
+            " CALL gds.alpha.similarity.cosine.stream({" +
+            "   nodeProjection: '*'," +
+            "   relationshipProjection: '*'," +
+            "   similarityCutoff:0.5," +
+            "   topK:5," +
+            "   data: data" +
+            " }) YIELD item1, item2, count1, count2, similarity" +
+            " RETURN gds.util.asNode(item1).id AS from, gds.util.asNode(item2).id AS to, similarity" +
+            " ORDER BY similarity DESC";
+
+        runQueryWithResultConsumer(query, results -> {
+            assertTrue(results.hasNext(), "Should have exactly two results");
+            var firstItem = results.next();
+            assertTrue(results.hasNext(), "Should have exactly two results");
+            var secondItem = results.next();
+            assertFalse(results.hasNext(), "Should have exactly two results");
+
+            var firstSimilarity = ((Number) firstItem.get("similarity")).doubleValue();
+            assertEquals(firstSimilarity, 0.999, 1e-3);
+            var secondSimilarity = ((Number) secondItem.get("similarity")).doubleValue();
+            assertEquals(secondSimilarity, 0.999, 1e-3);
+
+            var firstFrom = String.valueOf(firstItem.get("from"));
+            assertEquals(firstFrom, "1");
+            var firstTo = String.valueOf(firstItem.get("to"));
+            assertEquals(firstTo, "2");
+
+            var secondFrom = String.valueOf(secondItem.get("from"));
+            assertEquals(secondFrom, "2");
+            var secondTo = String.valueOf(secondItem.get("to"));
+            assertEquals(secondTo, "1");
+        });
+    }
+}
