5f116e630bb00490745249a5c1d8b3baa526619b
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2020-07-31 15:41:01 +0100

    Retrieve the available Docs versions from S3

--- doc/javascript/version.js
+++ doc/javascript/version.js
@@ -1,20 +1,50 @@
-window.docMeta = (function () {
-  var version = '1.4-preview';
-  var name = 'graph-data-science';
-  var href = window.location.href;
+window.versions = (async function() {
+  console.log("Calling versions()")
+  const versionsUrl =
+    'https://s3-eu-west-1.amazonaws.com/com.neo4j.graphalgorithms.dist/graph-data-science/doc-versions.json';
+  const versions = await jQuery.ajax({
+    type: 'GET',
+    url: versionsUrl,
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/vnd.github.v3+json'
+    },
+    crossOrigin: true,
+    success: function (data) {
+      return data;
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.error('Request failed...', jqXHR, textStatus, errorThrown);
+    }
+  });
+
+  return {
+    value: versions
+  };
+})();
+
+window.docMeta = (async function () {
+  console.log("1 window.docMeta")
+  const version = '1.4-preview';
+  const name = 'graph-data-science';
+  const href = window.location.href;
+
   return {
     name: name,
     version: version,
-    availableDocVersions: ['1.0', '1.1', '1.2', '1.3', '1.4-preview'],
+    availableDocVersions: window.versions,
     thisPubBaseUri: href.substring(0, href.indexOf(name) + name.length) + '/' + version,
     unversionedDocBaseUri: href.substring(0, href.indexOf(name) + name.length) + '/',
     commonDocsBaseUri: href.substring(0, href.indexOf(name) - 1)
   }
+
 })();
 
 (function () {
-  var baseUri = window.docMeta.unversionedDocBaseUri + window.location.pathname.split(window.docMeta.name + '/')[1].split('/')[0] + '/';
-  var docPath = window.location.href.replace(baseUri, '');
-  window.neo4jPageId = docPath;
+  const pathname = window.location.pathname;
+  if (pathname.indexOf(window.docMeta.name) > -1) {
+    const baseUri = window.docMeta.unversionedDocBaseUri + pathname.split(window.docMeta.name + '/')[1].split('/')[0] + '/';
+    window.neo4jPageId = window.location.href.replace(baseUri, '');
+  }
 })();
 // vim: set sw=2 ts=2:
--- doc/javascript/versionswitcher.js
+++ doc/javascript/versionswitcher.js
@@ -1,20 +1,16 @@
-jQuery( window ).load( function() {
-  var location = window.location;
-      versionSwitcher( jQuery );
-} );
+jQuery(window).load(function () {
+  versionSwitcher(jQuery);
+});
 
 /**
  * Utility to browse different versions of the documentation. Requires the versions.js file loaded, which lists the
  * available (relevant) versions of a particular publication.
  */
-function versionSwitcher( $ )
-{
-  var MAX_STABLE_COUNT = 2;
-  var DOCS_BASE_URL = window.docMeta.commonDocsBaseUri;
-  var THIS_DOC_BASE_URI = window.docMeta.unversionedDocBaseUri;
+function versionSwitcher($) {
+  const THIS_DOC_BASE_URI = window.docMeta.unversionedDocBaseUri;
 
-  var currentVersion = window.docMeta.version;
-  var currentPage = window.neo4jPageId;
+  let currentVersion;
+  const currentPage = window.neo4jPageId;
 
   loadVersions();
 
@@ -23,38 +19,46 @@ function versionSwitcher( $ )
    * Non-existing entries will be unlinked. Current version will be marked as such.
    */
   function loadVersions() {
-    var $navHeader = $( 'header' );
-    var $additionalVersions = $( '<ul class="dropdown-menu dropdown-menu-right" role="menu" aria-labelledby="dropdownMenu1"/>' );
-    var versionCount = 0;
-    $.each( window.docMeta.availableDocVersions, function( index, version ) {
-      if ( version === currentVersion ) {
-        return;
-      }
-      else {
-        addVersion( version, $additionalVersions );
-        versionCount++;
-      }
-    } );
-    if ( versionCount == 0){
-      var $dropdown = $( '<div id="additional-versions"><div class="additional-versions">Version: ' + currentVersion + ' </div></div>' );
-    } else {
-      var $dropdown = $( '<div id="additional-versions"><div class="dropdown"><a class="dropdown-toggle"id="dropdownMenu1" data-toggle="dropdown">Version: ' + currentVersion + ' <i class="fa fa-caret-down"></i></a></div></div>' );
-      $dropdown.children().first().append( $additionalVersions );
-    }
-    $navHeader.append( $dropdown );
+    const $navHeader = $('header');
+    const $additionalVersions = $('<ul class="dropdown-menu dropdown-menu-right" role="menu" aria-labelledby="dropdownMenu1"/>');
+    let versionCount = 0;
+    window.docMeta.then(function (result) {
+      currentVersion = result.version
+      result.availableDocVersions.then(function(availableVersions) {
+        console.log('availableVersions', availableVersions)
+        availableVersions.value.forEach(function (version) {
+          if (version !== currentVersion) {
+            addVersion(version, $additionalVersions);
+            versionCount++;
+          }
+        });
+
+        let $dropdown;
+        if (versionCount === 0) {
+          $dropdown = $('<div id="additional-versions"><div class="additional-versions">Version: ' + currentVersion + ' </div></div>');
+        } else {
+          $dropdown = $('<div id="additional-versions"><div class="dropdown"><a class="dropdown-toggle"id="dropdownMenu1" data-toggle="dropdown">Version: ' + currentVersion + ' <i class="fa fa-caret-down"></i></a></div></div>');
+          $dropdown.children().first().append($additionalVersions);
+        }
+        $navHeader.append($dropdown);
+      });
+
+
+    });
+
   }
 
-  function addVersion( version, $container ) {
-    var $optionWrapper = $( '<li />' );
-    var $newOption = $( '<a role="menuitem">' + version + '</a>' ).appendTo( $optionWrapper );
+  function addVersion(version, $container) {
+    var $optionWrapper = $('<li />');
+    var $newOption = $('<a role="menuitem">' + version + '</a>').appendTo($optionWrapper);
     var url = THIS_DOC_BASE_URI + version + '/' + currentPage;
-    $container.append( $optionWrapper );
-    checkUrlExistence( url, function() {
-        $newOption.attr( 'href', url );
-        $newOption.attr( 'title', 'See this page in version ' + version + '.' );
-      }, function() {
-        $newOption.attr( 'title', 'This page does not exist in version ' + version + '.' );
-        $optionWrapper.addClass( 'disabled' );
+    $container.append($optionWrapper);
+    checkUrlExistence(url, function () {
+        $newOption.attr('href', url);
+        $newOption.attr('title', 'See this page in version ' + version + '.');
+      }, function () {
+        $newOption.attr('title', 'This page does not exist in version ' + version + '.');
+        $optionWrapper.addClass('disabled');
       }
     );
   }
@@ -62,17 +66,18 @@ function versionSwitcher( $ )
   /**
    * Check if a specific URL exists. The success and failure functions will be automatically called on finish.
    */
-  function checkUrlExistence( url, success, failure ) {
+  function checkUrlExistence(url, success, failure) {
     var settings = {
-      'type' : 'HEAD',
-      'async' : true,
-      'url' : url
+      'type': 'HEAD',
+      'async': true,
+      'url': url
     };
-    if ( success )
+    if (success)
       settings.success = success;
-    if ( failure )
+    if (failure)
       settings.error = failure;
-    $.ajax( settings );
+    $.ajax(settings);
   }
 }
+
 // vim: set ts=2 sw=2:
