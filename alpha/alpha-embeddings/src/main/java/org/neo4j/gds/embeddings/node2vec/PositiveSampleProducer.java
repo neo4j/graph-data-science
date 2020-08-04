bd0829bf16b48ce46a8771818f2b2fea681e60e7
Mats Rydberg <mats@neotechnology.com>
2020-08-04 16:45:40 +0200

    Fix bug where PositiveSampleProducer would sample outside its batch
    This caused a StackOverflowError due to recursive calls that don't reach the end of the walk array
    
    Co-authored-by: Jonatan Jäderberg <jonatan.jaderberg@gmail.com>

--- alpha/alpha-embeddings/src/main/java/org/neo4j/gds/embeddings/node2vec/PositiveSampleProducer.java
+++ alpha/alpha-embeddings/src/main/java/org/neo4j/gds/embeddings/node2vec/PositiveSampleProducer.java
@@ -57,7 +57,7 @@ public class PositiveSampleProducer {
         prefixWindowSize = (int) ceilDiv(windowSize - 1, 2);
         postfixWindowSize = (windowSize - 1) / 2;
 
-        this.walkIndex = batchStart -1;
+        this.walkIndex = batchStart - 1;
         this.centerWordIndex = -1;
         this.contextWordIndex = 1;
         nextWalk();
@@ -85,7 +85,7 @@ public class PositiveSampleProducer {
             progressLogger.logProgress();
         }
 
-        if (walkIndex < walks.size()) {
+        if (hasNext()) {
             progressLogger.logProgress();
             this.currentWalk = walks.get(walkIndex);
             centerWordIndex = -1;
@@ -114,7 +114,7 @@ public class PositiveSampleProducer {
             contextWordIndex++;
         }
 
-        if(contextWordIndex >= Math.min(centerWordIndex + postfixWindowSize + 1, currentWalk.length)) {
+        if (contextWordIndex >= Math.min(centerWordIndex + postfixWindowSize + 1, currentWalk.length)) {
             nextCenterWord();
         }
     }
