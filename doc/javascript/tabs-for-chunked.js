a8515cb85c485f1607475192a686500e275da4e6
Jacob Sznajdman <breakanalysis@gmail.com>
2020-08-10 17:13:09 +0200

    Make stream tab default in docs
    Co-Authored-By: Sören Reichardt <soren.reichardt@neotechnology.com>

--- doc/javascript/tabs-for-chunked.js
+++ doc/javascript/tabs-for-chunked.js
@@ -1,9 +1,9 @@
 function tabTheSource($content) {
     var SESSION_STORAGE_KEY = 'active_procedure_mode';
     var MODES = {
+        'stream': 'Stream mode',
         'mutate': 'Mutate mode',
         'stats': 'Stats mode',
-        'stream': 'Stream mode',
         'write': 'Write mode'
     };
     updateSelectedModeFromQueryParams(MODES, SESSION_STORAGE_KEY);
