acdfffa2a3dee6b0d2d55850e9ee0e83cbf2bbeb
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2020-08-12 08:07:20 +0100

    Extract methods used in training and embedding generation in common Base class
    Co-authored-by: Sören Reichardt <soren.reichardt@neotechnology.com>

---
+++ alpha/alpha-embeddings/src/main/java/org/neo4j/gds/embeddings/graphsage/GraphSageBase.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.embeddings.graphsage;
+
+import org.neo4j.gds.embeddings.graphsage.ddl4j.Variable;
+import org.neo4j.gds.embeddings.graphsage.ddl4j.functions.MatrixConstant;
+import org.neo4j.gds.embeddings.graphsage.ddl4j.functions.NormalizeRows;
+import org.neo4j.gds.embeddings.graphsage.ddl4j.tensor.Matrix;
+import org.neo4j.gds.embeddings.graphsage.subgraph.SubGraph;
+import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.graphalgo.core.utils.paged.HugeObjectArray;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class GraphSageBase {
+
+    private Variable<Matrix> featureVariables(long[] nodeIds, HugeObjectArray<double[]> features) {
+        int dimension = features.get(0).length;
+        double[] data = new double[nodeIds.length * dimension];
+        IntStream
+            .range(0, nodeIds.length)
+            .forEach(nodeOffset -> System.arraycopy(
+                features.get(nodeIds[nodeOffset]),
+                0,
+                data,
+                nodeOffset * dimension,
+                dimension
+            ));
+        return new MatrixConstant(data, nodeIds.length, dimension);
+    }
+
+    protected Variable<Matrix> embeddingVariable(Graph graph, long[] nodeIds, HugeObjectArray<double[]> features, Layer[] layers) {
+        List<NeighborhoodFunction> neighborhoodFunctions = Arrays
+            .stream(layers)
+            .map(layer -> (NeighborhoodFunction) layer::neighborhoodFunction)
+            .collect(Collectors.toList());
+        Collections.reverse(neighborhoodFunctions);
+        List<SubGraph> subGraphs = SubGraph.buildSubGraphs(nodeIds, neighborhoodFunctions, graph);
+
+        Variable<Matrix> previousLayerRepresentations = featureVariables(
+            subGraphs.get(subGraphs.size() - 1).nextNodes,
+            features
+        );
+
+        for (int layerNr = layers.length - 1; layerNr >= 0; layerNr--) {
+            Layer layer = layers[layers.length - layerNr - 1];
+            previousLayerRepresentations = layer
+                .aggregator()
+                .aggregate(
+                    previousLayerRepresentations,
+                    subGraphs.get(layerNr).adjacency,
+                    subGraphs.get(layerNr).selfAdjacency
+                );
+        }
+        return new NormalizeRows(previousLayerRepresentations);
+    }
+}
