32d3f11b7f67f67b2ecc9cec65d17fb81cebf534
Martin Junghanns <martin.junghanns@neo4j.com>
2020-08-18 11:29:08 +0200

    Convert K1ColoringExample to pregel-bootstrap module
    Co-authored-by: Max Kießling <max.kiessling@neotechnology.com>

---
+++ examples/pregel-bootstrap/src/test/java/gds/example/ExamplePregelComputationAlgoTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package gds.example;
+
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.TestSupport;
+import org.neo4j.graphalgo.beta.pregel.Pregel;
+import org.neo4j.graphalgo.core.concurrency.Pools;
+import org.neo4j.graphalgo.core.utils.mem.AllocationTracker;
+import org.neo4j.graphalgo.extension.GdlExtension;
+import org.neo4j.graphalgo.extension.GdlGraph;
+import org.neo4j.graphalgo.extension.Inject;
+import org.neo4j.graphalgo.extension.TestGraph;
+
+import java.util.HashMap;
+
+import static gds.example.ExamplePregelComputation.KEY;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@GdlExtension
+class ExamplePregelComputationAlgoTest {
+
+    @GdlGraph
+    private static final String MY_TEST_GRAPH =
+        "CREATE" +
+        "  (alice)" +
+        ", (bob)" +
+        ", (eve)" +
+        ", (alice)-[:LIKES]->(bob)" +
+        ", (bob)-[:LIKES]->(alice)" +
+        ", (eve)-[:DISLIKES]->(alice)" +
+        ", (eve)-[:DISLIKES]->(bob)";
+
+    @Inject
+    private TestGraph graph;
+    
+    @Test
+    void runExamplePregelComputation() {
+
+        int batchSize = 10;
+        int maxIterations = 10;
+
+        var config = ImmutableExampleConfig.builder()
+            .maxIterations(maxIterations)
+            .build();
+
+        var pregelJob = Pregel.create(
+            graph,
+            config,
+            new ExamplePregelComputation(),
+            batchSize,
+            Pools.DEFAULT,
+            AllocationTracker.EMPTY
+        );
+
+        var result = pregelJob.run();
+
+        assertTrue(result.didConverge(), "Algorithm did not converge.");
+        assertEquals(1, result.ranIterations());
+
+        var expected = new HashMap<String, Long>();
+        expected.put("alice", 0L);
+        expected.put("bob", 1L);
+        expected.put("eve", 2L);
+
+        TestSupport.assertLongValues(graph, (nodeId) -> result.nodeValues().longValue(KEY, nodeId), expected);
+    }
+}
---
+++ examples/pregel-bootstrap/src/test/java/gds/example/ExamplePregelComputationProcTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package gds.example;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.BaseProcTest;
+import org.neo4j.graphalgo.GdsCypher;
+import org.neo4j.graphalgo.extension.GdlGraph;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static gds.example.ExamplePregelComputation.KEY;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.neo4j.graphalgo.TestSupport.mapEquals;
+
+class ExamplePregelComputationProcTest extends BaseProcTest {
+
+    @GdlGraph
+    private static final String MY_TEST_GRAPH =
+        "CREATE" +
+        "  (alice)" +
+        ", (bob)" +
+        ", (eve)" +
+        ", (alice)-[:LIKES]->(bob)" +
+        ", (bob)-[:LIKES]->(alice)" +
+        ", (eve)-[:DISLIKES]->(alice)" +
+        ", (eve)-[:DISLIKES]->(bob)";
+
+    @BeforeEach
+    void setup() throws Exception {
+        runQuery(MY_TEST_GRAPH);
+        registerProcedures(ExamplePregelComputationStreamProc.class);
+    }
+
+    @Test
+    void stream() {
+        // GdsCypher creates procedure statements, such as:
+        // CALL pregel.example.stream({
+        //   nodeProjection: "*", relationshipProjection: "*", maxIterations: 10
+        // }) YIELD nodeId, values
+        var query = GdsCypher.call()
+            .loadEverything()
+            .algo("pregel", "example")
+            .streamMode()
+            .addParameter("maxIterations", 10)
+            .yields("nodeId", "values");
+
+        var actual = new HashMap<Long, Long>();
+        
+        runQueryWithRowConsumer(query, r -> {
+            actual.put(
+                r.getNumber("nodeId").longValue(),
+                ((Map<String, Long>) r.get("values")).get(KEY)
+            );
+        });
+
+        assertThat(actual, mapEquals(Map.of(
+            0L, 0L,
+            1L, 1L,
+            2L, 2L
+        )));
+    }
+}
