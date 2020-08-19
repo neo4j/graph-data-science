05105854b0c8ae9af777133ba76225507392fe63
Florentin Dörre <florentin.dorre@neotechnology.com>
2020-08-19 17:22:04 +0200

    Set a default allocater for random graph generator

--- algo/src/main/java/org/neo4j/graphalgo/beta/generator/RandomGraphGeneratorBuilder.java
+++ algo/src/main/java/org/neo4j/graphalgo/beta/generator/RandomGraphGeneratorBuilder.java
@@ -36,7 +36,7 @@ public class RandomGraphGeneratorBuilder {
     private Aggregation aggregation = Aggregation.NONE;
     private Orientation orientation = Orientation.NATURAL;
     private RandomGraphGeneratorConfig.AllowSelfLoops allowSelfLoops = RandomGraphGeneratorConfig.AllowSelfLoops.NO;
-    private AllocationTracker allocationTracker;
+    private AllocationTracker allocationTracker = AllocationTracker.EMPTY;
 
     public RandomGraphGeneratorBuilder nodeCount(long nodeCount) {
         this.nodeCount = nodeCount;
