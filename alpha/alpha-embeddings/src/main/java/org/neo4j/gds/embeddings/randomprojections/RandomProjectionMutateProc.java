70e2b54f19ba034414d2254bcd77736de3cb6193
Max Kießling <max.kiessling@neotechnology.com>
2020-09-03 15:00:11 +0200

    Add Mutate mode for random projection

---
+++ alpha/alpha-embeddings/src/main/java/org/neo4j/gds/embeddings/randomprojections/RandomProjectionMutateProc.java
@@ -0,0 +1,119 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.embeddings.randomprojections;
+
+import org.neo4j.graphalgo.AlgorithmFactory;
+import org.neo4j.graphalgo.MutateProc;
+import org.neo4j.graphalgo.api.NodeProperties;
+import org.neo4j.graphalgo.api.nodeproperties.FloatArrayNodeProperties;
+import org.neo4j.graphalgo.config.GraphCreateConfig;
+import org.neo4j.graphalgo.core.CypherMapWrapper;
+import org.neo4j.graphalgo.result.AbstractResultBuilder;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.neo4j.procedure.Mode.READ;
+
+public class RandomProjectionMutateProc extends MutateProc<RandomProjection, RandomProjection, RandomProjectionMutateProc.MutateResult, RandomProjectionMutateConfig> {
+
+    @Procedure(value = "gds.alpha.randomProjection.mutate", mode = READ)
+    @Description("Random Projection produces node embeddings via the fastrp algorithm")
+    public Stream<RandomProjectionMutateProc.MutateResult> mutate(
+        @Name(value = "graphName") Object graphNameOrConfig,
+        @Name(value = "configuration", defaultValue = "{}") Map<String, Object> configuration
+    ) {
+        ComputationResult<RandomProjection, RandomProjection, RandomProjectionMutateConfig> computationResult = compute(
+            graphNameOrConfig,
+            configuration
+        );
+        return mutate(computationResult);
+    }
+
+    @Override
+    protected NodeProperties getNodeProperties(ComputationResult<RandomProjection, RandomProjection, RandomProjectionMutateConfig> computationResult) {
+        return (FloatArrayNodeProperties) nodeId -> computationResult.result().embeddings().get(nodeId);
+    }
+
+    @Override
+    protected AbstractResultBuilder<MutateResult> resultBuilder(ComputationResult<RandomProjection, RandomProjection, RandomProjectionMutateConfig> computeResult) {
+        return new MutateResult.Builder();
+    }
+
+    @Override
+    protected RandomProjectionMutateConfig newConfig(
+        String username,
+        Optional<String> graphName,
+        Optional<GraphCreateConfig> maybeImplicitCreate,
+        CypherMapWrapper config
+    ) {
+        return RandomProjectionMutateConfig.of(username, graphName, maybeImplicitCreate, config);
+    }
+
+    @Override
+    protected AlgorithmFactory<RandomProjection, RandomProjectionMutateConfig> algorithmFactory() {
+        return new RandomProjectionFactory<>();
+    }
+
+    public static final class MutateResult {
+
+        public final long nodePropertiesWritten;
+        public final long mutateMillis;
+        public final long nodeCount;
+        public final long createMillis;
+        public final long computeMillis;
+        public final Map<String, Object> configuration;
+
+        MutateResult(
+            long nodeCount,
+            long nodePropertiesWritten,
+            long createMillis,
+            long computeMillis,
+            long mutateMillis,
+            Map<String, Object> config
+        ) {
+            this.nodeCount = nodeCount;
+            this.nodePropertiesWritten = nodePropertiesWritten;
+            this.createMillis = createMillis;
+            this.computeMillis = computeMillis;
+            this.mutateMillis = mutateMillis;
+            this.configuration = config;
+        }
+
+        static final class Builder extends AbstractResultBuilder<MutateResult> {
+
+            @Override
+            public MutateResult build() {
+                return new MutateResult(
+                    nodeCount,
+                    nodePropertiesWritten,
+                    createMillis,
+                    computeMillis,
+                    mutateMillis,
+                    config.toMap()
+                );
+            }
+        }
+    }
+}
---
+++ alpha/alpha-embeddings/src/test/java/org/neo4j/gds/embeddings/randomprojections/RandomProjectionMutateProcTest.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.embeddings.randomprojections;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.neo4j.graphalgo.AlgoBaseProc;
+import org.neo4j.graphalgo.GdsCypher;
+import org.neo4j.graphalgo.Orientation;
+import org.neo4j.graphalgo.core.CypherMapWrapper;
+import org.neo4j.graphalgo.functions.NodePropertyFunc;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+class RandomProjectionMutateProcTest extends RandomProjectionProcTest<RandomProjectionMutateConfig>{
+
+    @BeforeEach
+    void setupNodePropertyFunc() throws Exception {
+        registerFunctions(
+            NodePropertyFunc.class
+        );
+    }
+
+    @Override
+    public Class<? extends AlgoBaseProc<RandomProjection, RandomProjection, RandomProjectionMutateConfig>> getProcedureClazz() {
+        return RandomProjectionMutateProc.class;
+    }
+
+    @Override
+    public RandomProjectionMutateConfig createConfig(CypherMapWrapper userInput) {
+        return RandomProjectionMutateConfig.of(getUsername(), Optional.empty(), Optional.empty(), userInput);
+    }
+
+    @Override
+    public CypherMapWrapper createMinimalConfig(CypherMapWrapper userInput) {
+        CypherMapWrapper minimalConfig = super.createMinimalConfig(userInput);
+
+        if (!minimalConfig.containsKey("mutateProperty")) {
+            return minimalConfig.withString("mutateProperty", "embedding");
+        }
+        return minimalConfig;
+    }
+
+    @ParameterizedTest
+    @MethodSource("org.neo4j.gds.embeddings.randomprojections.RandomProjectionProcTest#weights")
+    void shouldMutateNonZeroEmbeddings(List<Float> weights) {
+        String loadedGraphName = "loadGraph";
+
+        var graphCreateQuery = GdsCypher.call()
+            .withNodeLabel("Node")
+            .withRelationshipType("REL", Orientation.UNDIRECTED)
+            .graphCreate(loadedGraphName)
+            .yields();
+
+        System.out.println(graphCreateQuery);
+        runQuery(graphCreateQuery);
+
+
+        int embeddingSize = 128;
+        int maxIterations = 4;
+        GdsCypher.ParametersBuildStage queryBuilder = GdsCypher.call()
+            .explicitCreation(loadedGraphName)
+            .algo("gds.alpha.randomProjection")
+            .mutateMode()
+            .addParameter("embeddingSize", embeddingSize)
+            .addParameter("maxIterations", maxIterations)
+            .addParameter("mutateProperty", "embedding");
+
+        if (!weights.isEmpty()) {
+            queryBuilder.addParameter("iterationWeights", weights);
+        }
+        String query = queryBuilder.yields();
+
+        runQuery(query);
+
+        int expectedEmbeddingsDimension = weights.isEmpty()
+            ? embeddingSize * maxIterations
+            : embeddingSize;
+        runQueryWithRowConsumer("MATCH (n:Node) RETURN gds.util.nodeProperty('loadGraph', id(n), 'embedding') as embedding", row -> {
+            float[] embeddings = (float[]) row.get("embedding");
+            assertEquals(expectedEmbeddingsDimension, embeddings.length);
+            boolean allMatch = true;
+            for (float embedding : embeddings) {
+                if (Float.compare(embedding, 0.0F) != 0) {
+                    allMatch = false;
+                    break;
+                }
+            }
+            assertFalse(allMatch);
+        });
+    }
+}
