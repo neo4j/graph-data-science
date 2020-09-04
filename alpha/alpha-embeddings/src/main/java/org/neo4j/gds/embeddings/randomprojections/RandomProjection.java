d1b1bde2113a722ed42093f0fa1c529bb5e27b1f
Max Kießling <max.kiessling@neotechnology.com>
2020-09-04 14:56:01 +0200

    Remove lock from RandomProjection.HighQualityRandom

--- alpha/alpha-embeddings/src/main/java/org/neo4j/gds/embeddings/randomprojections/RandomProjection.java
+++ alpha/alpha-embeddings/src/main/java/org/neo4j/gds/embeddings/randomprojections/RandomProjection.java
@@ -19,6 +19,7 @@
  */
 package org.neo4j.gds.embeddings.randomprojections;
 
+import org.jetbrains.annotations.TestOnly;
 import org.neo4j.graphalgo.Algorithm;
 import org.neo4j.graphalgo.api.Graph;
 import org.neo4j.graphalgo.core.concurrency.ParallelUtil;
@@ -33,8 +34,6 @@ import org.neo4j.graphalgo.utils.CloseableThreadLocal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
 
 import static org.neo4j.graphalgo.utils.StringFormatting.formatWithLocale;
 
@@ -104,6 +103,7 @@ public class RandomProjection extends Algorithm<RandomProjection, RandomProjecti
         return this.embeddings;
     }
 
+    @TestOnly
     HugeObjectArray<float[]> currentEmbedding(int iteration) {
         return iteration % 2 == 0
             ? this.embeddingA
@@ -243,8 +243,7 @@ public class RandomProjection extends Algorithm<RandomProjection, RandomProjecti
         }
     }
 
-    public class HighQualityRandom extends Random {
-        private Lock l = new ReentrantLock();
+    private static class HighQualityRandom extends Random {
         private long u;
         private long v = 4101842887655102017L;
         private long w = 1;
@@ -254,32 +253,24 @@ public class RandomProjection extends Algorithm<RandomProjection, RandomProjecti
         }
 
         public HighQualityRandom(long seed) {
-            l.lock();
             u = seed ^ v;
             nextLong();
             v = u;
             nextLong();
             w = v;
             nextLong();
-            l.unlock();
         }
 
         public long nextLong() {
-            l.lock();
-            try {
-                u = u * 2862933555777941757L + 7046029254386353087L;
-                v ^= v >>> 17;
-                v ^= v << 31;
-                v ^= v >>> 8;
-                w = 4294957665L * (w & 0xffffffff) + (w >>> 32);
-                long x = u ^ (u << 21);
-                x ^= x >>> 35;
-                x ^= x << 4;
-                long ret = (x + v) ^ w;
-                return ret;
-            } finally {
-                l.unlock();
-            }
+            u = u * 2862933555777941757L + 7046029254386353087L;
+            v ^= v >>> 17;
+            v ^= v << 31;
+            v ^= v >>> 8;
+            w = 4294957665L * w + (w >>> 32);
+            long x = u ^ (u << 21);
+            x ^= x >>> 35;
+            x ^= x << 4;
+            return (x + v) ^ w;
         }
 
         protected int next(int bits) {
