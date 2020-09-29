b4b9cd56a250eb69c4933e28c4b6e90a58d7ae50
Max Kießling <max.kiessling@neotechnology.com>
2020-09-29 16:13:31 +0200

    Update SmokeTest procedure count

--- doc/src/test/java/org/neo4j/graphalgo/doc/AppendixAProcedureListingTest.java
+++ doc/src/test/java/org/neo4j/graphalgo/doc/AppendixAProcedureListingTest.java
@@ -73,7 +73,7 @@ class AppendixAProcedureListingTest extends BaseProcTest {
         registeredProcedures.add("gds.list");
 
         // If you find yourself updating this count, please also update the count in SmokeTest.kt
-        int expectedCount = 188;
+        int expectedCount = 189;
         assertEquals(
             expectedCount,
             registeredProcedures.size(),
