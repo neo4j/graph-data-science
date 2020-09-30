853705722f66183883b61f0d5d1eb5fea5660533
Martin Junghanns <martin.junghanns@neo4j.com>
2020-09-30 10:02:30 +0200

    Add test for HABS.set(start, end)

--- core/src/test/java/org/neo4j/graphalgo/core/utils/paged/HugeAtomicBitSetTest.java
+++ core/src/test/java/org/neo4j/graphalgo/core/utils/paged/HugeAtomicBitSetTest.java
@@ -20,11 +20,14 @@
 package org.neo4j.graphalgo.core.utils.paged;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.neo4j.graphalgo.core.utils.mem.AllocationTracker;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.neo4j.graphalgo.utils.StringFormatting.formatWithLocale;
 
 class HugeAtomicBitSetTest {
 
@@ -63,6 +66,20 @@ class HugeAtomicBitSetTest {
         assertFalse(bitSet.get(0));
         bitSet.getAndSet(0);
         assertTrue(bitSet.get(0));
+    }
+
+    @ParameterizedTest
+    @CsvSource({"0,41", "0,7", "10,20"})
+    void setRange(int startIndex, int endIndex) {
+        var bitSet = HugeAtomicBitSet.create(42, AllocationTracker.empty());
+        bitSet.set(startIndex, endIndex);
+        for (int i = 0; i < bitSet.capacity(); i++) {
+            if (i < startIndex || i > endIndex) {
+                assertFalse(bitSet.get(i), formatWithLocale("index %d expected to be false", i));
+            } else {
+                assertTrue(bitSet.get(i), formatWithLocale("index %d expected to be true", i));
+            }
+        }
     }
 
     @Test
