bb93ea4c801b840aa0aa05daa782232ed58da48f
Soeren Reichardt <soren.reichardt@neotechnology.com>
2020-10-08 11:05:06 +0200

    Add postProcessingMillis to AbstractResultBuilder
    Co-authored-by: Veselin Nikolov <veselin.nikolov@neotechnology.com>

--- algo-common/src/main/java/org/neo4j/graphalgo/result/AbstractResultBuilder.java
+++ algo-common/src/main/java/org/neo4j/graphalgo/result/AbstractResultBuilder.java
@@ -27,6 +27,7 @@ public abstract class AbstractResultBuilder<WRITE_RESULT> {
     protected long computeMillis = -1;
     protected long writeMillis = -1;
     protected long mutateMillis = -1;
+    protected long postProcessingMillis = -1;
     protected long nodeCount;
     protected long nodePropertiesWritten;
     protected long relationshipsWritten;
@@ -49,6 +50,11 @@ public abstract class AbstractResultBuilder<WRITE_RESULT> {
 
     public AbstractResultBuilder<WRITE_RESULT> withMutateMillis(long mutateMillis) {
         this.mutateMillis = mutateMillis;
+        return this;
+    }
+
+    public AbstractResultBuilder<WRITE_RESULT> withPostProcessingMillis(long postProcessingMillis) {
+        this.postProcessingMillis = postProcessingMillis;
         return this;
     }
 
