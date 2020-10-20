4475ba01081238a6e9c6ac7fa19fc7d2a27ac3dd
Soeren Reichardt <soren.reichardt@neotechnology.com>
2020-10-20 15:24:07 +0200

    Refactor checked property extraction into embeddings util class
    Co-authored-by: Veselin Nikolov <veselin.nikolov@neotechnology.com>

--- algo/src/main/java/org/neo4j/gds/embeddings/fastrp/FastRP.java
+++ algo/src/main/java/org/neo4j/gds/embeddings/fastrp/FastRP.java
@@ -35,6 +35,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import static org.neo4j.gds.embeddings.EmbeddingUtils.getCheckedDoubleNodeProperty;
 import static org.neo4j.graphalgo.utils.StringFormatting.formatWithLocale;
 
 public class FastRP extends Algorithm<FastRP, FastRP> {
@@ -206,13 +207,7 @@ public class FastRP extends Algorithm<FastRP, FastRP> {
         }
         for (int j = 0; j < nodePropertyNames.size(); j++) {
             String feature = nodePropertyNames.get(j);
-            double featureValue = graph.nodeProperties(feature).doubleValue(nodeId);
-            if (Double.isNaN(featureValue)) {
-                throw new IllegalArgumentException(formatWithLocale(
-                    "Missing node property for property key `%s`. Consider using a default value in the property projection.",
-                    feature
-                ));
-            }
+            double featureValue = getCheckedDoubleNodeProperty(graph, feature, nodeId);
             if (featureValue != 0.0D) {
                 for (int i = baseEmbeddingDimension; i < embeddingDimension; i++) {
                     randomVector[i] += featureValue * propertyVectors[j][i - baseEmbeddingDimension];
