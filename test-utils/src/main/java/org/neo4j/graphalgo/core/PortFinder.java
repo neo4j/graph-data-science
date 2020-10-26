4e5a7fff157fa396c74ae95f3165749cf0b32a6f
Max Kießling <max.kiessling@neo4j.com>
2020-10-26 10:41:17 +0100

    Address PR review comments
    Co-authored-by: Soeren Reichardt <soren.reichardt@neotechnology.com>

---
+++ test-utils/src/main/java/org/neo4j/graphalgo/core/PortFinder.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.core;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.util.Random;
+import java.util.stream.IntStream;
+
+public class PortFinder {
+    public static int freePort() {
+        var random = new Random();
+        return IntStream.concat(
+            IntStream.of(1337, 13370),
+            random.ints(1024, 65536)
+        )
+            .filter(PortFinder::portAvailable)
+            .findFirst()
+            .orElse(0);
+    }
+
+    private static boolean portAvailable(int port) {
+        try (ServerSocket serverSocket = new ServerSocket()) {
+            serverSocket.setReuseAddress(false);
+            serverSocket.bind(new InetSocketAddress(InetAddress.getLocalHost(), port), 1);
+            return true;
+        } catch (Exception ex) {
+            return false;
+        }
+    }
+}
