50958dc855bde69c6b0a8e201c743198563dc8a1
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2020-11-09 08:15:17 +0000

    Update docs version dropdown label

--- doc/javascript/version.js
+++ doc/javascript/version.js
@@ -1,5 +1,5 @@
 window.docMeta = (async function () {
-  const version = '1.4-preview';
+  const version = '1.5-preview';
   const name = 'graph-data-science';
 
   const href = window.location.href;
