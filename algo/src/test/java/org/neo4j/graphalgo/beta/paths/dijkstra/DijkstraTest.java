139b82835dfc18f3bc35b646d8eeb1aa6696f02c
Martin Junghanns <martin.junghanns@neo4j.com>
2020-11-25 11:50:54 +0100

    Copy Dijkstra to algo and add skeleton config hierarchy
    Co-authored-by: Max Kießling <max.kiessling@neotechnology.com>
    Co-authored-by: Sören Reichardt <soren.reichardt@neotechnology.com>

---
+++ algo/src/test/java/org/neo4j/graphalgo/beta/paths/dijkstra/DijkstraTest.java
@@ -0,0 +1,213 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.beta.paths.dijkstra;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.graphalgo.api.RelationshipProperties;
+import org.neo4j.graphalgo.extension.GdlExtension;
+import org.neo4j.graphalgo.extension.GdlGraph;
+import org.neo4j.graphalgo.extension.IdFunction;
+import org.neo4j.graphalgo.extension.Inject;
+import org.neo4j.graphdb.Label;
+import org.neo4j.graphdb.RelationshipType;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@GdlExtension
+final class DijkstraTest {
+
+    static ImmutableDijkstraStreamConfig.Builder defaultConfigBuilder() {
+        return ImmutableDijkstraStreamConfig.builder()
+            .path(true)
+            .concurrency(1);
+    }
+
+    @Nested
+    class ShortestPathWithDirectedWeights {
+
+        // https://en.wikipedia.org/wiki/Shortest_path_problem#/media/File:Shortest_path_with_direct_weights.svg
+        @GdlGraph
+        private static final String DB_CYPHER =
+            "CREATE" +
+            "  (a:Label1)" +
+            ", (b:Label1)" +
+            ", (c:Label1)" +
+            ", (d:Label1)" +
+            ", (e:Label1)" +
+            ", (f:Label1)" +
+
+            ", (a)-[:TYPE1 {cost: 4}]->(b)" +
+            ", (a)-[:TYPE1 {cost: 2}]->(c)" +
+            ", (b)-[:TYPE1 {cost: 5}]->(c)" +
+            ", (b)-[:TYPE1 {cost: 10}]->(d)" +
+            ", (c)-[:TYPE1 {cost: 3}]->(e)" +
+            ", (d)-[:TYPE1 {cost: 11}]->(f)" +
+            ", (e)-[:TYPE1 {cost: 4}]->(d)";
+
+        @Inject
+        private Graph graph;
+
+        @Inject
+        private IdFunction idFunction;
+
+        @Test
+        void test1() {
+            ShortestPath expected = expected(
+                graph,
+                idFunction,
+                "a", "c", "e", "d", "f"
+            );
+            long[] nodeIds = expected.nodeIds;
+
+            DijkstraStreamConfig config = defaultConfigBuilder()
+                .sourceNode(nodeIds[0])
+                .targetNode(nodeIds[nodeIds.length - 1])
+                .build();
+            Dijkstra shortestPathDijkstra = new Dijkstra(graph, config);
+            shortestPathDijkstra.compute();
+            long[] path = Arrays
+                .stream(shortestPathDijkstra.getFinalPath().toArray())
+                .mapToLong(graph::toOriginalNodeId)
+                .toArray();
+
+            assertEquals(expected.weight, shortestPathDijkstra.getTotalCost(), 0.1);
+            assertArrayEquals(nodeIds, path);
+        }
+
+        @Test
+        void testResultStream() {
+            Label label = Label.label("Label1");
+            RelationshipType type = RelationshipType.withName("TYPE1");
+            ShortestPath expected = expected(
+                graph,
+                idFunction,
+                "a", "c", "e", "d", "f"
+            );
+            long head = expected.nodeIds[0], tail = expected.nodeIds[expected.nodeIds.length - 1];
+
+            DijkstraStreamConfig config = defaultConfigBuilder().sourceNode(head).targetNode(tail).build();
+            Dijkstra shortestPathDijkstra = new Dijkstra(graph, config);
+            shortestPathDijkstra.compute();
+            Stream<Dijkstra.Result> resultStream = shortestPathDijkstra.resultStream();
+
+            assertEquals(expected.weight, shortestPathDijkstra.getTotalCost(), 0.1);
+            assertEquals(expected.nodeIds.length, resultStream.count());
+        }
+    }
+
+    @Nested
+    class MoreComplexGraph {
+
+        // https://www.cise.ufl.edu/~sahni/cop3530/slides/lec326.pdf
+        // without the additional 14 edge
+        @GdlGraph
+        private static final String DB_CYPHER2 =
+            "CREATE" +
+            "  (n1:Label2)" +
+            ", (n2:Label2)" +
+            ", (n3:Label2)" +
+            ", (n4:Label2)" +
+            ", (n5:Label2)" +
+            ", (n6:Label2)" +
+            ", (n7:Label2)" +
+
+            ", (n1)-[:TYPE2 {cost: 6}]->(n2)" +
+            ", (n1)-[:TYPE2 {cost: 2}]->(n3)" +
+            ", (n1)-[:TYPE2 {cost: 16}]->(n4)" +
+            ", (n2)-[:TYPE2 {cost: 4}]->(n5)" +
+            ", (n2)-[:TYPE2 {cost: 5}]->(n4)" +
+            ", (n3)-[:TYPE2 {cost: 7}]->(n2)" +
+            ", (n3)-[:TYPE2 {cost: 3}]->(n5)" +
+            ", (n3)-[:TYPE2 {cost: 8}]->(n6)" +
+            ", (n4)-[:TYPE2 {cost: 7}]->(n3)" +
+            ", (n5)-[:TYPE2 {cost: 4}]->(n4)" +
+            ", (n5)-[:TYPE2 {cost: 10}]->(n7)" +
+            ", (n6)-[:TYPE2 {cost: 1}]->(n7)";
+
+        @Inject
+        private Graph graph;
+
+        @Inject
+        private IdFunction idFunction;
+
+        @Test
+        void test2() {
+            // graph 2
+            ShortestPath expected = expected(
+                graph,
+                idFunction,
+                "n1", "n3", "n6", "n7"
+            );
+
+            long[] nodeIds = expected.nodeIds;
+
+            DijkstraStreamConfig config = defaultConfigBuilder()
+                .sourceNode(nodeIds[0])
+                .targetNode(nodeIds[nodeIds.length - 1])
+                .build();
+            Dijkstra shortestPathDijkstra = new Dijkstra(graph, config);
+            shortestPathDijkstra.compute();
+            long[] path = Arrays
+                .stream(shortestPathDijkstra.getFinalPath().toArray())
+                .mapToLong(graph::toOriginalNodeId)
+                .toArray();
+
+            assertEquals(expected.weight, shortestPathDijkstra.getTotalCost(), 0.1);
+            assertArrayEquals(nodeIds, path);
+        }
+    }
+
+    private ShortestPath expected(
+        RelationshipProperties graph,
+        IdFunction idFunction,
+        String... nodeIds
+    ) {
+
+        var nodes = new long[nodeIds.length];
+        var weight = 0.0;
+        for (int i = 0; i < nodeIds.length; i++) {
+            var currentNode = idFunction.of(nodeIds[i]);
+            var j = i + 1;
+            if (j < nodeIds.length) {
+                var nextNode = idFunction.of(nodeIds[j]);
+                weight += graph.relationshipProperty(currentNode, nextNode);
+            }
+            nodes[i] = currentNode;
+        }
+
+        return new ShortestPath(nodes, weight);
+    }
+
+    private static final class ShortestPath {
+        private final long[] nodeIds;
+        private final double weight;
+
+        private ShortestPath(long[] nodeIds, double weight) {
+            this.nodeIds = nodeIds;
+            this.weight = weight;
+        }
+    }
+}
---
+++ core/src/main/java/org/neo4j/graphalgo/core/utils/queue/IntPriorityQueue.java
@@ -0,0 +1,339 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.neo4j.graphalgo.core.utils.queue;
+
+import com.carrotsearch.hppc.IntDoubleScatterMap;
+import org.neo4j.graphalgo.core.utils.collection.primitive.PrimitiveIntIterable;
+import org.neo4j.graphalgo.core.utils.collection.primitive.PrimitiveIntIterator;
+import org.neo4j.graphalgo.core.utils.mem.AllocationTracker;
+import org.neo4j.graphalgo.core.utils.paged.HugeArrays;
+import org.neo4j.graphalgo.core.utils.paged.HugeCursor;
+import org.neo4j.graphalgo.core.utils.paged.HugeIntArray;
+
+/**
+ * A PriorityQueue specialized for ints that maintains a partial ordering of
+ * its elements such that the smallest value can always be found in constant time.
+ * The definition of what <i>small</i> means is up to the implementing subclass.
+ * <p>
+ * Put()'s and pop()'s require log(size) time but the remove() cost implemented here is linear.
+ * <p>
+ * <b>NOTE</b>: Iteration order is not specified.
+ *
+ * Implementation has been copied from https://issues.apache.org/jira/browse/SOLR-2092
+ * and slightly adapted to our needs.
+ */
+public abstract class IntPriorityQueue implements PrimitiveIntIterable {
+
+    public static final int DEFAULT_CAPACITY = 14;
+
+    private static final int[] EMPTY_INT = new int[0];
+
+    private HugeIntArray heap;
+    private long size = 0;
+
+    /**
+     * Creates a new queue with the given capacity.
+     * The queue dynamically grows to hold all elements.
+     */
+    IntPriorityQueue(final int initialCapacity) {
+        final int heapSize;
+        if (0 == initialCapacity) {
+            // We allocate 1 extra to avoid if statement in top()
+            heapSize = 2;
+        } else {
+            // NOTE: we add +1 because all access to heap is
+            // 1-based not 0-based.  heap[0] is unused.
+            heapSize = initialCapacity + 1;
+        }
+        this.heap = HugeIntArray.newArray(HugeArrays.oversize(heapSize, Integer.BYTES), AllocationTracker.empty());
+    }
+
+    /**
+     * Defines the ordering of the queue.
+     * Returns true iff {@code a} is strictly less than {@code b}.
+     * <p>
+     * The default behavior assumes a min queue, where the smallest value is on top.
+     * To implement a max queue, return {@code b < a}.
+     * The resulting order is not stable.
+     */
+    protected abstract boolean lessThan(int a, int b);
+
+    /**
+     * Adds the cost for the given element.
+     *
+     * @return true if the cost was changed, indicating that the node is already in the queue.
+     */
+    protected abstract boolean addCost(int element, double cost);
+
+    /**
+     * remove given element from cost management
+     */
+    protected abstract void removeCost(int element);
+
+    /**
+     * Gets the cost for the given element.
+     */
+    protected abstract double cost(int element);
+
+    /**
+     * Adds an int associated with the given weight to a queue in log(size) time.
+     * <p>
+     * NOTE: The default implementation does nothing with the cost parameter.
+     * It is up to the implementation how the cost parameter is used.
+     */
+    public final void add(int element, double cost) {
+        addCost(element, cost);
+        size++;
+        ensureCapacityForInsert();
+        heap.set(size, element);
+        upHeap(size);
+    }
+
+    private void add(int element) {
+        size++;
+        ensureCapacityForInsert();
+        heap.set(size, element);
+        upHeap(size);
+    }
+
+    /**
+     * @return the least element of the queue in constant time.
+     */
+    public final int top() {
+        // We don't need to check size here: if maxSize is 0,
+        // then heap is length 2 array with both entries null.
+        // If size is 0 then heap[1] is already null.
+        return heap.get(1);
+    }
+
+    /**
+     * Removes and returns the least element of the queue in log(size) time.
+     *
+     * @return the least element of the queue in log(size) time while removing it.
+     */
+    public final int pop() {
+        if (size > 0) {
+            int result = heap.get(1);    // save first value
+            heap.set(1, heap.get(size));    // move last to first
+            size--;
+            downHeap(1);           // adjust heap
+            removeCost(result);
+            return result;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * @return the number of elements currently stored in the queue.
+     */
+    public final long size() {
+        return size;
+    }
+
+    /**
+     * @return true iff there are currently no elements stored in the queue.
+     */
+    public final boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Removes all entries from the queue.
+     */
+    public void clear() {
+        size = 0;
+    }
+
+    /**
+     * Updates the heap because the cost of an element has changed, possibly from the outside.
+     * Cost is linear with the size of the queue.
+     */
+    public final void update(int element) {
+        long pos = findElementPosition(element);
+        if (pos != 0) {
+            if (!upHeap(pos) && pos < size) {
+                downHeap(pos);
+            }
+        }
+    }
+
+    public final void set(int element, double cost) {
+        if (addCost(element, cost)) {
+            update(element);
+        } else {
+            add(element);
+        }
+    }
+
+    private long findElementPosition(int element) {
+        final long limit = size + 1;
+        final HugeIntArray data = heap;
+        HugeCursor<int[]> cursor = data.initCursor(data.newCursor(), 1, limit);
+        while (cursor.next()) {
+            int[] internalArray = cursor.array;
+            int i = cursor.offset;
+            int localLimit = cursor.limit - 4;
+            for (; i <= localLimit; i += 4) {
+                if (internalArray[i] == element) return i + cursor.base;
+                if (internalArray[i + 1] == element) return i + 1 + cursor.base;
+                if (internalArray[i + 2] == element) return i + 2 + cursor.base;
+                if (internalArray[i + 3] == element) return i + 3 + cursor.base;
+            }
+            for (; i < cursor.limit; ++i) {
+                if (internalArray[i] == element) return i + cursor.base;
+            }
+        }
+        return 0;
+    }
+
+    /**
+     * Removes all entries from the queue, releases all buffers.
+     * The queue can no longer be used afterwards.
+     */
+    public void release() {
+        size = 0;
+        heap = null;
+    }
+
+    private boolean upHeap(long origPos) {
+        long i = origPos;
+        int node = heap.get(i);          // save bottom node
+        long j = i >>> 1;
+        while (j > 0 && lessThan(node, heap.get(j))) {
+            heap.set(i, heap.get(j));       // shift parents down
+            i = j;
+            j = j >>> 1;
+        }
+        heap.set(i, node);              // install saved node
+        return i != origPos;
+    }
+
+    private void downHeap(long i) {
+        int node = heap.get(i);          // save top node
+        long j = i << 1;              // find smaller child
+        long k = j + 1;
+        if (k <= size && lessThan(heap.get(k), heap.get(j))) {
+            j = k;
+        }
+        while (j <= size && lessThan(heap.get(j), node)) {
+            heap.set(i, heap.get(j));       // shift up child
+            i = j;
+            j = i << 1;
+            k = j + 1;
+            if (k <= size && lessThan(heap.get(k), heap.get(j))) {
+                j = k;
+            }
+        }
+        heap.set(i, node);              // install saved node
+    }
+
+    private void ensureCapacityForInsert() {
+        if (size >= heap.size()) {
+            long oversize = HugeArrays.oversize(size + 1, Integer.BYTES);
+            heap = heap.copyOf(oversize, AllocationTracker.empty());
+        }
+    }
+
+    @Override
+    public PrimitiveIntIterator iterator() {
+        return new PrimitiveIntIterator() {
+
+            int i = 1;
+
+            @Override
+            public boolean hasNext() {
+                return i <= size;
+            }
+
+            /**
+             * @throws ArrayIndexOutOfBoundsException when the iterator is exhausted.
+             */
+            @Override
+            public int next() {
+                return heap.get(i++);
+            }
+        };
+    }
+
+    public static IntPriorityQueue min(int capacity) {
+        return new AbstractPriorityQueue(capacity) {
+            @Override
+            protected boolean lessThan(int a, int b) {
+                return costs.get(a) < costs.get(b);
+            }
+        };
+    }
+
+    public static IntPriorityQueue max(int capacity) {
+        return new AbstractPriorityQueue(capacity) {
+            @Override
+            protected boolean lessThan(int a, int b) {
+                return costs.get(a) > costs.get(b);
+            }
+        };
+    }
+
+    public static IntPriorityQueue min() {
+        return min(DEFAULT_CAPACITY);
+    }
+
+    public static IntPriorityQueue max() {
+        return max(DEFAULT_CAPACITY);
+    }
+
+    private static abstract class AbstractPriorityQueue extends IntPriorityQueue {
+
+        protected final IntDoubleScatterMap costs;
+
+        public AbstractPriorityQueue(int initialCapacity) {
+            super(initialCapacity);
+            this.costs = new IntDoubleScatterMap(initialCapacity);
+        }
+
+        @Override
+        protected boolean addCost(int element, double cost) {
+            return costs.put(element, cost) != 0D;
+        }
+
+        @Override
+        protected void removeCost(final int element) {
+            costs.remove(element);
+        }
+
+        @Override
+        protected double cost(int element) {
+            return costs.get(element);
+        }
+
+        @Override
+        public void clear() {
+            super.clear();
+            costs.clear();
+        }
+
+        @Override
+        public void release() {
+            super.release();
+            costs.keys = EMPTY_INT;
+            costs.clear();
+            costs.keys = null;
+            costs.values = null;
+        }
+    }
+
+}
