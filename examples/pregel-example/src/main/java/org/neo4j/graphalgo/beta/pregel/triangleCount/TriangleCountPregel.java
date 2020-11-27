4fd3c148186a0d9c117ca2f780e68bc3e7ced724
FlorentinD <florentin.dorre@neotechnology.com>
2020-11-27 12:16:51 +0100

    Use `getNeighbours` and replace the bitset with a hashset

--- examples/pregel-example/src/main/java/org/neo4j/graphalgo/beta/pregel/triangleCount/TriangleCountPregel.java
+++ examples/pregel-example/src/main/java/org/neo4j/graphalgo/beta/pregel/triangleCount/TriangleCountPregel.java
@@ -19,7 +19,7 @@
  */
 package org.neo4j.graphalgo.beta.pregel.triangleCount;
 
-import com.carrotsearch.hppc.BitSet;
+import com.carrotsearch.hppc.LongHashSet;
 import org.neo4j.graphalgo.api.nodeproperties.ValueType;
 import org.neo4j.graphalgo.beta.pregel.Pregel;
 import org.neo4j.graphalgo.beta.pregel.PregelComputation;
@@ -28,6 +28,9 @@ import org.neo4j.graphalgo.beta.pregel.PregelSchema;
 import org.neo4j.graphalgo.beta.pregel.annotation.GDSMode;
 import org.neo4j.graphalgo.beta.pregel.annotation.PregelProcedure;
 
+import java.util.concurrent.atomic.LongAdder;
+import java.util.stream.LongStream;
+
 /**
  * ! Assuming an unweighted graph
  */
@@ -35,13 +38,11 @@ import org.neo4j.graphalgo.beta.pregel.annotation.PregelProcedure;
 public class TriangleCountPregel implements PregelComputation<TriangleCountPregelConfig> {
 
     public static final String TRIANGLE_COUNT = "TRIANGLES";
-    private static final String NEIGHBOURS = "NEIGHBOURS";
 
     @Override
     public PregelSchema schema() {
         return new PregelSchema.Builder()
             .add(TRIANGLE_COUNT, ValueType.LONG)
-            .add(NEIGHBOURS, ValueType.LONG_ARRAY, PregelSchema.Visibility.PRIVATE)
             .build();
     }
 
@@ -49,53 +50,41 @@ public class TriangleCountPregel implements PregelComputation<TriangleCountPrege
     public void compute(PregelContext.ComputeContext<TriangleCountPregelConfig> context, Pregel.Messages messages) {
         if (context.isInitialSuperstep()) {
             context.setNodeValue(TRIANGLE_COUNT, 0);
-            // assuming out-degree == in-degree
-            context.setNodeValue(NEIGHBOURS, new long[context.degree()]);
-            // send node id to each neighbour -> build neighbour set for each node
-            context.sendToNeighbors(context.nodeId());
-        } else if (context.superstep() == Phase.GATHER_NEIGHBORS.step) {
-            long[] neighbours = context.longArrayNodeValue(NEIGHBOURS);
-            int idx = 0;
-            for (Double message : messages) {
-                neighbours[idx] = message.longValue();
-                idx++;
-            }
         } else if (context.superstep() == Phase.MERGE_NEIGHBORS.step) {
-            long[] neighbours = context.longArrayNodeValue(NEIGHBOURS);
-            var isNeighbourFromA = new BitSet(context.nodeCount());
+            var neighbourStream = context.getNeighbours();
+            if (context.isMultiGraph()) {
+                neighbourStream = neighbourStream.distinct();
+            }
+
+            long[] neighbours = neighbourStream.toArray();
+            var isNeighbourFromA = new LongHashSet(neighbours.length);
+
             for (long nodeB : neighbours) {
-                isNeighbourFromA.set(nodeB);
+                isNeighbourFromA.add(nodeB);
             }
 
             long nodeA = context.nodeId();
-            long trianglesFromNodeA = 0;
-            long lastNodeB = -1;
+            LongAdder trianglesFromNodeA = new LongAdder();
+
             for (long nodeB : neighbours) {
-                if (nodeB == lastNodeB) {
-                    continue;
-                }
-                lastNodeB = nodeB;
-
                 if (nodeB > nodeA) {
-                    long[] followingNeighbours = context.longArrayNodeValue(NEIGHBOURS, nodeB);
-                    long lastNodeC = -1;
-
-                    // find common neighbors
-                    // check indexed neighbours of A
-                    for (long nodeC : followingNeighbours) {
-                        if (lastNodeC != nodeC && nodeC > nodeB && isNeighbourFromA.get(nodeC)) {
-                            trianglesFromNodeA++;
-                            lastNodeC = nodeC;
+                    LongStream cNodes = context.getNeighbours(nodeB);
+                    if (context.isMultiGraph()) {
+                        cNodes = cNodes.distinct();
+                    }
+                    cNodes.forEach(nodeC -> {
+                        // find common neighbors
+                        // check indexed neighbours of A
+                        if (nodeC > nodeB && isNeighbourFromA.contains(nodeC)) {
+                            trianglesFromNodeA.increment();
                             context.sendTo(nodeB, 1);
                             context.sendTo(nodeC, 1);
                         }
-                    }
+                    });
                 }
             }
-            context.setNodeValue(TRIANGLE_COUNT, trianglesFromNodeA);
+            context.setNodeValue(TRIANGLE_COUNT, trianglesFromNodeA.longValue());
         } else if (context.superstep() == Phase.COUNT_TRIANGLES.step) {
-            // free arrays again
-            context.setNodeValue(NEIGHBOURS, (long[]) null);
             long triangles = context.longNodeValue(TRIANGLE_COUNT);
             for (Double ignored : messages) {
                 triangles++;
@@ -107,9 +96,8 @@ public class TriangleCountPregel implements PregelComputation<TriangleCountPrege
     }
 
     enum Phase {
-        GATHER_NEIGHBORS(1),
-        MERGE_NEIGHBORS(2),
-        COUNT_TRIANGLES(3);
+        MERGE_NEIGHBORS(1),
+        COUNT_TRIANGLES(2);
 
         final long step;
 
