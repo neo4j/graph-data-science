a6c96d8ff22cb08059dbb839e56dfa827abb271c
Soren <soer3nreichardt@googlemail.com>
2020-12-02 16:27:22 +0100

    Implement path factory
    Co-authored-by: Martin Junghanns <martin.junghanns@neotechnology.com>

---
+++ algo/src/main/java/org/neo4j/graphalgo/beta/paths/PathFactory.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.beta.paths;
+
+import org.neo4j.graphalgo.impl.util.PathImpl;
+import org.neo4j.graphdb.Path;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.kernel.api.KernelTransaction;
+
+import java.util.List;
+
+public final class PathFactory {
+
+    public static final long DEFAULT_RELATIONSHIP_OFFSET = -1L;
+
+    private PathFactory() {}
+
+    public static Path create(
+        KernelTransaction kernelTx,
+        long relationshipIdOffset,
+        List<Long> nodeIds,
+        List<Double> costs,
+        RelationshipType relationshipType,
+        String costPropertyName
+    ) {
+        var tx = kernelTx.internalTransaction();
+        var firstNodeId = nodeIds.get(0);
+        var pathBuilder = new PathImpl.Builder(tx.getNodeById(firstNodeId));
+
+        for (int i = 0; i < nodeIds.size() - 1; i++) {
+            long sourceNodeId = nodeIds.get(i);
+            long targetNodeId = nodeIds.get(i + 1);
+
+            var relationship = new VirtualRelationship(
+                relationshipIdOffset--,
+                tx.getNodeById(sourceNodeId),
+                tx.getNodeById(targetNodeId),
+                relationshipType
+            );
+            var costDifference = costs.get(i + 1) - costs.get(i);
+            relationship.setProperty(costPropertyName, costDifference);
+            pathBuilder = pathBuilder.push(relationship);
+        }
+
+        return pathBuilder.build();
+    }
+}
---
+++ algo/src/test/java/org/neo4j/graphalgo/beta/paths/PathFactoryTest.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.graphalgo.beta.paths;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.BaseProcTest;
+import org.neo4j.graphalgo.compat.GraphDatabaseApiProxy;
+import org.neo4j.graphdb.RelationshipType;
+import org.neo4j.internal.kernel.api.exceptions.TransactionFailureException;
+import org.neo4j.kernel.api.KernelTransaction;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.neo4j.graphalgo.beta.paths.PathFactory.DEFAULT_RELATIONSHIP_OFFSET;
+
+class PathFactoryTest extends BaseProcTest {
+
+    @Nested
+    class SingleNode {
+        static final String DB_CYPHER = "CREATE ()";
+
+        KernelTransaction ktx;
+
+        @BeforeEach
+        void setup() {
+            runQuery(DB_CYPHER);
+            ktx = GraphDatabaseApiProxy.newKernelTransaction(db).ktx();
+        }
+
+        @AfterEach
+        void tearDown() throws TransactionFailureException {
+            ktx.close();
+        }
+
+        @Test
+        void emptyPath() {
+            var nodeIds = List.of(0L);
+            var costs = List.of(0.0D);
+            var path = PathFactory.create(
+                ktx,
+                DEFAULT_RELATIONSHIP_OFFSET,
+                nodeIds,
+                costs,
+                RelationshipType.withName("REL"),
+                "prop"
+            );
+
+            assertEquals(0, path.length());
+
+        }
+    }
+
+    @Nested
+    class MultipleNodes {
+        static final String DB_CYPHER =
+            "CREATE" +
+            "  (a)" +
+            ", (b)" +
+            ", (c)" +
+            ", (a)-[:R]->(b)" +
+            ", (b)-[:R]->(c)";
+
+        KernelTransaction ktx;
+
+        @BeforeEach
+        void setup() {
+            runQuery(DB_CYPHER);
+            ktx = GraphDatabaseApiProxy.newKernelTransaction(db).ktx();
+        }
+
+        @AfterEach
+        void tearDown() throws TransactionFailureException {
+            ktx.close();
+        }
+
+        @Test
+        void pathWithCosts() {
+            var nodeIds = List.of(0L, 1L, 2L);
+            var costs = List.of(0.0D, 1.0D, 4.0D);
+            var path = PathFactory.create(
+                ktx,
+                DEFAULT_RELATIONSHIP_OFFSET,
+                nodeIds,
+                costs,
+                RelationshipType.withName("REL"),
+                "prop"
+            );
+
+            assertEquals(2, path.length());
+
+            path.relationships().forEach(relationship -> {
+                var actualCost = (double) relationship.getProperty("prop");
+                var expectedCost = costs.get((int) relationship.getEndNodeId()) - costs.get((int) relationship.getStartNodeId());
+
+                assertEquals(expectedCost, actualCost, 1E-4);
+            });
+        }
+    }
+}
