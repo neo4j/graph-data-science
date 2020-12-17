e85a336e331714a968a2b40011f8e6bc21833243
Martin Junghanns <martin.junghanns@neo4j.com>
2020-12-17 12:22:02 +0100

    Let AStar write config extend path options config

--- algo/src/main/java/org/neo4j/graphalgo/beta/paths/astar/config/ShortestPathAStarWriteConfig.java
+++ algo/src/main/java/org/neo4j/graphalgo/beta/paths/astar/config/ShortestPathAStarWriteConfig.java
@@ -21,6 +21,7 @@ package org.neo4j.graphalgo.beta.paths.astar.config;
 
 import org.neo4j.graphalgo.annotation.Configuration;
 import org.neo4j.graphalgo.annotation.ValueClass;
+import org.neo4j.graphalgo.beta.paths.WritePathOptionsConfig;
 import org.neo4j.graphalgo.config.GraphCreateConfig;
 import org.neo4j.graphalgo.config.WriteRelationshipConfig;
 import org.neo4j.graphalgo.core.CypherMapWrapper;
@@ -30,7 +31,7 @@ import java.util.Optional;
 @ValueClass
 @Configuration
 @SuppressWarnings("immutables:subtype")
-public interface ShortestPathAStarWriteConfig extends ShortestPathAStarBaseConfig, WriteRelationshipConfig {
+public interface ShortestPathAStarWriteConfig extends ShortestPathAStarBaseConfig, WriteRelationshipConfig, WritePathOptionsConfig {
 
     static ShortestPathAStarWriteConfig of(
         String username,
