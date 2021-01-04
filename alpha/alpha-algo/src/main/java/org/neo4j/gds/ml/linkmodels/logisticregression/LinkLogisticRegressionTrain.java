0f5a5434154b0211901e448c0f5e99923af8ff15
Jacob Sznajdman <breakanalysis@gmail.com>
2021-01-04 16:15:27 +0100

    Implement Train algorithm for LinkLogisticRegression
    Co-Authored-By: Veselin Nikolov <veselin.nikolov@neotechnology.com>

---
+++ alpha/alpha-algo/src/main/java/org/neo4j/gds/ml/linkmodels/logisticregression/LinkLogisticRegressionTrain.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.linkmodels.logisticregression;
+
+import org.neo4j.gds.ml.Training;
+import org.neo4j.gds.ml.TrainingSettings;
+import org.neo4j.graphalgo.Algorithm;
+import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.logging.Log;
+
+public class LinkLogisticRegressionTrain extends Algorithm<LinkLogisticRegressionTrain, LinkLogisticRegressionPredictor> {
+    private final Graph graph;
+    private final TrainingSettings trainingSettings;
+    private final LinkLogisticRegressionTrainConfig config;
+    private final Log log;
+
+    public LinkLogisticRegressionTrain(
+        Graph graph,
+        TrainingSettings trainingSettings,
+        LinkLogisticRegressionTrainConfig config,
+        Log log
+    ) {
+        this.graph = graph;
+        this.trainingSettings = trainingSettings;
+        this.config = config;
+        this.log = log;
+    }
+
+    @Override
+    public LinkLogisticRegressionPredictor compute() {
+        var objective = new LinkLogisticRegressionObjective(
+            config.featureProperties(),
+            LinkFeatureCombiner.valueOf(config.linkFeatureCombiner()),
+            graph
+        );
+        var training = new Training(trainingSettings, log, graph.nodeCount());
+        training.train(objective, () -> trainingSettings.batchQueue(graph.nodeCount()), config.concurrency());
+        return new LinkLogisticRegressionPredictor(objective.modelData);
+    }
+
+    @Override
+    public LinkLogisticRegressionTrain me() {
+        return this;
+    }
+
+    @Override
+    public void release() {
+
+    }
+}
---
+++ alpha/alpha-algo/src/test/java/org/neo4j/gds/ml/linkmodels/logisticregression/LinkLogisticRegressionTrainTest.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2017-2020 "Neo4j,"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.linkmodels.logisticregression;
+
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
+import org.neo4j.gds.ml.ImmutableTrainingSettings;
+import org.neo4j.graphalgo.TestLog;
+import org.neo4j.graphalgo.api.Graph;
+import org.neo4j.graphalgo.extension.GdlExtension;
+import org.neo4j.graphalgo.extension.GdlGraph;
+import org.neo4j.graphalgo.extension.Inject;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.neo4j.gds.embeddings.graphsage.ddl4j.Dimensions.COLUMNS_INDEX;
+import static org.neo4j.gds.embeddings.graphsage.ddl4j.Dimensions.ROWS_INDEX;
+
+@GdlExtension
+class LinkLogisticRegressionTrainTest {
+
+    @GdlGraph
+    private static final String DB_QUERY =
+        "CREATE " +
+        "  (n1:N {a: 2.0, b: 1.2})" +
+        ", (n2:N {a: 1.3, b: 0.5})" +
+        ", (n3:N {a: 0.0, b: 2.8})" +
+        ", (n4:N {a: 1.0, b: 0.9})" +
+        ", (n1)-[:T {label: 1.0}]->(n2)" +
+        ", (n3)-[:T {label: 1.0}]->(n4)" +
+        ", (n1)-[:T {label: 0.0}]->(n3)" +
+        ", (n2)-[:T {label: 0.0}]->(n4)";
+
+    @Inject
+    private Graph graph;
+
+    @Test
+    void shouldComputeWithDefaultAdamOptimizerAndStreakStopper() {
+        var config =
+            ImmutableLinkLogisticRegressionTrainConfig.builder()
+                .featureProperties(List.of("a", "b"))
+                .concurrency(1)
+                .build();
+
+        var trainingSettings = ImmutableTrainingSettings.builder()
+            .maxIterations(100000)
+            .tolerance(1e-4)
+            .build();
+        var linearRegression = new LinkLogisticRegressionTrain(graph, trainingSettings, config, new TestLog());
+
+        var result = linearRegression.compute().modelData();
+
+        assertThat(result).isNotNull();
+
+        var trainedWeights = result.weights();
+        assertThat(trainedWeights.dimension(ROWS_INDEX)).isEqualTo(1);
+        assertThat(trainedWeights.dimension(COLUMNS_INDEX)).isEqualTo(3);
+
+        assertThat(trainedWeights.data().data()).containsExactly(
+            new double[]{-1.0681821169962793, 1.0115009499444914, -0.1381213947059403},
+            Offset.offset(1e-8)
+        );
+    }
+}
