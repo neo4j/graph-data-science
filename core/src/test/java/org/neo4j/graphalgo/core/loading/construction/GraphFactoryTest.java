087b98a6c6064a38e80fdb8c90be9e35de772cb8
Paul Horn <paul.horn@neotechnology.com>
2021-01-14 15:56:54 +0100

    Remove unused import from test

--- core/src/test/java/org/neo4j/graphalgo/core/loading/construction/GraphFactoryTest.java
+++ core/src/test/java/org/neo4j/graphalgo/core/loading/construction/GraphFactoryTest.java
@@ -19,7 +19,6 @@
  */
 package org.neo4j.graphalgo.core.loading.construction;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
