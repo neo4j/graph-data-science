014f23c618282636401f233509679d86c2b31571
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2021-02-11 09:48:24 +0000

    Add a test to verify that training with different features produces different metrics

--- alpha/alpha-algo/src/test/java/org/neo4j/gds/ml/nodemodels/NodeClassificationTrainTest.java
+++ alpha/alpha-algo/src/test/java/org/neo4j/gds/ml/nodemodels/NodeClassificationTrainTest.java
@@ -20,9 +20,13 @@
 package org.neo4j.gds.ml.nodemodels;
 
 import org.assertj.core.data.Percentage;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.neo4j.gds.ml.nodemodels.logisticregression.ImmutableNodeClassificationTrainConfig;
+import org.neo4j.gds.ml.nodemodels.logisticregression.NodeClassificationTrainConfig;
+import org.neo4j.gds.ml.nodemodels.metrics.Metric;
 import org.neo4j.graphalgo.TestLog;
+import org.neo4j.graphalgo.core.GdsEdition;
 import org.neo4j.graphalgo.extension.GdlExtension;
 import org.neo4j.graphalgo.extension.GdlGraph;
 import org.neo4j.graphalgo.extension.Inject;
@@ -32,7 +36,6 @@ import java.util.List;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.neo4j.gds.ml.nodemodels.metrics.Metric.F1_MACRO;
 
 @GdlExtension
 class NodeClassificationTrainTest {
@@ -42,53 +45,52 @@ class NodeClassificationTrainTest {
 
     @GdlGraph
     private static final String DB_QUERY =
-        "  (:N {a: 1.2, b: 1.2, t: 0})" +
-        ", (:N {a: 2.8, b: 2.5, t: 0})" +
-        ", (:N {a: 3.3, b: 0.5, t: 0})" +
-        ", (:N {a: 1.0, b: 0.5, t: 0})" +
-        ", (:N {a: 1.32, b: 0.5, t: 0})" +
-        ", (:N {a: 1.3, b: 1.5, t: 1})" +
-        ", (:N {a: 5.3, b: 10.5, t: 1})" +
-        ", (:N {a: 1.3, b: 2.5, t: 1})" +
-        ", (:N {a: 0.0, b: 66.8, t: 1})" +
-        ", (:N {a: 0.1, b: 2.8, t: 1})" +
-        ", (:N {a: 0.66, b: 2.8, t: 1})" +
-        ", (:N {a: 2.0, b: 10.8, t: 1})" +
-        ", (:N {a: 5.0, b: 7.8, t: 1})" +
-        ", (:N {a: 4.0, b: 5.8, t: 1})" +
-        ", (:N {a: 1.0, b: 0.9, t: 1})";
+        "  (:N {bananas: 100.0, arrayProperty: [1.2, 1.2], a: 1.2, b: 1.2, t: 0})" +
+        ", (:N {bananas: 100.0, arrayProperty: [2.8, 2.5], a: 2.8, b: 2.5, t: 0})" +
+        ", (:N {bananas: 100.0, arrayProperty: [3.3, 0.5], a: 3.3, b: 0.5, t: 0})" +
+        ", (:N {bananas: 100.0, arrayProperty: [1.0, 0.5], a: 1.0, b: 0.5, t: 0})" +
+        ", (:N {bananas: 100.0, arrayProperty: [1.32, 0.5], a: 1.32, b: 0.5, t: 0})" +
+        ", (:N {bananas: 100.0, arrayProperty: [1.3, 1.5], a: 1.3, b: 1.5, t: 1})" +
+        ", (:N {bananas: 100.0, arrayProperty: [5.3, 10.5], a: 5.3, b: 10.5, t: 1})" +
+        ", (:N {bananas: 100.0, arrayProperty: [1.3, 2.5], a: 1.3, b: 2.5, t: 1})" +
+        ", (:N {bananas: 100.0, arrayProperty: [0.0, 66.8], a: 0.0, b: 66.8, t: 1})" +
+        ", (:N {bananas: 100.0, arrayProperty: [0.1, 2.8], a: 0.1, b: 2.8, t: 1})" +
+        ", (:N {bananas: 100.0, arrayProperty: [0.66, 2.8], a: 0.66, b: 2.8, t: 1})" +
+        ", (:N {bananas: 100.0, arrayProperty: [2.0, 10.8], a: 2.0, b: 10.8, t: 1})" +
+        ", (:N {bananas: 100.0, arrayProperty: [5.0, 7.8], a: 5.0, b: 7.8, t: 1})" +
+        ", (:N {bananas: 100.0, arrayProperty: [4.0, 5.8], a: 4.0, b: 5.8, t: 1})" +
+        ", (:N {bananas: 100.0, arrayProperty: [1.0, 0.9], a: 1.0, b: 0.9, t: 1})";
 
     @Inject
     TestGraph graph;
 
-    @Test
-    void selectsTheBestModel() {
+    @ParameterizedTest
+    @EnumSource(Metric.class)
+    void selectsTheBestModel(Metric metric) {
 
-        Map<String, Object> model1 = Map.of( "penalty", 1, "maxIterations", 0);
-        Map<String, Object> model2 = Map.of( "penalty", 1, "maxIterations", 10000, "tolerance", 1e-5);
+        Map<String, Object> model1 = Map.of("penalty", 1, "maxIterations", 0);
+        Map<String, Object> model2 = Map.of("penalty", 1, "maxIterations", 10000, "tolerance", 1e-5);
 
         Map<String, Object> expectedWinner = model2;
         var log = new TestLog();
+        var config = createConfig(
+            List.of(model1, model2),
+            "model",
+            List.of("a", "b"),
+            metric,
+            1L
+        );
+
         var ncTrain = new NodeClassificationTrain(
             graph,
-            ImmutableNodeClassificationTrainConfig.builder()
-                .modelName("model")
-                .featureProperties(List.of("a", "b"))
-                .holdoutFraction(0.33)
-                .validationFolds(2)
-                .concurrency(1)
-                .randomSeed(1L)
-                .targetProperty("t")
-                .metrics(List.of(F1_MACRO))
-                .params(List.of(model1, model2))
-                .build(),
+            config,
             log
         );
 
         var model = ncTrain.compute();
 
         var customInfo = (NodeClassificationModelInfo) model.customInfo();
-        var validationScores = customInfo.metrics().get(F1_MACRO).validation();
+        var validationScores = customInfo.metrics().get(metric).validation();
 
         assertThat(validationScores).hasSize(2);
         var actualWinnerParams = customInfo.bestParameters();
@@ -96,5 +98,92 @@ class NodeClassificationTrainTest {
         double model1Score = validationScores.get(0).avg();
         double model2Score = validationScores.get(1).avg();
         assertThat(model1Score).isNotCloseTo(model2Score, Percentage.withPercentage(0.2));
+    }
+
+    @ParameterizedTest
+    @EnumSource(Metric.class)
+    void shouldProduceDifferentMetricsForDifferentTrainings(Metric metric) {
+
+        GdsEdition.instance().setToEnterpriseAndRun(() -> {
+
+            var log = new TestLog();
+            var modelCandidates = List.of(
+                Map.<String, Object>of("penalty", 0.0625, "maxIterations", 1000),
+                Map.<String, Object>of("penalty", 0.125, "maxIterations", 1000),
+                Map.<String, Object>of("penalty", 0.25, "maxIterations", 1000),
+                Map.<String, Object>of("penalty", 0.5, "maxIterations", 1000),
+                Map.<String, Object>of("penalty", 1.0, "maxIterations", 1000),
+                Map.<String, Object>of("penalty", 2.0, "maxIterations", 1000),
+                Map.<String, Object>of("penalty", 4.0, "maxIterations", 1000)
+            );
+
+            var bananasConfig = createConfig(
+                modelCandidates,
+                "bananasModel",
+                List.of("bananas"),
+                metric,
+                1337L
+            );
+            var bananasTrain = new NodeClassificationTrain(
+                graph,
+                bananasConfig,
+                log
+            );
+
+            var arrayPropertyConfig = createConfig(modelCandidates,
+                "arrayPropertyModel",
+                List.of("arrayProperty"),
+                metric,
+                42L
+            );
+            var arrayPropertyTrain = new NodeClassificationTrain(
+                graph,
+                arrayPropertyConfig,
+                log
+            );
+
+            var bananasModel = bananasTrain.compute();
+            var arrayPropertyModel = arrayPropertyTrain.compute();
+
+            assertThat(arrayPropertyModel)
+                .usingRecursiveComparison()
+                .withFailMessage("The trained models are exactly the same instance!")
+                .isNotSameAs(bananasModel);
+
+            assertThat(arrayPropertyModel.data())
+                .usingRecursiveComparison()
+                .withFailMessage("Should not produce the same trained `data`!")
+                .isNotEqualTo(bananasModel.data());
+
+            var bananasCustomInfo = (NodeClassificationModelInfo) bananasModel.customInfo();
+            var bananasValidationScore = bananasCustomInfo.metrics().get(metric);
+
+            var arrayPropertyCustomInfo = (NodeClassificationModelInfo) arrayPropertyModel.customInfo();
+            var arrayPropertyValidationScores = arrayPropertyCustomInfo.metrics().get(metric);
+
+            assertThat(arrayPropertyValidationScores)
+                .usingRecursiveComparison()
+                .isNotSameAs(bananasValidationScore)
+                .isNotEqualTo(bananasValidationScore);
+        });
+    }
+
+    private NodeClassificationTrainConfig createConfig(
+        Iterable<Map<String, Object>> modelCandidates,
+        String modelName,
+        Iterable<String> featureProperties,
+        Metric metric, long randomSeed
+    ) {
+        return ImmutableNodeClassificationTrainConfig.builder()
+            .modelName(modelName)
+            .featureProperties(featureProperties)
+            .holdoutFraction(0.33)
+            .validationFolds(2)
+            .concurrency(1)
+            .randomSeed(randomSeed)
+            .targetProperty("t")
+            .metrics(List.of(metric))
+            .params(modelCandidates)
+            .build();
     }
 }
