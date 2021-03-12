bc61762ce1339aee19a99169771a3abb243c3177
Martin Junghanns <martin.junghanns@neo4j.com>
2021-03-12 11:26:29 +0100

    Add evaluator tests for binary expressions
    Co-authored-by: Max Kießling <max.kiessling@neotechnology.com>

--- core/src/test/java/org/neo4j/graphalgo/beta/filter/expr/ExpressionEvaluatorTest.java
+++ core/src/test/java/org/neo4j/graphalgo/beta/filter/expr/ExpressionEvaluatorTest.java
@@ -23,6 +23,9 @@ import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
 import net.jqwik.api.constraints.DoubleRange;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.opencypher.v9_0.parser.javacc.ParseException;
 
 import java.util.List;
@@ -112,6 +115,75 @@ class ExpressionEvaluatorTest {
         assertThat(expr.evaluate(EMPTY_CONTEXT) == TRUE).isTrue();
     }
 
+    @Property
+    void notEqual(
+        @ForAll @DoubleRange(min = 1E10, minIncluded = false) double left,
+        @ForAll @DoubleRange(max = 1E10, maxIncluded = false) double right
+    ) throws ParseException {
+        var expr = ExpressionParser.parse(formatWithLocale("%f <> %f", left, right));
+        assertThat(expr.evaluate(EMPTY_CONTEXT) == TRUE).isTrue();
+
+        expr = ExpressionParser.parse(formatWithLocale("%f != %f", left, right));
+        assertThat(expr.evaluate(EMPTY_CONTEXT) == TRUE).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"NOT TRUE", "NOT (1337 > 42)"})
+    void notTrue(String cypher) throws ParseException {
+        var expr = ExpressionParser.parse(cypher);
+        assertThat(expr.evaluate(EMPTY_CONTEXT) == TRUE).isFalse();
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"NOT FALSE", "NOT (1337 < 42)"})
+    void notFalse(String cypher) throws ParseException {
+        var expr = ExpressionParser.parse(cypher);
+        assertThat(expr.evaluate(EMPTY_CONTEXT) == TRUE).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "false,false,false",
+        "true,false,false",
+        "false,true,false",
+        "true,true,true"
+    })
+    void and(boolean left, boolean right, boolean expected) throws ParseException {
+        var expr = ExpressionParser.parse(formatWithLocale("%s AND %s", left, right));
+        assertThat(expr.evaluate(EMPTY_CONTEXT) == TRUE).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "false,false,false",
+        "true,false,true",
+        "false,true,true",
+        "true,true,true"
+    })
+    void or(boolean left, boolean right, boolean expected) throws ParseException {
+        var expr = ExpressionParser.parse(formatWithLocale("%s OR %s", left, right));
+        assertThat(expr.evaluate(EMPTY_CONTEXT) == TRUE).isEqualTo(expected);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+        "false,false,false",
+        "true,false,true",
+        "false,true,true",
+        "true,true,false"
+    })
+    void xor(boolean left, boolean right, boolean expected) throws ParseException {
+        var expr = ExpressionParser.parse(formatWithLocale("%s XOR %s", left, right));
+        assertThat(expr.evaluate(EMPTY_CONTEXT) == TRUE).isEqualTo(expected);
+    }
+
+    @Test
+    void property() throws ParseException {
+        var expr = ExpressionParser.parse("n.foo");
+        var context = TestEvaluationContext.of("foo", 42, true);
+        assertThat(expr.evaluate(context) == 42).isTrue();
+    }
+
     private static final EvaluationContext EMPTY_CONTEXT = new EvaluationContext() {
         @Override
         double getProperty(String propertyKey) {
@@ -123,4 +195,34 @@ class ExpressionEvaluatorTest {
             throw new UnsupportedOperationException();
         }
     };
+
+    static class TestEvaluationContext extends EvaluationContext {
+
+        private final String propertyKey;
+        private final double value;
+
+        private final boolean hasLabelOrRelType;
+
+        TestEvaluationContext(String propertyKey, double value, boolean hasLabelOrRelType) {
+            this.propertyKey = propertyKey;
+            this.value = value;
+            this.hasLabelOrRelType = hasLabelOrRelType;
+        }
+
+        static EvaluationContext of(String propertyKey, double value, boolean hasLabelOrRelType) {
+            return new TestEvaluationContext(propertyKey, value, hasLabelOrRelType);
+        }
+
+        @Override
+        double getProperty(String propertyKey) {
+            assertThat(propertyKey).isEqualTo(this.propertyKey);
+            return value;
+        }
+
+        @Override
+        boolean hasLabelsOrTypes(List<String> labelsOrTypes) {
+            return hasLabelOrRelType;
+        }
+    }
+
 }
