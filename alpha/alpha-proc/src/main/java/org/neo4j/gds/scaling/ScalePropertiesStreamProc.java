a35621538c28ceb3ec4a90bb1fd0aa4c7fe17392
Florentin Dörre <florentin.dorre@neotechnology.com>
2021-03-22 12:55:08 +0100

    Add ScalePropertiesStreamProc
    Co-authored-by: Mats Rydberg <mats@neotechnology.com>

---
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/scaling/ScalePropertiesStreamProc.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.scaling;
+
+import org.neo4j.graphalgo.AlgorithmFactory;
+import org.neo4j.graphalgo.AlphaAlgorithmFactory;
+import org.neo4j.graphalgo.StreamProc;
+import org.neo4j.graphalgo.api.NodeProperties;
+import org.neo4j.graphalgo.api.nodeproperties.DoubleArrayNodeProperties;
+import org.neo4j.graphalgo.config.GraphCreateConfig;
+import org.neo4j.graphalgo.core.CypherMapWrapper;
+import org.neo4j.graphalgo.core.concurrency.Pools;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ScalePropertiesStreamProc extends StreamProc<ScaleProperties, ScaleProperties.Result, ScalePropertiesStreamProc.Result, ScalePropertiesStreamConfig> {
+
+    @Procedure("gds.alpha.scaleProperties.stream")
+    @Description("Scale node properties")
+    public Stream<Result> stream(
+        @Name(value = "graphName") Object graphNameOrConfig,
+        @Name(value = "configuration", defaultValue = "{}") Map<String, Object> configuration
+    ) {
+        return stream(compute(graphNameOrConfig, configuration));
+    }
+
+    @Override
+    protected NodeProperties nodeProperties(ComputationResult<ScaleProperties, ScaleProperties.Result, ScalePropertiesStreamConfig> computationResult) {
+        return (DoubleArrayNodeProperties) computationResult.result().scaledProperties()::get;
+    }
+
+    @Override
+    protected ScalePropertiesStreamConfig newConfig(
+        String username,
+        Optional<String> graphName,
+        Optional<GraphCreateConfig> maybeImplicitCreate,
+        CypherMapWrapper config
+    ) {
+        return ScalePropertiesStreamConfig.of(username, graphName, maybeImplicitCreate, config);
+    }
+
+    @Override
+    protected AlgorithmFactory<ScaleProperties, ScalePropertiesStreamConfig> algorithmFactory() {
+        return (AlphaAlgorithmFactory<ScaleProperties, ScalePropertiesStreamConfig>) (graph, configuration, tracker, log, eventTracker) -> new ScaleProperties(
+            graph,
+            configuration,
+            tracker,
+            Pools.DEFAULT
+        );
+    }
+
+    @Override
+    protected Result streamResult(long originalNodeId, long internalNodeId, NodeProperties nodeProperties) {
+        return new Result(originalNodeId, nodeProperties.doubleArrayValue(internalNodeId));
+    }
+
+    public static class Result {
+        public final long nodeId;
+        public final List<Double> scaledProperty;
+
+        public Result(long nodeId, double[] scaledProperty) {
+            this.nodeId = nodeId;
+            this.scaledProperty = Arrays.stream(scaledProperty).boxed().collect(Collectors.toList());
+        }
+    }
+}
---
+++ alpha/alpha-proc/src/test/java/org/neo4j/gds/scaling/ScalePropertiesStreamProcTest.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.scaling;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.BaseProcTest;
+import org.neo4j.graphalgo.GdsCypher;
+import org.neo4j.graphalgo.catalog.GraphCreateProc;
+
+import java.util.List;
+import java.util.Map;
+
+class ScalePropertiesStreamProcTest extends BaseProcTest {
+
+    private static final String DB_CYPHER =
+        "CREATE" +
+        " (n0:A {id: 0})" +
+        ",(n1:A {id: 1})" +
+        ",(n2:A {id: 2})" +
+        ",(n3:A {id: 3})" +
+        ",(n4:A {id: 4})" +
+        ",(n5:A {id: 5})";
+    private static final String GRAPH_NAME = "graph";
+
+    @BeforeEach
+    void setup() throws Exception {
+        runQuery(DB_CYPHER);
+
+        registerProcedures(GraphCreateProc.class, ScalePropertiesStreamProc.class);
+        var loadQuery = GdsCypher
+            .call()
+            .withAnyRelationshipType()
+            .withNodeLabel("A")
+            .withNodeProperty("id")
+            .graphCreate(GRAPH_NAME)
+            .yields();
+        runQuery(loadQuery);
+    }
+
+    @Test
+    void stream() {
+        var query = GdsCypher
+            .call()
+            .explicitCreation(GRAPH_NAME)
+            .algo("gds.alpha.scaleProperties")
+            .streamMode()
+            .addParameter("nodeProperties", List.of("id"))
+            .addParameter("scalers", List.of("Mean"))
+            .yields();
+
+        assertCypherResult(query, List.of(
+            Map.of("nodeId", 0L, "scaledProperty", List.of(-1 / 2D)),
+            Map.of("nodeId", 1L, "scaledProperty", List.of(-3 / 10D)),
+            Map.of("nodeId", 2L, "scaledProperty", List.of(-1 / 10D)),
+            Map.of("nodeId", 3L, "scaledProperty", List.of(1 / 10D)),
+            Map.of("nodeId", 4L, "scaledProperty", List.of(3 / 10D)),
+            Map.of("nodeId", 5L, "scaledProperty", List.of(1 / 2D))
+        ));
+    }
+
+}
