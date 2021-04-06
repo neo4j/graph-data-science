ff2e47d76b0d0141a055d38ced40ae936afb1845
Max Kießling <max.kiessling@neotechnology.com>
2021-04-06 15:05:54 +0200

    Improve RandomWalk performance by using a better partition scheme and limiting the number of neibough draws
    Co-authored-by: Lasse Westh-Nielsen <lassewesth@gmail.com>

--- alpha/alpha-embeddings/src/main/java/org/neo4j/gds/embeddings/node2vec/RandomWalk.java
+++ alpha/alpha-embeddings/src/main/java/org/neo4j/gds/embeddings/node2vec/RandomWalk.java
@@ -26,7 +26,6 @@ import org.neo4j.graphalgo.api.Graph;
 import org.neo4j.graphalgo.core.concurrency.ParallelUtil;
 import org.neo4j.graphalgo.core.concurrency.Pools;
 import org.neo4j.graphalgo.core.utils.BitUtil;
-import org.neo4j.graphalgo.core.utils.partition.Partition;
 import org.neo4j.graphalgo.core.utils.partition.PartitionUtils;
 import org.neo4j.graphalgo.core.utils.queue.QueueBasedSpliterator;
 
@@ -34,10 +33,13 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 public class RandomWalk extends Algorithm<RandomWalk, Stream<long[]>> {
+    // The number of tries we will make to draw a random neighbour according to p and q
+    private static final int MAX_TRIES = 100;
 
     private final Graph graph;
     private final int steps;
@@ -46,6 +48,7 @@ public class RandomWalk extends Algorithm<RandomWalk, Stream<long[]>> {
     private final int queueSize;
     private final double returnParam;
     private final double inOutParam;
+    private final AtomicLong nodeIndex;
 
     public RandomWalk(
         Graph graph,
@@ -63,6 +66,7 @@ public class RandomWalk extends Algorithm<RandomWalk, Stream<long[]>> {
         this.queueSize = queueSize;
         this.returnParam = returnParam;
         this.inOutParam = inOutParam;
+        nodeIndex = new AtomicLong(0);
     }
 
     @Override
@@ -75,8 +79,8 @@ public class RandomWalk extends Algorithm<RandomWalk, Stream<long[]>> {
             concurrency,
             graph.nodeCount(),
             BitUtil.ceilDiv(graph.nodeCount(), concurrency),
-            (partition) -> new RandomWalkTask(
-                partition,
+            (partition) -> RandomWalkTask.of(
+                nodeIndex::getAndIncrement,
                 graph.concurrentCopy(),
                 walksPerNode,
                 steps,
@@ -105,22 +109,23 @@ public class RandomWalk extends Algorithm<RandomWalk, Stream<long[]>> {
     @Override
     public void release() { }
 
-    private static class RandomWalkTask implements Runnable {
-        private final Partition partition;
+    private static final class RandomWalkTask implements Runnable {
         private final Graph graph;
         private final int numWalks;
         private final int walkLength;
-        private final double returnParam;
-        private final double inOutParam;
         private final Random random;
         private final BlockingQueue<long[]> walks;
-        private MutableInt index;
-        private MutableLong randomNeighbour;
+        private final NextNodeSupplier nextNodeSupplier;
+        private final MutableInt index;
+        private final MutableLong randomNeighbour;
         private final long[][] buffer;
-        private MutableInt bufferPosition;
+        private final MutableInt bufferPosition;
+        private final double normalizedReturnProbability;
+        private final double normalizedSameDistanceProbability;
+        private final double normalizedInOutProbability;
 
-        public RandomWalkTask(
-            Partition partition,
+        static RandomWalkTask of(
+            NextNodeSupplier nextNodeSupplier,
             Graph graph,
             int numWalks,
             int walkLength,
@@ -128,13 +133,41 @@ public class RandomWalk extends Algorithm<RandomWalk, Stream<long[]>> {
             double inOutParam,
             BlockingQueue<long[]> walks
         ) {
-            this.partition = partition;
+            var maxProbability = Math.max(Math.max(1 / returnParam, 1.0), 1 / inOutParam);
+            var normalizedReturnProbability = (1 / returnParam) / maxProbability;
+            var normalizedSameDistanceProbability = 1 / maxProbability;
+            var normalizedInOutProbability = (1 / inOutParam) / maxProbability;
+
+            return new RandomWalkTask(
+                nextNodeSupplier,
+                graph,
+                numWalks,
+                walkLength,
+                walks,
+                normalizedReturnProbability,
+                normalizedSameDistanceProbability,
+                normalizedInOutProbability
+            );
+        }
+
+        private RandomWalkTask(
+            NextNodeSupplier nextNodeSupplier,
+            Graph graph,
+            int numWalks,
+            int walkLength,
+            BlockingQueue<long[]> walks,
+            double normalizedReturnProbability,
+            double normalizedSameDistanceProbability,
+            double normalizedInOutProbability
+        ) {
+            this.nextNodeSupplier = nextNodeSupplier;
             this.graph = graph;
             this.numWalks = numWalks;
             this.walkLength = walkLength;
-            this.returnParam = returnParam;
-            this.inOutParam = inOutParam;
             this.walks = walks;
+            this.normalizedReturnProbability = normalizedReturnProbability;
+            this.normalizedSameDistanceProbability = normalizedSameDistanceProbability;
+            this.normalizedInOutProbability = normalizedInOutProbability;
 
             this.random = new Random();
             this.index = new MutableInt(0);
@@ -145,63 +178,80 @@ public class RandomWalk extends Algorithm<RandomWalk, Stream<long[]>> {
 
         @Override
         public void run() {
-            var maxProbability = Math.max(Math.max(1 / returnParam, 1.0), 1 / inOutParam);
+            long nodeId;
 
-            var normalizedReturnProbability = (1 / returnParam) / maxProbability;
-            var normalizedSameDistanceProbability = 1 / maxProbability;
-            var normalizedInOutProbability = (1 / inOutParam) / maxProbability;
+            while(true) {
+                nodeId = nextNodeSupplier.nextNode();
 
-            partition.consume(nodeId -> {
-                if (graph.degree(nodeId) <= 0) {
-                    return;
+                if (nodeId >= graph.nodeCount()) break;
+
+                if (graph.degree(nodeId) == 0) {
+                    continue;
                 }
 
                 for (int walkIndex = 0; walkIndex < numWalks; walkIndex++) {
-                    var walk = new long[walkLength];
-                    Arrays.fill(walk, -1L);
-                    walk[0] = nodeId;
-                    walk[1] = randomNeighbour(nodeId);
+                    var nextWalk = walk(nodeId);
 
-                    for (int i = 2; i < walkLength; i++) {
-                        var currentNode = walk[i - 1];
-                        var currentNodeDegree = graph.degree(currentNode);
-
-                        if (currentNodeDegree == 0) {
-                            // We have arrived at a node with no outgoing neighbors, we can stop walking
-                            break;
-                        } else if (currentNodeDegree == 1) {
-                            // This node only has one neighbour, no need to test
-                            walk[i] = randomNeighbour(currentNode);
-                        } else {
-                            long newNode;
-                            while (true) {
-                                newNode = randomNeighbour(currentNode);
-                                var r = random.nextDouble();
-                                if (newNode == walk[i -2]) {
-                                    if (r < normalizedReturnProbability) {
-                                        break;
-                                    }
-                                } else if (isNeighbour(walk[i -2], newNode)) {
-                                    if (r < normalizedSameDistanceProbability) {
-                                        break;
-                                    }
-                                } else if ( r < normalizedInOutProbability) {
-                                    break;
-                                }
-                            }
-                            walk[i] = newNode;
-                        }
-                    }
-
-                    buffer[bufferPosition.getAndIncrement()] = walk;
+                    buffer[bufferPosition.getAndIncrement()] = nextWalk;
                     if (bufferPosition.getValue() == buffer.length) {
                         flushBuffer();
                     }
                 }
-            });
+            }
 
             flushBuffer();
             System.out.println("Thread finished at = " + System.currentTimeMillis());
+        }
+
+        private long[] walk(long startNode) {
+            var walk = new long[walkLength];
+            Arrays.fill(walk, -1L);
+            walk[0] = startNode;
+            walk[1] = randomNeighbour(startNode);
+
+            for (int i = 2; i < walkLength; i++) {
+                var nextNode = walkOneStep(walk[i - 2], walk[i - 1]);
+                if (nextNode == -1) {
+                    break;
+                } else {
+                    walk[i] = nextNode;
+                }
+            }
+            return walk;
+        }
+
+        private long walkOneStep(long previousNode, long currentNode) {
+            var currentNodeDegree = graph.degree(currentNode);
+
+            if (currentNodeDegree == 0) {
+                // We have arrived at a node with no outgoing neighbors, we can stop walking
+                return -1;
+            } else if (currentNodeDegree == 1) {
+                // This node only has one neighbour, no need to test
+                return randomNeighbour(currentNode);
+            } else {
+                var tries = 0;
+                while (tries < MAX_TRIES) {
+                    var newNode = randomNeighbour(currentNode);
+                    var r = random.nextDouble();
+
+                    if (newNode == previousNode) {
+                        if (r < normalizedReturnProbability) {
+                            return newNode;
+                        }
+                    } else if (isNeighbour(previousNode, newNode)) {
+                        if (r < normalizedSameDistanceProbability) {
+                            return newNode;
+                        }
+                    } else if (r < normalizedInOutProbability) {
+                        return newNode;
+                    }
+                    tries++;
+                }
+
+                // We did not find a valid neighbour in `MAX_TRIES` tries, so we just pick a random one.
+                return randomNeighbour(currentNode);
+            }
         }
 
         private long randomNeighbour(long node) {
@@ -238,5 +288,10 @@ public class RandomWalk extends Algorithm<RandomWalk, Stream<long[]>> {
             }
             bufferPosition.setValue(0);
         }
+    }
+
+    @FunctionalInterface
+    interface NextNodeSupplier {
+        long nextNode();
     }
 }
