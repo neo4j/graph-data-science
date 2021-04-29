0d8bfed5c620467b1b2caaefb35f7a26e706856f
Paul Horn <developer@knutwalker.de>
2021-04-29 19:19:29 +0200

    Implement multiple graphNames for drop

--- proc/catalog/src/main/java/org/neo4j/graphalgo/catalog/GraphDropProc.java
+++ proc/catalog/src/main/java/org/neo4j/graphalgo/catalog/GraphDropProc.java
@@ -19,14 +19,18 @@
  */
 package org.neo4j.graphalgo.catalog;
 
+import org.jetbrains.annotations.NotNull;
 import org.neo4j.graphalgo.core.loading.GraphStoreCatalog;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 import java.util.stream.Stream;
 
+import static org.neo4j.graphalgo.utils.StringFormatting.formatWithLocale;
 import static org.neo4j.procedure.Mode.READ;
 
 public class GraphDropProc extends CatalogProc {
@@ -36,21 +40,62 @@ public class GraphDropProc extends CatalogProc {
     @Procedure(name = "gds.graph.drop", mode = READ)
     @Description(DESCRIPTION)
     public Stream<GraphInfo> drop(
-        @Name(value = "graphName") String graphName,
+        @Name(value = "graphName") Object graphName,
         @Name(value = "failIfMissing", defaultValue = "true") boolean failIfMissing,
         @Name(value = "dbName", defaultValue = "") String dbName
     ) {
-        validateGraphName(graphName);
-
-        if (dbName.isEmpty()) {
-            dbName = databaseId().name();
+        final List<String> graphNames;
+        if (graphName instanceof Collection<?>) {
+            Collection<?> names = (Collection<?>) graphName;
+            graphNames = new ArrayList<>(names.size());
+            int index = 0;
+            for (Object name : names) {
+                if (name instanceof String || name == null) {
+                    graphNames.add(validateGraphName((String) name));
+                } else {
+                    throw typeMismatch(name, index);
+                }
+                index++;
+            }
+        } else if (graphName instanceof String || graphName == null) {
+            graphNames = List.of(validateGraphName((String) graphName));
+        } else {
+            throw typeMismatch(graphName, -1);
         }
 
-        AtomicReference<GraphInfo> result = new AtomicReference<>();
-        GraphStoreCatalog.remove(username(), dbName, graphName, (graphStoreWithConfig) ->
-            result.set(GraphInfo.withoutMemoryUsage(graphStoreWithConfig.config(), graphStoreWithConfig.graphStore())), failIfMissing
-        );
+        var databaseName = dbName.isEmpty() ? databaseId().name() : dbName;
+        var username = username();
 
-        return Stream.ofNullable(result.get());
+        if (failIfMissing) {
+            for (String name : graphNames) {
+                GraphStoreCatalog.get(username, databaseName, name);
+            }
+        }
+
+        var result = Stream.<GraphInfo>builder();
+        for (String name : graphNames) {
+            GraphStoreCatalog.remove(
+                username(),
+                databaseName,
+                name,
+                graphStoreWithConfig -> result.add(
+                    GraphInfo.withoutMemoryUsage(
+                        graphStoreWithConfig.config(),
+                        graphStoreWithConfig.graphStore()
+                    )
+                ),
+                failIfMissing
+            );
+        }
+
+        return result.build();
+    }
+
+    private IllegalArgumentException typeMismatch(@NotNull Object invalid, int index) {
+        return new IllegalArgumentException(formatWithLocale(
+            "Type mismatch%s: expected String but was %s.",
+            index >= 0 ? (" at index " + index) : "",
+            invalid.getClass().getSimpleName()
+        ));
     }
 }
