a50bbb01374354aee950f5f31ca00662f875a0d6
Florentin Dörre <florentin.dorre@neotechnology.com>
2021-05-07 16:55:36 +0200

    Implement weighted uniform sampler
    Based on A-Res Algorithm
    
    Co-authored-by: Veselin Nikolov <veselin.nikolov@neotechnology.com>

---
+++ ml-core/src/main/java/org/neo4j/gds/ml/core/batch/WeightedUniformReservoirRSampler.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.core.batch;
+
+import org.neo4j.graphalgo.api.RelationshipCursor;
+import org.neo4j.graphalgo.core.utils.queue.BoundedLongPriorityQueue;
+
+import java.util.Random;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+/*
+ * Weighted Reservoir Sampling based on Algorithm A-Res
+ */
+public class WeightedUniformReservoirRSampler {
+
+    private final Random random;
+
+    public WeightedUniformReservoirRSampler(long randomSeed) {
+        this.random = new Random(randomSeed);
+    }
+
+    public LongStream sample(Stream<RelationshipCursor> input, long inputSize, int numberOfSamples) {
+        if (numberOfSamples == 0) {
+            return LongStream.empty();
+        }
+
+        if (numberOfSamples >= inputSize) {
+            return input.mapToLong(RelationshipCursor::targetId);
+        }
+
+        var reservoir = BoundedLongPriorityQueue.max(numberOfSamples);
+        var inputIterator = input.iterator();
+
+        for (int i = 0; i < numberOfSamples; i++) {
+            var rel = inputIterator.next();
+            var priority = Math.pow(random.nextDouble(), 1 / rel.property());
+            reservoir.offer(rel.targetId(), priority);
+        }
+
+        double x = Math.log(random.nextDouble()) / Math.log(reservoir.elementAt(0));
+
+        while (inputIterator.hasNext()) {
+            var rel = inputIterator.next();
+            var priority = Math.pow(random.nextDouble(), 1 / rel.property());
+            reservoir.offer(rel.targetId(), priority);
+        }
+
+        return reservoir.elements();
+    }
+}
---
+++ ml-core/src/test/java/org/neo4j/gds/ml/core/batch/WeightedUniformRSamplerTest.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.core.batch;
+
+import com.carrotsearch.hppc.LongLongHashMap;
+import com.carrotsearch.hppc.cursors.LongLongCursor;
+import org.assertj.core.data.Offset;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.neo4j.graphalgo.api.ImmutableRelationshipCursor;
+
+import java.util.Set;
+import java.util.function.Consumer;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WeightedUniformRSamplerTest {
+
+    @Test
+    void shouldSampleSubsetOfNeighbors() {
+        var highWeightNodes = Set.of(33L, 66L, 99L);
+        int numberOfSamples = highWeightNodes.size();
+
+        var sampled = new LongLongHashMap();
+        var sampler = new WeightedUniformReservoirRSampler(19L);
+
+        var input = LongStream.range(1, 100).mapToObj(targetId -> {
+            var weight = highWeightNodes.contains(targetId) ? 99D : 1D;
+            return ImmutableRelationshipCursor.of(0, targetId, weight);
+        }).collect(Collectors.toList());
+
+        var tries = 1000;
+        for (int i = 0; i < tries; i++) {
+            sampler.sample(input.stream(), input.size(), numberOfSamples)
+                .forEach(nodeId -> sampled.addTo(nodeId, 1));
+        }
+
+        assertThat(sampled.keys()).hasSize(input.size());
+
+        highWeightNodes
+            .forEach(highNode -> {
+                var highSample = sampled.remove(highNode);
+                assertThat(highSample)
+                    .withFailMessage("Unexpected sample %d for high weight node %d", highSample, highNode)
+                    .isCloseTo(680L, Offset.offset(20L));
+        });
+
+        sampled.forEach((Consumer<LongLongCursor>) lowNodeSamples ->
+            assertThat(lowNodeSamples.value)
+                .withFailMessage("Unexpected sample %d for low weight node %d", lowNodeSamples.value, lowNodeSamples.key)
+                .isCloseTo(10L, Offset.offset(10L))
+        );
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 4, 17, 99})
+    void shouldSampleTheCorrectNumber(int numberOfSamples) {
+        var input = LongStream.range(0, 18).mapToObj(targetId -> {
+            var weight = 1D;
+            return ImmutableRelationshipCursor.of(0, targetId, weight);
+        }).collect(Collectors.toList());
+
+        var sampler = new WeightedUniformReservoirRSampler(19L);
+        var sample = sampler.sample(input.stream(), 18, numberOfSamples);
+
+        var expectedSize = Math.min(18, numberOfSamples);
+
+        assertThat(sample)
+            .hasSize(expectedSize)
+            .doesNotHaveDuplicates();
+    }
+
+    @Test
+    void duplicateElements() {
+        var input = LongStream.of(1, 1, 1).mapToObj(targetId -> {
+            var weight = 1D;
+            return ImmutableRelationshipCursor.of(0, targetId, weight);
+        }).collect(Collectors.toList());
+
+
+        var sampler = new WeightedUniformReservoirRSampler(19L);
+        var sample = sampler.sample(input.stream(), 3,2).toArray();
+
+        assertThat(sample).containsExactly(1, 1);
+    }
+}
