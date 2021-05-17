0ff2e68c1a476e5041d0e69170abc356ac56a997
Martin Junghanns <martin.junghanns@neo4j.com>
2021-05-17 18:12:44 +0200

    Add community size filter

--- proc/community/src/main/java/org/neo4j/graphalgo/CommunityProcCompanion.java
+++ proc/community/src/main/java/org/neo4j/graphalgo/CommunityProcCompanion.java
@@ -24,11 +24,19 @@ import org.neo4j.graphalgo.api.nodeproperties.ConsecutiveLongNodeProperties;
 import org.neo4j.graphalgo.api.nodeproperties.LongIfChangedNodeProperties;
 import org.neo4j.graphalgo.api.nodeproperties.LongNodeProperties;
 import org.neo4j.graphalgo.config.AlgoBaseConfig;
+import org.neo4j.graphalgo.config.CommunitySizeConfig;
 import org.neo4j.graphalgo.config.ConsecutiveIdsConfig;
 import org.neo4j.graphalgo.config.SeedConfig;
+import org.neo4j.graphalgo.core.concurrency.Pools;
 import org.neo4j.graphalgo.core.utils.mem.AllocationTracker;
+import org.neo4j.graphalgo.core.utils.paged.HugeSparseLongArray;
+import org.neo4j.graphalgo.core.utils.statistics.CommunityStatistics;
+import org.neo4j.values.storable.LongValue;
+import org.neo4j.values.storable.Value;
 
-public class CommunityProcCompanion {
+public final class CommunityProcCompanion {
+
+    private CommunityProcCompanion() {}
 
     public static <ALGO extends Algorithm<ALGO, RESULT>, RESULT, CONFIG extends AlgoBaseConfig & SeedConfig & ConsecutiveIdsConfig> NodeProperties nodeProperties(
         AlgoBaseProc.ComputationResult<ALGO, RESULT, CONFIG> computationResult,
@@ -44,16 +52,81 @@ public class CommunityProcCompanion {
         var seedProperty = config.seedProperty();
         var resultPropertyEqualsSeedProperty = isIncremental && resultProperty.equals(seedProperty);
 
+        LongNodeProperties result;
+
         if (resultPropertyEqualsSeedProperty && !consecutiveIds) {
-            return LongIfChangedNodeProperties.of(graphStore, seedProperty, nodeProperties);
+            result = LongIfChangedNodeProperties.of(graphStore, seedProperty, nodeProperties);
         } else if (consecutiveIds && !isIncremental) {
-            return new ConsecutiveLongNodeProperties(
+            result = new ConsecutiveLongNodeProperties(
                 nodeProperties,
                 computationResult.graph().nodeCount(),
                 tracker
             );
         } else {
-            return nodeProperties;
+            result = nodeProperties;
+        }
+
+        if (config instanceof CommunitySizeConfig) {
+            var csc = (CommunitySizeConfig) config;
+            if (csc.hasMinCommunitySize()) {
+                var communitySizes = CommunityStatistics.communitySizes(
+                    result.size(),
+                    result::longValue,
+                    Pools.DEFAULT,
+                    config.concurrency(),
+                    tracker
+                );
+                result = new CommunitySizeFilter(result, communitySizes, csc.minCommunitySize());
+            }
+        }
+
+        return result;
+    }
+
+    private static class CommunitySizeFilter implements LongNodeProperties {
+
+        private final LongNodeProperties properties;
+
+        private final HugeSparseLongArray communitySizes;
+
+        private final long minCommunitySize;
+
+        CommunitySizeFilter(LongNodeProperties properties, HugeSparseLongArray communitySizes, long minCommunitySize) {
+            this.properties = properties;
+            this.communitySizes = communitySizes;
+            this.minCommunitySize = minCommunitySize;
+        }
+
+        @Override
+        public long size() {
+            return properties.size();
+        }
+
+        @Override
+        public long longValue(long nodeId) {
+            return properties.longValue(nodeId);
+        }
+
+        /**
+         * Returning null indicates that the value is not written to Neo4j.
+         *
+         * The filter is applied in the latest stage before writing to Neo4j.
+         * Since the wrapped node properties may have additional logic in value(),
+         * we need to check if they already filtered the value. Only in the case
+         * where the wrapped properties pass on the value, we can apply a filter.
+         */
+        @Override
+        public Value value(long nodeId) {
+            var value = properties.value(nodeId);
+
+            if (value == null) {
+                return null;
+            }
+
+            // This cast is safe since we handle LongNodeProperties.
+            var communityId = ((LongValue) value).longValue();
+
+            return communitySizes.get(communityId) >= minCommunitySize ? value : null;
         }
     }
 }
