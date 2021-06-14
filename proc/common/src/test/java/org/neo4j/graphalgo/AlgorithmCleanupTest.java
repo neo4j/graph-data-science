2f3a0b3a0adb29aa4d952fff4e28b76ba6d5dbeb
Paul Horn <developer@knutwalker.de>
2021-06-14 14:46:41 +0200

    Move import to graphalgo

--- proc/common/src/test/java/org/neo4j/graphalgo/AlgorithmCleanupTest.java
+++ proc/common/src/test/java/org/neo4j/graphalgo/AlgorithmCleanupTest.java
@@ -23,7 +23,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.neo4j.gds.TestProgressEventTracker;
-import org.neo4j.gds.catalog.GraphCreateProc;
+import org.neo4j.graphalgo.catalog.GraphCreateProc;
 import org.neo4j.graphalgo.core.loading.GraphStoreCatalog;
 import org.neo4j.graphalgo.test.TestProc;
 import org.neo4j.logging.NullLog;
