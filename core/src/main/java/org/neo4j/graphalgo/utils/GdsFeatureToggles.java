3ab9e66c491dbc5bf82a7fa71b71481a25f16775
Martin Junghanns <martin.junghanns@neo4j.com>
2021-07-01 15:53:51 +0200

    Enable reordering by default
    Co-authored-by: Paul Horn <paul.horn@neotechnology.com>

--- core/src/main/java/org/neo4j/graphalgo/utils/GdsFeatureToggles.java
+++ core/src/main/java/org/neo4j/graphalgo/utils/GdsFeatureToggles.java
@@ -34,7 +34,7 @@ public enum GdsFeatureToggles {
     USE_PARALLEL_PROPERTY_VALUE_INDEX(false),
     USE_BIT_ID_MAP(true),
     USE_UNCOMPRESSED_ADJACENCY_LIST(false),
-    USE_REORDERED_ADJACENCY_LIST(false);
+    USE_REORDERED_ADJACENCY_LIST(true);
 
     public boolean isEnabled() {
         return current.get();
