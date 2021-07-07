a32dcb90eabb12eb9cca2c42f79eee889d34eecf
Martin Junghanns <martin.junghanns@neo4j.com>
2021-07-07 12:46:48 +0200

    Disable uncompressed

--- core/src/main/java/org/neo4j/graphalgo/utils/GdsFeatureToggles.java
+++ core/src/main/java/org/neo4j/graphalgo/utils/GdsFeatureToggles.java
@@ -33,7 +33,7 @@ public enum GdsFeatureToggles {
     USE_PROPERTY_VALUE_INDEX(false),
     USE_PARALLEL_PROPERTY_VALUE_INDEX(false),
     USE_BIT_ID_MAP(true),
-    USE_UNCOMPRESSED_ADJACENCY_LIST(true),
+    USE_UNCOMPRESSED_ADJACENCY_LIST(false),
     USE_REORDERED_ADJACENCY_LIST(true);
 
     public boolean isEnabled() {
