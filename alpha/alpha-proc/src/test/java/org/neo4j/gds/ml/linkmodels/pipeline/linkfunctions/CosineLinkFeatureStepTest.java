90f0d5d9e4aac7678d5f064817c611e77eceeec5
Jacob Sznajdman <breakanalysis@gmail.com>
2021-07-12 16:23:37 +0200

    Move linkmodels/pipeline to alpha-proc

--- alpha/alpha-proc/src/test/java/org/neo4j/gds/ml/linkmodels/pipeline/linkfunctions/CosineLinkFeatureStepTest.java
+++ alpha/alpha-proc/src/test/java/org/neo4j/gds/ml/linkmodels/pipeline/linkfunctions/CosineLinkFeatureStepTest.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.gds.ml.pipeline.linkfunctions;
+package org.neo4j.gds.ml.linkmodels.pipeline.linkfunctions;
 
 import org.junit.jupiter.api.Test;
 import org.neo4j.gds.ml.linkmodels.pipeline.LinkFeatureStepFactory;
@@ -33,10 +33,10 @@ import org.neo4j.graphalgo.extension.Inject;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @GdlExtension
-final class L2LinkFeatureStepTest {
+final class CosineLinkFeatureStepTest {
     @GdlGraph(orientation = Orientation.UNDIRECTED)
     static String GRAPH =
         "(a:N {noise: 42, z: 13, array: [3.0,2.0]}), " +
@@ -55,15 +55,15 @@ final class L2LinkFeatureStepTest {
     Graph graph;
 
     @Test
-    public void runL2LinkFeatureStep() {
+    public void runCosineLinkFeatureStep() {
         var numRelationships = 6;
-        var expectedLinkDimension = 4;
+        var expectedLinkDimension = 1;
         var linkFeatures = HugeObjectArray.newArray(double[].class, numRelationships, AllocationTracker.empty());
 
         linkFeatures.setAll(x -> new double[expectedLinkDimension]);
 
         var step = LinkFeatureStepFactory.create(
-            "L2",
+            "cosine",
             Map.of("featureProperties", List.of("noise", "z", "array"))
         );
 
@@ -71,8 +71,13 @@ final class L2LinkFeatureStepTest {
 
         var delta = 0.0001D;
 
-        assertArrayEquals(new double[]{Math.pow(42 - 1337, 2), Math.pow(13 - 0D, 2), Math.pow(3 - 1D, 2), Math.pow(2 - 1D, 2)}, linkFeatures.get(0), delta);
-        assertArrayEquals(new double[]{Math.pow(42 - 42, 2), Math.pow(13 - 2, 2), Math.pow(3 - 8, 2), Math.pow(2 - 2.3D, 2)}, linkFeatures.get(1), delta);
-        assertArrayEquals(new double[]{Math.pow(42 - 42, 2), Math.pow(13 - 9, 2), Math.pow(3 - 0.1D, 2), Math.pow(2 - 91.0D, 2)}, linkFeatures.get(2), delta);
+        var norm0 = Math.sqrt(42 * 42 + 13 * 13 + 3 * 3 + 2 * 2);
+        var norm1 = Math.sqrt(1337 * 1337 + 0 * 0 + 1 * 1 + 1 * 1);
+        var norm2 = Math.sqrt(42 * 42 + 2 * 2 + 8 * 8 + 2.3 * 2.3);
+        var norm3 = Math.sqrt(42 * 42 + 9 * 9 + 0.1 * 0.1 + 91 * 91);
+
+        assertEquals((42 * 1337 + 13 * 0D + 3 * 1D + 2 * 1D) / norm0 / norm1, linkFeatures.get(0)[0], delta);
+        assertEquals((42 * 42 + 13 * 2 + 3 * 8 + 2 * 2.3D) / norm0 / norm2, linkFeatures.get(1)[0], delta);
+        assertEquals((42 * 42 + 13 * 9 + 3 * 0.1D + 2 * 91.0D) / norm0 / norm3 , linkFeatures.get(2)[0], delta);
     }
 }
--- alpha/alpha-proc/src/test/java/org/neo4j/gds/ml/linkmodels/pipeline/linkfunctions/L2LinkFeatureStepTest.java
+++ alpha/alpha-proc/src/test/java/org/neo4j/gds/ml/linkmodels/pipeline/linkfunctions/L2LinkFeatureStepTest.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.gds.ml.pipeline.linkfunctions;
+package org.neo4j.gds.ml.linkmodels.pipeline.linkfunctions;
 
 import org.junit.jupiter.api.Test;
 import org.neo4j.gds.ml.linkmodels.pipeline.LinkFeatureStepFactory;
@@ -33,10 +33,10 @@ import org.neo4j.graphalgo.extension.Inject;
 import java.util.List;
 import java.util.Map;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 @GdlExtension
-final class CosineLinkFeatureStepTest {
+final class L2LinkFeatureStepTest {
     @GdlGraph(orientation = Orientation.UNDIRECTED)
     static String GRAPH =
         "(a:N {noise: 42, z: 13, array: [3.0,2.0]}), " +
@@ -55,15 +55,15 @@ final class CosineLinkFeatureStepTest {
     Graph graph;
 
     @Test
-    public void runCosineLinkFeatureStep() {
+    public void runL2LinkFeatureStep() {
         var numRelationships = 6;
-        var expectedLinkDimension = 1;
+        var expectedLinkDimension = 4;
         var linkFeatures = HugeObjectArray.newArray(double[].class, numRelationships, AllocationTracker.empty());
 
         linkFeatures.setAll(x -> new double[expectedLinkDimension]);
 
         var step = LinkFeatureStepFactory.create(
-            "cosine",
+            "L2",
             Map.of("featureProperties", List.of("noise", "z", "array"))
         );
 
@@ -71,13 +71,8 @@ final class CosineLinkFeatureStepTest {
 
         var delta = 0.0001D;
 
-        var norm0 = Math.sqrt(42 * 42 + 13 * 13 + 3 * 3 + 2 * 2);
-        var norm1 = Math.sqrt(1337 * 1337 + 0 * 0 + 1 * 1 + 1 * 1);
-        var norm2 = Math.sqrt(42 * 42 + 2 * 2 + 8 * 8 + 2.3 * 2.3);
-        var norm3 = Math.sqrt(42 * 42 + 9 * 9 + 0.1 * 0.1 + 91 * 91);
-
-        assertEquals((42 * 1337 + 13 * 0D + 3 * 1D + 2 * 1D) / norm0 / norm1, linkFeatures.get(0)[0], delta);
-        assertEquals((42 * 42 + 13 * 2 + 3 * 8 + 2 * 2.3D) / norm0 / norm2, linkFeatures.get(1)[0], delta);
-        assertEquals((42 * 42 + 13 * 9 + 3 * 0.1D + 2 * 91.0D) / norm0 / norm3 , linkFeatures.get(2)[0], delta);
+        assertArrayEquals(new double[]{Math.pow(42 - 1337, 2), Math.pow(13 - 0D, 2), Math.pow(3 - 1D, 2), Math.pow(2 - 1D, 2)}, linkFeatures.get(0), delta);
+        assertArrayEquals(new double[]{Math.pow(42 - 42, 2), Math.pow(13 - 2, 2), Math.pow(3 - 8, 2), Math.pow(2 - 2.3D, 2)}, linkFeatures.get(1), delta);
+        assertArrayEquals(new double[]{Math.pow(42 - 42, 2), Math.pow(13 - 9, 2), Math.pow(3 - 0.1D, 2), Math.pow(2 - 91.0D, 2)}, linkFeatures.get(2), delta);
     }
 }
