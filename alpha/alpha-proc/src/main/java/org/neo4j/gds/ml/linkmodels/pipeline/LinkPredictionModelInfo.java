90e71fe3d488234ee18129a6a19496762dccdbea
Florentin Dörre <florentin.dorre@neotechnology.com>
2021-07-28 15:05:01 +0200

    Port LPTrainConfig and LPModelInfo
    For the pipeline version, we do not need the featureProperties and LinkFeatureCombiners in the config as they are part of the pipeline.
    The ModelInfo needs to be ported as it is config dependent.
    
    Co-authored-by: Jacob Sznajdman <breakanalysis@gmail.com>

---
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/linkmodels/pipeline/LinkPredictionModelInfo.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.linkmodels.pipeline;
+
+import org.neo4j.gds.ml.linkmodels.metrics.LinkMetric;
+import org.neo4j.gds.ml.linkmodels.pipeline.logisticRegression.LinkLogisticRegressionTrainConfig;
+import org.neo4j.gds.ml.nodemodels.MetricData;
+import org.neo4j.graphalgo.annotation.ValueClass;
+import org.neo4j.graphalgo.core.model.Model;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@ValueClass
+public interface LinkPredictionModelInfo extends Model.Mappable {
+
+    LinkLogisticRegressionTrainConfig bestParameters();
+
+    Map<LinkMetric, MetricData<LinkLogisticRegressionTrainConfig>> metrics();
+
+    @Override
+    default Map<String, Object> toMap() {
+        return Map.of(
+            "bestParameters", bestParameters().toMap(),
+            "metrics", metrics().entrySet().stream().collect(Collectors.toMap(
+                entry -> entry.getKey().name(),
+                entry -> entry.getValue().toMap()
+            ))
+        );
+    }
+
+    static LinkPredictionModelInfo of(
+        LinkLogisticRegressionTrainConfig bestParameters,
+        Map<LinkMetric, MetricData<LinkLogisticRegressionTrainConfig>> metrics
+    ) {
+        return ImmutableLinkPredictionModelInfo.of(bestParameters, metrics);
+    }
+}
---
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/linkmodels/pipeline/LinkPredictionTrainConfig.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.linkmodels.pipeline;
+
+import org.immutables.value.Value;
+import org.neo4j.gds.ml.linkmodels.metrics.LinkMetric;
+import org.neo4j.gds.ml.linkmodels.pipeline.logisticRegression.LinkLogisticRegressionTrainConfig;
+import org.neo4j.graphalgo.annotation.Configuration;
+import org.neo4j.graphalgo.annotation.ValueClass;
+import org.neo4j.graphalgo.config.AlgoBaseConfig;
+import org.neo4j.graphalgo.config.GraphCreateConfig;
+import org.neo4j.graphalgo.config.ModelConfig;
+import org.neo4j.graphalgo.config.RandomSeedConfig;
+import org.neo4j.graphalgo.core.CypherMapWrapper;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@ValueClass
+@Configuration
+@SuppressWarnings("immutables:subtype")
+public interface LinkPredictionTrainConfig extends AlgoBaseConfig, ModelConfig, RandomSeedConfig {
+
+    @Configuration.IntegerRange(min = 2)
+    int validationFolds();
+
+    @Configuration.DoubleRange(min = 0, minInclusive = false)
+    double negativeClassWeight();
+
+    List<Map<String, Object>> params();
+
+    @Value.Derived
+    @Configuration.Ignore
+    default List<LinkLogisticRegressionTrainConfig> paramConfigs() {
+        return params().stream().map(params -> LinkLogisticRegressionTrainConfig.of(
+            concurrency(),
+            params
+        )).collect(Collectors.toList());
+    }
+
+    @Configuration.DoubleRange(min = 0.0, minInclusive = false)
+    double holdOutFraction();
+
+    @Configuration.DoubleRange(min = 0.0, minInclusive = false)
+    double negativeSamplingRatio();
+
+    @Configuration.Ignore
+    @Value.Default
+    default List<LinkMetric> metrics() {
+        return List.of(LinkMetric.AUCPR);
+    }
+
+    static LinkPredictionTrainConfig of(
+        String username,
+        Optional<String> graphName,
+        Optional<GraphCreateConfig> maybeImplicitCreate,
+        CypherMapWrapper config
+    ) {
+        return new LinkPredictionTrainConfigImpl(
+            graphName,
+            maybeImplicitCreate,
+            username,
+            config
+        );
+    }
+
+    static ImmutableLinkPredictionTrainConfig.Builder builder() {
+        return ImmutableLinkPredictionTrainConfig.builder();
+    }
+}
