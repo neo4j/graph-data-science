3287849de5481bcb33a2c267b56642a523ca4947
Florentin Dörre <florentin.dorre@neotechnology.com>
2021-08-02 17:23:10 +0200

    Start LinkFeaturePipelineCreatePoc
    Co-authored-by: Jacob Sznajdman <breakanalysis@gmail.com>

---
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/linkmodels/pipeline/LinkFeaturePipelineCreateProc.java
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.linkmodels.pipeline;
+
+import org.neo4j.graphalgo.BaseProc;
+import org.neo4j.graphalgo.annotation.Configuration;
+import org.neo4j.graphalgo.annotation.ValueClass;
+import org.neo4j.graphalgo.api.schema.GraphSchema;
+import org.neo4j.graphalgo.config.BaseConfig;
+import org.neo4j.graphalgo.config.ModelConfig;
+import org.neo4j.graphalgo.core.model.Model;
+import org.neo4j.graphalgo.core.model.ModelCatalog;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.neo4j.procedure.Mode.READ;
+
+public class LinkFeaturePipelineCreateProc extends BaseProc {
+    public static final String PIPELINE_MODEL_TYPE = "Link prediction training pipeline";
+
+
+    @Procedure(name = "gds.alpha.ml.pipeline.linkPrediction.create", mode = READ)
+    @Description("Creates a link prediction pipeline in the model catalog.")
+    public Stream<PipelineInfo> database(@Name("pipelineName") String pipelineName) {
+        var model = Model.<Object, PipelineDummyTrainConfig>of(
+            username(),
+            pipelineName,
+            PIPELINE_MODEL_TYPE,
+            GraphSchema.empty(),
+            new Object(),
+            PipelineDummyTrainConfig.of(username()),
+            // TODO actual modelinfo
+            Model.Mappable.EMPTY
+        );
+
+        ModelCatalog.set(model);
+
+        return Stream.of(new PipelineInfo(pipelineName, List.of(), List.of(), Map.of(), List.of()));
+    }
+
+    public static class PipelineInfo {
+        public final String name;
+        public final List<Object> nodePropertySteps;
+        public final List<Object> featureSteps;
+        public final Map<String, Object> splitConfig;
+        public final Object parameterSpace;
+
+        PipelineInfo(String pipelineName, List<Object> nodePropertySteps, List<Object> featureSteps, Map<String, Object> splitConfig, Object parameterSpace
+        ) {
+            this.name = pipelineName;
+            this.nodePropertySteps = nodePropertySteps;
+            this.featureSteps = featureSteps;
+            this.splitConfig = splitConfig;
+            this.parameterSpace = parameterSpace;
+        }
+    }
+
+    @ValueClass
+    @Configuration
+    interface PipelineDummyTrainConfig extends BaseConfig, ModelConfig {
+        static PipelineDummyTrainConfig of(String username) {
+            return ImmutablePipelineDummyTrainConfig.of(username, "");
+        }
+    }
+}
---
+++ alpha/alpha-proc/src/test/java/org/neo4j/gds/ml/linkmodels/pipeline/LinkFeaturePipelineCreateProcTest.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.linkmodels.pipeline;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.BaseProcTest;
+import org.neo4j.graphalgo.core.model.ModelCatalog;
+
+import java.util.List;
+import java.util.Map;
+
+public class LinkFeaturePipelineCreateProcTest extends BaseProcTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+        registerProcedures(LinkFeaturePipelineCreateProc.class);
+    }
+
+    @AfterEach
+    void tearDown() {
+        ModelCatalog.removeAllLoadedModels();
+    }
+
+    @Test
+    void createPipeline() {
+        assertCypherResult(
+            "CALL gds.alpha.ml.pipeline.linkPrediction.create('myPipeline')",
+            List.of(Map.of(
+                "name", "myPipeline",
+                "nodePropertySteps", List.of(),
+                "featureSteps", List.of(),
+                // TODO: actual default splitConfig
+                "splitConfig", Map.of(),
+                "parameterSpace", List.of()
+            ))
+        );
+    }
+
+    @Test
+    void failOnCreatingPipelineWithExistingName() {
+        runQuery("CALL gds.alpha.ml.pipeline.linkPrediction.create('myPipeline')");
+        assertError("CALL gds.alpha.ml.pipeline.linkPrediction.create('myPipeline')", "Model with name `myPipeline` already exists.");
+    }
+
+    @Disabled("White space models are allowed atm.")
+    @Test
+    void failOnCreatingPipelineWithInvalidName() {
+        runQuery("CALL gds.alpha.ml.pipeline.linkPrediction.create('   ')");
+        assertError("CALL gds.alpha.ml.pipeline.linkPrediction.create('myPipeline')", "Model with name must contain at least one non white-space character.");
+    }
+}
