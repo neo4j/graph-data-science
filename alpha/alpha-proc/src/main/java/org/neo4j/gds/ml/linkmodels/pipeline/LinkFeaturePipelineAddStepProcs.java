fd094494dea172c6bb862c4c0ddd85625e62f921
Jacob Sznajdman <breakanalysis@gmail.com>
2021-08-03 14:21:10 +0200

    Add proc for adding nodeProperty steps
    Co-Authored-By: Florentin Dörre <florentin.dorre@neotechnology.com>

---
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/linkmodels/pipeline/LinkFeaturePipelineAddStepProcs.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.linkmodels.pipeline;
+
+import org.neo4j.graphalgo.BaseProc;
+import org.neo4j.graphalgo.core.model.Model;
+import org.neo4j.graphalgo.core.model.ModelCatalog;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.neo4j.gds.ml.linkmodels.pipeline.LinkFeaturePipelineCreateProc.PIPELINE_MODEL_TYPE;
+import static org.neo4j.graphalgo.utils.StringFormatting.formatWithLocale;
+import static org.neo4j.procedure.Mode.READ;
+
+public class LinkFeaturePipelineAddStepProcs extends BaseProc {
+
+    @Procedure(name = "gds.alpha.ml.pipeline.linkPrediction.addNodeProperty", mode = READ)
+    @Description("Add a node property step to an existing link prediction pipeline.")
+    public Stream<PipelineInfoResult> add(
+        @Name("pipelineName") String pipelineName,
+        @Name("procedureName") String taskName,
+        @Name("procedureConfiguration") Map<String, Object> procedureConfig
+    ) {
+        Model<?, ?> model = ModelCatalog.getUntyped(username(), pipelineName, true);
+
+        assert model != null;
+        if (!model.algoType().equals(PIPELINE_MODEL_TYPE)) {
+            throw new IllegalArgumentException(formatWithLocale(
+                "Steps can only be added to a model of type `%s`. But model `%s` is of type `%s`.",
+                PIPELINE_MODEL_TYPE,
+                pipelineName,
+                model.algoType()
+            ));
+        }
+
+        PipelineModelInfo pipeline = (PipelineModelInfo) model.customInfo();
+        pipeline.addNodePropertyStep(new NodePropertyStep(taskName, procedureConfig));
+
+        return Stream.of(new PipelineInfoResult(pipelineName, pipeline));
+    }
+}
---
+++ alpha/alpha-proc/src/test/java/org/neo4j/gds/ml/linkmodels/pipeline/LinkFeaturePipelineAddStepProcsTest.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.linkmodels.pipeline;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.neo4j.graphalgo.BaseProcTest;
+import org.neo4j.graphalgo.api.schema.GraphSchema;
+import org.neo4j.graphalgo.core.model.Model;
+import org.neo4j.graphalgo.core.model.ModelCatalog;
+import org.neo4j.graphalgo.model.catalog.TestTrainConfig;
+
+import java.util.List;
+import java.util.Map;
+
+class LinkFeaturePipelineAddStepProcsTest extends BaseProcTest {
+
+    @BeforeEach
+    void setUp() throws Exception {
+        registerProcedures(LinkFeaturePipelineAddStepProcs.class, LinkFeaturePipelineCreateProc.class);
+
+        runQuery("CALL gds.alpha.ml.pipeline.linkPrediction.create('myPipeline')");
+    }
+
+    @AfterEach
+    void tearDown() {
+        ModelCatalog.removeAllLoadedModels();
+    }
+
+    @Test
+    void shouldAddNodePropertyStep() {
+        assertCypherResult("CALL gds.alpha.ml.pipeline.linkPrediction.addNodeProperty('myPipeline', 'pageRank', {mutateProperty: 'pr'})",
+            List.of(Map.of("name", "myPipeline",
+                "splitConfig", Map.of(),
+                "nodePropertySteps", List.of(
+                    Map.of(
+                        "name", "pageRank",
+                        "config", Map.of("mutateProperty", "pr")
+                    )),
+                "featureSteps", List.of(),
+                "parameterSpace", List.of()
+            ))
+        );
+    }
+
+    @Test
+    void shouldAddTwoNodePropertyStep() {
+        runQuery("CALL gds.alpha.ml.pipeline.linkPrediction.addNodeProperty('myPipeline', 'pageRank', {mutateProperty: 'pr'})");
+        assertCypherResult("CALL gds.alpha.ml.pipeline.linkPrediction.addNodeProperty('myPipeline', 'pageRank', {mutateProperty: 'pr2'})",
+            List.of(Map.of("name", "myPipeline",
+                "splitConfig", Map.of(),
+                "nodePropertySteps", List.of(
+                    Map.of(
+                        "name", "pageRank",
+                        "config", Map.of("mutateProperty", "pr")
+                    ),
+                    Map.of(
+                        "name", "pageRank",
+                        "config", Map.of("mutateProperty", "pr2")
+                    )),
+                "featureSteps", List.of(),
+                "parameterSpace", List.of()
+            ))
+        );
+    }
+
+    @Test
+    void shouldThrowIfPipelineDoesntExist() {
+        assertError(
+            "CALL gds.alpha.ml.pipeline.linkPrediction.addNodeProperty('ceci nest pas une pipe', 'pageRank', {mutateProperty: 'pr'})",
+            "Model with name `ceci nest pas une pipe` does not exist."
+        );
+    }
+
+    @Disabled
+    @Test
+    void shouldThrowInvalidStepName() {
+        assertError(
+            "CALL gds.alpha.ml.pipeline.linkPrediction.addNodeProperty('myPipeline', 'juggleSpoons', {mutateProperty: 'pr'})",
+            "Invalid procedure name `juggleSpoons` for pipelining."
+        );
+    }
+
+    @Test
+    void shouldThrowIfAddingToANonPipeline() {
+        var model1 = Model.of(
+            getUsername(),
+            "testModel1",
+            "testAlgo1",
+            GraphSchema.empty(),
+            "testData",
+            TestTrainConfig.of()
+        );
+
+        ModelCatalog.set(model1);
+        assertError(
+            "CALL gds.alpha.ml.pipeline.linkPrediction.addNodeProperty('testModel1', 'pageRank', {mutateProperty: 'pr'})",
+            "Steps can only be added to a model of type `Link prediction training pipeline`. But model `testModel1` is of type `testAlgo1`."
+        );
+    }
+}
