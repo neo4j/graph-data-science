950f2bfcb7f5bd338692f4e400ca2081e229c6c7
Florentin Dörre <florentin.dorre@neotechnology.com>
2021-08-04 13:50:26 +0200

    Apply renaming of `graph algo` package to imports
    Co-authored-by: Jacob Sznajdman <breakanalysis@gmail.com>

--- alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/linkmodels/pipeline/LinkPredictionSplitConfig.java
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/linkmodels/pipeline/LinkPredictionSplitConfig.java
@@ -20,9 +20,9 @@
 package org.neo4j.gds.ml.linkmodels.pipeline;
 
 
-import org.neo4j.graphalgo.annotation.Configuration;
-import org.neo4j.graphalgo.annotation.ValueClass;
-import org.neo4j.graphalgo.core.CypherMapWrapper;
+import org.neo4j.gds.annotation.Configuration;
+import org.neo4j.gds.annotation.ValueClass;
+import org.neo4j.gds.core.CypherMapWrapper;
 
 @ValueClass
 @Configuration
--- alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/linkmodels/pipeline/PipelineUtils.java
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/linkmodels/pipeline/PipelineUtils.java
@@ -19,11 +19,11 @@
  */
 package org.neo4j.gds.ml.linkmodels.pipeline;
 
-import org.neo4j.graphalgo.core.model.Model;
-import org.neo4j.graphalgo.core.model.ModelCatalog;
+import org.neo4j.gds.core.model.Model;
+import org.neo4j.gds.core.model.ModelCatalog;
 
 import static org.neo4j.gds.ml.linkmodels.pipeline.LinkFeaturePipelineCreateProc.PIPELINE_MODEL_TYPE;
-import static org.neo4j.graphalgo.utils.StringFormatting.formatWithLocale;
+import static org.neo4j.gds.utils.StringFormatting.formatWithLocale;
 
 public final class PipelineUtils {
 
