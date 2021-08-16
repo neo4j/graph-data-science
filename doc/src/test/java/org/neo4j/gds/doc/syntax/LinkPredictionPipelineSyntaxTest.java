0c83c0a0e0fc38e226c05b06ad5e97ce2bba57a5
Florentin Dörre <florentin.dorre@neotechnology.com>
2021-08-16 13:22:33 +0200

    Add syntax section for pipeline.predict.mutate

--- doc/src/test/java/org/neo4j/gds/doc/syntax/LinkPredictionPipelineSyntaxTest.java
+++ doc/src/test/java/org/neo4j/gds/doc/syntax/LinkPredictionPipelineSyntaxTest.java
@@ -30,7 +30,8 @@ class LinkPredictionPipelineSyntaxTest extends SyntaxTestBase {
             SyntaxModeMeta.of(SyntaxMode.PIPELINE_CONFIGURE_SPLIT),
             SyntaxModeMeta.of(SyntaxMode.PIPELINE_CONFIGURE_PARAMS),
             SyntaxModeMeta.of(SyntaxMode.PIPELINE_ADD_FEATURE),
-            SyntaxModeMeta.of(SyntaxMode.TRAIN)
+            SyntaxModeMeta.of(SyntaxMode.TRAIN),
+            SyntaxModeMeta.of(SyntaxMode.MUTATE)
         );
     }
 
