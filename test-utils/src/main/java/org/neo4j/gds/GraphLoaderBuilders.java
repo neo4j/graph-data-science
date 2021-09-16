8e6d7045cf076e9c7f2b89e241b7606df59e3377
Florentin Dörre <florentin.dorre@neotechnology.com>
2021-09-16 14:05:59 +0200

    Pass empty task registry factory in tests

--- test-utils/src/main/java/org/neo4j/gds/GraphLoaderBuilders.java
+++ test-utils/src/main/java/org/neo4j/gds/GraphLoaderBuilders.java
@@ -33,6 +33,7 @@ import org.neo4j.gds.core.TransactionContext;
 import org.neo4j.gds.core.concurrency.Pools;
 import org.neo4j.gds.core.utils.TerminationFlag;
 import org.neo4j.gds.core.utils.mem.AllocationTracker;
+import org.neo4j.gds.core.utils.progress.EmptyTaskRegistryFactory;
 import org.neo4j.kernel.internal.GraphDatabaseAPI;
 import org.neo4j.logging.Log;
 import org.neo4j.logging.NullLog;
@@ -193,6 +194,7 @@ public final class GraphLoaderBuilders {
                 .executor(executorService.orElse(Pools.DEFAULT))
                 .allocationTracker(allocationTracker.orElse(AllocationTracker.empty()))
                 .terminationFlag(terminationFlag.orElse(TerminationFlag.RUNNING_TRUE))
+                .taskRegistryFactory(EmptyTaskRegistryFactory.INSTANCE)
                 .log(log.orElse(NullLog.getInstance()))
                 .build())
             .username(userName
