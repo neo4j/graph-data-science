84cb87b988c909b237982dc93d95eb0cc4f293b7
Paul Horn <paul.horn@neotechnology.com>
2021-09-23 13:46:05 +0200

    Add new compat layer for Cypher Storage Engine for 4.3.3

---
+++ cypher/4.3.4+/storage-engine-adapter/src/main/java/org/neo4j/gds/compat/_434/InMemoryStorageEngineFactory434.java
@@ -0,0 +1,129 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.compat._434;
+
+import org.neo4j.annotations.service.ServiceProvider;
+import org.neo4j.configuration.Config;
+import org.neo4j.configuration.helpers.DatabaseReadOnlyChecker;
+import org.neo4j.gds.storageengine.InMemoryTransactionStateVisitor;
+import org.neo4j.index.internal.gbptree.RecoveryCleanupWorkCollector;
+import org.neo4j.internal.id.IdController;
+import org.neo4j.internal.id.IdGeneratorFactory;
+import org.neo4j.internal.recordstorage.AbstractInMemoryMetaDataProvider;
+import org.neo4j.internal.recordstorage.AbstractInMemoryStorageEngineFactory;
+import org.neo4j.internal.recordstorage.InMemoryStorageReader434;
+import org.neo4j.internal.schema.IndexConfigCompleter;
+import org.neo4j.internal.schema.SchemaState;
+import org.neo4j.io.fs.FileSystemAbstraction;
+import org.neo4j.io.layout.DatabaseLayout;
+import org.neo4j.io.pagecache.PageCache;
+import org.neo4j.io.pagecache.context.CursorContext;
+import org.neo4j.io.pagecache.tracing.PageCacheTracer;
+import org.neo4j.kernel.impl.store.MetaDataStore;
+import org.neo4j.kernel.impl.store.StoreFactory;
+import org.neo4j.kernel.impl.store.StoreType;
+import org.neo4j.lock.LockService;
+import org.neo4j.logging.LogProvider;
+import org.neo4j.memory.MemoryTracker;
+import org.neo4j.monitoring.DatabaseHealth;
+import org.neo4j.storageengine.api.ConstraintRuleAccessor;
+import org.neo4j.storageengine.api.StorageEngine;
+import org.neo4j.token.TokenHolders;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@ServiceProvider
+public class InMemoryStorageEngineFactory434 extends AbstractInMemoryStorageEngineFactory {
+
+    public static final String IN_MEMORY_STORAGE_ENGINE_NAME_43 = "in-memory-434";
+
+    private final InMemoryMetaDataProviderImpl metadataProvider = new InMemoryMetaDataProviderImpl();
+
+    @Override
+    public StorageEngine instantiate(
+        FileSystemAbstraction fs,
+        DatabaseLayout databaseLayout,
+        Config config,
+        PageCache pageCache,
+        TokenHolders tokenHolders,
+        SchemaState schemaState,
+        ConstraintRuleAccessor constraintSemantics,
+        IndexConfigCompleter indexConfigCompleter,
+        LockService lockService,
+        IdGeneratorFactory idGeneratorFactory,
+        IdController idController,
+        DatabaseHealth databaseHealth,
+        LogProvider logProvider,
+        RecoveryCleanupWorkCollector recoveryCleanupWorkCollector,
+        PageCacheTracer cacheTracer,
+        boolean createStoreIfNotExists,
+        DatabaseReadOnlyChecker readOnlyChecker,
+        MemoryTracker memoryTracker
+    ) {
+        StoreFactory factory = new StoreFactory(
+            databaseLayout,
+            config,
+            idGeneratorFactory,
+            pageCache,
+            fs,
+            logProvider,
+            cacheTracer,
+            readOnlyChecker
+        );
+
+        factory.openNeoStores(createStoreIfNotExists, StoreType.LABEL_TOKEN);
+
+        return new InMemoryStorageEngineImpl.Builder(databaseLayout, tokenHolders, metadataProvider)
+            .withCommandCreationContextSupplier(InMemoryCommandCreationContextImpl::new)
+            .withStorageReaderFn(InMemoryStorageReader434::new)
+            .withTxStateVisitorFn(InMemoryTransactionStateVisitor::new)
+            .withCountsStoreFn(InMemoryCountsStoreImpl::new)
+            .withCommandCreationContextSupplier(InMemoryCommandCreationContextImpl::new)
+            .build();
+    }
+
+    @Override
+    public String name() {
+        return IN_MEMORY_STORAGE_ENGINE_NAME_43;
+    }
+
+    @Override
+    protected AbstractInMemoryMetaDataProvider metadataProvider() {
+        return metadataProvider;
+    }
+
+    @Override
+    public void setExternalStoreUUID(
+        FileSystemAbstraction fs,
+        DatabaseLayout databaseLayout,
+        PageCache pageCache,
+        CursorContext cursorContext,
+        UUID externalStoreId
+    ) throws IOException {
+        MetaDataStore.setExternalStoreUUID(
+            pageCache,
+            databaseLayout.metadataStore(),
+            externalStoreId,
+            databaseLayout.getDatabaseName(),
+            cursorContext
+        );
+    }
+}
