a84578463e2a53bb6f4e45500064e79429667dd8
Martin Junghanns <martin.junghanns@neo4j.com>
2021-10-14 16:04:46 +0200

    Add memory estimation for HugeSparseArrays
    Co-authored-by: Max Kießling <max.kiessling@neotechnology.com>
    Co-authored-by: Adam Schill Collberg <adam.schill.collberg@protonmail.com>

--- collections-memory-estimation/src/main/java/org/neo4j/gds/collections/HugeSparseArrays.java
+++ collections-memory-estimation/src/main/java/org/neo4j/gds/collections/HugeSparseArrays.java
@@ -27,34 +27,147 @@ public final class HugeSparseArrays {
 
     static final int DEFAULT_PAGE_SHIFT = 12;
 
+    // primitive types
+
+    public static MemoryRange estimateByte(long maxId, long maxEntries) {
+        return estimatePrimitive(byte.class, maxId, maxEntries, DEFAULT_PAGE_SHIFT);
+    }
+
+    public static MemoryRange estimateByte(long maxId, long maxEntries, int pageShift) {
+        return estimatePrimitive(byte.class, maxId, maxEntries, pageShift);
+    }
+
+    public static MemoryRange estimateShort(long maxId, long maxEntries) {
+        return estimatePrimitive(short.class, maxId, maxEntries, DEFAULT_PAGE_SHIFT);
+    }
+
+    public static MemoryRange estimateShort(long maxId, long maxEntries, int pageShift) {
+        return estimatePrimitive(short.class, maxId, maxEntries, pageShift);
+    }
+
+    public static MemoryRange estimateInt(long maxId, long maxEntries) {
+        return estimatePrimitive(int.class, maxId, maxEntries, DEFAULT_PAGE_SHIFT);
+    }
+
+    public static MemoryRange estimateInt(long maxId, long maxEntries, int pageShift) {
+        return estimatePrimitive(int.class, maxId, maxEntries, pageShift);
+    }
+
     public static MemoryRange estimateLong(long maxId, long maxEntries) {
-        return estimate(long.class, maxId, maxEntries, DEFAULT_PAGE_SHIFT);
+        return estimatePrimitive(long.class, maxId, maxEntries, DEFAULT_PAGE_SHIFT);
     }
 
     public static MemoryRange estimateLong(long maxId, long maxEntries, int pageShift) {
-        return estimate(long.class, maxId, maxEntries, pageShift);
+        return estimatePrimitive(long.class, maxId, maxEntries, pageShift);
     }
 
-    public static MemoryRange estimateLongArray(long maxId, long maxEntries) {
-        return estimate(long[].class, maxId, maxEntries, DEFAULT_PAGE_SHIFT);
+    public static MemoryRange estimateFloat(long maxId, long maxEntries) {
+        return estimatePrimitive(float.class, maxId, maxEntries, DEFAULT_PAGE_SHIFT);
     }
 
-    public static MemoryRange estimateLongArray(long maxId, long maxEntries, int pageShift) {
-        return estimate(long[].class, maxId, maxEntries, pageShift);
+    public static MemoryRange estimateFloat(long maxId, long maxEntries, int pageShift) {
+        return estimatePrimitive(float.class, maxId, maxEntries, pageShift);
     }
 
-    static MemoryRange estimate(Class<?> valueClazz, long maxId, long maxEntries, int pageShift) {
-        var collectionClazz = collectionClazz(valueClazz);
-        var pageSizeInBytes = pageSizeInBytes(valueClazz, pageSize(pageShift));
-        return memoryEstimation(collectionClazz, maxId, maxEntries, pageShift, pageSizeInBytes);
+    public static MemoryRange estimateDouble(long maxId, long maxEntries) {
+        return estimatePrimitive(double.class, maxId, maxEntries, DEFAULT_PAGE_SHIFT);
     }
 
-    private static MemoryRange memoryEstimation(
-        Class<?> clazz,
+    public static MemoryRange estimateDouble(long maxId, long maxEntries, int pageShift) {
+        return estimatePrimitive(double.class, maxId, maxEntries, pageShift);
+    }
+
+    // array types
+
+    public static MemoryRange estimateArray(long maxId, long maxEntries) {
+        return estimateArray(maxId, maxEntries, DEFAULT_PAGE_SHIFT);
+    }
+
+    public static MemoryRange estimateArray(long maxId, long maxEntries, int pageShift) {
+        // If the value array length is not known upfront, we estimate assuming an empty array.
+        // Therefore, we can use any array type here since we only measure object references.
+        return estimateArray(long[].class, maxId, maxEntries, 0, pageShift);
+    }
+
+    public static MemoryRange estimateByteArray(long maxId, long maxEntries, int averageEntryLength) {
+        return estimateArray(byte[].class, maxId, maxEntries, averageEntryLength, DEFAULT_PAGE_SHIFT);
+    }
+
+    public static MemoryRange estimateByteArray(long maxId, long maxEntries, int averageEntryLength, int pageShift) {
+        return estimateArray(byte[].class, maxId, maxEntries, averageEntryLength, pageShift);
+    }
+
+    public static MemoryRange estimateShortArray(long maxId, long maxEntries, int averageEntryLength) {
+        return estimateArray(short[].class, maxId, maxEntries, averageEntryLength, DEFAULT_PAGE_SHIFT);
+    }
+
+    public static MemoryRange estimateShortArray(long maxId, long maxEntries, int averageEntryLength, int pageShift) {
+        return estimateArray(short[].class, maxId, maxEntries, averageEntryLength, pageShift);
+    }
+
+    public static MemoryRange estimateIntArray(long maxId, long maxEntries, int averageEntryLength) {
+        return estimateArray(int[].class, maxId, maxEntries, averageEntryLength, DEFAULT_PAGE_SHIFT);
+    }
+
+    public static MemoryRange estimateIntArray(long maxId, long maxEntries, int averageEntryLength, int pageShift) {
+        return estimateArray(int[].class, maxId, maxEntries, averageEntryLength, pageShift);
+    }
+
+    public static MemoryRange estimateLongArray(long maxId, long maxEntries, int averageEntryLength) {
+        return estimateArray(long[].class, maxId, maxEntries, averageEntryLength, DEFAULT_PAGE_SHIFT);
+    }
+
+    public static MemoryRange estimateLongArray(long maxId, long maxEntries, int averageEntryLength, int pageShift) {
+        return estimateArray(long[].class, maxId, maxEntries, averageEntryLength, pageShift);
+    }
+
+    public static MemoryRange estimateFloatArray(long maxId, long maxEntries, int averageEntryLength) {
+        return estimateArray(float[].class, maxId, maxEntries, averageEntryLength, DEFAULT_PAGE_SHIFT);
+    }
+
+    public static MemoryRange estimateFloatArray(long maxId, long maxEntries, int averageEntryLength, int pageShift) {
+        return estimateArray(float[].class, maxId, maxEntries, averageEntryLength, pageShift);
+    }
+
+    public static MemoryRange estimateDoubleArray(long maxId, long maxEntries, int averageEntryLength) {
+        return estimateArray(double[].class, maxId, maxEntries, averageEntryLength, DEFAULT_PAGE_SHIFT);
+    }
+
+    public static MemoryRange estimateDoubleArray(long maxId, long maxEntries, int averageEntryLength, int pageShift) {
+        return estimateArray(double[].class, maxId, maxEntries, averageEntryLength, pageShift);
+    }
+
+    static MemoryRange estimatePrimitive(Class<?> valueClazz, long maxId, long maxEntries, int pageShift) {
+        var collectionAndPageSize = CollectionAndElementSize.ofPrimitive(valueClazz);
+        return memoryEstimationPrimitive(
+            collectionAndPageSize,
+            maxId,
+            maxEntries,
+            pageShift
+        );
+    }
+
+    static MemoryRange estimateArray(
+        Class<?> valueClazz,
+        long maxId,
+        long maxEntries,
+        int averageEntryLength,
+        int pageShift
+    ) {
+        var collectionAndPageSize = CollectionAndElementSize.ofArray(valueClazz, averageEntryLength);
+        return memoryEstimationArray(
+            collectionAndPageSize,
+            maxId,
+            maxEntries,
+            pageShift
+        );
+    }
+
+    private static MemoryRange memoryEstimationPrimitive(
+        CollectionAndElementSize collectionAndElementSize,
         long maxIndex,
         long expectedNumValues,
-        int pageShift,
-        long pageSizeInBytes
+        int pageShift
     ) {
         assert (expectedNumValues <= maxIndex);
 
@@ -68,38 +181,125 @@ public final class HugeSparseArrays {
         long maxEntriesForWorstCase = Math.min(maxIndex, expectedNumValues * pageSize);
         int numPagesForExpectedNumValuesWorstCase = PageUtil.numPagesFor(maxEntriesForWorstCase, pageShift, pageMask);
 
-        long classSize = MemoryUsage.sizeOfInstance(clazz);
+        long classSize = MemoryUsage.sizeOfInstance(collectionAndElementSize.collectionClazz);
         long pagesSize = MemoryUsage.sizeOfObjectArray(numPagesForSize);
-        long minRequirements = numPagesForExpectedNumValuesBestCase * pageSizeInBytes;
-        long maxRequirements = numPagesForExpectedNumValuesWorstCase * pageSizeInBytes;
+
+        long minRequirements = numPagesForExpectedNumValuesBestCase * MemoryUsage.sizeOfArray(
+            pageSize,
+            collectionAndElementSize.elementSizeInBytes
+        );
+        long maxRequirements = numPagesForExpectedNumValuesWorstCase * MemoryUsage.sizeOfArray(
+            pageSize,
+            collectionAndElementSize.elementSizeInBytes
+        );
 
         return MemoryRange.of(classSize + pagesSize).add(MemoryRange.of(minRequirements, maxRequirements));
     }
 
-    private static int pageSize(int pageShift) {
-        return 1 << pageShift;
+    private static MemoryRange memoryEstimationArray(
+        CollectionAndElementSize collectionAndElementSize,
+        long maxIndex,
+        long expectedNumValues,
+        int pageShift
+    ) {
+        assert (expectedNumValues <= maxIndex);
+
+        int pageSize = 1 << pageShift;
+        int pageMask = pageSize - 1;
+
+        int numPagesForSize = PageUtil.numPagesFor(maxIndex, pageShift, pageMask);
+        int numPagesForExpectedNumValuesBestCase = PageUtil.numPagesFor(expectedNumValues, pageShift, pageMask);
+
+        // Worst-case distribution assumes at least one entry per page
+        long maxEntriesForWorstCase = Math.min(maxIndex, expectedNumValues * pageSize);
+        int numPagesForExpectedNumValuesWorstCase = PageUtil.numPagesFor(maxEntriesForWorstCase, pageShift, pageMask);
+
+        long classSize = MemoryUsage.sizeOfInstance(collectionAndElementSize.collectionClazz);
+        long pagesSize = MemoryUsage.sizeOfObjectArray(numPagesForSize);
+
+        var pagesSizeBestCase = numPagesForExpectedNumValuesBestCase * MemoryUsage.sizeOfObjectArray(pageSize);
+        var pagesSizeWorstCase = numPagesForExpectedNumValuesWorstCase * MemoryUsage.sizeOfObjectArray(pageSize);
+
+        long valuesSize = expectedNumValues * collectionAndElementSize.elementSizeInBytes;
+
+        return MemoryRange
+            .of(classSize + pagesSize)
+            .add(MemoryRange.of(valuesSize))
+            .add(MemoryRange.of(pagesSizeBestCase, pagesSizeWorstCase));
     }
 
-    private static long pageSizeInBytes(Class<?> valueClazz, int pageSize) {
-        if (valueClazz.isAssignableFrom(long.class)) {
-            return MemoryUsage.sizeOfLongArray(pageSize);
-        }
-        if (valueClazz.isAssignableFrom(long[].class)) {
-            return MemoryUsage.sizeOfObjectArray(pageSize);
-        }
-        throw new IllegalArgumentException("Unsupported value class: " + valueClazz);
-    }
+    private static final class CollectionAndElementSize {
+        Class<?> collectionClazz;
 
-    private static Class<?> collectionClazz(Class<?> valueClazz) {
-        if (valueClazz.isAssignableFrom(long.class)) {
-            return HugeSparseLongArraySon.class;
-        }
-        if (valueClazz.isAssignableFrom(long[].class)) {
-            return HugeSparseLongArrayArraySon.class;
-        }
-        throw new IllegalArgumentException("Unsupported value class: " + valueClazz);
-    }
+        long elementSizeInBytes;
 
+        private CollectionAndElementSize(Class<?> collectionClazz, long elementSizeInBytes) {
+            this.collectionClazz = collectionClazz;
+            this.elementSizeInBytes = elementSizeInBytes;
+        }
+
+        static CollectionAndElementSize ofPrimitive(Class<?> valueClazz) {
+            if (valueClazz.isAssignableFrom(byte.class)) {
+                return new CollectionAndElementSize(HugeSparseByteArraySon.class, Byte.BYTES);
+            }
+            if (valueClazz.isAssignableFrom(short.class)) {
+                return new CollectionAndElementSize(HugeSparseShortArraySon.class, Short.BYTES);
+            }
+            if (valueClazz.isAssignableFrom(int.class)) {
+                return new CollectionAndElementSize(HugeSparseIntArraySon.class, Integer.BYTES);
+            }
+            if (valueClazz.isAssignableFrom(long.class)) {
+                return new CollectionAndElementSize(HugeSparseLongArraySon.class, Long.BYTES);
+            }
+            if (valueClazz.isAssignableFrom(float.class)) {
+                return new CollectionAndElementSize(HugeSparseFloatArraySon.class, Float.BYTES);
+            }
+            if (valueClazz.isAssignableFrom(double.class)) {
+                return new CollectionAndElementSize(HugeSparseDoubleArraySon.class, Double.BYTES);
+            }
+            throw new IllegalArgumentException("Unsupported primitive value class: " + valueClazz);
+        }
+
+        static CollectionAndElementSize ofArray(Class<?> valueClazz, int averageEntryLength) {
+            if (valueClazz.isAssignableFrom(byte[].class)) {
+                return new CollectionAndElementSize(
+                    HugeSparseByteArrayArraySon.class,
+                    MemoryUsage.sizeOfArray(averageEntryLength, Byte.BYTES)
+                );
+            }
+            if (valueClazz.isAssignableFrom(short[].class)) {
+                return new CollectionAndElementSize(
+                    HugeSparseShortArrayArraySon.class,
+                    MemoryUsage.sizeOfArray(averageEntryLength, Short.BYTES)
+                );
+            }
+            if (valueClazz.isAssignableFrom(int[].class)) {
+                return new CollectionAndElementSize(
+                    HugeSparseIntArrayArraySon.class,
+                    MemoryUsage.sizeOfArray(averageEntryLength, Integer.BYTES)
+                );
+            }
+            if (valueClazz.isAssignableFrom(long[].class)) {
+                return new CollectionAndElementSize(
+                    HugeSparseLongArrayArraySon.class,
+                    MemoryUsage.sizeOfArray(averageEntryLength, Long.BYTES)
+                );
+            }
+            if (valueClazz.isAssignableFrom(float[].class)) {
+                return new CollectionAndElementSize(
+                    HugeSparseFloatArrayArraySon.class,
+                    MemoryUsage.sizeOfArray(averageEntryLength, Float.BYTES)
+                );
+            }
+            if (valueClazz.isAssignableFrom(double[].class)) {
+                return new CollectionAndElementSize(
+                    HugeSparseDoubleArrayArraySon.class,
+                    MemoryUsage.sizeOfArray(averageEntryLength, Double.BYTES)
+                );
+            }
+            throw new IllegalArgumentException("Unsupported primitive value class: " + valueClazz);
+        }
+    }
 
     private HugeSparseArrays() {}
 }
--- collections-memory-estimation/src/test/java/org/neo4j/gds/collections/HugeSparseArraysTest.java
+++ collections-memory-estimation/src/test/java/org/neo4j/gds/collections/HugeSparseArraysTest.java
@@ -29,15 +29,24 @@ import java.util.stream.Stream;
 import static io.qala.datagen.RandomShortApi.integer;
 import static io.qala.datagen.RandomValue.between;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.neo4j.gds.collections.HugeSparseArrays.DEFAULT_PAGE_SHIFT;
 
 class HugeSparseArraysTest {
 
     static Stream<Class<?>> valueClasses() {
         return Stream.of(
+            byte.class,
+            short.class,
+            int.class,
             long.class,
-            long[].class
+            float.class,
+            double.class,
+            byte[].class,
+            short[].class,
+            int[].class,
+            long[].class,
+            float[].class,
+            double[].class
         );
     }
 
@@ -46,12 +55,9 @@ class HugeSparseArraysTest {
     void shouldComputeMemoryEstimationForBestCase(Class<?> valueClazz) {
         long size = integer(Integer.MAX_VALUE);
 
-        var memoryRange = HugeSparseArrays.estimate(
-            valueClazz,
-            size,
-            size,
-            DEFAULT_PAGE_SHIFT
-        );
+        var memoryRange = valueClazz.isPrimitive()
+            ? HugeSparseArrays.estimatePrimitive(valueClazz, size, size, DEFAULT_PAGE_SHIFT)
+            : HugeSparseArrays.estimateArray(valueClazz, size, size, 0, DEFAULT_PAGE_SHIFT);
 
         assertThat(memoryRange.min).isEqualTo(memoryRange.max);
     }
@@ -63,28 +69,56 @@ class HugeSparseArraysTest {
         long maxEntries = integer(Integer.MAX_VALUE);
         long maxId = between(maxEntries + pageSize, maxEntries * pageSize).Long();
 
-        var memoryRange = HugeSparseArrays.estimate(
-            valueClazz,
-            maxId,
-            maxEntries,
-            DEFAULT_PAGE_SHIFT
-        );
+        var memoryRange = valueClazz.isPrimitive()
+            ? HugeSparseArrays.estimatePrimitive(valueClazz, maxId, maxEntries, DEFAULT_PAGE_SHIFT)
+            : HugeSparseArrays.estimateArray(valueClazz, maxId, maxEntries, 0, DEFAULT_PAGE_SHIFT);
 
         assertThat(memoryRange.min).isLessThan(memoryRange.max);
     }
 
-    static Stream<Arguments> expectedRanges() {
+    static Stream<Arguments> expectedRangesPrimitiveTypes() {
         return Stream.of(
+            // byte
+            Arguments.of(byte.class, 0L, 0L, 48L, 48L),
+            Arguments.of(byte.class, 100L, 100L, 4_168L, 4_168L),
+            Arguments.of(byte.class, 100_000_000_000L, 1L, 97_660_416L, 97_660_416L),
+            Arguments.of(byte.class, 100_000_000_000L, 10_000_000L, 107_697_808L, 41_217_656_304L),
+            Arguments.of(byte.class, 100_000_000_000L, 100_000_000L, 198_050_784L, 100_488_283_360L),
+            // short
+            Arguments.of(short.class, 0L, 0L, 48L, 48L),
+            Arguments.of(short.class, 100L, 100L, 8_264L, 8_264L),
+            Arguments.of(short.class, 100_000_000_000L, 1L, 97_664_512L, 97_664_512L),
+            Arguments.of(short.class, 100_000_000_000L, 10_000_000L, 117_700_240L, 82_177_656_304L),
+            Arguments.of(short.class, 100_000_000_000L, 100_000_000L, 298_054_624L, 200_488_285_408L),
+            // int
+            Arguments.of(int.class, 0L, 0L, 48L, 48L),
+            Arguments.of(int.class, 100L, 100L, 16456L, 16456L),
+            Arguments.of(int.class, 100_000_000_000L, 1L, 97_672_704L, 97_672_704L),
+            Arguments.of(int.class, 100_000_000_000L, 10_000_000L, 137_705_104L, 164_097_656_304L),
+            Arguments.of(int.class, 100_000_000_000L, 100_000_000L, 498_062_304L, 400_488_289_504L),
+            // long
             Arguments.of(long.class, 0L, 0L, 48L, 48L),
             Arguments.of(long.class, 100L, 100L, 32840L, 32840L),
             Arguments.of(long.class, 100_000_000_000L, 1L, 97_689_088L, 97_689_088L),
             Arguments.of(long.class, 100_000_000_000L, 10_000_000L, 177_714_832L, 327_937_656_304L),
-            Arguments.of(long.class, 100_000_000_000L, 100_000_000L, 898_077_664L, 800_488_297_696L)
+            Arguments.of(long.class, 100_000_000_000L, 100_000_000L, 898_077_664L, 800_488_297_696L),
+            // float
+            Arguments.of(float.class, 0L, 0L, 48L, 48L),
+            Arguments.of(float.class, 100L, 100L, 16456L, 16456L),
+            Arguments.of(float.class, 100_000_000_000L, 1L, 97_672_704L, 97_672_704L),
+            Arguments.of(float.class, 100_000_000_000L, 10_000_000L, 137_705_104L, 164_097_656_304L),
+            Arguments.of(float.class, 100_000_000_000L, 100_000_000L, 498_062_304L, 400_488_289_504L),
+            // double
+            Arguments.of(double.class, 0L, 0L, 48L, 48L),
+            Arguments.of(double.class, 100L, 100L, 32840L, 32840L),
+            Arguments.of(double.class, 100_000_000_000L, 1L, 97_689_088L, 97_689_088L),
+            Arguments.of(double.class, 100_000_000_000L, 10_000_000L, 177_714_832L, 327_937_656_304L),
+            Arguments.of(double.class, 100_000_000_000L, 100_000_000L, 898_077_664L, 800_488_297_696L)
         );
     }
 
     @ParameterizedTest
-    @MethodSource("expectedRanges")
+    @MethodSource("expectedRangesPrimitiveTypes")
     void shouldComputeMemoryEstimation(
         Class<?> valueClazz,
         long maxId,
@@ -92,9 +126,104 @@ class HugeSparseArraysTest {
         long expectedMin,
         long expectedMax
     ) {
-        assertEquals(
-            MemoryRange.of(expectedMin, expectedMax),
-            HugeSparseArrays.estimate(valueClazz, maxId, maxEntries, DEFAULT_PAGE_SHIFT)
+        assertThat(MemoryRange.of(expectedMin, expectedMax))
+            .isEqualTo(HugeSparseArrays.estimatePrimitive(
+                valueClazz,
+                maxId,
+                maxEntries,
+                DEFAULT_PAGE_SHIFT
+            ));
+    }
+
+    static Stream<Arguments> expectedRangesArrayTypes() {
+        return Stream.of(
+            // byte
+            Arguments.of(byte[].class, 0, 0L, 0L, 48L, 48L),
+            Arguments.of(byte[].class, 10, 0L, 0L, 48L, 48L),
+            Arguments.of(byte[].class, 0, 100L, 100L, 18_056L, 18_056L),
+            Arguments.of(byte[].class, 10, 100L, 100L, 19_656L, 19_656L),
+            Arguments.of(byte[].class, 0, 100_000_000_000L, 1L, 97_672_720L, 97_672_720L),
+            Arguments.of(byte[].class, 10, 100_000_000_000L, 1L, 97_672_736L, 97_672_736L),
+            Arguments.of(byte[].class, 0, 100_000_000_000L, 10_000_000L, 297_705_104L, 164_257_656_304L),
+            Arguments.of(byte[].class, 10, 100_000_000_000L, 10_000_000L, 457_705_104L, 164_417_656_304L),
+            Arguments.of(byte[].class, 0, 100_000_000_000L, 100_000_000L, 2_098_062_304L, 402_088_289_504L),
+            Arguments.of(byte[].class, 10, 100_000_000_000L, 100_000_000L, 3_698_062_304L, 403_688_289_504L),
+            // short
+            Arguments.of(short[].class, 0, 0L, 0L, 48L, 48L),
+            Arguments.of(short[].class, 10, 0L, 0L, 48L, 48L),
+            Arguments.of(short[].class, 0, 100L, 100L, 18056L, 18056L),
+            Arguments.of(short[].class, 10, 100L, 100L, 20456L, 20456L),
+            Arguments.of(short[].class, 0, 100_000_000_000L, 1L, 97672720L, 97672720L),
+            Arguments.of(short[].class, 10, 100_000_000_000L, 1L, 97672744L, 97672744L),
+            Arguments.of(short[].class, 0, 100_000_000_000L, 10_000_000L, 297705104L, 164257656304L),
+            Arguments.of(short[].class, 10, 100_000_000_000L, 10_000_000L, 537705104L, 164497656304L),
+            Arguments.of(short[].class, 0, 100_000_000_000L, 100_000_000L, 2098062304L, 402088289504L),
+            Arguments.of(short[].class, 10, 100_000_000_000L, 100_000_000L, 4498062304L, 404488289504L),
+            // int
+            Arguments.of(int[].class, 0, 0L, 0L, 48L, 48L),
+            Arguments.of(int[].class, 10, 0L, 0L, 48L, 48L),
+            Arguments.of(int[].class, 0, 100L, 100L, 18056L, 18056L),
+            Arguments.of(int[].class, 10, 100L, 100L, 22056L, 22056L),
+            Arguments.of(int[].class, 0, 100_000_000_000L, 1L, 97672720L, 97672720L),
+            Arguments.of(int[].class, 10, 100_000_000_000L, 1L, 97672760L, 97672760L),
+            Arguments.of(int[].class, 0, 100_000_000_000L, 10_000_000L, 297705104L, 164257656304L),
+            Arguments.of(int[].class, 10, 100_000_000_000L, 10_000_000L, 697705104L, 164657656304L),
+            Arguments.of(int[].class, 0, 100_000_000_000L, 100_000_000L, 2098062304L, 402088289504L),
+            Arguments.of(int[].class, 10, 100_000_000_000L, 100_000_000L, 6098062304L, 406088289504L),
+            // long
+            Arguments.of(long[].class, 0, 0L, 0L, 48L, 48L),
+            Arguments.of(long[].class, 10, 0L, 0L, 48L, 48L),
+            Arguments.of(long[].class, 0, 100L, 100L, 18056L, 18056L),
+            Arguments.of(long[].class, 10, 100L, 100L, 26056L, 26056L),
+            Arguments.of(long[].class, 0, 100_000_000_000L, 1L, 97672720L, 97672720L),
+            Arguments.of(long[].class, 10, 100_000_000_000L, 1L, 97672800L, 97672800L),
+            Arguments.of(long[].class, 0, 100_000_000_000L, 10_000_000L, 297705104L, 164257656304L),
+            Arguments.of(long[].class, 10, 100_000_000_000L, 10_000_000L, 1097705104L, 165057656304L),
+            Arguments.of(long[].class, 0, 100_000_000_000L, 100_000_000L, 2098062304L, 402088289504L),
+            Arguments.of(long[].class, 10, 100_000_000_000L, 100_000_000L, 10098062304L, 410088289504L),
+            // float
+            Arguments.of(float[].class, 0, 0L, 0L, 48L, 48L),
+            Arguments.of(float[].class, 10, 0L, 0L, 48L, 48L),
+            Arguments.of(float[].class, 0, 100L, 100L, 18056L, 18056L),
+            Arguments.of(float[].class, 10, 100L, 100L, 22056L, 22056L),
+            Arguments.of(float[].class, 0, 100_000_000_000L, 1L, 97672720L, 97672720L),
+            Arguments.of(float[].class, 10, 100_000_000_000L, 1L, 97672760L, 97672760L),
+            Arguments.of(float[].class, 0, 100_000_000_000L, 10_000_000L, 297705104L, 164257656304L),
+            Arguments.of(float[].class, 10, 100_000_000_000L, 10_000_000L, 697705104L, 164657656304L),
+            Arguments.of(float[].class, 0, 100_000_000_000L, 100_000_000L, 2098062304L, 402088289504L),
+            Arguments.of(float[].class, 10, 100_000_000_000L, 100_000_000L, 6098062304L, 406088289504L),
+            // double
+            Arguments.of(double[].class, 0, 0L, 0L, 48L, 48L),
+            Arguments.of(double[].class, 10, 0L, 0L, 48L, 48L),
+            Arguments.of(double[].class, 0, 100L, 100L, 18056L, 18056L),
+            Arguments.of(double[].class, 10, 100L, 100L, 26056L, 26056L),
+            Arguments.of(double[].class, 0, 100_000_000_000L, 1L, 97672720L, 97672720L),
+            Arguments.of(double[].class, 10, 100_000_000_000L, 1L, 97672800L, 97672800L),
+            Arguments.of(double[].class, 0, 100_000_000_000L, 10_000_000L, 297705104L, 164257656304L),
+            Arguments.of(double[].class, 10, 100_000_000_000L, 10_000_000L, 1097705104L, 165057656304L),
+            Arguments.of(double[].class, 0, 100_000_000_000L, 100_000_000L, 2098062304L, 402088289504L),
+            Arguments.of(double[].class, 10, 100_000_000_000L, 100_000_000L, 10098062304L, 410088289504L)
         );
     }
+
+    @ParameterizedTest
+    @MethodSource("expectedRangesArrayTypes")
+    void shouldComputeMemoryEstimationForArrayTypes(
+        Class<?> valueClazz,
+        int averageEntryLength,
+        long maxId,
+        long maxEntries,
+        long expectedMin,
+        long expectedMax
+    ) {
+        assertThat(MemoryRange.of(expectedMin, expectedMax)).isEqualTo(HugeSparseArrays.estimateArray(
+            valueClazz,
+            maxId,
+            maxEntries,
+            averageEntryLength,
+            DEFAULT_PAGE_SHIFT
+        ));
+    }
+
+
 }
