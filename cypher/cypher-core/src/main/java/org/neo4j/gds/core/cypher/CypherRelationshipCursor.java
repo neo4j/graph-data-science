4af7c93a8103f4aacdcb86efb88bd4081ab590ac
Mats Rydberg <mats@neotechnology.com>
2021-10-15 16:31:50 +0200

    Suppress `immutables:subtype` warnings

--- cypher/cypher-core/src/main/java/org/neo4j/gds/core/cypher/CypherRelationshipCursor.java
+++ cypher/cypher-core/src/main/java/org/neo4j/gds/core/cypher/CypherRelationshipCursor.java
@@ -24,6 +24,7 @@ import org.neo4j.gds.annotation.ValueClass;
 import org.neo4j.gds.api.RelationshipCursor;
 
 @ValueClass
+@SuppressWarnings("immutables:subtype")
 public interface CypherRelationshipCursor extends RelationshipCursor {
     long id();
     RelationshipType relationshipType();
