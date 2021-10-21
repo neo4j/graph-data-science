6721e81965b10085c7ebf38f5c1aae8d1f51870c
Jacob Sznajdman <breakanalysis@gmail.com>
2021-10-21 17:54:59 +0200

    Implement create proc for nc-pipeline

---
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/nodemodels/pipeline/NodeClassificationPipelineCreateProc.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.nodemodels.pipeline;
+
+import org.neo4j.gds.BaseProc;
+import org.neo4j.gds.api.schema.GraphSchema;
+import org.neo4j.gds.config.StringIdentifierValidations;
+import org.neo4j.gds.core.model.Model;
+import org.neo4j.gds.core.model.ModelCatalog;
+import org.neo4j.gds.ml.pipelinecommon.PipelineDummyTrainConfig;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.stream.Stream;
+
+import static org.neo4j.procedure.Mode.READ;
+
+@SuppressWarnings("immutables:subtype")
+public class NodeClassificationPipelineCreateProc extends BaseProc {
+    public static final String PIPELINE_MODEL_TYPE = "Node classification training pipeline";
+
+    @Procedure(name = "gds.alpha.ml.pipeline.nodeClassification.create", mode = READ)
+    @Description("Creates a node classification pipeline in the model catalog.")
+    public Stream<PipelineInfoResult> create(@Name("pipelineName") String input) {
+        var pipelineName = StringIdentifierValidations.validateNoWhiteCharacter(input, "pipelineName");
+
+        var model = Model.of(
+            username(),
+            pipelineName,
+            PIPELINE_MODEL_TYPE,
+            GraphSchema.empty(),
+            new Object(),
+            PipelineDummyTrainConfig.of(username()),
+            new NodeClassificationTrainingPipeline()
+        );
+
+        ModelCatalog.set(model);
+
+        return Stream.of(new PipelineInfoResult(pipelineName, model.customInfo()));
+    }
+}
