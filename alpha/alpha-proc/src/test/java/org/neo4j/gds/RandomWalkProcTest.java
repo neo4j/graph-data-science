08e293450b112ce51a0afe6c5d959b7b56f04fe7
Max Kießling <max.kiessling@neotechnology.com>
2021-11-01 17:56:00 +0100

    Implement procedure tests for random walk

--- alpha/alpha-proc/src/test/java/org/neo4j/gds/RandomWalkProcTest.java
+++ alpha/alpha-proc/src/test/java/org/neo4j/gds/RandomWalkProcTest.java
@@ -20,255 +20,172 @@
 package org.neo4j.gds;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
-import org.neo4j.gds.walking.RandomWalkProc;
-import org.neo4j.graphdb.Label;
+import org.junit.jupiter.api.TestFactory;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.neo4j.gds.core.CypherMapWrapper;
+import org.neo4j.gds.test.config.RelationshipWeightConfigProcTest;
+import org.neo4j.gds.traversal.RandomWalk;
+import org.neo4j.gds.traversal.RandomWalkStreamConfig;
+import org.neo4j.gds.walking.RandomWalkStreamProc;
 import org.neo4j.graphdb.Path;
-import org.neo4j.graphdb.Relationship;
-import org.neo4j.graphdb.ResourceIterator;
-import org.neo4j.graphdb.Result;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
 
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
-import java.util.Set;
+import java.util.Optional;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.neo4j.gds.NodeLabel.ALL_NODES;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-class RandomWalkProcTest extends BaseProcTest {
-
-    private static final int NODE_COUNT = 54;
+@SuppressWarnings("unchecked")
+class RandomWalkProcTest extends BaseProcTest implements AlgoBaseProcTest<RandomWalk, RandomWalkStreamConfig, Stream<long[]>> {
 
     private static final String DB_CYPHER =
         "CREATE" +
-        "  (a:Node {name:'a'})" +
-        ", (b:Fred {name:'b'})" +
-        ", (c:Fred {name:'c'})" +
-        ", (d:Bob {name:'d'})" +
-        ", (a)-[:OF_TYPE {cost:5, blue: 1}]->(b)" +
-        ", (a)-[:OF_TYPE {cost:10, blue: 1}]->(c)" +
-        ", (c)-[:DIFFERENT {cost:2, blue: 0}]->(b)" +
-        ", (b)-[:OF_TYPE {cost:5, blue: 1}]->(c)" +
-        "  WITH * UNWIND range(0, $count - 1) AS id " +
-        "  CREATE (n:Node {name: '' + id})" +
-        "  CREATE " +
-        "    (n)-[:OF_TYPE {cost: 5, blue: 1}]->(a)," +
-        "    (b)-[:OF_TYPE {cost: 5, blue: 1}]->(n)";
+        "  (a:Node1)" +
+        ", (b:Node1)" +
+        ", (c:Node2)" +
+        ", (d:Isolated)" +
+        ", (e:Isolated)" +
+        ", (a)-[:REL1]->(b)" +
+        ", (b)-[:REL1]->(a)" +
+        ", (a)-[:REL1]->(c)" +
+        ", (c)-[:REL2]->(a)" +
+        ", (b)-[:REL2]->(c)" +
+        ", (c)-[:REL2]->(b)";
 
     @BeforeEach
     void setup() throws Exception {
-        registerProcedures(RandomWalkProc.class);
-        runQuery(DB_CYPHER, Collections.singletonMap("count", NODE_COUNT - 4));
+        registerProcedures(RandomWalkStreamProc.class);
+        runQuery(DB_CYPHER);
     }
 
     @Test
-    void shouldHaveGivenStartNodeRandom() {
+    void shouldRunSimpleConfig() {
         String query = GdsCypher.call()
             .loadEverything(Orientation.UNDIRECTED)
             .algo("gds", "alpha", "randomWalk")
             .streamMode()
-            .addParameter("start", 1)
-            .addParameter("steps", 1)
-            .addParameter("walks", 1)
+            .addParameter("walksPerNode", 3)
+            .addParameter("walkLength", 10)
             .yields();
 
-        runQueryWithResultConsumer(query, r -> {
-            ResourceIterator<List<Long>> result = r.columnAs("nodeIds");
-            List<Long> path = result.next();
-            assertEquals(1L, path.get(0).longValue());
-            assertNotEquals(1L, path.get(1).longValue());
-        });
+        Collection<List<Long>> result = new ArrayList<>();
+
+        runQueryWithRowConsumer(query, row -> result.add((List<Long>) row.get("nodeIds")));
+
+        int expectedNumberOfWalks = 3 * 3;
+        assertEquals(expectedNumberOfWalks, result.size());
+        List<Long> walkForNodeZero = result
+            .stream()
+            .filter(arr -> arr.get(0) == 0)
+            .findFirst()
+            .orElse(List.of());
+        assertEquals(10, walkForNodeZero.size());
     }
 
     @Test
-    void shouldHaveResultsRandom() {
+    void shouldReturnPath() {
         String query = GdsCypher.call()
             .loadEverything(Orientation.UNDIRECTED)
             .algo("gds", "alpha", "randomWalk")
             .streamMode()
-            .addParameter("steps", 1)
-            .addParameter("walks", 5)
+            .addParameter("walksPerNode", 3)
+            .addParameter("walkLength", 10)
+            .addParameter("returnPath", true)
             .yields();
 
-        assertTrue(runQuery(query, Result::hasNext));
-    }
+        List<List<Long>> nodeIds = new ArrayList<>();
+        List<Path> paths = new ArrayList<>();
 
-    @Test
-    void shouldHaveSameTypesForStartNodesRandom() {
-        String query = GdsCypher.call()
-            .loadEverything(Orientation.UNDIRECTED)
-            .algo("gds", "alpha", "randomWalk")
-            .streamMode()
-            .addParameter("start", "Fred")
-            .addParameter("steps", 2)
-            .addParameter("walks", 5)
-            .addParameter("path", true)
-            .yields();
-
-        // TODO: make this test predictable (i.e. set random seed)
-        int count = runQuery(query, r -> {
-            ResourceIterator<Path> results = r.columnAs("path");
-            int resultCount = 0;
-            while (results.hasNext()) {
-                Path path = results.next();
-                assertTrue(path.startNode().hasLabel(Label.label("Fred")), "Nodes should be of type 'Fred'.");
-                assertEquals(2, path.length());
-                Relationship firstRel = path.relationships().iterator().next();
-                assertEquals(path.startNode(), firstRel.getStartNode());
-                resultCount++;
-            }
-            return resultCount;
-        });
-        assertEquals(5, count);
-    }
-
-    @Test
-    void shouldHaveStartedFromEveryNodeRandom() {
-        String query = GdsCypher.call()
-            .loadEverything(Orientation.UNDIRECTED)
-            .algo("gds", "alpha", "randomWalk")
-            .streamMode()
-            .addParameter("steps", 1)
-            .addParameter("walks", 100)
-            .yields();
-
-        runQueryWithResultConsumer(query, r -> {
-            ResourceIterator<List<Long>> results = r.columnAs("nodeIds");
-                Set<Long> nodeIds = new HashSet<>();
-            while (results.hasNext()) {
-                List<Long> path = results.next();
-                nodeIds.add(path.get(0));
-            }
-            assertEquals(NODE_COUNT, nodeIds.size(), "Should have visited all nodes.");
-        });
-    }
-
-    @Test
-    void shouldNotFailRandom() {
-        String query = GdsCypher.call()
-            .loadEverything(Orientation.UNDIRECTED)
-            .algo("gds", "alpha", "randomWalk")
-            .streamMode()
-            .addParameter("start", 2)
-            .addParameter("steps", 7)
-            .addParameter("walks", 2)
-            .yields();
-
-        Boolean noMoreNext = runQuery(query, r -> {
-            r.next();
-            r.next();
-
-            return r.hasNext();
+        runQueryWithRowConsumer(query, row -> {
+            nodeIds.add((List<Long>) row.get("nodeIds"));
+            paths.add((Path) row.get("path"));
         });
 
-        assertTrue(!noMoreNext, "There should be only two results.");
+        for (int i = 0; i < paths.size(); i++) {
+            var nodes = nodeIds.get(i);
+            var path = paths.get(i);
+
+            AtomicInteger indexInPath = new AtomicInteger(0);
+            path.nodes().forEach(node -> assertEquals(node.getId(), nodes.get(indexInPath.getAndIncrement())));
+        }
     }
 
-    @Test
-    void shouldHaveGivenStartNode() {
+    static Stream<Arguments> sourceNodes() {
+        return Stream.of(
+            arguments(List.of(0L, 1L), List.of(0L, 1L)),
+            arguments(List.of(0L), 0L)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("sourceNodes")
+    void shouldParseSourceNodeIds(Collection<Long> parsedSourceNodes, Object rawSourceNodes) {
         String query = GdsCypher.call()
             .loadEverything(Orientation.UNDIRECTED)
             .algo("gds", "alpha", "randomWalk")
             .streamMode()
-            .addParameter("start", 1)
-            .addParameter("steps", 1)
-            .addParameter("walks", 1)
-            .addParameter("mode", "node2vec")
-            .addParameter("return", 1)
-            .addParameter("inOut", 1)
+            .addParameter("walksPerNode", 1)
+            .addParameter("walkLength", 3)
+            .addParameter("returnPath", true)
+            .addParameter("sourceNodes", rawSourceNodes)
             .yields();
 
-        List<Long> result = runQuery(query, r -> r.<List<Long>>columnAs("nodeIds").next());
-        assertThat(1L, equalTo(result.get(0)));
+        List<List<Long>> nodeIds = new ArrayList<>();
+        runQueryWithRowConsumer(query, row -> nodeIds.add((List<Long>) row.get("nodeIds")));
+
+        assertThat(nodeIds).matches(walks -> walks.size() == parsedSourceNodes.size());
+        for (long sourceNode : parsedSourceNodes) {
+            assertThat(nodeIds).anyMatch(walk -> walk.get(0) == sourceNode);
+        }
     }
 
-    @Test
-    void shouldHaveResultsN2V() {
-        String query = GdsCypher.call()
-            .loadEverything(Orientation.UNDIRECTED)
-            .algo("gds", "alpha", "randomWalk")
-            .streamMode()
-            .addParameter("steps", 1)
-            .addParameter("walks", 5)
-            .addParameter("mode", "node2vec")
-            .addParameter("return", 1)
-            .addParameter("inOut", 1)
-            .yields();
-
-        runQueryWithResultConsumer(query, r -> {
-            ResourceIterator<List<Long>> results = r.columnAs("nodeIds");
-            assertTrue(results.hasNext());
-        });
+    @Override
+    public Class<? extends AlgoBaseProc<RandomWalk, Stream<long[]>, RandomWalkStreamConfig>> getProcedureClazz() {
+        return RandomWalkStreamProc.class;
     }
 
-    @Test
-    void shouldHaveStartedFromEveryNodeN2V() {
-        String query = GdsCypher.call()
-            .loadEverything(Orientation.UNDIRECTED)
-            .algo("gds", "alpha", "randomWalk")
-            .streamMode()
-            .addParameter("steps", 1)
-            .addParameter("walks", 100)
-            .addParameter("mode", "node2vec")
-            .addParameter("return", 1)
-            .addParameter("inOut", 1)
-            .yields();
-
-        runQueryWithResultConsumer(query, r -> {
-            ResourceIterator<List<Long>> results = r.columnAs("nodeIds");
-            Set<Long> nodeIds = new HashSet<>();
-            while (results.hasNext()) {
-                List<Long> record = results.next();
-                nodeIds.add(record.get(0));
-            }
-            assertEquals(NODE_COUNT, nodeIds.size(), "Should have visited all nodes.");
-        });
+    @Override
+    public GraphDatabaseAPI graphDb() {
+        return db;
     }
 
-    @Test
-    void shouldNotFailN2V() {
-        String query = GdsCypher.call()
-            .loadEverything(Orientation.UNDIRECTED)
-            .algo("gds", "alpha", "randomWalk")
-            .streamMode()
-            .addParameter("start", 2)
-            .addParameter("steps", 7)
-            .addParameter("walks", 2)
-            .addParameter("mode", "node2vec")
-            .addParameter("return", 1)
-            .addParameter("inOut", 1)
-            .yields();
-
-        runQueryWithResultConsumer(query, r -> {
-            ResourceIterator<List<Long>> results = r.columnAs("nodeIds");
-            results.next();
-            results.next();
-            assertTrue(!results.hasNext(), "There should be only two results.");
-        });
+    @Override
+    public RandomWalkStreamConfig createConfig(CypherMapWrapper mapWrapper) {
+        return RandomWalkStreamConfig.of("", Optional.empty(), Optional.empty(), mapWrapper);
     }
 
-    @Test
-    void shouldReturnPartialWalks() {
-        String query = GdsCypher.call()
-            .withNodeLabel(ALL_NODES.name)
-            .withRelationshipType("OF_TYPE")
-            .algo("gds", "alpha", "randomWalk")
-            .streamMode()
-            .addParameter("start", 0)
-            .addParameter("steps", 7)
-            .addParameter("walks", 2)
-            .yields();
+    @Override
+    public void assertResultEquals(Stream<long[]> result1, Stream<long[]> result2) {
+        var resultList1 = result1.collect(Collectors.toList());
+        var resultList2 = result2.collect(Collectors.toList());
 
-        runQueryWithResultConsumer(query, r -> {
-            ResourceIterator<List<Long>> results = r.columnAs("nodeIds");
-            assertTrue(results.next().size() > 1);
-            assertTrue(results.next().size() > 1);
-            assertTrue(!results.hasNext(), "There should be only two results.");
-        });
+        for (int i = 0; i < resultList1.size(); i++) {
+            var path1 = resultList1.get(i);
+            var path2 = resultList2.get(i);
+            assertThat(path1.length).isEqualTo(path2.length);
+        }
+    }
+
+    @Override
+    public boolean releaseAlgorithm() {
+        return false;
+    }
+
+    @TestFactory
+    Stream<DynamicTest> configTests() {
+        return Stream.of(
+            RelationshipWeightConfigProcTest.allTheTests(proc(), createMinimalConfig())
+        ).flatMap(Collection::stream);
     }
 }
