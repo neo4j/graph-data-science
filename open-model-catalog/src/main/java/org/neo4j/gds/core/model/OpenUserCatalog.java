87f0946f95ff916272262f0af192361ebdae38a4
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2021-11-03 22:13:49 +0100

    Move out UserCatalog

---
+++ open-model-catalog/src/main/java/org/neo4j/gds/core/model/OpenUserCatalog.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.core.model;
+
+import org.neo4j.gds.model.ModelConfig;
+
+import java.util.Collection;
+import java.util.Locale;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Stream;
+
+import static org.neo4j.gds.core.StringSimilarity.prettySuggestions;
+
+class OpenUserCatalog {
+    private static final long ALLOWED_MODELS_COUNT = 3;
+
+    private final Map<String, Model<?, ?, ?>> userModels = new ConcurrentHashMap<>();
+
+    Set<String> availableModelNames() {
+        return userModels.keySet();
+    }
+
+    public void set(Model<?, ?, ?> model) {
+        checkStorable(model.name(), model.algoType());
+        userModels.put(model.name(), model);
+    }
+
+    void setUnsafe(Model<?, ?, ?> model) {
+        userModels.put(model.name(), model);
+    }
+
+    public <D, C extends ModelConfig, I extends Model.Mappable> Model<D, C, I> get(
+        String modelName,
+        Class<D> dataClass,
+        Class<C> configClass,
+        Class<I> infoClass
+    ) {
+        return get(
+            getUntyped(modelName),
+            dataClass,
+            configClass,
+            infoClass
+        );
+    }
+
+    private <D, C extends ModelConfig, I extends Model.Mappable> Model<D, C, I> get(
+        Model<?, ?, ?> model,
+        Class<D> dataClass,
+        Class<C> configClass,
+        Class<I> infoClass
+    ) {
+        if (model != null) {
+            var modelName = model.name();
+            if (!dataClass.isInstance(model.data())) {
+                throw new IllegalArgumentException(formatWithLocale(
+                    "The model `%s` has data with different types than expected. " +
+                    "Expected data type: `%s`, invoked with model data type: `%s`.",
+                    modelName,
+                    model.data().getClass().getName(),
+                    dataClass.getName()
+                ));
+            }
+            if (!configClass.isInstance(model.trainConfig())) {
+                throw new IllegalArgumentException(formatWithLocale(
+                    "The model `%s` has a training config with different types than expected. " +
+                    "Expected train config type: `%s`, invoked with model config type: `%s`.",
+                    modelName,
+                    model.trainConfig().getClass().getName(),
+                    configClass.getName()
+                ));
+            }
+
+
+            if (!infoClass.isInstance(model.customInfo())) {
+                throw new IllegalArgumentException(formatWithLocale(
+                    "The model `%s` has a customInfo with different types than expected. " +
+                    "Expected customInfo type: `%s`, invoked with model info type: `%s`.",
+                    modelName,
+                    model.customInfo().getClass().getName(),
+                    infoClass.getName()
+                ));
+            }
+        }
+
+        // We just did the check
+        // noinspection unchecked
+        return (Model<D, C, I>) model;
+    }
+
+    public boolean exists(String modelName) {
+        return userModels.containsKey(modelName);
+    }
+
+    public Optional<String> type(String modelName) {
+        return Optional.ofNullable(userModels.get(modelName))
+            .map(Model::algoType);
+    }
+
+    public Model<?, ?, ?> drop(String modelName, boolean failOnMissing) {
+        var storedModel = userModels.remove(modelName);
+
+        if (failOnMissing && storedModel == null) {
+            throw new NoSuchElementException(prettySuggestions(
+                formatWithLocale("Model with name `%s` does not exist.", modelName),
+                modelName,
+                userModels.keySet()
+            ));
+        } else {
+            return storedModel;
+        }
+    }
+
+    public Collection<Model<?, ?, ?>> list() {
+        return userModels.values();
+    }
+
+    public Model<?, ?, ?> list(String modelName) {
+        return getUntyped(modelName);
+    }
+
+    public void removeAllLoadedModels() {
+        userModels.clear();
+    }
+
+    Stream<Model<?, ?, ?>> streamModels() {
+        return userModels.values().stream();
+    }
+
+    public OpenUserCatalog join(OpenUserCatalog other) {
+        userModels.putAll(other.userModels);
+        return this;
+    }
+
+    void checkStorable(String modelName, String modelType) {
+        verifyModelNameIsUnique(modelName);
+        verifyModelsLimit(modelType);
+    }
+
+    private void verifyModelNameIsUnique(String model) {
+        if (exists(model)) {
+            throw new IllegalArgumentException(formatWithLocale(
+                "Model with name `%s` already exists.",
+                model
+            ));
+        }
+    }
+
+    private void verifyModelsLimit(String modelType) {
+        if (!canStoreModel(modelType)) {
+            throw new IllegalArgumentException(
+                formatWithLocale(
+                    "Storing more than `%d` models in the catalog is not available in openGDS. " +
+                    "Please consider licensing the Graph Data Science library. " +
+                    "See documentation at https://neo4j.com/docs/graph-data-science/",
+                    ALLOWED_MODELS_COUNT
+                )
+            );
+        }
+    }
+
+    private boolean canStoreModel(String modelType) {
+        return modelsPerType(modelType) < ALLOWED_MODELS_COUNT;
+    }
+
+    private long modelsPerType(String modelType) {
+        return userModels.values()
+            .stream()
+            .filter(model -> model.algoType().equals(modelType))
+            .count();
+    }
+
+    Model<?, ?, ?> getUntyped(String modelName) {
+        return userModels.get(modelName);
+    }
+
+    private static String formatWithLocale(String template, Object... inputs) {
+        return String.format(Locale.ENGLISH, template, inputs);
+    }
+}
