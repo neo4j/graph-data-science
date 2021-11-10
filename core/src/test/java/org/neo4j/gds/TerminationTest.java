5cef864041fc542f060feaeb43bc7b96ab144256
Martin Junghanns <martin.junghanns@neo4j.com>
2021-11-10 15:26:01 +0100

    Disable TerminationTest
    Co-authored-by: Adam Schill Collberg <adam.schill.collberg@protonmail.com>

--- core/src/test/java/org/neo4j/gds/TerminationTest.java
+++ core/src/test/java/org/neo4j/gds/TerminationTest.java
@@ -20,6 +20,7 @@
 package org.neo4j.gds;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.neo4j.gds.compat.Neo4jProxy;
 import org.neo4j.gds.core.concurrency.ParallelUtil;
@@ -104,6 +105,7 @@ class TerminationTest extends BaseProcTest {
         ParallelUtil.run(runnables, Pools.DEFAULT);
     }
 
+    @Disabled
     @Test
     void test() {
         assertThrows(
