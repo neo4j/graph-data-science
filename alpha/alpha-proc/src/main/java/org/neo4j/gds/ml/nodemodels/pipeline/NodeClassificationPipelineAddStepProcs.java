51263bbf8cd4de46508f0251154efa075adddae1
Ana Fernandez Vidal <ana.vidal@neo4j.com>
2021-11-11 16:26:45 +0100

    Enable addSteps procedures for NC
    Co-authored-by: Jacob Sznajdman <breakanalysis@gmail.com>
    Co-authored-by: Ioannis Panagiotas <ioannis.panagiotas@neotechnology.com>

--- alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/nodemodels/pipeline/NodeClassificationPipelineAddStepProcs.java
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/nodemodels/pipeline/NodeClassificationPipelineAddStepProcs.java
@@ -20,15 +20,19 @@
 package org.neo4j.gds.ml.nodemodels.pipeline;
 
 import org.neo4j.gds.BaseProc;
+import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
 
 import java.util.Map;
 import java.util.stream.Stream;
 
+import static org.neo4j.procedure.Mode.READ;
+
 public class NodeClassificationPipelineAddStepProcs extends BaseProc {
 
-//    @Procedure(name = "gds.alpha.ml.pipeline.nodeClassification.addNodeProperty", mode = READ)
-//    @Description("Add a node property step to an existing node classification pipeline.")
+    @Procedure(name = "gds.alpha.ml.pipeline.nodeClassification.addNodeProperty", mode = READ)
+    @Description("Add a node property step to an existing node classification pipeline.")
     public Stream<PipelineInfoResult> addNodeProperty(
         @Name("pipelineName") String pipelineName,
         @Name("procedureName") String taskName,
@@ -43,8 +47,8 @@ public class NodeClassificationPipelineAddStepProcs extends BaseProc {
         ));
     }
 
-//    @Procedure(name = "gds.alpha.ml.pipeline.nodeClassification.addFeatures", mode = READ)
-//    @Description("Add one or several features to an existing node classification pipeline.")
+    @Procedure(name = "gds.alpha.ml.pipeline.nodeClassification.addFeatures", mode = READ)
+    @Description("Add one or several features to an existing node classification pipeline.")
     public Stream<PipelineInfoResult> addFeatures(
         @Name("pipelineName") String pipelineName,
         @Name("nodeProperties") Object nodeProperties
