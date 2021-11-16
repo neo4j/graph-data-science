efdcdb3000c3f7f4f76f553c0cecda436199c6c1
Max Kießling <max.kiessling@neotechnology.com>
2021-11-16 11:46:38 +0100

    Adapt PageRank procedures to new validation schema

--- proc/centrality/src/main/java/org/neo4j/gds/pagerank/PageRankWriteProc.java
+++ proc/centrality/src/main/java/org/neo4j/gds/pagerank/PageRankWriteProc.java
@@ -27,6 +27,7 @@ import org.neo4j.gds.config.GraphCreateConfig;
 import org.neo4j.gds.core.CypherMapWrapper;
 import org.neo4j.gds.result.AbstractResultBuilder;
 import org.neo4j.gds.results.MemoryEstimateResult;
+import org.neo4j.gds.validation.ValidationConfig;
 import org.neo4j.internal.kernel.api.procs.ProcedureCallContext;
 import org.neo4j.procedure.Description;
 import org.neo4j.procedure.Name;
@@ -83,9 +84,8 @@ public class PageRankWriteProc extends WriteProc<PageRankAlgorithm, PageRankResu
     }
 
     @Override
-    protected void validateConfigsBeforeLoad(GraphCreateConfig graphCreateConfig, PageRankWriteConfig config) {
-        super.validateConfigsBeforeLoad(graphCreateConfig, config);
-        PageRankProc.validateAlgoConfig(config, log);
+    public ValidationConfig<PageRankWriteConfig> getValidationConfig() {
+        return PageRankProc.getValidationConfig(log);
     }
 
     @Override
