b5f0d1dafcda479c1b5ab1c8b041b47c010a03b0
Florentin Dörre <florentin.dorre@neotechnology.com>
2021-11-17 15:21:15 +0100

    Resolve ModelCatalog via neo4j
    As via use the ProcedureContext to resolve the ModelCatalog, we can reuse this provider to inject the ModelCatalog into proc tests.
    
    Co-authored-by: Veselin Nikolov <veselin.nikolov@neotechnology.com>

---
+++ test-utils/src/main/java/org/neo4j/gds/extension/Neo4jModelCatalogResolver.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.extension;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.neo4j.configuration.GraphDatabaseSettings;
+import org.neo4j.dbms.api.DatabaseManagementService;
+import org.neo4j.gds.core.model.ModelCatalog;
+import org.neo4j.kernel.internal.GraphDatabaseAPI;
+
+import java.util.Optional;
+
+import static org.neo4j.gds.extension.ExtensionUtil.injectInstance;
+
+public class Neo4jModelCatalogResolver implements BeforeEachCallback, AfterEachCallback {
+
+    // taken from org.neo4j.test.extension.DbmsSupportController
+    private static final ExtensionContext.Namespace DBMS_NAMESPACE = ExtensionContext.Namespace.create(
+        "org",
+        "neo4j",
+        "dbms"
+    );
+
+    // taken from org.neo4j.test.extension.DbmsSupportController
+    private static final String DBMS_KEY = "service";
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        GraphDatabaseAPI db = (GraphDatabaseAPI) getDbms(context)
+            .map(dbms -> dbms.database(GraphDatabaseSettings.DEFAULT_DATABASE_NAME))
+            .orElseThrow(() -> new IllegalStateException("No database was found."));
+
+        injectFields(context, getModelCatalog(db));
+    }
+
+    private ModelCatalog getModelCatalog(GraphDatabaseAPI db) {
+        return db.getDependencyResolver().resolveDependency(ModelCatalog.class);
+    }
+
+    private Optional<DatabaseManagementService> getDbms(ExtensionContext context) {
+        return Optional.ofNullable(context.getStore(DBMS_NAMESPACE).get(DBMS_KEY, DatabaseManagementService.class));
+    }
+
+    private void injectFields(ExtensionContext context, ModelCatalog modelCatalog) {
+        context.getRequiredTestInstances().getAllInstances().forEach(testInstance -> {
+            injectInstance(
+                testInstance,
+                modelCatalog,
+                ModelCatalog.class
+            );
+        });
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        GraphDatabaseAPI db = (GraphDatabaseAPI) getDbms(context)
+            .map(dbms -> dbms.database(GraphDatabaseSettings.DEFAULT_DATABASE_NAME))
+            .orElseThrow(() -> new IllegalStateException("No database was found."));
+
+        getModelCatalog(db).removeAllLoadedModels();
+    }
+}
