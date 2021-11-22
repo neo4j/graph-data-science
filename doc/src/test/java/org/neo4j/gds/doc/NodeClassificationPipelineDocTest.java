a93a8ef9ec4173078e96964a664bfbabf124d54c
Ana Fernandez Vidal <ana.vidal@neo4j.com>
2021-11-22 19:42:47 +0100

    Start documenting NC pipelines and add tests.
    Co-authored-by: Florentin Dörre <florentin.dorre@neotechnology.com>

---
+++ doc/src/test/java/org/neo4j/gds/doc/NodeClassificationPipelineDocTest.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.doc;
+
+import org.neo4j.gds.catalog.GraphCreateProc;
+import org.neo4j.gds.catalog.GraphStreamNodePropertiesProc;
+import org.neo4j.gds.catalog.GraphStreamRelationshipPropertiesProc;
+import org.neo4j.gds.functions.AsNodeFunc;
+import org.neo4j.gds.ml.linkmodels.pipeline.LinkPredictionPipelineAddStepProcs;
+import org.neo4j.gds.ml.linkmodels.pipeline.LinkPredictionPipelineConfigureParamsProc;
+import org.neo4j.gds.ml.linkmodels.pipeline.LinkPredictionPipelineConfigureSplitProc;
+import org.neo4j.gds.ml.linkmodels.pipeline.LinkPredictionPipelineCreateProc;
+import org.neo4j.gds.ml.linkmodels.pipeline.predict.LinkPredictionPipelineMutateProc;
+import org.neo4j.gds.ml.linkmodels.pipeline.predict.LinkPredictionPipelineStreamProc;
+import org.neo4j.gds.ml.linkmodels.pipeline.train.LinkPredictionPipelineTrainProc;
+import org.neo4j.gds.ml.nodemodels.pipeline.NodeClassificationPipelineAddStepProcs;
+import org.neo4j.gds.ml.nodemodels.pipeline.NodeClassificationPipelineConfigureParamsProc;
+import org.neo4j.gds.ml.nodemodels.pipeline.NodeClassificationPipelineConfigureSplitProc;
+import org.neo4j.gds.ml.nodemodels.pipeline.NodeClassificationPipelineCreateProc;
+import org.neo4j.gds.ml.nodemodels.pipeline.predict.NodeClassificationPipelineMutateProc;
+
+import java.util.List;
+
+class NodeClassificationPipelineDocTest extends DocTestBase {
+
+    @Override
+    List<Class<?>> functions() {
+        return List.of(AsNodeFunc.class);
+    }
+
+    @Override
+    protected List<Class<?>> procedures() {
+        return List.of(
+            NodeClassificationPipelineCreateProc.class,
+            NodeClassificationPipelineMutateProc.class,
+//            NodeClassificationPipelineStreamProc.class,
+//            NodeClassificationPipelineTrainProc.class,
+            NodeClassificationPipelineAddStepProcs.class,
+            NodeClassificationPipelineConfigureSplitProc.class,
+            NodeClassificationPipelineConfigureParamsProc.class,
+            GraphStreamNodePropertiesProc.class,
+            GraphCreateProc.class
+        );
+    }
+
+    @Override
+    protected String adocFile() {
+        return "algorithms/alpha/pipelines/nodeclassification-pipeline/nodeclassification.adoc";
+    }
+
+}
