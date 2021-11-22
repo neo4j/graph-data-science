68098642d2938f74e500c8e9074e3ff5b6704eba
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2021-11-22 12:08:13 +0100

    Add NodeClassification pipeline write proc
    Co-Authored-By: Paul Horn <paul.horn@neotechnology.com>

---
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/nodemodels/pipeline/predict/NodeClassificationPipelineWriteProc.java
@@ -0,0 +1,197 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.ml.nodemodels.pipeline.predict;
+
+import org.neo4j.gds.AlgorithmFactory;
+import org.neo4j.gds.GraphStoreValidation;
+import org.neo4j.gds.WriteProc;
+import org.neo4j.gds.api.GraphStore;
+import org.neo4j.gds.api.nodeproperties.DoubleArrayNodeProperties;
+import org.neo4j.gds.config.GraphCreateConfig;
+import org.neo4j.gds.core.CypherMapWrapper;
+import org.neo4j.gds.core.model.ModelCatalog;
+import org.neo4j.gds.core.write.NodeProperty;
+import org.neo4j.gds.ml.nodemodels.logisticregression.NodeClassificationResult;
+import org.neo4j.gds.ml.nodemodels.logisticregression.NodeLogisticRegressionData;
+import org.neo4j.gds.ml.nodemodels.pipeline.NodeClassificationPipelineModelInfo;
+import org.neo4j.gds.ml.nodemodels.pipeline.NodeClassificationPipelineTrainConfig;
+import org.neo4j.gds.result.AbstractResultBuilder;
+import org.neo4j.gds.results.StandardWriteResult;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Mode;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static org.neo4j.gds.ml.nodemodels.pipeline.NodeClassificationPipelineCompanion.PREDICT_DESCRIPTION;
+import static org.neo4j.gds.utils.StringFormatting.formatWithLocale;
+
+public class NodeClassificationPipelineWriteProc
+    extends WriteProc<
+    NodeClassificationPredictPipelineExecutor,
+    NodeClassificationResult,
+    NodeClassificationPipelineWriteProc.WriteResult,
+    NodeClassificationPredictPipelineWriteConfig>
+{
+    @Context
+    public ModelCatalog modelCatalog;
+
+    @Procedure(name = "gds.alpha.ml.pipeline.nodeClassification.predict.write", mode = Mode.WRITE)
+    @Description(PREDICT_DESCRIPTION)
+    public Stream<WriteResult> write(
+        @Name(value = "graphName") Object graphNameOrConfig,
+        @Name(value = "configuration", defaultValue = "{}") Map<String, Object> configuration
+    ) {
+        return write(compute(graphNameOrConfig, configuration));
+    }
+
+    @Override
+    protected void validateConfigsAfterLoad(
+        GraphStore graphStore,
+        GraphCreateConfig graphCreateConfig,
+        NodeClassificationPredictPipelineWriteConfig config
+    ) {
+        super.validateConfigsAfterLoad(graphStore, graphCreateConfig, config);
+        config.predictedProbabilityProperty()
+            .ifPresent(
+                property -> GraphStoreValidation.validateNodePropertyDoesNotExist(
+                    graphStore,
+                    config.nodeLabelIdentifiers(graphStore),
+                    property
+                )
+            );
+        config.predictedProbabilityProperty().ifPresent(predictedProbabilityProperty -> {
+            if (config.writeProperty().equals(predictedProbabilityProperty)) {
+                throw new IllegalArgumentException(
+                    formatWithLocale(
+                        "Configuration parameters `%s` and `%s` must be different (both were `%s`)",
+                        "writeProperty",
+                        "predictedProbabilityProperty",
+                        predictedProbabilityProperty
+                    )
+                );
+            }
+        });
+
+        var trainConfig = modelCatalog.get(
+            config.username(),
+            config.modelName(),
+            NodeLogisticRegressionData.class,
+            NodeClassificationPipelineTrainConfig.class,
+            NodeClassificationPipelineModelInfo.class
+        ).trainConfig();
+        GraphStoreValidation.validate(graphStore, trainConfig);
+    }
+
+    @Override
+    protected List<NodeProperty> nodePropertyList(ComputationResult<NodeClassificationPredictPipelineExecutor, NodeClassificationResult, NodeClassificationPredictPipelineWriteConfig> computationResult) {
+        var config = computationResult.config();
+        var writeProperty = config.writeProperty();
+        var result = computationResult.result();
+        var classProperties = result.predictedClasses().asNodeProperties();
+        var nodeProperties = new ArrayList<NodeProperty>();
+        nodeProperties.add(NodeProperty.of(writeProperty, classProperties));
+
+        result.predictedProbabilities().ifPresent((probabilityProperties) -> {
+            var properties = new DoubleArrayNodeProperties() {
+                @Override
+                public long size() {
+                    return computationResult.graph().nodeCount();
+                }
+
+                @Override
+                public double[] doubleArrayValue(long nodeId) {
+                    return probabilityProperties.get(nodeId);
+                }
+            };
+
+            nodeProperties.add(NodeProperty.of(
+                config.predictedProbabilityProperty().orElseThrow(),
+                properties
+            ));
+        });
+
+        return nodeProperties;
+    }
+
+
+    @Override
+    protected AbstractResultBuilder<WriteResult> resultBuilder(ComputationResult<NodeClassificationPredictPipelineExecutor, NodeClassificationResult, NodeClassificationPredictPipelineWriteConfig> computeResult) {
+        return new WriteResult.Builder();
+    }
+
+    @Override
+    protected NodeClassificationPredictPipelineWriteConfig newConfig(
+        String username,
+        Optional<String> graphName,
+        Optional<GraphCreateConfig> maybeImplicitCreate,
+        CypherMapWrapper config
+    ) {
+        return NodeClassificationPredictPipelineWriteConfig.of(username, graphName, maybeImplicitCreate, config);
+    }
+
+    @Override
+    protected AlgorithmFactory<NodeClassificationPredictPipelineExecutor, NodeClassificationPredictPipelineWriteConfig> algorithmFactory() {
+        return new NodeClassificationPredictPipelineAlgorithmFactory<>(this, databaseId(), modelCatalog);
+    }
+
+    @SuppressWarnings("unused")
+    public static final class WriteResult extends StandardWriteResult {
+
+        public final long nodePropertiesWritten;
+
+        WriteResult(
+            long createMillis,
+            long computeMillis,
+            long writeMillis,
+            long nodePropertiesWritten,
+            Map<String, Object> configuration
+        ) {
+            super(
+                createMillis,
+                computeMillis,
+                0L,
+                writeMillis,
+                configuration
+            );
+            this.nodePropertiesWritten = nodePropertiesWritten;
+        }
+
+        static class Builder extends AbstractResultBuilder<WriteResult> {
+
+            @Override
+            public WriteResult build() {
+                return new WriteResult(
+                    createMillis,
+                    computeMillis,
+                    writeMillis,
+                    nodePropertiesWritten,
+                    config.toMap()
+                );
+            }
+        }
+    }
+}
