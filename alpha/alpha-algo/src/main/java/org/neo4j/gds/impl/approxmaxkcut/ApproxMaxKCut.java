9444b523a81dc41e9adfe30b53d2b6574ea91e7c
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2021-11-26 10:56:52 +0100

    Move PlaceNodesRandomly into own file

--- alpha/alpha-algo/src/main/java/org/neo4j/gds/impl/approxmaxkcut/ApproxMaxKCut.java
+++ alpha/alpha-algo/src/main/java/org/neo4j/gds/impl/approxmaxkcut/ApproxMaxKCut.java
@@ -31,23 +31,20 @@ import org.neo4j.gds.core.utils.mem.AllocationTracker;
 import org.neo4j.gds.core.utils.paged.HugeAtomicByteArray;
 import org.neo4j.gds.core.utils.paged.HugeAtomicDoubleArray;
 import org.neo4j.gds.core.utils.paged.HugeByteArray;
-import org.neo4j.gds.core.utils.paged.HugeLongArray;
 import org.neo4j.gds.core.utils.partition.Partition;
 import org.neo4j.gds.core.utils.partition.PartitionUtils;
 import org.neo4j.gds.core.utils.progress.tasks.ProgressTracker;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLongArray;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+
+import static org.neo4j.gds.impl.approxmaxkcut.PlaceNodesRandomly.randomNonNegativeLong;
 
 /*
  * Implements a parallelized version of a GRASP (optionally with VNS) maximum k-cut approximation algorithm.
@@ -72,12 +69,12 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
     private final AllocationTracker allocationTracker;
     private final WeightTransformer weightTransformer;
     private final Comparator comparator;
+    private final PlaceNodesRandomly placeNodesRandomly;
     private final HugeByteArray[] candidateSolutions;
     private final AtomicDoubleArray[] costs;
     private final HugeAtomicDoubleArray nodeToCommunityWeights;
     private final HugeAtomicByteArray swapStatus;
     private AtomicLongArray currCardinalities;
-    private final List<Long> rangePartitionActualBatchSizes;
     private final List<Partition> degreePartition;
     private HugeByteArray neighborSolution;
     private AtomicLongArray neighborCardinalities;
@@ -128,10 +125,12 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
             Optional.of(config.minBatchSize())
         );
 
-        this.rangePartitionActualBatchSizes = PartitionUtils.rangePartitionActualBatchSizes(
-            config.concurrency(),
-            graph.nodeCount(),
-            Optional.of(config.minBatchSize())
+        this.placeNodesRandomly = new PlaceNodesRandomly(
+            config,
+            random,
+            graph,
+            executor,
+            progressTracker
         );
     }
 
@@ -198,7 +197,7 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
             var currCandidateSolution = candidateSolutions[currIdx];
             var currCost = costs[currIdx];
 
-            placeNodesRandomly(currCandidateSolution);
+            placeNodesRandomly.compute(currCandidateSolution, currCardinalities);
 
             if (!running()) break;
 
@@ -220,84 +219,6 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
         progressTracker.endSubTask();
 
         return CutResult.of(candidateSolutions[bestIdx], costs[bestIdx].get(0));
-    }
-
-    // Assign the duty of assigning nodes to fill up minimum community size requirements among partitions in a
-    // sufficiently random way.
-    private long[][] minCommunitySizesToPartitions(List<Long> batchSizes) {
-        // Balance granularity of communities' min sizes over partition such that it's sufficiently random while not
-        // requiring too many iterations.
-        double SIZE_TO_CHUNK_FACTOR = batchSizes.size() * 8.0;
-        var chunkSizes = config
-            .minCommunitySizes()
-            .stream()
-            .mapToLong(minSz -> (long) Math.ceil(minSz / SIZE_TO_CHUNK_FACTOR))
-            .toArray();
-
-        var currPartitionCounts = new long[batchSizes.size()];
-        var remainingMinCommunitySizeCounts = new ArrayList<>(config.minCommunitySizes());
-
-        var minCommunitiesPerPartition = new long[config.concurrency()][];
-        Arrays.setAll(minCommunitiesPerPartition, i -> new long[config.k()]);
-
-        var activePartitions = IntStream
-            .range(0, batchSizes.size())
-            .filter(partition -> batchSizes.get(partition) > 0)
-            .boxed()
-            .collect(Collectors.toList());
-        var activeCommunities = IntStream
-            .range(0, config.k())
-            .filter(community -> config.minCommunitySizes().get(community) > 0)
-            .boxed()
-            .collect(Collectors.toList());
-
-        while (!activeCommunities.isEmpty()) {
-            int partitionIdx = random.nextInt(activePartitions.size());
-            int communityIdx = random.nextInt(activeCommunities.size());
-            int partition = activePartitions.get(partitionIdx);
-            int community = activeCommunities.get(communityIdx);
-            long increment = Math.min(
-                Math.min(chunkSizes[community], batchSizes.get(partition) - currPartitionCounts[partition]),
-                remainingMinCommunitySizeCounts.get(community)
-            );
-
-            minCommunitiesPerPartition[partition][community] += increment;
-            currPartitionCounts[partition] += increment;
-            if (currPartitionCounts[partition] == batchSizes.get(partition)) {
-                activePartitions.remove(partitionIdx);
-            }
-
-            remainingMinCommunitySizeCounts.set(community, remainingMinCommunitySizeCounts.get(community) - increment);
-            if (remainingMinCommunitySizeCounts.get(community) == 0) {
-                activeCommunities.remove(communityIdx);
-            }
-        }
-
-        return minCommunitiesPerPartition;
-    }
-
-    private void placeNodesRandomly(HugeByteArray candidateSolution) {
-        assert graph.nodeCount() >= config.k();
-
-        var minCommunitiesPerPartition = minCommunitySizesToPartitions(rangePartitionActualBatchSizes);
-        for (byte i = 0; i < config.k(); i++) {
-            currCardinalities.set(i, config.minCommunitySizes().get(i));
-        }
-
-        var partitionIndex = new AtomicInteger(0);
-        var tasks = PartitionUtils.rangePartition(
-            config.concurrency(),
-            graph.nodeCount(),
-            partition -> new PlaceNodesRandomly(
-                candidateSolution,
-                minCommunitiesPerPartition[partitionIndex.getAndIncrement()],
-                partition
-            ),
-            Optional.of(config.minBatchSize())
-        );
-        progressTracker.beginSubTask();
-        ParallelUtil.runWithConcurrency(config.concurrency(), tasks, executor);
-        progressTracker.endSubTask();
     }
 
     /*
@@ -362,20 +283,6 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
         progressTracker.endSubTask();
 
         progressTracker.endSubTask();
-    }
-
-    // Handle that `Math.abs(Long.MIN_VALUE) == Long.MIN_VALUE`.
-    // `min` is inclusive, and `max` is exclusive.
-    private long randomNonNegativeLong(Random rand, long min, long max) {
-        assert min >= 0;
-        assert max > min;
-
-        long randomNum;
-        do {
-            randomNum = rand.nextLong();
-        } while (randomNum == Long.MIN_VALUE);
-
-        return (Math.abs(randomNum) % (max - min)) + min;
     }
 
     private boolean perturbSolution(
@@ -473,76 +380,6 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
         }
 
         progressTracker.endSubTask();
-    }
-
-    private final class PlaceNodesRandomly implements Runnable {
-
-        private final HugeByteArray candidateSolution;
-        private final long[] minNodesPerCommunity;
-        private final Partition partition;
-
-        PlaceNodesRandomly(
-            HugeByteArray candidateSolution,
-            long[] minNodesPerCommunity,
-            Partition partition
-        ) {
-            this.candidateSolution = candidateSolution;
-            this.minNodesPerCommunity = minNodesPerCommunity;
-            this.partition = partition;
-        }
-
-        @Override
-        public void run() {
-            Random rand;
-            if (config.concurrency() > 1) {
-                rand = ThreadLocalRandom.current();
-            } else {
-                // We want the ability to obtain a deterministic result for single-threaded computations.
-                rand = random;
-            }
-
-            var nodes = shuffle(rand, partition.startNode(), partition.nodeCount());
-
-            // Fill in the nodes that this partition is required to provide to each community.
-            long offset = 0;
-            for (byte i = 0; i < config.k(); i++) {
-                for (long j = 0; j < minNodesPerCommunity[i]; j++) {
-                    candidateSolution.set(nodes.get(offset++), i);
-                }
-            }
-
-            // Assign the rest of the nodes of the partition to random communities.
-            var localCardinalities = new long[config.k()];
-            for (long i = offset; i < nodes.size(); i++) {
-                byte randomCommunity = (byte) rand.nextInt(config.k());
-                localCardinalities[randomCommunity]++;
-                candidateSolution.set(nodes.get(i), randomCommunity);
-            }
-
-            for (int i = 0; i < config.k(); i++) {
-                currCardinalities.addAndGet(i, localCardinalities[i]);
-            }
-
-            progressTracker.logProgress(partition.nodeCount());
-        }
-
-        private HugeLongArray shuffle(Random random, long minInclusive, long length) {
-            HugeLongArray elements = HugeLongArray.newArray(length, AllocationTracker.empty());
-
-            for (long i = 0; i < length; i++) {
-                long nextToAdd = minInclusive + i;
-                long j = randomNonNegativeLong(random, 0, i + 1);
-                if (j == i) {
-                    elements.set(i, nextToAdd);
-                } else {
-                    elements.set(i, elements.get(j));
-                    elements.set(j, nextToAdd);
-                }
-            }
-
-            return elements;
-        }
-
     }
 
     private final class ComputeNodeToCommunityWeights implements Runnable {
