2adcb045b277fa67794f27bdaf230aa9498afede
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2021-11-26 11:26:06 +0100

    Move LocalSearch into own file

--- alpha/alpha-algo/src/main/java/org/neo4j/gds/impl/approxmaxkcut/ApproxMaxKCut.java
+++ alpha/alpha-algo/src/main/java/org/neo4j/gds/impl/approxmaxkcut/ApproxMaxKCut.java
@@ -19,30 +19,18 @@
  */
 package org.neo4j.gds.impl.approxmaxkcut;
 
-import org.apache.commons.lang3.mutable.MutableBoolean;
-import org.apache.commons.lang3.mutable.MutableDouble;
 import org.neo4j.gds.Algorithm;
 import org.neo4j.gds.annotation.ValueClass;
 import org.neo4j.gds.api.Graph;
 import org.neo4j.gds.api.nodeproperties.LongNodeProperties;
-import org.neo4j.gds.core.concurrency.ParallelUtil;
 import org.neo4j.gds.core.utils.AtomicDoubleArray;
 import org.neo4j.gds.core.utils.mem.AllocationTracker;
-import org.neo4j.gds.core.utils.paged.HugeAtomicByteArray;
-import org.neo4j.gds.core.utils.paged.HugeAtomicDoubleArray;
 import org.neo4j.gds.core.utils.paged.HugeByteArray;
-import org.neo4j.gds.core.utils.partition.Partition;
-import org.neo4j.gds.core.utils.partition.PartitionUtils;
 import org.neo4j.gds.core.utils.progress.tasks.ProgressTracker;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
 import java.util.Random;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicLongArray;
-import java.util.stream.Collectors;
 
 import static org.neo4j.gds.impl.approxmaxkcut.PlaceNodesRandomly.randomNonNegativeLong;
 
@@ -60,22 +48,16 @@ import static org.neo4j.gds.impl.approxmaxkcut.PlaceNodesRandomly.randomNonNegat
  */
 public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutResult> {
 
-    private static final double DEFAULT_WEIGHT = 0.0D;
-
     private Graph graph;
     private final Random random;
-    private final ExecutorService executor;
     private final ApproxMaxKCutConfig config;
     private final AllocationTracker allocationTracker;
-    private final WeightTransformer weightTransformer;
     private final Comparator comparator;
     private final PlaceNodesRandomly placeNodesRandomly;
+    private final LocalSearch localSearch;
     private final HugeByteArray[] candidateSolutions;
     private final AtomicDoubleArray[] costs;
-    private final HugeAtomicDoubleArray nodeToCommunityWeights;
-    private final HugeAtomicByteArray swapStatus;
     private AtomicLongArray currCardinalities;
-    private final List<Partition> degreePartition;
     private HugeByteArray neighborSolution;
     private AtomicLongArray neighborCardinalities;
 
@@ -89,10 +71,8 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
         super(progressTracker);
         this.graph = graph;
         this.random = new Random(config.randomSeed().orElseGet(() -> new Random().nextLong()));
-        this.executor = executor;
         this.config = config;
         this.allocationTracker = allocationTracker;
-        this.weightTransformer = config.hasRelationshipWeightProperty() ? weight -> weight : unused -> 1.0D;
         this.comparator = config.minimize() ? (lhs, rhs) -> lhs < rhs : (lhs, rhs) -> lhs > rhs;
 
         // We allocate two arrays in order to be able to compare results between iterations "GRASP style".
@@ -108,22 +88,7 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
         costs[0].set(0, config.minimize() ? Double.MAX_VALUE : Double.MIN_VALUE);
         costs[1].set(0, config.minimize() ? Double.MAX_VALUE : Double.MIN_VALUE);
 
-        // Used by `localSearch()` to keep track of the costs for swapping a node to another community.
-        // TODO: If we had pull-based traversal we could have a |V| sized int array here instead of the |V|*k sized
-        //  double array.
-        this.nodeToCommunityWeights = HugeAtomicDoubleArray.newArray(graph.nodeCount() * config.k(), allocationTracker);
-
-        // Used by `localSearch()` to keep track of whether we can swap a node into another community or not.
-        this.swapStatus = HugeAtomicByteArray.newArray(graph.nodeCount(), allocationTracker);
-
         this.currCardinalities = new AtomicLongArray(config.k());
-
-        this.degreePartition = PartitionUtils.degreePartition(
-            graph,
-            config.concurrency(),
-            partition -> partition,
-            Optional.of(config.minBatchSize())
-        );
 
         this.placeNodesRandomly = new PlaceNodesRandomly(
             config,
@@ -131,6 +96,14 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
             graph,
             executor,
             progressTracker
+        );
+        this.localSearch = new LocalSearch(
+            graph,
+            comparator,
+            config,
+            executor,
+            progressTracker,
+            allocationTracker
         );
     }
 
@@ -157,27 +130,8 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
         }
     }
 
-    /*
-     * Used to improve readability of `localSearch()`.
-     */
-    private static final class NodeSwapStatus {
-        // No thread has tried to swap the node yet, and no incoming neighbor has tried to mark it.
-        static final byte UNTOUCHED = 0;
-        // The node has been swapped to another community, or a thread is currently attempting to swap it.
-        static final byte SWAPPING = 1;
-        // The node has had one of its incoming neighbors swapped (or at least attempted) so the improvement costs may be invalid.
-        static final byte NEIGHBOR = 2;
-
-        private NodeSwapStatus() {}
-    }
-
     @FunctionalInterface
-    private interface WeightTransformer {
-        double accept(double weight);
-    }
-
-    @FunctionalInterface
-    private interface Comparator {
+    interface Comparator {
         boolean accept(double lhs, double rhs);
     }
 
@@ -204,7 +158,7 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
             if (config.vnsMaxNeighborhoodOrder() > 0) {
                 variableNeighborhoodSearch(currIdx);
             } else {
-                localSearch(currCandidateSolution, currCost, currCardinalities);
+                localSearch.compute(currCandidateSolution, currCost, currCardinalities, this::running);
             }
 
             // Store the newly computed candidate solution if it was better than the previous. Then reuse the previous data
@@ -219,70 +173,6 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
         progressTracker.endSubTask();
 
         return CutResult.of(candidateSolutions[bestIdx], costs[bestIdx].get(0));
-    }
-
-    /*
-     * This is a Local Search procedure modified to run more efficiently in parallel. Instead of restarting the while
-     * loop whenever anything has changed in the candidate solution we try to continue as long as we can in order to
-     * avoid the overhead of rescheduling our tasks on threads and possibly losing hot caches.
-     */
-    private void localSearch(
-        HugeByteArray candidateSolution,
-        AtomicDoubleArray cost,
-        AtomicLongArray cardinalities
-    ) {
-        var change = new AtomicBoolean(true);
-
-        progressTracker.beginSubTask();
-
-        progressTracker.beginSubTask();
-        while (change.get() && running()) {
-            nodeToCommunityWeights.setAll(0.0D);
-            var nodeToCommunityWeightTasks = degreePartition.stream()
-                .map(partition ->
-                    new ComputeNodeToCommunityWeights(
-                        graph.concurrentCopy(),
-                        candidateSolution,
-                        partition
-                    )
-                ).collect(Collectors.toList());
-            progressTracker.beginSubTask();
-            ParallelUtil.runWithConcurrency(config.concurrency(), nodeToCommunityWeightTasks, executor);
-            progressTracker.endSubTask();
-
-            swapStatus.setAll(NodeSwapStatus.UNTOUCHED);
-            change.set(false);
-            var swapTasks = degreePartition.stream()
-                .map(partition ->
-                    new SwapForLocalImprovements(
-                        graph.concurrentCopy(),
-                        candidateSolution,
-                        cardinalities,
-                        change,
-                        partition
-                    )
-                ).collect(Collectors.toList());
-            progressTracker.beginSubTask();
-            ParallelUtil.runWithConcurrency(config.concurrency(), swapTasks, executor);
-            progressTracker.endSubTask();
-        }
-        progressTracker.endSubTask();
-
-        cost.set(0, 0);
-        var costTasks = degreePartition.stream()
-            .map(partition ->
-                new ComputeCost(
-                    graph.concurrentCopy(),
-                    candidateSolution,
-                    cost,
-                    partition
-                )
-            ).collect(Collectors.toList());
-        progressTracker.beginSubTask();
-        ParallelUtil.runWithConcurrency(config.concurrency(), costTasks, executor);
-        progressTracker.endSubTask();
-
-        progressTracker.endSubTask();
     }
 
     private boolean perturbSolution(
@@ -347,7 +237,7 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
                 }
             }
 
-            localSearch(neighborSolution, neighborCost, neighborCardinalities);
+            localSearch.compute(neighborSolution, neighborCost, neighborCardinalities, this::running);
 
             if (comparator.accept(neighborCost.get(0), bestCost.get(0))) {
                 var tmpCandidateSolution = bestCandidateSolution;
@@ -380,220 +270,6 @@ public class ApproxMaxKCut extends Algorithm<ApproxMaxKCut, ApproxMaxKCut.CutRes
         }
 
         progressTracker.endSubTask();
-    }
-
-    private final class ComputeNodeToCommunityWeights implements Runnable {
-
-        private final Graph graph;
-        private final HugeByteArray candidateSolution;
-        private final Partition partition;
-
-        ComputeNodeToCommunityWeights(
-            Graph graph,
-            HugeByteArray candidateSolution,
-            Partition partition
-        ) {
-            this.graph = graph;
-            this.candidateSolution = candidateSolution;
-            this.partition = partition;
-        }
-
-        @Override
-        public void run() {
-            // We keep a local tab to minimize atomic accesses.
-            var outgoingImprovementCosts = new double[config.k()];
-
-            partition.consume(nodeId -> {
-                Arrays.fill(outgoingImprovementCosts, 0.0D);
-
-                graph.forEachRelationship(
-                    nodeId,
-                    DEFAULT_WEIGHT,
-                    (sourceNodeId, targetNodeId, weight) -> {
-                        // Loops don't affect the cut cost.
-                        if (sourceNodeId == targetNodeId) return true;
-
-                        double transformedWeight = weightTransformer.accept(weight);
-
-                        outgoingImprovementCosts[candidateSolution.get(targetNodeId)] += transformedWeight;
-
-                        // This accounts for the `nodeToCommunityWeight` for the incoming relationship
-                        // `sourceNodeId -> targetNodeId` from `targetNodeId`'s point of view.
-                        // TODO: We could avoid these cache-unfriendly accesses of the outgoing relationships if we had
-                        //  a way to traverse incoming relationships (pull-based traversal).
-                        nodeToCommunityWeights.getAndAdd(
-                            targetNodeId * config.k() + candidateSolution.get(sourceNodeId),
-                            transformedWeight
-                        );
-
-                        return true;
-                    }
-                );
-
-                for (int i = 0; i < config.k(); i++) {
-                    nodeToCommunityWeights.getAndAdd(nodeId * config.k() + i, outgoingImprovementCosts[i]);
-                }
-            });
-
-            progressTracker.logProgress(partition.nodeCount());
-        }
-    }
-
-    private final class SwapForLocalImprovements implements Runnable {
-
-        private final Graph graph;
-        private final HugeByteArray candidateSolution;
-        private final AtomicLongArray cardinalities;
-        private final AtomicBoolean change;
-        private final Partition partition;
-
-        SwapForLocalImprovements(
-            Graph graph,
-            HugeByteArray candidateSolution,
-            AtomicLongArray cardinalities,
-            AtomicBoolean change,
-            Partition partition
-        ) {
-            this.graph = graph;
-            this.candidateSolution = candidateSolution;
-            this.cardinalities = cardinalities;
-            this.change = change;
-            this.partition = partition;
-        }
-
-        @Override
-        public void run() {
-            var failedSwap = new MutableBoolean();
-            var localChange = new MutableBoolean(false);
-
-            partition.consume(nodeId -> {
-                byte currCommunity = candidateSolution.get(nodeId);
-                byte bestCommunity = bestCommunity(nodeId, currCommunity);
-
-                if (bestCommunity == currCommunity) return;
-
-                if (cardinalities.getAndUpdate(
-                    currCommunity,
-                    currCount -> {
-                        if (currCount > config.minCommunitySizes().get(currCommunity)) {
-                            return currCount - 1;
-                        }
-                        return currCount;
-                    }
-                ) == config.minCommunitySizes().get(currCommunity)) {
-                    return;
-                }
-
-                localChange.setTrue();
-
-                // If no incoming neighbors has marked us as NEIGHBOR, we can attempt to swap the current node.
-                if (!swapStatus.compareAndSet(nodeId, NodeSwapStatus.UNTOUCHED, NodeSwapStatus.SWAPPING)) {
-                    cardinalities.getAndIncrement(currCommunity);
-                    return;
-                }
-
-                failedSwap.setFalse();
-
-                // We check all outgoing neighbors to see that they are not SWAPPING. And if they aren't we mark them
-                // NEIGHBOR so that they don't try to swap later.
-                graph.forEachRelationship(
-                    nodeId,
-                    DEFAULT_WEIGHT,
-                    (sourceNodeId, targetNodeId, weight) -> {
-                        // Loops should not stop us.
-                        if (targetNodeId == sourceNodeId) return true;
-
-                        // We try to mark the outgoing neighbor as NEIGHBOR to make sure it doesn't swap as well.
-                        if (!swapStatus.compareAndSet(
-                            targetNodeId,
-                            NodeSwapStatus.UNTOUCHED,
-                            NodeSwapStatus.NEIGHBOR
-                        )) {
-                            if (swapStatus.get(targetNodeId) != NodeSwapStatus.NEIGHBOR) {
-                                // This outgoing neighbor must be SWAPPING and so we can no longer rely on the computed
-                                // improvement cost of our current node being correct and therefore we abort.
-                                failedSwap.setTrue();
-                                return false;
-                            }
-                        }
-                        return true;
-                    }
-                );
-
-                if (failedSwap.isTrue()) {
-                    // Since we didn't complete the swap for this node we can unmark it as SWAPPING and thus not
-                    // stopping incoming neighbor nodes of swapping because of this node's status.
-                    swapStatus.set(nodeId, NodeSwapStatus.NEIGHBOR);
-                    cardinalities.getAndIncrement(currCommunity);
-                    return;
-                }
-
-                candidateSolution.set(nodeId, bestCommunity);
-                cardinalities.getAndIncrement(bestCommunity);
-            });
-
-            if (localChange.getValue()) change.set(true);
-
-            progressTracker.logProgress(partition.nodeCount());
-        }
-
-        private byte bestCommunity(long nodeId, byte currCommunity) {
-            final long NODE_OFFSET = nodeId * config.k();
-            byte bestCommunity = currCommunity;
-            double bestCommunityWeight = nodeToCommunityWeights.get(NODE_OFFSET + currCommunity);
-
-            for (byte i = 0; i < config.k(); i++) {
-                var nodeToCommunityWeight = nodeToCommunityWeights.get(NODE_OFFSET + i);
-                if (comparator.accept(bestCommunityWeight, nodeToCommunityWeight)) {
-                    bestCommunity = i;
-                    bestCommunityWeight = nodeToCommunityWeight;
-                }
-            }
-
-            return bestCommunity;
-        }
-    }
-
-    private final class ComputeCost implements Runnable {
-
-        private final Graph graph;
-        private final HugeByteArray candidateSolution;
-        private final AtomicDoubleArray cost;
-        private final Partition partition;
-
-        ComputeCost(
-            Graph graph,
-            HugeByteArray candidateSolution,
-            AtomicDoubleArray cost,
-            Partition partition
-        ) {
-            this.graph = graph;
-            this.candidateSolution = candidateSolution;
-            this.cost = cost;
-            this.partition = partition;
-        }
-
-        @Override
-        public void run() {
-            // We keep a local tab to minimize atomic accesses.
-            var localCost = new MutableDouble(0.0);
-
-            partition.consume(nodeId -> {
-                graph.forEachRelationship(
-                    nodeId,
-                    DEFAULT_WEIGHT,
-                    (sourceNodeId, targetNodeId, weight) -> {
-                        if (candidateSolution.get(sourceNodeId) != candidateSolution.get(targetNodeId)) {
-                            localCost.add(weightTransformer.accept(weight));
-                        }
-                        return true;
-                    }
-                );
-            });
-            cost.add(0, localCost.doubleValue());
-
-            progressTracker.logProgress(partition.nodeCount());
-        }
     }
 
     @Override
