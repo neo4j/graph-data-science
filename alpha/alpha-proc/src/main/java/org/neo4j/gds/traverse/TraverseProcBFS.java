9a68d4cb4d50f947141ffe0b0ab8f5c0dca7d1a8
Max Kießling <max.kiessling@neotechnology.com>
2021-12-14 10:40:45 +0100

    Use ComputationResultConsumer in Traversal procs

---
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/traverse/TraverseProcBFS.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.traverse;
+
+import org.neo4j.gds.core.CypherMapWrapper;
+import org.neo4j.gds.impl.traverse.TraverseConfig;
+import org.neo4j.gds.impl.walking.WalkResult;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.neo4j.procedure.Mode.READ;
+
+public class TraverseProcBFS extends TraverseProc {
+
+    private static final String DESCRIPTION =
+        "BFS is a traversal algorithm, which explores all of the neighbor nodes at " +
+        "the present depth prior to moving on to the nodes at the next depth level.";
+
+    @Procedure(name = "gds.alpha.bfs.stream", mode = READ)
+    @Description(DESCRIPTION)
+    public Stream<WalkResult> bfs(
+        @Name(value = "graphName") String graphName,
+        @Name(value = "configuration", defaultValue = "{}") Map<String, Object> configuration
+    ) {
+        var computationResult = compute(graphName, configuration);
+        return computationResultConsumer().consume(computationResult, executionContext());
+    }
+
+    @Override
+    protected TraverseConfig newConfig(String username, CypherMapWrapper userInput) {
+        return TraverseConfig.of(true, userInput);
+    }
+}
