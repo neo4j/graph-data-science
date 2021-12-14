38deb884cbaac05dbe8bbec83466b7ebc5ee2dde
Max Kießling <max.kiessling@neotechnology.com>
2021-12-14 11:18:35 +0100

    Fix expected classes for PregelProcessorTest

--- pregel-proc-generator/src/test/resources/expected/ComputationWriteProc.java
+++ pregel-proc-generator/src/test/resources/expected/ComputationWriteProc.java
@@ -80,7 +80,7 @@ public final class ComputationWriteProc extends PregelWriteProc<ComputationAlgor
     }
 
     @Override
-    protected GraphAlgorithmFactory<ComputationAlgorithm, PregelProcedureConfig> algorithmFactory() {
+    public GraphAlgorithmFactory<ComputationAlgorithm, PregelProcedureConfig> algorithmFactory() {
         return new GraphAlgorithmFactory<ComputationAlgorithm, PregelProcedureConfig>() {
             @Override
             public ComputationAlgorithm build(Graph graph, PregelProcedureConfig configuration,
