f2ff1cf0e08ff0c0542323073e017d7ba57e136a
Max Kießling <max.kiessling@neo4j.com>
2021-12-20 12:22:12 +0100

    Add documentation to the GdsCallable annotation

--- proc/common/src/main/java/org/neo4j/gds/pipeline/GdsCallable.java
+++ proc/common/src/main/java/org/neo4j/gds/pipeline/GdsCallable.java
@@ -24,6 +24,12 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * This annotation is used to identify callables within the GDS library.
+ * All callable is a unit that is surfaced via a Cypher procedure and encapsulates an end-to-end process.
+ * For example an algorithm alone is not a callable, however, an algorithm in combination with a mode,
+ * like mutate or stream, is a callable.
+ */
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 public @interface GdsCallable {
