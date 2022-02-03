b74cb85b5610a1742d401adaf7bd420868a5256e
Martin Junghanns <m.junghanns@mailbox.org>
2022-02-03 12:14:03 +0100

    Add HugeSparseObjectArrayList
    Co-Authored-By: Max Kießling <max.kiessling@neotechnology.com>

---
+++ collections/src/test/java/org/neo4j/gds/collections/arraylist/HugeSparseObjectArrayListTest.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.collections.arraylist;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ThreadLocalRandom;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HugeSparseObjectArrayListTest {
+
+    @Test
+    void shouldSetAndGet() {
+        var random = ThreadLocalRandom.current();
+        var array = HugeSparseObjectArrayList.of(42L, Long.class);
+        long index = random.nextLong(0, 4096L + 1337L);
+        long value = random.nextLong();
+        array.set(index, value);
+        assertThat(array.get(index)).isEqualTo(value);
+    }
+
+    @Test
+    void shouldSetAndGetWithInitialCapacity() {
+        var random = ThreadLocalRandom.current();
+        var array = HugeSparseObjectArrayList.of(42L, 1337 * 1337, Long.class);
+        long index = random.nextLong(0, 4096L + 1337L);
+        long value = random.nextLong();
+        array.set(index, value);
+        assertThat(array.get(index)).isEqualTo(value);
+    }
+
+    @Test
+    void shouldOnlySetIfAbsent() {
+        var random = ThreadLocalRandom.current();
+        var array = HugeSparseObjectArrayList.of(42L, Long.class);
+        long index = random.nextLong(0, 4096L + 1337L);
+        long value = 1337L;
+        assertThat(array.setIfAbsent(index, value)).isTrue();
+        assertThat(array.setIfAbsent(index, value)).isFalse();
+    }
+
+    @Test
+    void shouldReturnDefaultValue() {
+        var array = HugeSparseObjectArrayList.of(42L, Long.class);
+        // > PAGE_SIZE;
+        var index = 224242;
+        array.set(index, 1337L);
+        for (long i = 0; i < index; i++) {
+            assertThat(array.get(i)).isEqualTo(42L);
+        }
+        assertThat(array.get(index)).isEqualTo(1337L);
+    }
+
+    @Test
+    void shouldHaveSaneCapacity() {
+        var array = HugeSparseObjectArrayList.of(42L, Long.class);
+        // > PAGE_SIZE;
+        var index = 224242;
+        long value = 1337L;
+        array.set(index, value);
+        assertThat(array.capacity()).isGreaterThanOrEqualTo(index);
+    }
+
+    @Test
+    void shouldReportContainsCorrectly() {
+        var random = ThreadLocalRandom.current();
+        var array = HugeSparseObjectArrayList.of(42L, Long.class);
+
+        long index = random.nextLong(0, 4096L + 1337L);
+        long value = 1337L;
+        array.set(index, value);
+        assertThat(array.contains(index)).isTrue();
+        assertThat(array.contains(index + 1)).isFalse();
+    }
+
+    @Test
+    void forAll() {
+        var array = HugeSparseObjectArrayList.of(42L, Long.class);
+
+        var expected = Map.of(42L, 1337L, 42 + 8192L, 1338L, 42 + 8192 + 4096L, 1338L);
+        expected.forEach(array::set);
+
+        var actual = new HashMap<>();
+        array.forAll(actual::put);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+}
