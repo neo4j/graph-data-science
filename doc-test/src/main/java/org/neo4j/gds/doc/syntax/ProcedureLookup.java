37c95e20ee5973a13a5f758a3cd5a09ed22833f3
Paul Horn <paul.horn@neotechnology.com>
2022-02-14 15:03:09 +0100

    Add support for user aggregations in Procedure Lookup
    Co-Authored-By: Sören Reichardt <soren.reichardt@neotechnology.com>

--- doc-test/src/main/java/org/neo4j/gds/doc/syntax/ProcedureLookup.java
+++ doc-test/src/main/java/org/neo4j/gds/doc/syntax/ProcedureLookup.java
@@ -19,40 +19,91 @@
  */
 package org.neo4j.gds.doc.syntax;
 
+import org.neo4j.gds.annotation.ReturnType;
+import org.neo4j.gds.annotation.ValueClass;
 import org.neo4j.procedure.Name;
 import org.neo4j.procedure.Procedure;
+import org.neo4j.procedure.UserAggregationFunction;
+import org.neo4j.procedure.UserAggregationResult;
+import org.neo4j.procedure.UserAggregationUpdate;
 import org.reflections.Reflections;
-import org.reflections.scanners.MethodAnnotationsScanner;
+import org.reflections.scanners.Scanners;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.neo4j.gds.utils.StringFormatting.formatWithLocale;
 
 final class ProcedureLookup {
 
-    private final List<Method> procedureMethods;
+    @ValueClass
+    interface AggregationMethods {
+        Method procedure();
 
-    public static ProcedureLookup forPackages(List<String> packages) {
-        var methods = packages.stream()
-            .map(pkg -> new Reflections(pkg, new MethodAnnotationsScanner()))
-            .map(r -> r.getMethodsAnnotatedWith(Procedure.class))
-            .flatMap(Collection::stream)
-            .collect(Collectors.toList());
+        Method update();
 
-        return new ProcedureLookup(methods);
+        Method result();
     }
 
-    private ProcedureLookup(List<Method> procedureMethods) {
+    private final List<Method> procedureMethods;
+    private final List<AggregationMethods> aggregationMethods;
+
+    public static ProcedureLookup forPackages(List<String> packages) {
+        var reflections = packages.stream()
+            .map(pkg -> new Reflections(pkg, Scanners.MethodsAnnotated))
+            .collect(Collectors.toList());
+
+        var methods = reflections.stream()
+            .flatMap(r -> r.getMethodsAnnotatedWith(Procedure.class).stream())
+            .collect(Collectors.toList());
+
+        var aggregationMethods = reflections.stream()
+            .flatMap(r -> {
+                return r.getMethodsAnnotatedWith(UserAggregationFunction.class).stream().map(procedure -> {
+                    var aggregatorType = procedure.getReturnType();
+                    var update = Arrays
+                        .stream(aggregatorType.getDeclaredMethods())
+                        .filter(m -> m.isAnnotationPresent(UserAggregationUpdate.class))
+                        .findFirst()
+                        .orElseThrow(() -> new IllegalArgumentException(
+                            "UserAggregationFunction without a UserAggregationUpdate"));
+                    var result = Arrays
+                        .stream(aggregatorType.getDeclaredMethods())
+                        .filter(m -> m.isAnnotationPresent(UserAggregationResult.class))
+                        .findFirst()
+                        .orElseThrow(() -> new IllegalArgumentException(
+                            "UserAggregationFunction without a UserAggregationResult"));
+                    return ImmutableAggregationMethods.of(
+                        procedure, update, result
+                    );
+                });
+            }).collect(Collectors.toList());
+
+
+        return new ProcedureLookup(methods, aggregationMethods);
+    }
+
+    private ProcedureLookup(List<Method> procedureMethods, List<AggregationMethods> aggregationMethods) {
         this.procedureMethods = procedureMethods;
+        this.aggregationMethods = aggregationMethods;
     }
 
     Class<?> findResultType(String fullyQualifiedProcedureName) {
-        var method = findProcedureMethod(fullyQualifiedProcedureName);
+        var method = tryFindProcedureMethod(fullyQualifiedProcedureName)
+            .or(() -> tryFindAggregationResultMethod(fullyQualifiedProcedureName))
+            .orElseThrow(() -> unknownProcedure(fullyQualifiedProcedureName));
+
+        var returnType = method.getAnnotation(ReturnType.class);
+        if (returnType != null) {
+            return returnType.value();
+        }
+
         var resultType = (ParameterizedType) method.getGenericReturnType();
         var actualTypeArgument = resultType.getActualTypeArguments()[0];
         if (actualTypeArgument instanceof Class) {
@@ -66,7 +117,9 @@ final class ProcedureLookup {
     }
 
     List<String> findArgumentNames(String fullyQualifiedProcedureName) {
-        var method = findProcedureMethod(fullyQualifiedProcedureName);
+        var method = tryFindProcedureMethod(fullyQualifiedProcedureName)
+            .or(() -> tryFindAggregationUpdateMethod(fullyQualifiedProcedureName))
+            .orElseThrow(() -> unknownProcedure(fullyQualifiedProcedureName));
 
         var parameters = method.getParameters();
         var result = new ArrayList<String>(parameters.length);
@@ -81,17 +134,45 @@ final class ProcedureLookup {
         return result;
     }
 
-    private Method findProcedureMethod(String fullyQualifiedProcedureName) {
+    private Optional<Method> tryFindProcedureMethod(String fullyQualifiedProcedureName) {
         return procedureMethods
             .stream()
             .filter(method -> {
                 var annotation = method.getAnnotation(Procedure.class);
-                return annotation.name().equals(fullyQualifiedProcedureName) || annotation.value().equals(fullyQualifiedProcedureName);
+                return annotation.name().equals(fullyQualifiedProcedureName) || annotation
+                    .value()
+                    .equals(fullyQualifiedProcedureName);
             })
-            .findFirst()
-            .orElseThrow(() -> new IllegalArgumentException(formatWithLocale(
-                "Unknown procedure: `%s`",
-                fullyQualifiedProcedureName
-            )));
+            .findFirst();
+    }
+
+    private Optional<Method> tryFindAggregationUpdateMethod(String fullyQualifiedProcedureName) {
+        return findAggregations(fullyQualifiedProcedureName)
+            .map(AggregationMethods::update)
+            .findFirst();
+    }
+
+    private Optional<Method> tryFindAggregationResultMethod(String fullyQualifiedProcedureName) {
+        return findAggregations(fullyQualifiedProcedureName)
+            .map(AggregationMethods::result)
+            .findFirst();
+    }
+
+    private Stream<AggregationMethods> findAggregations(String fullyQualifiedProcedureName) {
+        return aggregationMethods
+            .stream()
+            .filter(aggregation -> {
+                var annotation = aggregation.procedure().getAnnotation(UserAggregationFunction.class);
+                return annotation.name().equals(fullyQualifiedProcedureName) || annotation
+                    .value()
+                    .equals(fullyQualifiedProcedureName);
+            });
+    }
+
+    private IllegalArgumentException unknownProcedure(String fullyQualifiedProcedureName) {
+        return new IllegalArgumentException(formatWithLocale(
+            "Unknown procedure: `%s`",
+            fullyQualifiedProcedureName
+        ));
     }
 }
