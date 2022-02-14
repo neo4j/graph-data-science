2f8cfcf7ad858316588b22002f82773156c7287a
Martin Junghanns <m.junghanns@mailbox.org>
2022-02-14 17:47:16 +0100

    Use generated arrays in ChunkedAdjacencyLists
    Co-authored-by: Max Kießling <max.kiessling@neotechnology.com>

--- core/src/main/java/org/neo4j/gds/core/loading/ChunkedAdjacencyLists.java
+++ core/src/main/java/org/neo4j/gds/core/loading/ChunkedAdjacencyLists.java
@@ -21,9 +21,10 @@ package org.neo4j.gds.core.loading;
 
 import org.eclipse.collections.api.block.function.primitive.LongToLongFunction;
 import org.neo4j.gds.collections.DrainingIterator;
-import org.neo4j.gds.collections.arraylist.HugeSparseIntArrayList;
-import org.neo4j.gds.collections.arraylist.HugeSparseLongArrayList;
-import org.neo4j.gds.collections.arraylist.HugeSparseObjectArrayList;
+import org.neo4j.gds.collections.HugeSparseByteArrayList;
+import org.neo4j.gds.collections.HugeSparseIntList;
+import org.neo4j.gds.collections.HugeSparseLongArrayList;
+import org.neo4j.gds.collections.HugeSparseLongList;
 import org.neo4j.gds.core.utils.mem.MemoryEstimation;
 import org.neo4j.gds.core.utils.mem.MemoryEstimations;
 import org.neo4j.gds.core.utils.mem.MemoryRange;
@@ -49,11 +50,11 @@ public final class ChunkedAdjacencyLists {
     private static final byte[] EMPTY_BYTES = new byte[0];
     private static final long[] EMPTY_PROPERTIES = new long[0];
 
-    private final HugeSparseObjectArrayList<byte[]> targetLists;
-    private final Map<Integer, HugeSparseObjectArrayList<long[]>> properties;
-    private final HugeSparseIntArrayList positions;
-    private final HugeSparseLongArrayList lastValues;
-    private final HugeSparseIntArrayList lengths;
+    private final HugeSparseByteArrayList targetLists;
+    private final Map<Integer, HugeSparseLongArrayList> properties;
+    private final HugeSparseIntList positions;
+    private final HugeSparseLongList lastValues;
+    private final HugeSparseIntList lengths;
 
     public static MemoryEstimation memoryEstimation(long avgDegree, long nodeCount, int propertyCount) {
         // Best case scenario:
@@ -87,7 +88,7 @@ public final class ChunkedAdjacencyLists {
 
     private static long estimateArraysSize(long size, LongToLongFunction primitiveArraySizeEstimator) {
         assert size >= 0;
-        long sizeOfInstance = MemoryUsage.sizeOfInstance(HugeSparseIntArrayList.class);
+        long sizeOfInstance = MemoryUsage.sizeOfInstance(HugeSparseIntList.class);
 
         int numPages = HugeArrays.numberOfPages(size);
 
@@ -104,16 +105,16 @@ public final class ChunkedAdjacencyLists {
     }
 
     private ChunkedAdjacencyLists(int numberOfProperties, long initialCapacity) {
-        this.targetLists = HugeSparseObjectArrayList.of(EMPTY_BYTES, initialCapacity, byte[].class);
-        this.positions = HugeSparseIntArrayList.of(0, initialCapacity);
-        this.lastValues = HugeSparseLongArrayList.of(0, initialCapacity);
-        this.lengths = HugeSparseIntArrayList.of(0, initialCapacity);
+        this.targetLists = HugeSparseByteArrayList.of(EMPTY_BYTES, initialCapacity);
+        this.positions = HugeSparseIntList.of(0, initialCapacity);
+        this.lastValues = HugeSparseLongList.of(0, initialCapacity);
+        this.lengths = HugeSparseIntList.of(0, initialCapacity);
 
 
         if (numberOfProperties > 0) {
             this.properties = new HashMap<>(numberOfProperties);
             for (int i = 0; i < numberOfProperties; i++) {
-                this.properties.put(i, HugeSparseObjectArrayList.of(EMPTY_PROPERTIES, initialCapacity, long[].class));
+                this.properties.put(i, HugeSparseLongArrayList.of(EMPTY_PROPERTIES, initialCapacity));
             }
         } else {
             this.properties = null;
@@ -275,11 +276,11 @@ public final class ChunkedAdjacencyLists {
         private final long[][] propertiesBuffer;
 
         CompositeDrainingIterator(
-            HugeSparseObjectArrayList<byte[]> targets,
-            Map<Integer, HugeSparseObjectArrayList<long[]>> properties,
-            HugeSparseIntArrayList positions,
-            HugeSparseLongArrayList lastValues,
-            HugeSparseIntArrayList lengths
+            HugeSparseByteArrayList targets,
+            Map<Integer, HugeSparseLongArrayList> properties,
+            HugeSparseIntList positions,
+            HugeSparseLongList lastValues,
+            HugeSparseIntList lengths
         ) {
             this.targetListIterator = targets.drainingIterator();
             this.targetListBatch = targetListIterator.drainingBatch();
@@ -298,7 +299,7 @@ public final class ChunkedAdjacencyLists {
                 this.propertyIterators = properties
                     .values()
                     .stream()
-                    .map(HugeSparseObjectArrayList::drainingIterator)
+                    .map(HugeSparseLongArrayList::drainingIterator)
                     .collect(Collectors.toList());
                 this.propertyBatches = propertyIterators
                     .stream()
