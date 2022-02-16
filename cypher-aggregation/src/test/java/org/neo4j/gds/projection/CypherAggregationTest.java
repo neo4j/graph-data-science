26bcc091a3147c6438002165df57728a64af2a64
Sören Reichardt <soren.reichardt@neotechnology.com>
2022-02-16 12:05:55 +0100

    Project node labels in cypher aggregation
    Co-authored-by: Paul Horn <paul.horn@neotechnology.com>

--- cypher-aggregation/src/test/java/org/neo4j/gds/projection/CypherAggregationTest.java
+++ cypher-aggregation/src/test/java/org/neo4j/gds/projection/CypherAggregationTest.java
@@ -24,6 +24,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.neo4j.gds.BaseProcTest;
+import org.neo4j.gds.NodeLabel;
 import org.neo4j.gds.api.Graph;
 import org.neo4j.gds.api.nodeproperties.ValueType;
 import org.neo4j.gds.catalog.GraphDropProc;
@@ -152,6 +153,15 @@ class CypherAggregationTest extends BaseProcTest {
                 assertThat(graph.toOriginalNodeId(nodeId)).isEqualTo(neoNodeId);
             }
         });
+    }
+
+    @Test
+    void testNodeLabels() {
+        runQuery("MATCH (s) WHERE s:A or s:B " +
+                 "RETURN gds.alpha.graph('g', s)");
+
+        var graphStore = GraphStoreCatalog.get("", db.databaseId(), "g").graphStore();
+        assertThat(graphStore.nodeLabels()).extracting(NodeLabel::name).containsExactly("A", "B");
     }
 
     @Test
