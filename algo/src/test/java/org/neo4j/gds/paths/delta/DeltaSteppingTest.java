edeab6ab68f241135737aafe68a113248f11042c
Max Kießling <max.kiessling@neo4j.com>
2022-02-17 21:06:12 +0100

    Fix compilation after rebase
    Co-authored-by: Martin Junghanns <martin.junghanns@neotechnology.com>

--- algo/src/test/java/org/neo4j/gds/paths/delta/DeltaSteppingTest.java
+++ algo/src/test/java/org/neo4j/gds/paths/delta/DeltaSteppingTest.java
@@ -51,7 +51,6 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.neo4j.gds.assertj.Extractors.removingThreadId;
 import static org.neo4j.gds.compat.TestLog.INFO;
