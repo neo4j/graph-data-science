3718d9209949bd8f5a98a8dfbafea7dcbfa32dd1
ioannispan <ioannis.panagiotas@neotechnology.com>
2022-02-21 14:03:36 +0100

    Rewite Centrality Proc

--- alpha/alpha-proc/src/main/java/org/neo4j/gds/centrality/ClosenessCentralityProc.java
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/centrality/ClosenessCentralityProc.java
@@ -19,52 +19,77 @@
  */
 package org.neo4j.gds.centrality;
 
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import org.neo4j.gds.GraphAlgorithmFactory;
-import org.neo4j.gds.NodePropertiesWriter;
-import org.neo4j.gds.api.Graph;
-import org.neo4j.gds.core.CypherMapWrapper;
-import org.neo4j.gds.core.concurrency.Pools;
-import org.neo4j.gds.core.utils.mem.AllocationTracker;
-import org.neo4j.gds.core.utils.progress.tasks.ProgressTracker;
+import org.neo4j.gds.api.NodeProperties;
+import org.neo4j.gds.core.utils.paged.HugeDoubleArray;
+import org.neo4j.gds.executor.ComputationResult;
+import org.neo4j.gds.executor.validation.BeforeLoadValidation;
+import org.neo4j.gds.executor.validation.GraphProjectConfigValidations;
+import org.neo4j.gds.executor.validation.ValidationConfiguration;
 import org.neo4j.gds.impl.closeness.ClosenessCentralityConfig;
+import org.neo4j.gds.impl.closeness.ClosenessCentralityFactory;
 import org.neo4j.gds.impl.closeness.MSClosenessCentrality;
+import org.neo4j.gds.result.AbstractCentralityResultBuilder;
 
-public abstract class ClosenessCentralityProc<PROC_RESULT> extends NodePropertiesWriter<MSClosenessCentrality, MSClosenessCentrality, ClosenessCentralityConfig, PROC_RESULT> {
+import java.util.List;
 
-    protected static final String DESCRIPTION =
+final class ClosenessCentralityProc {
+
+    static final String DESCRIPTION =
         "Closeness centrality is a way of detecting nodes that are " +
         "able to spread information very efficiently through a graph.";
 
+    private ClosenessCentralityProc() {}
 
-    @Override
-    protected ClosenessCentralityConfig newConfig(String username, CypherMapWrapper config) {
-        return ClosenessCentralityConfig.of(config);
+    static <CONFIG extends ClosenessCentralityConfig> NodeProperties nodeProperties(ComputationResult<MSClosenessCentrality, MSClosenessCentrality, CONFIG> computeResult) {
+        return computeResult.result().getCentrality().asNodeProperties();
     }
 
-    @Override
-    public GraphAlgorithmFactory<MSClosenessCentrality, ClosenessCentralityConfig> algorithmFactory() {
-        return new GraphAlgorithmFactory<>() {
-            @Override
-            public String taskName() {
-                return "ClosenessCentrality";
-            }
+    static <CONFIG extends ClosenessCentralityConfig> GraphAlgorithmFactory<MSClosenessCentrality, CONFIG> algorithmFactory() {
+        return new ClosenessCentralityFactory<>();
+    }
 
+    static <PROC_RESULT, CONFIG extends ClosenessCentralityConfig> AbstractCentralityResultBuilder<PROC_RESULT> resultBuilder(
+        AbstractCentralityResultBuilder<PROC_RESULT> procResultBuilder,
+        ComputationResult<MSClosenessCentrality, MSClosenessCentrality, CONFIG> computeResult
+    ) {
+        if (computeResult.result() != null) {
+            HugeDoubleArray centrality = computeResult.result().getCentrality();
+            procResultBuilder.withCentralityFunction(centrality::get);
+        }
+        return procResultBuilder;
+    }
+
+    static <CONFIG extends ClosenessCentralityConfig> ValidationConfiguration<CONFIG> getValidationConfig() {
+        return new ValidationConfiguration<>() {
             @Override
-            public MSClosenessCentrality build(
-                Graph graph,
-                ClosenessCentralityConfig configuration,
-                AllocationTracker allocationTracker,
-                ProgressTracker progressTracker
-            ) {
-                return new MSClosenessCentrality(
-                    graph,
-                    configuration.concurrency(),
-                    configuration.improved(),
-                    allocationTracker,
-                    Pools.DEFAULT,
-                    progressTracker
+            public List<BeforeLoadValidation<CONFIG>> beforeLoadValidations() {
+                return List.of(
+                    new GraphProjectConfigValidations.OrientationValidation<>()
                 );
             }
         };
     }
+
+
 }
