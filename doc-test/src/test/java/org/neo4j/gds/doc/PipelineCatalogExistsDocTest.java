e91a7058e2e0c5ed3b9a6cb44b2079d28940b718
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-03-01 16:56:00 +0100

    Add pipeline catalog exists proc docs
    Co-Authored-By: Florentin Dörre <florentin.dorre@neotechnology.com>

---
+++ doc-test/src/test/java/org/neo4j/gds/doc/PipelineCatalogExistsDocTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.doc;
+
+import org.junit.jupiter.api.AfterEach;
+import org.neo4j.gds.ml.nodemodels.pipeline.NodeClassificationPipelineCreateProc;
+import org.neo4j.gds.ml.pipeline.PipelineCatalog;
+import org.neo4j.gds.pipeline.catalog.PipelineExistsProc;
+
+import java.util.List;
+
+class PipelineCatalogExistsDocTest extends DocTestBase {
+
+    @Override
+    protected List<Class<?>> procedures() {
+        return List.of(
+            PipelineExistsProc.class,
+            NodeClassificationPipelineCreateProc.class
+        );
+    }
+
+    @Override
+    protected String adocFile() {
+        return "pipeline-catalog/pipeline-catalog-exists.adoc";
+    }
+
+    @AfterEach
+    void afterAll() {
+        PipelineCatalog.removeAll();
+    }
+}
