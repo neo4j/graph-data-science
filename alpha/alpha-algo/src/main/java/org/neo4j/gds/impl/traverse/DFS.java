044431bd6d1d9ae02df22a04f8e9f7bf78e4f646
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2022-03-08 14:11:27 +0000

    Extract DFS in its owl class
    - Use the specific Stack data structure
    - Optimize the labelled loop
    
    Co-authored-by: Ioannis Panagiotas <ioannis.panagiotas@neotechnology.com>

---
+++ alpha/alpha-algo/src/main/java/org/neo4j/gds/impl/traverse/DFS.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.impl.traverse;
+
+import com.carrotsearch.hppc.BitSet;
+import com.carrotsearch.hppc.DoubleStack;
+import com.carrotsearch.hppc.LongArrayList;
+import com.carrotsearch.hppc.LongStack;
+import org.neo4j.gds.Algorithm;
+import org.neo4j.gds.api.Graph;
+import org.neo4j.gds.core.utils.progress.tasks.ProgressTracker;
+
+import static org.neo4j.gds.impl.Converters.longToIntConsumer;
+
+public class DFS extends Algorithm<long[]> {
+
+    private final Graph graph;
+    private final long startNodeId;
+    private final ExitPredicate exitPredicate;
+    private final Aggregator aggregatorFunction;
+    private final int nodeCount;
+
+    public DFS(
+        Graph graph,
+        long startNodeId,
+        ExitPredicate exitPredicate,
+        Aggregator aggregatorFunction,
+        ProgressTracker progressTracker
+    ) {
+
+        super(progressTracker);
+        this.graph = graph;
+        this.nodeCount = Math.toIntExact(graph.nodeCount());
+        this.startNodeId = startNodeId;
+        this.exitPredicate = exitPredicate;
+        this.aggregatorFunction = aggregatorFunction;
+    }
+
+    @Override
+    public long[] compute() {
+        progressTracker.beginSubTask();
+        var result = new LongArrayList(nodeCount);
+        var inResult = new BitSet(nodeCount);
+
+        var nodes = new LongStack(nodeCount);
+        var sources = new LongStack(nodeCount);
+        var weights = new DoubleStack(nodeCount);
+        var visited = new BitSet(nodeCount);
+        nodes.push(startNodeId);
+        sources.push(startNodeId);
+        weights.push(.0);
+        visited.set(startNodeId);
+
+        while (!nodes.isEmpty() && running()) {
+            final long source = sources.pop();
+            final long node = nodes.pop();
+            final double weight = weights.pop();
+
+            var exitPredicateResult = exitPredicate.test(source, node, weight);
+            if (exitPredicateResult == ExitPredicate.Result.CONTINUE) {
+                continue;
+            } else {
+                if (!inResult.getAndSet(node)) {
+                    result.add(node);
+                }
+                if (exitPredicateResult == ExitPredicate.Result.BREAK) {
+                    break;
+                }
+            }
+
+            // For disconnected graphs or early termination, this will not reach 100
+            progressTracker.logProgress(graph.degree(node));
+
+            graph.forEachRelationship(
+                node,
+                longToIntConsumer((s, t) -> {
+                    if (!visited.get(t)) {
+                        visited.set(t);
+                        sources.push(s);
+                        nodes.push(t);
+                        weights.push(aggregatorFunction.apply(s, t, weight));
+                    }
+                    return running();
+                })
+            );
+        }
+
+        return result.toArray();
+    }
+
+    @Override
+    public void release() {
+
+    }
+}
---
+++ alpha/alpha-algo/src/test/java/org/neo4j/gds/impl/traverse/DFSTest.java
@@ -0,0 +1,160 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.impl.traverse;
+
+import org.junit.jupiter.api.Test;
+import org.neo4j.gds.Orientation;
+import org.neo4j.gds.core.utils.progress.tasks.ProgressTracker;
+import org.neo4j.gds.extension.GdlExtension;
+import org.neo4j.gds.extension.GdlGraph;
+import org.neo4j.gds.extension.Inject;
+import org.neo4j.gds.extension.TestGraph;
+import org.neo4j.gds.impl.traverse.ExitPredicate.Result;
+
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.neo4j.gds.impl.traverse.Traverse.DEFAULT_AGGREGATOR;
+
+/**
+ *
+ * Graph:
+ *
+ *     (b)   (e)
+ *   2/ 1\ 2/ 1\
+ * >(a)  (d)  ((g))
+ *   1\ 2/ 1\ 2/
+ *    (c)   (f)
+ */
+@GdlExtension
+class DFSTest {
+
+
+    @GdlGraph(graphNamePrefix = "natural")
+    @GdlGraph(graphNamePrefix = "reverse", orientation = Orientation.REVERSE)
+    @GdlGraph(graphNamePrefix = "undirected", orientation = Orientation.UNDIRECTED)
+    private static final String CYPHER =
+        "CREATE " +
+        "  (a:Node)" +
+        ", (b:Node)" +
+        ", (c:Node)" +
+        ", (d:Node)" +
+        ", (e:Node)" +
+        ", (f:Node)" +
+        ", (g:Node)" +
+
+        ", (a)-[:REL {cost:2.0}]->(b)" +
+        ", (a)-[:REL {cost:1.0}]->(c)" +
+        ", (b)-[:REL {cost:1.0}]->(d)" +
+        ", (c)-[:REL {cost:2.0}]->(d)" +
+        ", (d)-[:REL {cost:1.0}]->(e)" +
+        ", (d)-[:REL {cost:2.0}]->(f)" +
+        ", (e)-[:REL {cost:2.0}]->(g)" +
+        ", (f)-[:REL {cost:1.0}]->(g)";
+
+    @GdlGraph(graphNamePrefix = "loop")
+    private static final String LOOP_CYPHER =
+        "CREATE (a)-[:REL]->(b)-[:REL]->(a)";
+
+
+    @Inject
+    private static TestGraph naturalGraph;
+
+    @Inject
+    private static TestGraph reverseGraph;
+
+    @Inject
+    private static TestGraph undirectedGraph;
+
+    @Inject
+    private static TestGraph loopGraph;
+
+    /**
+     * dfs on outgoing rels. until taregt 'a' is reached. the exit function
+     * immediately exits if target is reached
+     */
+    @Test
+    void testDfsToTargetOut() {
+        long source = naturalGraph.toMappedNodeId("a");
+        long target = naturalGraph.toMappedNodeId("g");
+        long[] nodes = new DFS(
+            naturalGraph,
+            source,
+            (s, t, w) -> t == target ? Result.BREAK : Result.FOLLOW,
+            DEFAULT_AGGREGATOR,
+            ProgressTracker.NULL_TRACKER
+        ).compute();
+
+        assertThat(nodes).isEqualTo(
+            Stream.of("a", "c", "d", "f", "g").mapToLong(naturalGraph::toOriginalNodeId).toArray()
+        );
+    }
+
+    /**
+     * dfs on outgoing rels. until taregt 'a' is reached. the exit function
+     * immediately exits if target is reached
+     */
+    @Test
+    void testExitConditionNeverTerminates() {
+        long source = naturalGraph.toMappedNodeId("a");
+        long[] nodes = new DFS(
+            naturalGraph,
+            source,
+            (s, t, w) -> Result.FOLLOW,
+            DEFAULT_AGGREGATOR,
+            ProgressTracker.NULL_TRACKER
+        ).compute();
+
+        assertThat(nodes).isEqualTo(
+            Stream.of("a", "c", "d", "f", "g", "e", "b").mapToLong(naturalGraph::toOriginalNodeId).toArray()
+        );
+    }
+
+    /**
+     * dfs on incoming rels. from 'g' until 'a' is reached. exit function
+     * immediately returns if target is reached
+     */
+    @Test
+    void testDfsToTargetIn() {
+        long source = reverseGraph.toMappedNodeId("g");
+        long target = reverseGraph.toMappedNodeId("a");
+        long[] nodes = new DFS(
+            reverseGraph,
+            source,
+            (s, t, w) -> t == target ? Result.BREAK : Result.FOLLOW,
+            DEFAULT_AGGREGATOR,
+            ProgressTracker.NULL_TRACKER
+        ).compute();
+
+        assertThat(nodes).isEqualTo(
+            Stream.of("g", "f", "d", "c", "a").mapToLong(naturalGraph::toOriginalNodeId).toArray()
+        );    }
+
+    @Test
+    void testDfsOnLoopGraph() {
+        new DFS(
+            loopGraph,
+            0,
+            (s, t, w) -> Result.FOLLOW,
+            Traverse.DEFAULT_AGGREGATOR,
+            ProgressTracker.NULL_TRACKER
+        ).compute();
+    }
+}
