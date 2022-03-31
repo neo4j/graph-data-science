b1afb852096c12e53c7387eb58fde8910f190d89
Max Kießling <max.kiessling@neotechnology.com>
2022-03-31 12:02:37 +0200

    Make sure to map configure start nodes to internal ids in RandomWalk

--- algo/src/main/java/org/neo4j/gds/traversal/RandomWalk.java
+++ algo/src/main/java/org/neo4j/gds/traversal/RandomWalk.java
@@ -24,6 +24,7 @@ import org.apache.commons.lang3.mutable.MutableInt;
 import org.apache.commons.lang3.mutable.MutableLong;
 import org.neo4j.gds.Algorithm;
 import org.neo4j.gds.api.Graph;
+import org.neo4j.gds.config.SourceNodesConfig;
 import org.neo4j.gds.core.concurrency.ParallelUtil;
 import org.neo4j.gds.core.concurrency.Pools;
 import org.neo4j.gds.core.utils.mem.AllocationTracker;
@@ -101,7 +102,7 @@ public final class RandomWalk extends Algorithm<RandomWalk, Stream<long[]>> {
 
         NextNodeSupplier nextNodeSupplier = config.sourceNodes() == null || config.sourceNodes().isEmpty()
             ? new NextNodeSupplier.GraphNodeSupplier(graph.nodeCount())
-            : new NextNodeSupplier.ListNodeSupplier(config.sourceNodes());
+            : NextNodeSupplier.ListNodeSupplier.of(config, graph);
 
         var tasks = IntStream
             .range(0, config.concurrency())
@@ -370,11 +371,16 @@ public final class RandomWalk extends Algorithm<RandomWalk, Stream<long[]>> {
             }
         }
 
-        class ListNodeSupplier implements NextNodeSupplier {
+        final class ListNodeSupplier implements NextNodeSupplier {
             private final List<Long> nodes;
             private final AtomicInteger nextIndex;
 
-            ListNodeSupplier(List<Long> nodes) {
+            static ListNodeSupplier of(SourceNodesConfig config, Graph graph) {
+                var mappedIds = config.sourceNodes().stream().map(graph::toMappedNodeId).collect(Collectors.toList());
+                return new ListNodeSupplier(mappedIds);
+            }
+
+            private ListNodeSupplier(List<Long> nodes) {
                 this.nodes = nodes;
                 this.nextIndex = new AtomicInteger(0);
             }
