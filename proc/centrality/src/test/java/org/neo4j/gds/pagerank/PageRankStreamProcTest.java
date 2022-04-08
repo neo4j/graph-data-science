61470bdf2e891544db480d3311926be5e3386ed4
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2022-04-09 01:23:10 +0200

    Remove unused PR test log

--- proc/centrality/src/test/java/org/neo4j/gds/pagerank/PageRankStreamProcTest.java
+++ proc/centrality/src/test/java/org/neo4j/gds/pagerank/PageRankStreamProcTest.java
@@ -23,7 +23,6 @@ import org.junit.jupiter.api.Test;
 import org.neo4j.gds.AlgoBaseProc;
 import org.neo4j.gds.GdsCypher;
 import org.neo4j.gds.compat.MapUtil;
-import org.neo4j.gds.compat.Neo4jProxy;
 import org.neo4j.gds.core.CypherMapWrapper;
 import org.neo4j.graphdb.QueryExecutionException;
 
@@ -106,13 +105,10 @@ class PageRankStreamProcTest extends PageRankProcTest<PageRankStreamConfig> {
             "cacheWeights", true
         )));
 
-        var log = Neo4jProxy.testLog();
-
-        assertThatThrownBy(() -> {
-            applyOnProcedure(proc -> {
-                ((PageRankStreamProc) proc).stream(DEFAULT_GRAPH_NAME, config.toMap());
-            });
-        })
+        assertThatThrownBy(() -> applyOnProcedure(proc -> ((PageRankStreamProc) proc).stream(
+            DEFAULT_GRAPH_NAME,
+            config.toMap()
+        )))
             .isInstanceOf(IllegalArgumentException.class)
             .hasMessageContaining("Unexpected configuration key: cacheWeights");
     }
