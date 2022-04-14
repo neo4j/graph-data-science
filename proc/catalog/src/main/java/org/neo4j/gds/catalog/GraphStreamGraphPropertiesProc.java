71ae2c31c451300ad8e1d5267b1676bd17b314a0
Max Kießling <max.kiessling@neo4j.com>
2022-04-14 18:32:25 +0200

    Add procedures to stream graph properties

---
+++ proc/catalog/src/main/java/org/neo4j/gds/catalog/GraphStreamGraphPropertiesProc.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.catalog;
+
+import org.neo4j.gds.ProcPreconditions;
+import org.neo4j.gds.api.GraphStore;
+import org.neo4j.gds.config.GraphStreamGraphPropertyConfig;
+import org.neo4j.gds.core.CypherMapWrapper;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.neo4j.procedure.Mode.READ;
+
+public class GraphStreamGraphPropertiesProc extends CatalogProc {
+
+    @Procedure(name = "gds.graph.streamGraphProperty", mode = READ)
+    @Description("Streams the given graph property.")
+    public Stream<PropertyResult> streamProperty(
+        @Name(value = "graphName") String graphName,
+        @Name(value = "graphProperty") String graphProperty,
+        @Name(value = "configuration", defaultValue = "{}") Map<String, Object> configuration
+    ) {
+        ProcPreconditions.check();
+        validateGraphName(graphName);
+
+        // input
+        var cypherConfig = CypherMapWrapper.create(configuration);
+        var config = GraphStreamGraphPropertyConfig.of(
+            graphName,
+            graphProperty,
+            cypherConfig
+        );
+
+        // validation
+        validateConfig(cypherConfig, config);
+        GraphStore graphStore = graphStoreFromCatalog(graphName, config).graphStore();
+        config.validate(graphStore);
+
+        return streamNGraphProperties(graphStore, config);
+    }
+
+    private Stream<PropertyResult> streamNGraphProperties(GraphStore graphStore, GraphStreamGraphPropertyConfig config) {
+        return graphStore
+            .graphPropertyValues(config.graphProperty())
+            .objects()
+            .map(PropertyResult::new);
+    }
+
+    @SuppressWarnings("unused")
+    public static class PropertyResult {
+        public final Object propertyValue;
+
+        PropertyResult(Object propertyValue) {
+            this.propertyValue = propertyValue;
+        }
+    }
+}
---
+++ proc/catalog/src/test/java/org/neo4j/gds/catalog/GraphStreamGraphPropertyProcTest.java
@@ -0,0 +1,151 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.catalog;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.neo4j.gds.BaseProcTest;
+import org.neo4j.gds.GdsCypher;
+import org.neo4j.gds.api.GraphStore;
+import org.neo4j.gds.api.properties.graph.DoubleGraphPropertyValues;
+import org.neo4j.gds.api.properties.graph.GraphPropertyValues;
+import org.neo4j.gds.api.properties.graph.LongArrayGraphPropertyValues;
+import org.neo4j.gds.api.properties.graph.LongGraphPropertyValues;
+import org.neo4j.gds.core.loading.GraphStoreCatalog;
+import org.neo4j.gds.extension.Neo4jGraph;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.DoubleStream;
+import java.util.stream.LongStream;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+import static org.neo4j.gds.compat.MapUtil.map;
+import static org.neo4j.gds.utils.StringFormatting.formatWithLocale;
+
+class GraphStreamGraphPropertyProcTest extends BaseProcTest {
+
+    private static final String TEST_GRAPH_SAME_PROPERTIES = "testGraph";
+    private static final String TEST_GRAPH_DIFFERENT_PROPERTIES = "testGraph2";
+
+    @Neo4jGraph
+    private static final String GRAPH = "CREATE (a)";
+
+    private GraphStore graphStore;
+
+
+    @BeforeEach
+    void setup() throws Exception {
+        registerProcedures(GraphProjectProc.class, GraphStreamGraphPropertiesProc.class);
+
+        runQuery(GdsCypher.call(DEFAULT_GRAPH_NAME).graphProject().withAnyLabel().withAnyRelationshipType().yields());
+
+        graphStore = GraphStoreCatalog.get("", db.databaseId(), DEFAULT_GRAPH_NAME).graphStore();
+    }
+
+    @AfterEach
+    void tearDown() {
+        GraphStoreCatalog.removeAllLoadedGraphs();
+    }
+
+    @ParameterizedTest
+    @MethodSource("org.neo4j.gds.catalog.GraphStreamGraphPropertyProcTest#graphPropertyValues")
+    void streamLoadedNodePropertiesForLabel(GraphPropertyValues values) {
+        graphStore.addGraphProperty("prop", values);
+
+        String graphWriteQuery = formatWithLocale(
+            "CALL gds.graph.streamGraphProperty('%s', 'prop')" + " YIELD propertyValue" + " RETURN propertyValue",
+            DEFAULT_GRAPH_NAME
+        );
+
+        assertCypherResult(
+            graphWriteQuery,
+            values.objects().map(property -> map("propertyValue", property)).collect(Collectors.toList())
+        );
+    }
+
+    static Stream<Arguments> graphPropertyValues() {
+        return Stream.of(arguments(new LongGraphPropertyValues() {
+            @Override
+            public LongStream longValues() {
+                return LongStream.range(0, 10);
+            }
+
+            @Override
+            public long size() {
+                return 10;
+
+            }
+        }), arguments(new DoubleGraphPropertyValues() {
+            @Override
+            public DoubleStream doubleValues() {
+                return DoubleStream.of(42.0, 1.337, Double.NaN);
+            }
+
+            @Override
+            public long size() {
+                return 3;
+            }
+        }), arguments(new LongArrayGraphPropertyValues() {
+            @Override
+            public Stream<long[]> longArrayValues() {
+                return Stream.of(new long[]{1L, 2L, 3L, 4L}, new long[]{42, 1337});
+            }
+
+            @Override
+            public long size() {
+                return 2;
+            }
+        }));
+    }
+    @Test
+    void shouldFailOnNonExistingNodeProperty() {
+        assertError("CALL gds.graph.streamGraphProperty($graph, 'UNKNOWN')",
+            Map.of("graph", DEFAULT_GRAPH_NAME),
+            "The specified graph property 'UNKNOWN' does not exist. The following properties exist in the graph []."
+        );
+    }
+
+    @Test
+    void shouldFailOnNonExistingNodePropertyForSpecificLabel() {
+        LongGraphPropertyValues values = new LongGraphPropertyValues() {
+            @Override
+            public LongStream longValues() {
+                return LongStream.range(0, 10);
+            }
+
+            @Override
+            public long size() {
+                return 10;
+
+            }
+        };
+        graphStore.addGraphProperty("prop", values);
+        assertError("CALL gds.graph.streamGraphProperty($graph, 'porp')",
+            Map.of("graph", DEFAULT_GRAPH_NAME),
+            "The specified graph property 'porp' does not exist. Did you mean: ['prop']."
+        );
+    }
+}
