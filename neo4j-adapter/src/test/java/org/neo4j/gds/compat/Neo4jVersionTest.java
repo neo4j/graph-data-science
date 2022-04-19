5d56810534ccb384b261ad4d3ddbaaadc1b7aeb0
Martin Junghanns <m.junghanns@mailbox.org>
2022-04-19 17:49:42 +0200

    Replace existing aura compat layer with 4.4.6-drop01.0

--- neo4j-adapter/src/test/java/org/neo4j/gds/compat/Neo4jVersionTest.java
+++ neo4j-adapter/src/test/java/org/neo4j/gds/compat/Neo4jVersionTest.java
@@ -36,7 +36,7 @@ class Neo4jVersionTest {
         "4.3.0, V_4_3",
         "4.4.0, V_4_4",
         "4.4.12, V_4_4",
-        "4.4.5-drop01.1, V_4_4_drop10"
+        "4.4.6-drop01.0, V_4_4_drop10"
     })
     void testParse(String input, Neo4jVersion expected) {
         assertEquals(expected.name(), Neo4jVersion.parse(input).name());
