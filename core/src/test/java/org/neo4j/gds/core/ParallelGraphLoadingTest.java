328f9e3308191332c95552ff1c675dd8e8dd48e6
Lasse Westh-Nielsen <lassewesth@gmail.com>
2022-04-21 12:29:16 +0200

    increasing timeout in relationship loading test that consistently fails in CI empirical evidence says it can time out at 2 seconds on my laptop. so let's give it every opportunity to succeed, with buffer and rounding up to make it more robust against system variability

--- core/src/test/java/org/neo4j/gds/core/ParallelGraphLoadingTest.java
+++ core/src/test/java/org/neo4j/gds/core/ParallelGraphLoadingTest.java
@@ -93,7 +93,7 @@ class ParallelGraphLoadingTest extends RandomGraphTestCase {
     }
 
     @Test
-    @Timeout(value = 5)
+    @Timeout(value = 10)
     void shouldLoadAllRelationships() {
         Graph graph = loadEverything();
         runInTransaction(db, tx -> graph.forEachNode(id -> testRelationships(tx, graph, id)));
