8511d776370a6aa0075515d84aa870dab305a518
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2022-05-09 11:48:36 +0100

    Implement LocalMovePhase
    Co-authored-by: Ioannis Panagiotas <ioannis.panagiotas@neotechnology.com>

---
+++ algo/src/main/java/org/neo4j/gds/leiden/LocalMovePhase.java
@@ -0,0 +1,209 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.leiden;
+
+import com.carrotsearch.hppc.LongDoubleHashMap;
+import com.carrotsearch.hppc.LongDoubleMap;
+import com.carrotsearch.hppc.cursors.LongDoubleCursor;
+import org.neo4j.gds.api.Graph;
+import org.neo4j.gds.core.utils.paged.HugeDoubleArray;
+import org.neo4j.gds.core.utils.paged.HugeLongArray;
+
+final class LocalMovePhase {
+
+    private final Graph graph;
+    private final HugeLongArray currentCommunities;
+    //Idx   - nodeId
+    //Value
+    //       - Unweighted : degree of node
+    //       - Weighted   : sum of the relationships weights of the node
+    private final HugeDoubleArray nodeVolumes;
+
+    // Idx   - communityId
+    // Value
+    //       - Unweighted : sum of the degrees of the nodes in the community.
+    //       - Weighted   : sum of the relationship weights of the nodes in the community.
+    // Note: the values also count relationships to nodes outside the community.
+    private final HugeDoubleArray communityVolumes;
+    private final double gamma;
+
+    static LocalMovePhase create(
+        Graph graph,
+        HugeLongArray seedCommunities,
+        HugeDoubleArray nodeVolumes,
+        HugeDoubleArray communityVolumes,
+        double gamma
+    ) {
+
+        return new LocalMovePhase(
+            graph,
+            seedCommunities,
+            nodeVolumes,
+            communityVolumes,
+            gamma
+        );
+    }
+
+    private LocalMovePhase(
+        Graph graph,
+        HugeLongArray seedCommunities,
+        HugeDoubleArray nodeVolumes,
+        HugeDoubleArray communityVolumes,
+        double gamma
+    ) {
+        this.graph = graph;
+        this.currentCommunities = seedCommunities;
+        this.gamma = gamma;
+        this.nodeVolumes = nodeVolumes;
+        this.communityVolumes = communityVolumes;
+    }
+
+    public LocalMovePhaseResult run() {
+        var queue = createQueue();
+
+        while (!queue.isEmpty()) {
+            long nodeId = queue.remove();
+            long currentNodeCommunityId = currentCommunities.get(nodeId);
+            double currentNodeVolume = nodeVolumes.get(nodeId);
+
+            // Remove the current node degree from the sum of degrees of its community
+            communityVolumes.addTo(currentNodeCommunityId, -currentNodeVolume);
+
+            var communityRelationshipWeights = communityRelationshipWeights(nodeId);
+
+            // Compute the "modularity" for the current node and current community
+            double currentBestGain = communityRelationshipWeights.get(currentNodeCommunityId) - currentNodeVolume * communityVolumes.get(
+                currentNodeCommunityId) * gamma;
+
+            long bestCommunityId = currentNodeCommunityId;
+
+            bestCommunityId = findBestCommunity(
+                communityRelationshipWeights,
+                currentBestGain,
+                currentNodeVolume,
+                bestCommunityId
+            );
+
+            tryToMoveNode(
+                queue,
+                nodeId,
+                currentNodeCommunityId,
+                currentNodeVolume,
+                bestCommunityId
+            );
+        }
+
+        return new LocalMovePhaseResult(currentCommunities, communityVolumes);
+    }
+
+    private long findBestCommunity(
+        LongDoubleMap communityRelationshipWeights,
+        double currentBestGain,
+        double currentNodeVolume,
+        long bestCommunityId
+    ) {
+
+        for(LongDoubleCursor cursor: communityRelationshipWeights) {
+            long candidateCommunityId = cursor.key;
+            double candidateCommunityRelationshipsWeight = cursor.value;
+
+            // Compute the modularity gain for the candidate community
+            double modularityGain =
+                candidateCommunityRelationshipsWeight - currentNodeVolume * communityVolumes.get(candidateCommunityId) * gamma;
+
+            boolean improves = modularityGain > currentBestGain
+                               || (Double.compare(modularityGain, currentBestGain) == 0
+                                   && candidateCommunityId < bestCommunityId && modularityGain > 0);
+            if (improves) {
+                bestCommunityId = candidateCommunityId;
+                currentBestGain = modularityGain;
+            }
+        }
+        return bestCommunityId;
+    }
+
+    private void tryToMoveNode(
+        NodesQueue queue,
+        long nodeId,
+        long currentNodeCommunityId,
+        double currentNodeVolume,
+        long bestCommunityId
+    ) {
+        boolean shouldChangeCommunity = bestCommunityId != currentNodeCommunityId;
+        if (shouldChangeCommunity) {
+            moveNodeToNewCommunity(
+                nodeId,
+                bestCommunityId,
+                currentNodeVolume
+            );
+
+            visitNeighboursAfterMove(nodeId, queue, bestCommunityId);
+        } else {
+            // We didn't move the node => re-add its degree to its current community sum of degrees
+            communityVolumes.addTo(currentNodeCommunityId, currentNodeVolume);
+        }
+    }
+
+    private void moveNodeToNewCommunity(
+        long nodeId,
+        long newCommunityId,
+        double currentNodeVolume
+    ) {
+        currentCommunities.set(nodeId, newCommunityId);
+
+
+        communityVolumes.addTo(newCommunityId, currentNodeVolume);
+    }
+
+    private LongDoubleMap communityRelationshipWeights(long nodeId) {
+        var communityRelationshipWeights = new LongDoubleHashMap();
+        graph.forEachRelationship(nodeId, 1.0, (s, t, relationshipWeight) -> {
+            long tCommunity = currentCommunities.get(t);
+            communityRelationshipWeights.addTo(tCommunity, relationshipWeight);
+
+            return true;
+        });
+        return communityRelationshipWeights;
+    }
+
+    // all neighbours of the node that do not belong to the node’s new community
+    // and that are not yet in the queue are added to the rear of the queue
+    private void visitNeighboursAfterMove(long nodeId, NodesQueue queue, long movedToCommunityId) {
+        graph.forEachRelationship(nodeId, (s, t) -> {
+            long tCommunity = currentCommunities.get(t);
+            boolean shouldAddInQueue = !queue.contains(t) && tCommunity != movedToCommunityId;
+            if (shouldAddInQueue) {
+                queue.add(t);
+            }
+            return true;
+        });
+    }
+
+    private NodesQueue createQueue() {
+        var queue = new NodesQueue(graph.nodeCount());
+
+        graph.forEachNode(nodeId -> {
+            queue.add(nodeId);
+            return true;
+        });
+        return queue;
+    }
+
+}
---
+++ algo/src/test/java/org/neo4j/gds/leiden/LocalMovePhaseKarateTest.java
@@ -0,0 +1,194 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.leiden;
+
+import org.junit.jupiter.api.Test;
+import org.neo4j.gds.Orientation;
+import org.neo4j.gds.core.utils.paged.HugeDoubleArray;
+import org.neo4j.gds.core.utils.paged.HugeLongArray;
+import org.neo4j.gds.extension.GdlExtension;
+import org.neo4j.gds.extension.GdlGraph;
+import org.neo4j.gds.extension.IdFunction;
+import org.neo4j.gds.extension.Inject;
+import org.neo4j.gds.extension.TestGraph;
+
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@GdlExtension
+class LocalMovePhaseKarateTest {
+
+    @GdlGraph(orientation = Orientation.UNDIRECTED)
+    private static final String DB_CYPHER =
+        "CREATE " +
+        "(a1: Node)," +
+        "(a2: Node)," +
+        "(a3: Node)," +
+        "(a4: Node)," +
+        "(a5: Node)," +
+        "(a6: Node)," +
+        "(a7: Node)," +
+        "(a8: Node)," +
+        "(a9: Node)," +
+        "(a10: Node)," +
+        "(a11: Node)," +
+        "(a12: Node)," +
+        "(a13: Node)," +
+        "(a14: Node)," +
+        "(a15: Node)," +
+        "(a16: Node)," +
+        "(a17: Node)," +
+        "(a18: Node)," +
+        "(a19: Node)," +
+        "(a20: Node)," +
+        "(a21: Node)," +
+        "(a22: Node)," +
+        "(a23: Node)," +
+        "(a24: Node)," +
+        "(a25: Node)," +
+        "(a26: Node)," +
+        "(a27: Node)," +
+        "(a28: Node)," +
+        "(a29: Node)," +
+        "(a30: Node)," +
+        "(a31: Node)," +
+        "(a32: Node)," +
+        "(a33: Node)," +
+        "(a34: Node)," +
+        "(a1)-[:R]->(a2)," +
+        "(a1)-[:R]->(a3)," +
+        "(a2)-[:R]->(a3)," +
+        "(a1)-[:R]->(a4)," +
+        "(a2)-[:R]->(a4)," +
+        "(a3)-[:R]->(a4)," +
+        "(a1)-[:R]->(a5)," +
+        "(a1)-[:R]->(a6)," +
+        "(a1)-[:R]->(a7)," +
+        "(a5)-[:R]->(a7)," +
+        "(a6)-[:R]->(a7)," +
+        "(a1)-[:R]->(a8)," +
+        "(a2)-[:R]->(a8)," +
+        "(a3)-[:R]->(a8)," +
+        "(a4)-[:R]->(a8)," +
+        "(a1)-[:R]->(a9)," +
+        "(a3)-[:R]->(a9)," +
+        "(a3)-[:R]->(a10)," +
+        "(a1)-[:R]->(a11)," +
+        "(a5)-[:R]->(a11)," +
+        "(a6)-[:R]->(a11)," +
+        "(a1)-[:R]->(a12)," +
+        "(a1)-[:R]->(a13)," +
+        "(a4)-[:R]->(a13)," +
+        "(a1)-[:R]->(a14)," +
+        "(a2)-[:R]->(a14)," +
+        "(a3)-[:R]->(a14)," +
+        "(a4)-[:R]->(a14)," +
+        "(a6)-[:R]->(a17)," +
+        "(a7)-[:R]->(a17)," +
+        "(a1)-[:R]->(a18)," +
+        "(a2)-[:R]->(a18)," +
+        "(a1)-[:R]->(a20)," +
+        "(a2)-[:R]->(a20)," +
+        "(a1)-[:R]->(a22)," +
+        "(a2)-[:R]->(a22)," +
+        "(a24)-[:R]->(a26)," +
+        "(a25)-[:R]->(a26)," +
+        "(a3)-[:R]->(a28) ," +
+        "(a24)-[:R]->(a28)," +
+        "(a25)-[:R]->(a28)," +
+        "(a3)-[:R]->(a29) ," +
+        "(a24)-[:R]->(a30)," +
+        "(a27)-[:R]->(a30)," +
+        "(a2)-[:R]->(a31) ," +
+        "(a9)-[:R]->(a31) ," +
+        "(a1)-[:R]->(a32) ," +
+        "(a25)-[:R]->(a32)," +
+        "(a26)-[:R]->(a32)," +
+        "(a29)-[:R]->(a32)," +
+        "(a3)-[:R]->(a33) ," +
+        "(a9)-[:R]->(a33) ," +
+        "(a15)-[:R]->(a33)," +
+        "(a16)-[:R]->(a33)," +
+        "(a19)-[:R]->(a33)," +
+        "(a21)-[:R]->(a33)," +
+        "(a23)-[:R]->(a33)," +
+        "(a24)-[:R]->(a33)," +
+        "(a30)-[:R]->(a33)," +
+        "(a31)-[:R]->(a33)," +
+        "(a32)-[:R]->(a33)," +
+        "(a9)-[:R]->(a34) ," +
+        "(a10)-[:R]->(a34)," +
+        "(a14)-[:R]->(a34)," +
+        "(a15)-[:R]->(a34)," +
+        "(a16)-[:R]->(a34)," +
+        "(a19)-[:R]->(a34)," +
+        "(a20)-[:R]->(a34)," +
+        "(a21)-[:R]->(a34)," +
+        "(a23)-[:R]->(a34)," +
+        "(a24)-[:R]->(a34)," +
+        "(a27)-[:R]->(a34)," +
+        "(a28)-[:R]->(a34)," +
+        "(a29)-[:R]->(a34)," +
+        "(a30)-[:R]->(a34)," +
+        "(a31)-[:R]->(a34)," +
+        "(a32)-[:R]->(a34)," +
+        "(a33)-[:R]->(a34)";
+
+    @Inject
+    private TestGraph graph;
+
+    @Inject
+    private IdFunction idFunction;
+
+    @Test
+    void testLocalMovePhase() {
+        var seed = HugeLongArray.newArray(graph.nodeCount());
+        seed.setAll(nodeId -> nodeId);
+
+        var nodeVolumes = HugeDoubleArray.newArray(graph.nodeCount());
+        nodeVolumes.setAll(graph::degree);
+
+        var communityVolumes = nodeVolumes.copyOf(graph.nodeCount());
+
+        double gamma = 1.0 / graph.relationshipCount();
+
+        var localMovePhase = LocalMovePhase.create(graph, seed, nodeVolumes, communityVolumes, gamma);
+
+        var communities = localMovePhase.run().communities();
+
+        var communitiesMap = LongStream
+            .range(0, communities.size())
+            .mapToObj(v -> "a" + (v + 1))
+            .collect(Collectors.groupingBy(v -> communities.get(idFunction.of(v))));
+
+        assertThat(communitiesMap.values())
+            .satisfiesExactlyInAnyOrder(
+                community -> assertThat(community).containsExactlyInAnyOrder("a5", "a11"),
+                community -> assertThat(community).containsExactlyInAnyOrder("a3", "a4", "a8", "a10", "a13", "a14"),
+                community -> assertThat(community).containsExactlyInAnyOrder("a6", "a7", "a17"),
+                community -> assertThat(community).containsExactlyInAnyOrder("a1", "a2", "a12", "a18", "a20", "a22"),
+                community -> assertThat(community).containsExactlyInAnyOrder("a24", "a25", "a26", "a28", "a29", "a32"),
+                community -> assertThat(community).containsExactlyInAnyOrder("a9", "a15", "a16", "a19", "a21", "a23", "a27", "a30", "a31", "a33", "a34")
+            );
+    }
+
+}
