690955e6bbbd914e6151c38a7609d08f932fb636
ioannispan <ioannis.panagiotas@neotechnology.com>
2022-05-12 12:40:14 +0200

    Put everything together in the flow of the algorithm

--- algo/src/main/java/org/neo4j/gds/kmeans/Kmeans.java
+++ algo/src/main/java/org/neo4j/gds/kmeans/Kmeans.java
@@ -24,9 +24,7 @@ import org.neo4j.gds.Algorithm;
 import org.neo4j.gds.api.Graph;
 import org.neo4j.gds.api.properties.nodes.NodePropertyValues;
 import org.neo4j.gds.core.concurrency.ParallelUtil;
-import org.neo4j.gds.core.utils.Intersections;
 import org.neo4j.gds.core.utils.paged.HugeIntArray;
-import org.neo4j.gds.core.utils.partition.Partition;
 import org.neo4j.gds.core.utils.partition.PartitionUtils;
 import org.neo4j.gds.core.utils.progress.tasks.ProgressTracker;
 
@@ -105,14 +103,16 @@ public class Kmeans extends Algorithm<KmeansResult> {
             return ImmutableKmeansResult.of(communities);
         }
         long nodeCount = graph.nodeCount();
-        double[][] clusterCenters = new double[k][dimensions];
+
+        ClusterManager clusterManager = ClusterManager.createClusterManager(nodePropertyValues, dimensions, k);
+
         communities.setAll(v -> UNASSIGNED);
 
         var tasks = PartitionUtils.rangePartition(
             concurrency,
             nodeCount,
-            partition -> new KmeansTask(
-                clusterCenters,
+            partition -> KmeansTask.createTask(
+                clusterManager,
                 nodePropertyValues,
                 communities,
                 k,
@@ -130,7 +130,8 @@ public class Kmeans extends Algorithm<KmeansResult> {
         //Temporary:
         KmeansSampler sampler = new KmeansUniformSampler();
         List<Long> initialCenterIds = sampler.sampleClusters(random, nodePropertyValues, nodeCount, k);
-        assignCenters(clusterCenters, initialCenterIds, dimensions);
+        clusterManager.assignCenter(initialCenterIds);
+
         //
         int iteration = 0;
         while (true) {
@@ -144,35 +145,20 @@ public class Kmeans extends Algorithm<KmeansResult> {
             if (kmeansIterationStopper.shouldQuit(swaps, ++iteration)) {
                 break;
             }
-            recomputeCenters(clusterCenters, tasks);
+            recomputeCenters(clusterManager, tasks);
 
         }
         progressTracker.endSubTask();
         return ImmutableKmeansResult.of(communities);
     }
 
-    private void recomputeCenters(double[][] clusterCenters, List<KmeansTask> tasks) {
-        long[] nodesInCluster = new long[k];
-        for (int centerId = 0; centerId < k; ++centerId) {
-            for (int dimension = 0; dimension < dimensions; ++dimension) {
-                clusterCenters[centerId][dimension] = 0.0;
-            }
-        }
-        for (KmeansTask task : tasks) {
-            for (int centerId = 0; centerId < k; ++centerId) {
-                var centerContribution = task.getCenterContribution(centerId);
-                nodesInCluster[centerId] += task.getNumAssignedAtCenter(centerId);
-                for (int dimension = 0; dimension < dimensions; ++dimension) {
-                    clusterCenters[centerId][dimension] += centerContribution[dimension];
-                }
-            }
-        }
-        for (int centerId = 0; centerId < k; ++centerId) {
-            for (int dimension = 0; dimension < dimensions; ++dimension) {
-                clusterCenters[centerId][dimension] /= (double) nodesInCluster[centerId];
-            }
-        }
+    private void recomputeCenters(ClusterManager clusterManager, List<KmeansTask> tasks) {
+        clusterManager.reset();
 
+        for (KmeansTask task : tasks) {
+            clusterManager.considerTaskForUpdate(task);
+        }
+        clusterManager.normalizeClusters();
     }
 
     @Override
@@ -196,95 +182,6 @@ public class Kmeans extends Algorithm<KmeansResult> {
         }
     }
 
-    private static final class KmeansTask implements Runnable {
 
-        private final ProgressTracker progressTracker;
-        private final Partition partition;
-        private final double[][] communityCoordinateSums;
-        private final NodePropertyValues nodePropertyValues;
-        private final HugeIntArray communities;
-        private final long[] communitySizes;
-        private final double[][] clusterCenters;
-        private final int k;
-        private final int dimensions;
 
-        private long swaps;
-
-        KmeansTask(
-            double[][] clusterCenters,
-            NodePropertyValues nodePropertyValues,
-            HugeIntArray communities,
-            int k,
-            int dimensions,
-            Partition partition,
-            ProgressTracker progressTracker
-        ) {
-            this.progressTracker = progressTracker;
-            this.partition = partition;
-            this.clusterCenters = clusterCenters;
-            this.communityCoordinateSums = new double[k][dimensions];
-            this.communitySizes = new long[k];
-            this.k = k;
-            this.dimensions = dimensions;
-            this.nodePropertyValues = nodePropertyValues;
-            this.communities = communities;
-        }
-
-        double[] getCenterContribution(int ith) {
-            return communityCoordinateSums[ith];
-        }
-
-        long getNumAssignedAtCenter(int ith) {
-            return communitySizes[ith];
-        }
-
-        long getSwaps() {
-            return swaps;
-        }
-
-        private double euclidean(double[] left, double[] right) {
-            return Math.sqrt(Intersections.sumSquareDelta(left, right, right.length));
-        }
-
-        @Override
-        public void run() {
-            var startNode = partition.startNode();
-            long endNode = startNode + partition.nodeCount();
-            swaps = 0;
-
-            for (int community = 0; community < k; ++community) {
-                communitySizes[community] = 0;
-                for (int dimension = 0; dimension < dimensions; ++dimension) {
-                    communityCoordinateSums[community][dimension] = 0.0;
-                }
-            }
-
-            for (long nodeId = startNode; nodeId < endNode; nodeId++) {
-                int community = 0;
-                var property = nodePropertyValues.doubleArrayValue(nodeId);
-                double smallestDistance = Double.MAX_VALUE;
-                for (int centerId = 0; centerId < k; ++centerId) {
-                    double distance = euclidean(property, clusterCenters[centerId]);
-                    if (Double.compare(distance, smallestDistance) < 0) {
-                        smallestDistance = distance;
-                        community = centerId;
-                    }
-                }
-                communitySizes[community]++;
-                int previousCommunity = communities.get(nodeId);
-                if (community != previousCommunity) {
-                    swaps++;
-                }
-                //Note for potential improvement : This is potentially costly when clusters have somewhat stabilized.
-                //Because we keep adding the same nodes to the same clusters. Perhaps instead of making the sum 0
-                //we keep as is and do a subtraction when a node changes its cluster.
-                //On that note,  maybe we can skip stable communities (i.e., communities that did not change between one iteration to another)
-                // or avoid calculating their distance from other nodes etc...
-                communities.set(nodeId, community);
-                for (int j = 0; j < dimensions; ++j) {
-                    communityCoordinateSums[community][j] += property[j];
-                }
-            }
-        }
-    }
 }
