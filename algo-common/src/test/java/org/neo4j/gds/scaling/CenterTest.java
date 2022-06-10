9323e4e2c7751caf7b478869e99e0ca75ef6b4e4
ioannispan <ioannis.panagiotas@neotechnology.com>
2022-06-10 11:23:07 +0200

    Add tests

---
+++ algo-common/src/test/java/org/neo4j/gds/scaling/CenterTest.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.scaling;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.neo4j.gds.api.properties.nodes.NodePropertyValues;
+import org.neo4j.gds.core.concurrency.Pools;
+import org.neo4j.gds.nodeproperties.DoubleTestPropertyValues;
+
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CenterTest {
+
+    private static Stream<Arguments> properties() {
+        return Stream.of(
+            Arguments.of(
+                new DoubleTestPropertyValues(nodeId -> nodeId),
+                4.5,
+                new double[]{-4.5, -3.5, -2.5, -1.5, -0.5, 0.5, 1.5, 2.5, 3.5, 4.5}
+            ),
+            Arguments.of(
+                new DoubleTestPropertyValues(nodeId -> -nodeId),
+                -4.5,
+                new double[]{4.5, 3.5, 2.5, 1.5, 0.5, -0.5, -1.5, -2.5, -3.5, -4.5}
+            )
+
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("properties")
+    void normalizes(NodePropertyValues properties, double avg, double[] expected) {
+        var scaler = (Center) Center.initialize(properties, 10, 1, Pools.DEFAULT);
+
+        assertThat(scaler.avg).isEqualTo(avg);
+
+        double[] actual = IntStream.range(0, 10).mapToDouble(scaler::scaleProperty).toArray();
+        assertThat(actual).containsSequence(expected);
+    }
+
+}
