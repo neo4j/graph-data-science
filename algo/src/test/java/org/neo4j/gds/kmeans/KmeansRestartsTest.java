ee18743e0242340a44afffc9e4455b2f2c6b1080
ioannispan <ioannis.panagiotas@neotechnology.com>
2022-06-16 17:22:20 +0200

    Fix test
    Co-authored-by: Jonatan Jäderberg <jonatan.jaderberg@gmail.com>

--- algo/src/test/java/org/neo4j/gds/kmeans/KmeansRestartsTest.java
+++ algo/src/test/java/org/neo4j/gds/kmeans/KmeansRestartsTest.java
@@ -46,8 +46,9 @@ class KmeansRestartsTest {
     @Inject
     private IdFunction idFunction;
 
+
     @Test
-    void shouldChangeWithRestarts() {
+    void shouldWorkWithoutRestarts() {
         var kmeansConfig = ImmutableKmeansStreamConfig.builder()
             .nodeProperty("kmeans")
             .concurrency(1)
@@ -58,26 +59,29 @@ class KmeansRestartsTest {
         var kmeansContext = ImmutableKmeansContext.builder().build();
 
         var kmeans = Kmeans.createKmeans(graph, kmeansConfig, kmeansContext);
-        var result1 = kmeans.compute();
-        var communities1 = result1.communities();
-        var distances1 = result1.distanceFromCenter();
-        var averageDistance1 = result1.averageDistanceToCentroid();
-        var centers1 = result1.centers();
+        var result = kmeans.compute();
+        var communities = result.communities();
+        var distances = result.distanceFromCenter();
+        var averageDistance = result.averageDistanceToCentroid();
+        var centers = result.centers();
 
-        assertThat(communities1.get(0)).isNotEqualTo(communities1.get(1));
-        assertThat(communities1.get(2)).isEqualTo(communities1.get(1));
+        assertThat(communities.get(0)).isNotEqualTo(communities.get(1));
+        assertThat(communities.get(2)).isEqualTo(communities.get(1));
 
-        assertThat(distances1.get(0)).isEqualTo(0d);
-        assertThat(distances1.get(1)).isCloseTo(49.45, Offset.offset(1e-4));
-        assertThat(distances1.get(2)).isCloseTo(49.45, Offset.offset(1e-4));
+        assertThat(distances.get(0)).isEqualTo(0d);
+        assertThat(distances.get(1)).isCloseTo(49.45, Offset.offset(1e-4));
+        assertThat(distances.get(2)).isCloseTo(49.45, Offset.offset(1e-4));
 
-        assertThat(centers1[0]).isEqualTo(new double[]{1.0});
-        assertThat(centers1[1][0]).isCloseTo(50.55, Offset.offset(1e-4));
+        assertThat(centers[0]).isEqualTo(new double[]{1.0});
+        assertThat(centers[1][0]).isCloseTo(50.55, Offset.offset(1e-4));
 
-        assertThat(averageDistance1).isCloseTo(distances1.stream().sum() / 3.0, Offset.offset(1e-4));
+        assertThat(averageDistance).isCloseTo(distances.stream().sum() / 3.0, Offset.offset(1e-4));
 
+    }
 
-        var kmeansConfig2 = ImmutableKmeansStreamConfig.builder()
+    @Test
+    void shouldChangeForWithRestarts() {
+        var kmeansConfig = ImmutableKmeansStreamConfig.builder()
             .nodeProperty("kmeans")
             .concurrency(1)
             .randomSeed(11L)
@@ -86,27 +90,28 @@ class KmeansRestartsTest {
             .k(2)
             .build();
 
-        var kmeans2 = Kmeans.createKmeans(graph, kmeansConfig2, kmeansContext);
-        var result2 = kmeans.compute();
-        var communities2 = result2.communities();
-        var distances2 = result2.distanceFromCenter();
-        var centers2 = result2.centers();
-        var averageDistance2 = result2.averageDistanceToCentroid();
+        var kmeansContext = ImmutableKmeansContext.builder().build();
 
-        assertThat(communities2.get(2)).isNotEqualTo(communities2.get(1));
-        assertThat(communities2.get(0)).isEqualTo(communities2.get(1));
+        var kmeans = Kmeans.createKmeans(graph, kmeansConfig, kmeansContext);
+        var result = kmeans.compute();
+        var communities = result.communities();
+        var distances = result.distanceFromCenter();
+        var centers = result.centers();
+        var averageDistance = result.averageDistanceToCentroid();
 
-        assertThat(distances2.get(2)).isEqualTo(0.0d);
-        assertThat(distances2.get(0)).isCloseTo(0.05, Offset.offset(1e-4));
-        assertThat(distances2.get(1)).isCloseTo(0.05, Offset.offset(1e-4));
+        assertThat(communities.get(2)).isNotEqualTo(communities.get(1));
+        assertThat(communities.get(0)).isEqualTo(communities.get(1));
 
-        assertThat(centers2[0][0]).isCloseTo(1.05, Offset.offset(1e-4));
-        assertThat(centers2[1]).isEqualTo(new double[]{100});
+        assertThat(distances.get(2)).isEqualTo(0.0d);
+        assertThat(distances.get(0)).isCloseTo(0.05, Offset.offset(1e-4));
+        assertThat(distances.get(1)).isCloseTo(0.05, Offset.offset(1e-4));
 
-        assertThat(averageDistance2).isCloseTo(distances2.stream().sum() / 3.0, Offset.offset(1e-4));
+        assertThat(centers[0][0]).isCloseTo(1.05, Offset.offset(1e-4));
+        assertThat(centers[1]).isEqualTo(new double[]{100});
 
-
+        assertThat(averageDistance).isCloseTo(distances.stream().sum() / 3.0, Offset.offset(1e-4));
     }
+
 
 
     @Test
