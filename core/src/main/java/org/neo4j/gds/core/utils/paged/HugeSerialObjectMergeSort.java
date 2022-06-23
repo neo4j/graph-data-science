7a6f1719de327013b3cfd16b5e28019d43a98cba
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-06-23 17:04:19 +0200

    Add HugeSerialObjectMergeSort
    Co-Authored-By: Jacob Sznajdman <breakanalysis@gmail.com>

---
+++ core/src/main/java/org/neo4j/gds/core/utils/paged/HugeSerialObjectMergeSort.java
@@ -0,0 +1,101 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.core.utils.paged;
+
+import java.util.function.ToDoubleFunction;
+
+
+public final class HugeSerialObjectMergeSort {
+    private HugeSerialObjectMergeSort() {}
+
+    public static <T> void sort(Class<T> componentClass, HugeObjectArray<T> array, ToDoubleFunction<T> toSortValue) {
+        HugeObjectArray<T> temp = HugeObjectArray.newArray(componentClass, array.size());
+        sort(array, array.size(), toSortValue, temp);
+    }
+
+    public static <T> void sort(
+        HugeObjectArray<T> array,
+        long size,
+        ToDoubleFunction<T> toSortValue,
+        HugeObjectArray<T> temp
+    ) {
+        long tempSize = 1;
+
+        while (tempSize < size) {
+            long i = 0;
+
+            while (i < size) {
+                long leftStart = i;
+                long leftEnd = i + tempSize - 1;
+                long rightStart = i + tempSize;
+                long rightEnd = i + 2 * tempSize - 1;
+
+                if (rightStart >= size) {
+                    break;
+                }
+
+                if (rightEnd >= size) {
+                    rightEnd = size - 1;
+                }
+
+                merge(array, temp, toSortValue, leftStart, leftEnd, rightStart, rightEnd);
+
+                for (long j = 0; j < rightEnd - leftStart + 1; j++) {
+                    array.set(i + j, temp.get(j));
+                }
+
+                i = i + 2 * tempSize;
+            }
+            tempSize *= 2;
+        }
+    }
+
+    private static <T> void merge(
+        HugeObjectArray<T> array,
+        HugeObjectArray<T> temp,
+        ToDoubleFunction<T> toSortValue,
+        long leftStart,
+        long leftEnd,
+        long rightStart,
+        long rightEnd
+    ) {
+        long idx = 0;
+
+        while (leftStart <= leftEnd && rightStart <= rightEnd) {
+            T lsIdx = array.get(leftStart);
+            T rsIdx = array.get(rightStart);
+            if (Double.compare(toSortValue.applyAsDouble(lsIdx), toSortValue.applyAsDouble(rsIdx)) <= 0) {
+                temp.set(idx++, lsIdx);
+                leftStart++;
+            } else {
+                temp.set(idx++, rsIdx);
+                rightStart++;
+            }
+        }
+
+        while (leftStart <= leftEnd) {
+            temp.set(idx++, array.get(leftStart++));
+        }
+
+        while (rightStart <= rightEnd) {
+            temp.set(idx++, array.get(rightStart++));
+        }
+    }
+}
---
+++ core/src/test/java/org/neo4j/gds/core/utils/paged/HugeSerialObjectMergeSortTest.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.core.utils.paged;
+
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.neo4j.gds.TestSupport;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HugeSerialObjectMergeSortTest {
+
+    static Stream<Arguments> sizeAndConcurrency() {
+        var random = new Random();
+        return TestSupport.crossArguments(
+            // array sizes
+            () -> Stream.concat(
+                Stream.concat(
+                    random.longs(5, 1, 100).boxed(),
+                    random.longs(5, 100, 1_000).boxed()
+                ),
+                Stream.concat(
+                    random.longs(5, 1_000, 10_000).boxed(),
+                    random.longs(5, 10_000, 1_000_000).boxed()
+                )
+            ).map(Arguments::of),
+            // single vs paged array
+            () -> List.of(true, false).stream().map(Arguments::of)
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("sizeAndConcurrency")
+    void sortArray(long size, boolean useSingleArray) {
+        var array = useSingleArray
+            ? HugeObjectArray.newSingleArray(Long.class, (int) size)
+            : HugeObjectArray.newPagedArray(Long.class, size);
+        var longs = ArrayUtils.toObject(new Random().longs(size, 0, size).toArray());
+        var doubles = new Random().doubles(size).toArray();
+        for (int i = 0; i < size; i++) {
+            array.set(i, longs[i]);
+        }
+
+        HugeSerialObjectMergeSort.sort(Long.class, array, l -> doubles[(int) l.longValue()]);
+
+        Arrays.sort(longs, Comparator.comparingDouble(lhs -> doubles[(int) (long) lhs]));
+        for (int i = 0; i < array.size(); i++) {
+            assertThat(longs[i]).isEqualTo(array.get(i));
+        }
+    }
+
+}
