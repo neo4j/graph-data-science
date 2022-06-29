1fb5fb299cc239c032ec5b0eb07b09621f48fb5e
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2022-06-29 23:41:30 +0200

    Use the correct suppress annotation

--- compatibility/4.3/neo4j-kernel-adapter/src/main/java/org/neo4j/gds/compat/_43/Neo4jProxyImpl.java
+++ compatibility/4.3/neo4j-kernel-adapter/src/main/java/org/neo4j/gds/compat/_43/Neo4jProxyImpl.java
@@ -19,6 +19,7 @@
  */
 package org.neo4j.gds.compat._43;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.neo4j.common.DependencyResolver;
 import org.neo4j.configuration.BootloaderSettings;
 import org.neo4j.configuration.Config;
@@ -608,6 +609,7 @@ public final class Neo4jProxyImpl implements Neo4jProxyApi {
     }
 
     @Override
+    @SuppressFBWarnings("NP_LOAD_OF_KNOWN_NULL_VALUE") // We assign nulls because it makes the code more readable
     public UserFunctionSignature userFunctionSignature(
         QualifiedName name,
         List<FieldSignature> inputSignature,
