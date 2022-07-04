add6ad0f7463c7bebe096e53ac809e8f35b55448
Florentin Dörre <florentin.dorre@neotechnology.com>
2022-07-04 12:28:47 +0200

    Test NodePropertyStepExecutor

--- pipeline/src/test/java/org/neo4j/gds/ml/pipeline/ExecutableNodePropertyStepTestUtil.java
+++ pipeline/src/test/java/org/neo4j/gds/ml/pipeline/ExecutableNodePropertyStepTestUtil.java
@@ -19,6 +19,7 @@
  */
 package org.neo4j.gds.ml.pipeline;
 
+import org.apache.commons.lang3.NotImplementedException;
 import org.neo4j.gds.NodeLabel;
 import org.neo4j.gds.RelationshipType;
 import org.neo4j.gds.api.GraphStore;
@@ -38,15 +39,21 @@ public class ExecutableNodePropertyStepTestUtil {
     public static class NodeIdPropertyStep implements ExecutableNodePropertyStep {
         private final GraphStore graphStore;
         private final String propertyName;
+        private String procName;
 
         public NodeIdPropertyStep(GraphStore graphStore, String propertyName) {
+            this(graphStore, "AddBogusNodePropertyStep", propertyName);
+        }
+
+        public NodeIdPropertyStep(GraphStore graphStore, String procName, String propertyName) {
             this.graphStore = graphStore;
             this.propertyName = propertyName;
+            this.procName = procName;
         }
 
         @Override
         public String procName() {
-            return "AddBogusNodePropertyStep";
+            return procName;
         }
 
         @Override
@@ -81,6 +88,52 @@ public class ExecutableNodePropertyStepTestUtil {
         @Override
         public Map<String, Object> toMap() {
             return Map.of();
+        }
+    }
+
+    static class TestNodePropertyStepWithFixedEstimation implements ExecutableNodePropertyStep {
+
+        private MemoryEstimation memoryEstimation;
+        private String procName;
+
+        public TestNodePropertyStepWithFixedEstimation(String procName, MemoryEstimation memoryEstimation) {
+            this.memoryEstimation = memoryEstimation;
+            this.procName = procName;
+        }
+
+        @Override
+        public Map<String, Object> toMap() {
+            throw new NotImplementedException();
+        }
+
+        @Override
+        public void execute(
+            ExecutionContext executionContext,
+            String graphName,
+            Collection<NodeLabel> nodeLabels,
+            Collection<RelationshipType> relTypes
+        ) {
+            throw new NotImplementedException();
+        }
+
+        @Override
+        public Map<String, Object> config() {
+            return Map.of();
+        }
+
+        @Override
+        public String procName() {
+            return procName;
+        }
+
+        @Override
+        public MemoryEstimation estimate(ModelCatalog modelCatalog, List<String> nodeLabels, List<String> relTypes) {
+            return memoryEstimation;
+        }
+
+        @Override
+        public String nodeProperty() {
+            throw new NotImplementedException();
         }
     }
 
