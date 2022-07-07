d122ec6c9d4b0ad0d0a42021be2b766ddac68fb7
Jacob Sznajdman <breakanalysis@gmail.com>
2022-07-07 19:18:26 +0200

    Add 'skeleton' for RWR

---
+++ graph-sampling/src/main/java/org/neo4j/gds/graphsampling/samplers/RandomWalkWithRestarts.java
@@ -0,0 +1,294 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.graphsampling.samplers;
+
+import org.apache.commons.lang3.mutable.MutableInt;
+import org.apache.commons.lang3.mutable.MutableLong;
+import org.neo4j.gds.NodeLabel;
+import org.neo4j.gds.RelationshipType;
+import org.neo4j.gds.api.DefaultValue;
+import org.neo4j.gds.api.Graph;
+import org.neo4j.gds.api.GraphStore;
+import org.neo4j.gds.api.IdMap;
+import org.neo4j.gds.api.PropertyState;
+import org.neo4j.gds.api.RelationshipIterator;
+import org.neo4j.gds.api.RelationshipProperty;
+import org.neo4j.gds.api.RelationshipPropertyStore;
+import org.neo4j.gds.api.Relationships;
+import org.neo4j.gds.api.properties.nodes.NodeProperty;
+import org.neo4j.gds.api.properties.nodes.NodePropertyStore;
+import org.neo4j.gds.api.schema.GraphSchema;
+import org.neo4j.gds.beta.filter.NodesFilter;
+import org.neo4j.gds.core.Aggregation;
+import org.neo4j.gds.core.concurrency.ParallelUtil;
+import org.neo4j.gds.core.loading.GraphStoreBuilder;
+import org.neo4j.gds.core.loading.construction.GraphFactory;
+import org.neo4j.gds.core.utils.paged.HugeAtomicBitSet;
+import org.neo4j.gds.graphsampling.config.RandomWalkWithRestartsConfig;
+import org.neo4j.values.storable.NumberType;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static org.neo4j.gds.api.AdjacencyCursor.NOT_FOUND;
+
+public class RandomWalkWithRestarts {
+    private final RandomWalkWithRestartsConfig config;
+    private final GraphStore inputGraphStore;
+
+    public RandomWalkWithRestarts(RandomWalkWithRestartsConfig config, GraphStore inputGraphStore) {
+        this.config = config;
+        this.inputGraphStore = inputGraphStore;
+    }
+
+    public GraphStore sample() {
+        var inputGraph = inputGraphStore.getGraph(
+            config.nodeLabelIdentifiers(inputGraphStore),
+            config.internalRelationshipTypes(inputGraphStore),
+            Optional.empty()
+        );
+        var rng = new Random();
+        config.randomSeed().ifPresent(rng::setSeed);
+
+        IdMap sampledNodes = sampleNodes(inputGraph, rng);
+
+        var nodePropertyStore = filterNodeProperties(inputGraph, sampledNodes);
+
+        Map<RelationshipType, Relationships.Topology> topologies = new HashMap<>();
+        Map<RelationshipType, RelationshipPropertyStore> relPropertyStores = new HashMap<>();
+        filterRelationshipsAndProperties(inputGraph, sampledNodes, topologies, relPropertyStores);
+
+        var filteredSchema = filterSchema(sampledNodes, topologies);
+
+        return new GraphStoreBuilder()
+            .databaseId(inputGraphStore.databaseId())
+            .capabilities(inputGraphStore.capabilities())
+            .schema(filteredSchema)
+            .nodes(sampledNodes)
+            .nodePropertyStore(nodePropertyStore)
+            .relationships(topologies)
+            .relationshipPropertyStores(relPropertyStores)
+            .concurrency(config.concurrency())
+            .build();
+
+    }
+
+    private IdMap sampleNodes(Graph inputGraph, Random rng) {
+        var sampledNodes = new AtomicLong();
+        boolean hasLabelInformation = !inputGraphStore.nodeLabels().isEmpty();
+        var nodesBuilder = GraphFactory.initNodesBuilder()
+            .concurrency(config.concurrency())
+            .maxOriginalId(inputGraph.highestNeoId())
+            .hasProperties(false)
+            .hasLabelInformation(hasLabelInformation)
+            .deduplicateIds(true)
+            .build();
+
+        long expectedNodes = Math.round(inputGraph.nodeCount() * config.samplingRatio());
+        final long startNode = config.startNode().map(inputGraph::toMappedNodeId).orElse(0L);
+        var currentNode = new MutableLong(startNode);
+        // must keep track of this because nodesBuilder may not have flushed its buffer, so importedNodes cannot be used atm
+        var seen = HugeAtomicBitSet.create(inputGraph.nodeCount());
+        while (sampledNodes.get() < expectedNodes) {
+            long originalId = inputGraph.toOriginalNodeId(currentNode.getValue());
+            if (hasLabelInformation) {
+                var nodeLabelList = inputGraph.nodeLabels(currentNode.getValue());
+                var nodeLabels = new NodeLabel[nodeLabelList.size()];
+                nodeLabelList.toArray(nodeLabels);
+                nodesBuilder.addNode(originalId, nodeLabels);
+            } else {
+                nodesBuilder.addNode(originalId);
+            }
+            seen.set(currentNode.getValue());
+            sampledNodes.set(seen.cardinality());
+            walk(currentNode, startNode, inputGraph, rng);
+        }
+        var idMapAndProperties = nodesBuilder.build();
+        return idMapAndProperties.idMap();
+    }
+
+    private NodePropertyStore filterNodeProperties(IdMap inputGraph, IdMap sampledNodes) {
+        var builder = NodePropertyStore.builder();
+
+        inputGraphStore.nodePropertyKeys().forEach(propertyKey -> {
+            var nodeProperty = inputGraphStore.nodeProperty(propertyKey);
+
+            NodesFilter.NodePropertiesBuilder<?> nodePropertiesBuilder = NodesFilter.getPropertiesBuilder(
+                inputGraph,
+                nodeProperty.values(),
+                config.concurrency()
+            );
+
+            ParallelUtil.parallelForEachNode(
+                sampledNodes.nodeCount(),
+                config.concurrency(),
+                filteredNode -> {
+                    var inputNode = inputGraph.toMappedNodeId(sampledNodes.toOriginalNodeId(filteredNode));
+                    nodePropertiesBuilder.accept(inputNode, filteredNode);
+                }
+            );
+
+            builder.putProperty(
+                propertyKey,
+                NodeProperty.of(
+                    propertyKey,
+                    nodeProperty.propertyState(),
+                    nodePropertiesBuilder.build(sampledNodes.nodeCount(), sampledNodes)
+                )
+            );
+        });
+        return builder.build();
+    }
+
+    private void filterRelationshipsAndProperties(
+        IdMap inputGraph,
+        IdMap sampledNodes,
+        Map<RelationshipType, Relationships.Topology> topologies,
+        Map<RelationshipType, RelationshipPropertyStore> relPropertyStores
+    ) {
+        for (RelationshipType relType : config.internalRelationshipTypes(inputGraphStore)) {
+            var relPropertyKeys = new ArrayList<>(inputGraphStore.relationshipPropertyKeys(relType));
+            var relationships = filterRelationships(inputGraph, sampledNodes, relType, relPropertyKeys);
+            var topology = relationships.get(0).topology();
+            if (topology.elementCount() > 0) {
+                topologies.put(relType, topology);
+                relPropertyStores.put(relType, relationshipPropertyStore(relPropertyKeys, relationships));
+            }
+        }
+    }
+
+    private List<Relationships> filterRelationships(
+        IdMap inputGraph,
+        IdMap sampledNodes,
+        RelationshipType relType,
+        ArrayList<String> relPropertyKeys
+    ) {
+        var propertyConfigs = relPropertyKeys
+            .stream()
+            .map(key -> GraphFactory.PropertyConfig.of(
+                Aggregation.NONE,
+                inputGraphStore.relationshipPropertyValues(relType, key).defaultValue()
+            ))
+            .collect(Collectors.toList());
+
+        var relationshipsBuilder = GraphFactory.initRelationshipsBuilder()
+            .nodes(sampledNodes)
+            .concurrency(config.concurrency())
+            .addAllPropertyConfigs(propertyConfigs)
+            .build();
+
+        var compositeIterator = inputGraphStore.getCompositeRelationshipIterator(relType, relPropertyKeys);
+
+        for (long nodeId = 0; nodeId < sampledNodes.nodeCount(); nodeId++) {
+
+            compositeIterator.forEachRelationship(
+                inputGraph.toMappedNodeId(sampledNodes.toOriginalNodeId(nodeId)),
+                (source, target, properties) -> {
+                    var neoTarget = inputGraph.toOriginalNodeId(target);
+                    var mappedTarget = sampledNodes.toMappedNodeId(neoTarget);
+                    var neoSource = inputGraph.toOriginalNodeId(source);
+
+                    if (mappedTarget != NOT_FOUND) {
+
+                        if (properties.length == 0) {
+                            relationshipsBuilder.add(neoSource, neoTarget);
+                        } else if (properties.length == 1) {
+                            relationshipsBuilder.add(neoSource, neoTarget, properties[0]);
+                        } else {
+                            relationshipsBuilder.add(neoSource, neoTarget, properties);
+                        }
+                    }
+                    return true;
+                }
+            );
+        }
+
+        return relationshipsBuilder.buildAll();
+    }
+
+
+    private RelationshipPropertyStore relationshipPropertyStore(
+        ArrayList<String> relPropertyKeys,
+        List<Relationships> relationships
+    ) {
+        var properties = IntStream.range(0, relPropertyKeys.size())
+            .boxed()
+            .collect(Collectors.toMap(
+                relPropertyKeys::get,
+                idx -> relationships.get(idx).properties().orElseThrow(IllegalStateException::new)
+            ));
+
+        var propertyStoreBuilder = RelationshipPropertyStore.builder();
+        properties.forEach((propertyKey, propertiesForKey) -> propertyStoreBuilder.putIfAbsent(
+            propertyKey,
+            RelationshipProperty.of(
+                propertyKey,
+                NumberType.FLOATING_POINT,
+                PropertyState.PERSISTENT,
+                propertiesForKey,
+                DefaultValue.of(propertiesForKey.defaultPropertyValue()),
+                Aggregation.NONE
+            )
+        ));
+
+        return propertyStoreBuilder.build();
+    }
+
+    private GraphSchema filterSchema(IdMap sampledNodes, Map<RelationshipType, Relationships.Topology> topologies) {
+        var nodeSchema = inputGraphStore.schema().nodeSchema().filter(sampledNodes.availableNodeLabels());
+        var relationshipSchema = inputGraphStore.schema()
+            .relationshipSchema()
+            .filter(topologies.keySet());
+
+        return GraphSchema.of(nodeSchema, relationshipSchema, inputGraphStore.schema().graphProperties());
+    }
+
+    private void walk(MutableLong currentNode, long startNode, Graph inputGraph, Random rng) {
+        int degree = inputGraph.degree(currentNode.getValue());
+        if (degree == 0 || rng.nextDouble() < config.returnProbability()) {
+            currentNode.setValue(startNode);
+            return;
+        }
+        int targetOffset = rng.nextInt(degree);
+        long target = getTarget(inputGraph, currentNode.getValue(), targetOffset);
+        currentNode.setValue(target);
+    }
+
+    private long getTarget(RelationshipIterator inputGraph, long sourceNode, int targetOffset) {
+        var targetsRemaining = new MutableInt(targetOffset);
+        var target = new MutableLong();
+        inputGraph.forEachRelationship(sourceNode, (src, trg) -> {
+            if (targetsRemaining.getValue() == 0) {
+                target.setValue(trg);
+                return false;
+            }
+            targetsRemaining.decrement();
+            return true;
+        });
+        return target.getValue();
+    }
+}
---
+++ graph-sampling/src/test/java/org/neo4j/gds/graphsampling/samplers/RandomWalkWithRestartsTest.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.graphsampling.samplers;
+
+import org.assertj.core.api.recursive.comparison.RecursiveComparisonConfiguration;
+import org.junit.jupiter.api.Test;
+import org.neo4j.gds.NodeLabel;
+import org.neo4j.gds.RelationshipType;
+import org.neo4j.gds.api.GraphStore;
+import org.neo4j.gds.extension.GdlExtension;
+import org.neo4j.gds.extension.GdlGraph;
+import org.neo4j.gds.extension.IdFunction;
+import org.neo4j.gds.extension.Inject;
+import org.neo4j.gds.graphsampling.config.ImmutableRandomWalkWithRestartsConfig;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.neo4j.gds.TestSupport.assertGraphEquals;
+import static org.neo4j.gds.TestSupport.fromGdl;
+
+@GdlExtension
+class RandomWalkWithRestartsTest {
+
+    @GdlGraph
+    private static final String DB_CYPHER =
+        "CREATE" +
+        "  (x:Z {prop: 42})" +
+        ", (x1:Z {prop: 43})" +
+        ", (x2:Z {prop: 44})" +
+        ", (x3:Z {prop: 45})" +
+        ", (a:N {prop: 46})" +
+        ", (b:N {prop: 47})" +
+        ", (c:N {prop: 48, attr: 48})" +
+        ", (d:N {prop: 49, attr: 48})" +
+        ", (e:M {prop: 50, attr: 48})" +
+        ", (f:M {prop: 51, attr: 48})" +
+        ", (g:M {prop: 52})" +
+        ", (h:M {prop: 53})" +
+        ", (i:X {prop: 54})" +
+        ", (j:M {prop: 55})" +
+        ", (x)-[:R1]->(x1)" +
+        ", (x)-[:R1]->(x2)" +
+        ", (x)-[:R1]->(x3)" +
+        ", (e)-[:R1]->(d)" +
+        ", (i)-[:R1]->(g)" +
+        ", (a)-[:R1 {cost: 10.0, distance: 5.8}]->(b)" +
+        ", (a)-[:R1 {cost: 10.0, distance: 4.8}]->(c)" +
+        ", (c)-[:R1 {cost: 10.0, distance: 5.8}]->(d)" +
+        ", (d)-[:R1 {cost:  4.2, distance: 2.6}]->(e)" +
+        ", (e)-[:R1 {cost: 10.0, distance: 5.8}]->(f)" +
+        ", (f)-[:R1 {cost: 10.0, distance: 9.9}]->(g)" +
+        ", (h)-[:R2 {cost: 10.0, distance: 5.8}]->(i)";
+
+    @Inject
+    private GraphStore graphStore;
+
+    @Inject
+    private IdFunction idFunction;
+
+    @Test
+    void shouldSampleAndFilterSchema() {
+        var config = ImmutableRandomWalkWithRestartsConfig.builder()
+            .startNode(idFunction.of("a"))
+            .samplingRatio(0.5)
+            .returnProbability(0.1)
+            .randomSeed(42L)
+            .build();
+
+        var rwr = new RandomWalkWithRestarts(config, graphStore);
+
+        var subgraph = rwr.sample();
+        assertThat(subgraph.getUnion().nodeCount()).isEqualTo(7);
+        assertThat(graphStore.schema().nodeSchema().filter(Set.of(NodeLabel.of("N"), NodeLabel.of("M")))).usingRecursiveComparison().isEqualTo(subgraph.schema().nodeSchema());
+        assertThat(graphStore.schema().relationshipSchema().filter(Set.of(RelationshipType.of("R1")))).usingRecursiveComparison().isEqualTo(subgraph.schema().relationshipSchema());
+        assertThat(graphStore.capabilities()).usingRecursiveComparison().isEqualTo(subgraph.capabilities());
+        assertThat(graphStore.databaseId()).usingRecursiveComparison().isEqualTo(subgraph.databaseId());
+
+        var expectedGraph =
+        "  (a:N {prop: 46})" +
+        ", (b:N {prop: 47})" +
+        ", (c:N {prop: 48, attr: 48})" +
+        ", (d:N {prop: 49, attr: 48})" +
+        ", (e:M {prop: 50, attr: 48})" +
+        ", (f:M {prop: 51, attr: 48})" +
+        ", (g:M {prop: 52})" +
+        ", (e)-[:R1]->(d)" +
+        ", (a)-[:R1 {distance: 5.8}]->(b)" +
+        ", (a)-[:R1 {distance: 4.8}]->(c)" +
+        ", (c)-[:R1 {distance: 5.8}]->(d)" +
+        ", (d)-[:R1 {distance: 2.6}]->(e)" +
+        ", (e)-[:R1 {distance: 5.8}]->(f)" +
+        ", (f)-[:R1 {distance: 9.9}]->(g)";
+        assertGraphEquals(fromGdl(expectedGraph), subgraph.getGraph("distance"));
+    }
+
+    @Test
+    void shouldFilterGraph() {
+        var config = ImmutableRandomWalkWithRestartsConfig.builder()
+            .addNodeLabels("M", "X")
+            .addRelationshipType("R1")
+            .samplingRatio(0.5)
+            .returnProbability(0.1)
+            .randomSeed(42L)
+            .build();
+
+        var rwr = new RandomWalkWithRestarts(config, graphStore);
+
+        var subgraph = rwr.sample();
+        assertThat(subgraph.getUnion().nodeCount()).isEqualTo(3);
+    }
+
+    @Test
+    void shouldRestartOnDeadEnd() {
+        var config = ImmutableRandomWalkWithRestartsConfig.builder()
+            .addNodeLabels("Z")
+            .addRelationshipType("R1")
+            .startNode(idFunction.of("x"))
+            .samplingRatio(0.999999999)
+            .returnProbability(0.0000000001)
+            .randomSeed(42L)
+            .build();
+
+        var rwr = new RandomWalkWithRestarts(config, graphStore);
+
+        var subgraph = rwr.sample();
+        assertThat(subgraph.getUnion().nodeCount()).isEqualTo(4);
+    }
+
+    @Test
+    void shouldBeDeterministic() {
+        var config = ImmutableRandomWalkWithRestartsConfig.builder()
+            .samplingRatio(0.5)
+            .startNode(idFunction.of("a"))
+            .returnProbability(0.1)
+            .randomSeed(42L)
+            .build();
+
+        var rwr = new RandomWalkWithRestarts(config, graphStore);
+
+        var subgraph1 = rwr.sample();
+        var subgraph2 = rwr.sample();
+
+        assertThat(subgraph1)
+            .usingRecursiveComparison(RecursiveComparisonConfiguration
+                .builder()
+                .withComparatorForType(Double::compare, Double.class)
+                .withIgnoredFields("modificationTime")
+                .build())
+            .isEqualTo(subgraph2);
+    }
+
+    //TODO: add some kind of maxTries parameter and test that if a startnode fails to produce sufficient samples, sampling terminates (thanks to picking a new startnode)
+    //TODO: test here or in proc test that it works when there is no label information. not sure if we can test this with GDL
+}
