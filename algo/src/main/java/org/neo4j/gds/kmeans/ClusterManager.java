a57b0b76488aeee89c9d1f29e71a41dc12ba3a81
ioannispan <ioannis.panagiotas@neotechnology.com>
2022-07-15 10:40:44 +0200

    if a centroid has no points set to it, do nto update to avoid NaN values

--- algo/src/main/java/org/neo4j/gds/kmeans/ClusterManager.java
+++ algo/src/main/java/org/neo4j/gds/kmeans/ClusterManager.java
@@ -29,6 +29,8 @@ import java.util.List;
 abstract class ClusterManager {
 
     final long[] nodesInCluster;
+
+    final boolean[] shouldReset;
     final NodePropertyValues nodePropertyValues;
     final int dimensions;
     final int k;
@@ -41,15 +43,22 @@ abstract class ClusterManager {
         this.nodePropertyValues = values;
         this.nodesInCluster = new long[k];
         this.currentlyAssigned = 0;
+        this.shouldReset = new boolean[k];
     }
 
-    public int getCurrentlyAssigned() {
+    int getCurrentlyAssigned() {
         return currentlyAssigned;
     }
 
     abstract void initialAssignCluster(long id);
 
-    abstract void reset();
+    void reset() {
+        for (int centroidId = 0; centroidId < k; ++centroidId) {
+            nodesInCluster[centroidId] = 0;
+            shouldReset[centroidId] = true;
+        }
+    }
+
 
     abstract void normalizeClusters();
 
@@ -103,18 +112,13 @@ class FloatClusterManager extends ClusterManager {
 
 
     @Override
-    public void reset() {
-        for (int centroidId = 0; centroidId < k; ++centroidId) {
-            nodesInCluster[centroidId] = 0;
-            Arrays.fill(centroids[centroidId], 0.0f);
-        }
-    }
-
-    @Override
     public void normalizeClusters() {
-        for (int centreId = 0; centreId < k; ++centreId) {
-            for (int dimension = 0; dimension < dimensions; ++dimension)
-                centroids[centreId][dimension] /= (float) nodesInCluster[centreId];
+        for (int centroidId = 0; centroidId < k; ++centroidId) {
+            for (int dimension = 0; dimension < dimensions; ++dimension) {
+                if (nodesInCluster[centroidId] > 0) {
+                    centroids[centroidId][dimension] /= (float) nodesInCluster[centroidId];
+                }
+            }
         }
     }
 
@@ -128,10 +132,17 @@ class FloatClusterManager extends ClusterManager {
     public void updateFromTask(KmeansTask task) {
         var floatKmeansTask = (FloatKmeansTask) task;
         for (int centroidId = 0; centroidId < k; ++centroidId) {
-            nodesInCluster[centroidId] += task.getNumAssignedAtCluster(centroidId);
-            var taskContributionToCluster = floatKmeansTask.getCentroidContribution(centroidId);
-            for (int dimension = 0; dimension < dimensions; ++dimension) {
-                centroids[centroidId][dimension] += taskContributionToCluster[dimension];
+            var contribution = task.getNumAssignedAtCluster(centroidId);
+            if (contribution > 0) {
+                if (shouldReset[centroidId]) {
+                    Arrays.fill(centroids[centroidId], 0.0f);
+                    shouldReset[centroidId] = false;
+                }
+                nodesInCluster[centroidId] += contribution;
+                var taskContributionToCluster = floatKmeansTask.getCentroidContribution(centroidId);
+                for (int dimension = 0; dimension < dimensions; ++dimension) {
+                    centroids[centroidId][dimension] += taskContributionToCluster[dimension];
+                }
             }
         }
     }
@@ -178,19 +189,15 @@ class DoubleClusterManager extends ClusterManager {
         this.centroids = new double[k][dimensions];
     }
 
-    @Override
-    public void reset() {
-        for (int centroidId = 0; centroidId < k; ++centroidId) {
-            nodesInCluster[centroidId] = 0;
-            Arrays.fill(centroids[centroidId], 0.0d);
-        }
-    }
 
     @Override
     public void normalizeClusters() {
         for (int centroidId = 0; centroidId < k; ++centroidId) {
-            for (int dimension = 0; dimension < dimensions; ++dimension)
-                centroids[centroidId][dimension] /= (double) nodesInCluster[centroidId];
+            for (int dimension = 0; dimension < dimensions; ++dimension) {
+                if (nodesInCluster[centroidId] > 0) {
+                    centroids[centroidId][dimension] /= (double) nodesInCluster[centroidId];
+                }
+            }
         }
     }
 
@@ -198,10 +205,17 @@ class DoubleClusterManager extends ClusterManager {
     public void updateFromTask(KmeansTask task) {
         var doubleKmeansTask = (DoubleKmeansTask) task;
         for (int centroidId = 0; centroidId < k; ++centroidId) {
-            nodesInCluster[centroidId] += task.getNumAssignedAtCluster(centroidId);
-            var taskContributionToCluster = doubleKmeansTask.getCentroidContribution(centroidId);
-            for (int dimension = 0; dimension < dimensions; ++dimension) {
-                centroids[centroidId][dimension] += taskContributionToCluster[dimension];
+            var contribution = task.getNumAssignedAtCluster(centroidId);
+            if (contribution > 0) {
+                if (shouldReset[centroidId]) {
+                    Arrays.fill(centroids[centroidId], 0.0d);
+                    shouldReset[centroidId] = false;
+                }
+                nodesInCluster[centroidId] += contribution;
+                var taskContributionToCluster = doubleKmeansTask.getCentroidContribution(centroidId);
+                for (int dimension = 0; dimension < dimensions; ++dimension) {
+                    centroids[centroidId][dimension] += taskContributionToCluster[dimension];
+                }
             }
         }
     }
