2857bca362c8738ed49f105173fd429211d9e0e7
Florentin Dörre <florentin.dorre@neotechnology.com>
2022-07-21 14:20:46 +0200

    Throw on directed relationships

--- alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/linkmodels/pipeline/train/LinkPredictionTrainConfig.java
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/ml/linkmodels/pipeline/train/LinkPredictionTrainConfig.java
@@ -21,17 +21,25 @@ package org.neo4j.gds.ml.linkmodels.pipeline.train;
 
 import org.immutables.value.Value;
 import org.jetbrains.annotations.TestOnly;
+import org.neo4j.gds.NodeLabel;
+import org.neo4j.gds.RelationshipType;
 import org.neo4j.gds.annotation.Configuration;
 import org.neo4j.gds.annotation.ValueClass;
+import org.neo4j.gds.api.GraphStore;
 import org.neo4j.gds.config.AlgoBaseConfig;
 import org.neo4j.gds.config.GraphCreateConfig;
 import org.neo4j.gds.config.RandomSeedConfig;
 import org.neo4j.gds.core.CypherMapWrapper;
 import org.neo4j.gds.ml.linkmodels.metrics.LinkMetric;
 import org.neo4j.gds.model.ModelConfig;
+import org.neo4j.gds.utils.StringJoining;
 
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static org.neo4j.gds.utils.StringFormatting.formatWithLocale;
 
 @ValueClass
 @Configuration
@@ -50,6 +58,25 @@ public interface LinkPredictionTrainConfig extends AlgoBaseConfig, ModelConfig, 
     @Value.Default
     default List<LinkMetric> metrics() {
         return List.of(LinkMetric.AUCPR);
+    }
+
+    @Configuration.GraphStoreValidationCheck
+    default void validateTargetRelIsUndirected(
+        GraphStore graphStore,
+        Collection<NodeLabel> selectedLabels,
+        Collection<RelationshipType> selectedRelationshipTypes
+    ) {
+        var invalidTypes = selectedRelationshipTypes.stream()
+            .filter(type -> !graphStore.isUndirected(type))
+            .map(RelationshipType::name)
+            .collect(Collectors.toList());
+
+        if (!invalidTypes.isEmpty()) {
+            throw new IllegalArgumentException(formatWithLocale(
+                "RelationshipTypes must be undirected, but found %s which are directed.",
+                StringJoining.join(invalidTypes)
+            ));
+        }
     }
 
     static LinkPredictionTrainConfig of(
