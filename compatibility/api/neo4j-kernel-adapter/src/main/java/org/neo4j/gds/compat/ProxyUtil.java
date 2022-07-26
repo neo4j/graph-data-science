97da27fd61c14926e5a4d886450f599e04c1ba31
Paul Horn <paul.horn@neotechnology.com>
2022-07-26 15:16:24 +0200

    Add GDS version to compat logs
    Co-Authored-By: Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>

--- compatibility/api/neo4j-kernel-adapter/src/main/java/org/neo4j/gds/compat/ProxyUtil.java
+++ compatibility/api/neo4j-kernel-adapter/src/main/java/org/neo4j/gds/compat/ProxyUtil.java
@@ -20,7 +20,10 @@
 package org.neo4j.gds.compat;
 
 import org.neo4j.gds.annotation.SuppressForbidden;
+import org.neo4j.logging.Log;
 
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import java.util.Locale;
 import java.util.ServiceLoader;
 import java.util.StringJoiner;
@@ -33,6 +36,7 @@ public final class ProxyUtil {
 
     @SuppressForbidden(reason = "This is the best we can do at the moment")
     public static <PROXY, FACTORY extends ProxyFactory<PROXY>> PROXY findProxy(Class<FACTORY> factoryClass) {
+        var log = new OutputStreamLogBuilder(System.out).build();
         var neo4jVersion = new AtomicReference<Neo4jVersion>();
         var availabilityLog = new StringJoiner(", ", "GDS compatibility: ", "");
         try {
@@ -52,22 +56,81 @@ public final class ProxyUtil {
                     return canLoad;
                 })
                 .findFirst()
-                .orElseThrow(() -> new LinkageError("GDS is not compatible with Neo4j version: " + neo4jVersion));
+                .orElseThrow(() -> new LinkageError(String.format(
+                    Locale.ENGLISH,
+                    "GDS %s is not compatible with Neo4j version: %s",
+                    gdsVersion(log),
+                    neo4jVersion
+                )));
             availabilityLog.add("selected: " + proxyFactory.description());
             return proxyFactory.load();
         } finally {
-            var log = new OutputStreamLogBuilder(System.out).build();
             if (LOG_ENVIRONMENT.getAndSet(false)) {
                 log.debug(
-                    "Java vendor: [%s] Java version: [%s] Java home: [%s] Detected Neo4j version: [%s]",
+                    "Java vendor: [%s] Java version: [%s] Java home: [%s] GDS version: [%s] Detected Neo4j version: [%s]",
                     System.getProperty("java.vendor"),
                     System.getProperty("java.version"),
                     System.getProperty("java.home"),
+                    gdsVersion(log),
                     neo4jVersion
                 );
             }
             log.info(availabilityLog.toString());
         }
+    }
+
+    private static final AtomicReference<String> GDS_VERSION = new AtomicReference<>();
+
+    private static String gdsVersion(Log log) {
+        if (GDS_VERSION.get() == null) {
+            GDS_VERSION.set(getGdsVersion(log));
+        }
+
+        return GDS_VERSION.get();
+    }
+
+    private static String getGdsVersion(Log log) {
+        try {
+            return findGdsVersion();
+        } catch (ClassNotFoundException e) {
+            log.warn("Could not determine GDS version, BuildInfoProperties is missing.", e);
+        } catch (NoSuchMethodException | IllegalAccessException e) {
+            log.warn("Could not determine GDS version, the according methods on BuildInfoProperties could not be found.", e);
+        } catch (Throwable e) {
+            log.warn("Could not determine GDS version, the according methods on BuildInfoProperties failed.", e);
+        }
+
+        return "Unknown";
+    }
+
+    private static String findGdsVersion() throws Throwable {
+        // The class that we use to get the GDS version lives in proc-sysinfo, which is part of the released GDS jar,
+        // but we don't want to depend on that here. One reason is that this class gets generated and re-generated
+        // on every build and having it at the top of the dependency graph would cause a lot of recompilation.
+        // Let's do a bit of class loading and reflection to get the version.
+        var lookup = MethodHandles.lookup();
+
+        var buildInfoPropertiesClass = Class.forName("org.neo4j.gds.BuildInfoProperties");
+
+        // equivalent to: BuildInfoProperties.get()
+        var buildInfoPropertiesHandle = lookup.findStatic(
+            buildInfoPropertiesClass,
+            "get",
+            MethodType.methodType(buildInfoPropertiesClass)
+        );
+
+        // equivalent to: buildInfoProperties.gdsVersion()
+        var gdsVersionHandle = lookup.findVirtual(
+            buildInfoPropertiesClass,
+            "gdsVersion",
+            MethodType.methodType(String.class)
+        );
+
+        // var buildInfoProperties = BuildInfoProperties.get()
+        var buildInfoProperties = buildInfoPropertiesHandle.invoke();
+        // var gdsVersion = buildInfoProperties.gdsVersion()
+        var gdsVersion = gdsVersionHandle.invoke(buildInfoProperties);
+        return String.valueOf(gdsVersion);
     }
 
     private ProxyUtil() {}
