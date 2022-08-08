4b073c068166c938d556eb96d092a2cf9bfbee38
Adam Schill Collberg <adam.schill.collberg@protonmail.com>
2022-08-08 17:00:30 +0200

    Break SeenNodes interface out into its own file

--- graph-sampling/src/main/java/org/neo4j/gds/graphsampling/samplers/rwr/RandomWalkWithRestarts.java
+++ graph-sampling/src/main/java/org/neo4j/gds/graphsampling/samplers/rwr/RandomWalkWithRestarts.java
@@ -116,10 +116,10 @@ public class RandomWalkWithRestarts implements NodesSampler {
 
         if (config.nodeLabelStratification()) {
             var expectedCounts = getExpectedCountsPerNodeLabelSet(inputGraph);
-            return new SeenNodesByLabelSet(inputGraph, expectedCounts, totalExpectedCount);
+            return new SeenNodes.SeenNodesByLabelSet(inputGraph, expectedCounts, totalExpectedCount);
         }
 
-        return new GlobalSeenNodes(
+        return new SeenNodes.GlobalSeenNodes(
             HugeAtomicBitSet.create(inputGraph.nodeCount()),
             totalExpectedCount
         );
@@ -393,94 +393,4 @@ public class RandomWalkWithRestarts implements NodesSampler {
         }
     }
 
-    interface SeenNodes {
-        /**
-         * Tries to add a node to the sample.
-         * Returns true iff it succeeded in doing so.
-         */
-        boolean addNode(long nodeId);
-
-        boolean hasSeenEnough();
-
-        HugeAtomicBitSet sampledNodes();
-    }
-
-    static class SeenNodesByLabelSet implements SeenNodes {
-        private final Graph inputGraph;
-        private final Map<Set<NodeLabel>, Long> seenPerLabelSet;
-        private final Map<Set<NodeLabel>, Long> expectedNodesPerLabelSet;
-        private final HugeAtomicBitSet seenBitSet;
-        private final long totalExpectedNodes;
-
-        SeenNodesByLabelSet(
-            Graph inputGraph,
-            Map<Set<NodeLabel>, Long> expectedNodesPerLabelSet,
-            long totalExpectedNodes
-        ) {
-            this.inputGraph = inputGraph;
-            this.expectedNodesPerLabelSet = expectedNodesPerLabelSet;
-            this.seenBitSet = HugeAtomicBitSet.create(inputGraph.nodeCount());
-            this.totalExpectedNodes = totalExpectedNodes;
-
-            this.seenPerLabelSet = new HashMap<>(expectedNodesPerLabelSet);
-            this.seenPerLabelSet.replaceAll((unused, value) -> 0L);
-        }
-
-        public boolean addNode(long nodeId) {
-            var labelSet = new HashSet<>(inputGraph.nodeLabels(nodeId));
-            // There's a slight race condition here which may cause there to be an extra node or two in a given
-            // node label set bucket, since the cardinality check and the set are not synchronized together.
-            // Since the sampling is inexact by nature this should be fine.
-            if (seenPerLabelSet.get(labelSet) < expectedNodesPerLabelSet.get(labelSet)) {
-                boolean added = !seenBitSet.getAndSet(nodeId);
-                if (added) {
-                    seenPerLabelSet.compute(labelSet, (unused, count) -> count + 1);
-                }
-                return added;
-            }
-
-            return false;
-        }
-
-        public boolean hasSeenEnough() {
-            if (seenBitSet.cardinality() < totalExpectedNodes) {
-                return false;
-            }
-
-            for (var entry : seenPerLabelSet.entrySet()) {
-                if (entry.getValue() < expectedNodesPerLabelSet.get(entry.getKey())) {
-                    // Should only happen is edge cases when the rounding is not fully consistent.
-                    return false;
-                }
-            }
-
-            return true;
-        }
-
-        public HugeAtomicBitSet sampledNodes() {
-            return seenBitSet;
-        }
-    }
-
-    static class GlobalSeenNodes implements SeenNodes {
-        private final HugeAtomicBitSet seenBitSet;
-        private final long expectedNodes;
-
-        GlobalSeenNodes(HugeAtomicBitSet seenBitSet, long expectedNodes) {
-            this.seenBitSet = seenBitSet;
-            this.expectedNodes = expectedNodes;
-        }
-
-        public boolean addNode(long nodeId) {
-            return !seenBitSet.getAndSet(nodeId);
-        }
-
-        public boolean hasSeenEnough() {
-            return seenBitSet.cardinality() >= expectedNodes;
-        }
-
-        public HugeAtomicBitSet sampledNodes() {
-            return seenBitSet;
-        }
-    }
 }
