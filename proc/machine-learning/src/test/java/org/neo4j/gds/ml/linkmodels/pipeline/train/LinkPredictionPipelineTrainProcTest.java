5ffaf85f5dde6599ee0a73874e5ad886a12174da
Brian Shi <brian.shi@neotechnology.com>
2022-08-09 11:37:11 +0100

    Fix LinkPredictionPipelineTrainProcTest for pipeline#validateBeforeExecution refactor
    Co-Authored-By: Mats Rydberg <mats@neo4j.org>

--- proc/machine-learning/src/test/java/org/neo4j/gds/ml/linkmodels/pipeline/train/LinkPredictionPipelineTrainProcTest.java
+++ proc/machine-learning/src/test/java/org/neo4j/gds/ml/linkmodels/pipeline/train/LinkPredictionPipelineTrainProcTest.java
@@ -196,6 +196,7 @@ class LinkPredictionPipelineTrainProcTest extends BaseProcTest {
     @Test
     void failsWhenMissingFeatures() {
         runQuery("CALL gds.beta.pipeline.linkPrediction.create('pipe2')");
+        runQuery("CALL gds.beta.pipeline.linkPrediction.configureSplit('pipe2', {trainFraction: 0.3})");
         runQuery("CALL gds.beta.pipeline.linkPrediction.addNodeProperty('pipe2', 'pageRank', {mutateProperty: 'pr'})");
         runQuery("CALL gds.beta.pipeline.linkPrediction.addLogisticRegression('pipe2')");
 
@@ -210,6 +211,7 @@ class LinkPredictionPipelineTrainProcTest extends BaseProcTest {
     @Test
     void failsWhenMissingNodeProperty() {
         runQuery("CALL gds.beta.pipeline.linkPrediction.create('pipe')");
+        runQuery("CALL gds.beta.pipeline.linkPrediction.configureSplit('pipe', {trainFraction: 0.3})");
         runQuery("CALL gds.beta.pipeline.linkPrediction.addLogisticRegression('pipe')");
         runQuery(
             "CALL gds.beta.pipeline.linkPrediction.addFeature('pipe', 'l2', {nodeProperties: ['missingNodeProperty']})");
