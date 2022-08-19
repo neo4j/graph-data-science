69f0a3d0f20b27a4f471ad5b625b8345fe653206
ioannispan <ioannis.panagiotas@neotechnology.com>
2022-08-19 14:49:30 +0300

    Add SetupQuery interface

---
+++ doc-test-tools/src/main/java/org/neo4j/gds/doc/syntax/SetupQuery.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.doc.syntax;
+
+import org.immutables.value.Value;
+import org.neo4j.gds.annotation.ValueClass;
+
+@ValueClass
+public interface SetupQuery {
+    String query();
+
+    @Value.Derived
+    default boolean runAsOperator() {
+        return !operator().equals("");
+    }
+
+    @Value.Default
+    default String operator() {
+        return "";
+    }
+
+    static ImmutableSetupQuery.Builder builder() {
+        return ImmutableSetupQuery.builder();
+    }
+}
