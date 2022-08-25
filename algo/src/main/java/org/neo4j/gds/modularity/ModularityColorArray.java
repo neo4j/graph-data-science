72ba1ff22264ed27ce1c6c43fe96a8edc4bc0ed9
ioannispan <ioannis.panagiotas@neotechnology.com>
2022-08-25 17:25:18 +0300

    Add a ModularityColorArray class

---
+++ algo/src/main/java/org/neo4j/gds/modularity/ModularityColorArray.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.modularity;
+
+import com.carrotsearch.hppc.LongLongHashMap;
+import com.carrotsearch.hppc.LongLongMap;
+import org.neo4j.gds.core.utils.paged.HugeLongArray;
+
+class ModularityColorArray {
+
+    private final HugeLongArray sortedNodesByColor;
+    private final HugeLongArray colorCoordinates;
+    private LongLongMap colorToId;
+
+    ModularityColorArray(
+        HugeLongArray sortedNodesByColor,
+        HugeLongArray colorCoordinates,
+        LongLongMap colorToId
+    ) {
+        this.colorCoordinates = colorCoordinates;
+        this.colorToId = colorToId;
+        this.sortedNodesByColor = sortedNodesByColor;
+    }
+
+    long getStartingCoordinate(long color) {
+        return colorCoordinates.get(colorToId.get(color));
+    }
+
+    long getEndingCoordinate(long color) {
+        return colorCoordinates.get(colorToId.get(color) + 1);
+    }
+
+    static ModularityColorArray createModularityColorArray(HugeLongArray colors, long nodeCount) {
+        var sortedNodesByColor = HugeLongArray.newArray(nodeCount);
+        LongLongMap colorCount = new LongLongHashMap();
+        LongLongMap colorToId = new LongLongHashMap();
+        long encounteredColors = 0;
+        for (long nodeId = 0; nodeId < nodeCount; ++nodeId) {
+            long color = colors.get(nodeId);
+            if (!colorToId.containsKey(color)) {
+                colorToId.put(color, encounteredColors++);
+            }
+            long colorId = colorToId.get(color);
+            colorCount.addTo(colorId, 1);
+        }
+
+        var colorCoordinates = HugeLongArray.newArray(encounteredColors + 1);
+        colorCoordinates.set(0, 0);
+        long nodeSum = 0;
+        for (long colorId = 0; colorId <= encounteredColors; ++colorId) {
+            if (colorId == encounteredColors) {
+                colorCoordinates.set(colorId, nodeCount);
+            } else {
+                nodeSum += colorCount.get(colorId);
+                colorCoordinates.set(colorId, nodeSum - 1);
+            }
+        }
+        for (long nodeId = 0; nodeId < nodeCount; ++nodeId) {
+            long color = colors.get(nodeId);
+            long colorId = colorToId.get(color);
+            long coordinate = colorCoordinates.get(colorId);
+            sortedNodesByColor.set(coordinate, nodeId);
+            colorCoordinates.set(colorId, coordinate - 1);
+        }
+        return new ModularityColorArray(sortedNodesByColor, colorCoordinates, colorToId);
+    }
+}
