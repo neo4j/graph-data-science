4f0823f098fb0aaa3645e7c3f30dbc5920d22b01
ioannispan <ioannis.panagiotas@neotechnology.com>
2022-11-04 22:37:32 +0100

    Do batched updates

--- algo/src/main/java/org/neo4j/gds/leiden/RelationshipCreator.java
+++ algo/src/main/java/org/neo4j/gds/leiden/RelationshipCreator.java
@@ -19,9 +19,11 @@
  */
 package org.neo4j.gds.leiden;
 
+import com.carrotsearch.hppc.BitSet;
 import org.neo4j.gds.Orientation;
 import org.neo4j.gds.api.RelationshipIterator;
 import org.neo4j.gds.core.loading.construction.RelationshipsBuilder;
+import org.neo4j.gds.core.utils.paged.HugeDoubleArray;
 import org.neo4j.gds.core.utils.paged.HugeLongArray;
 import org.neo4j.gds.core.utils.partition.Partition;
 import org.neo4j.gds.core.utils.progress.tasks.ProgressTracker;
@@ -34,6 +36,10 @@ final class RelationshipCreator implements Runnable {
     private final RelationshipIterator relationshipIterator;
     private final Partition partition;
     private final ProgressTracker progressTracker;
+    private final HugeDoubleArray encounteredCommunityWeights;
+    private final HugeLongArray encounteredCommunities;
+
+    private long encountereCommunitiesCounter;
 
     RelationshipCreator(
         HugeLongArray communities,
@@ -49,27 +55,104 @@ final class RelationshipCreator implements Runnable {
         this.relationshipIterator = relationshipIterator;
         this.partition = partition;
         this.progressTracker = progressTracker;
+        this.encounteredCommunityWeights = HugeDoubleArray.newArray(communities.size());
+        this.encounteredCommunities = HugeLongArray.newArray(communities.size());
+        encounteredCommunityWeights.setAll(v -> -1);
+    }
+
+
+    private void updateRelationships(long sourceCommunityId, long numEncounteredCommunities) {
+        for (long communityIndexId = 0; communityIndexId < numEncounteredCommunities; ++communityIndexId) {
+            long communityId = encounteredCommunities.get(communityIndexId);
+            double weight = encounteredCommunityWeights.get(communityId);
+            relationshipsBuilder.add(sourceCommunityId, communityId, weight);
+            encounteredCommunityWeights.set(communityId, -1);
+        }
     }
 
     @Override
     public void run() {
-        partition.consume(nodeId -> {
-            long communityId = communities.get(nodeId);
+        HugeLongArray sortedByCommunity = getNodesSortedByCommunity(partition, communities);
+        long partitionCount = partition.nodeCount();
+        long previousCommunity = -1;
+        for (long nodeIndexId = 0; nodeIndexId < partitionCount; ++nodeIndexId) {
+            long nodeId = sortedByCommunity.get(nodeIndexId);
+            long currentCommunity = communities.get(nodeId);
+
+            boolean shouldUpdate = (nodeIndexId > 0 && previousCommunity != currentCommunity);
+
+            if (shouldUpdate) {
+                updateRelationships(previousCommunity, encountereCommunitiesCounter);
+                encountereCommunitiesCounter = 0;
+            }
+
             relationshipIterator.forEachRelationship(nodeId, 1.0, (source, target, property) -> {
                 // do not allow self-loops
                 long targetCommunityId = communities.get(target);
-                if (communityId != targetCommunityId) { //we do not include  self-edges in next graph
+                if (currentCommunity != targetCommunityId) { //we do not include  self-edges in next graph
                     //if orientation is natural it means a-[weight]->b will be examined only from a
                     // hence we should add it as we encounter it from a
                     //otherwise  a<-[weight]->b will be visited from both a and b.
                     // To not include it twice we break a tie based on id.
-                    if (orientation == Orientation.NATURAL || communityId > targetCommunityId) {
-                        relationshipsBuilder.add(communityId, targetCommunityId, property);
+                    if (orientation == Orientation.NATURAL || currentCommunity > targetCommunityId) {
+                        double valueToAdd = property;
+                        if (encounteredCommunityWeights.get(targetCommunityId) == -1) {
+                            encounteredCommunities.set(encountereCommunitiesCounter++, targetCommunityId);
+                            valueToAdd++;
+                        }
+                        encounteredCommunityWeights.addTo(targetCommunityId, valueToAdd);
                     }
                 }
                 return true;
             });
+
+            shouldUpdate = (nodeIndexId == (partitionCount - 1));
+            if (shouldUpdate) {
+                updateRelationships(currentCommunity, encountereCommunitiesCounter);
+            }
+            previousCommunity = currentCommunity;
             progressTracker.logProgress();
-        });
+        }
+
+    }
+
+    static HugeLongArray getNodesSortedByCommunity(Partition partition, HugeLongArray communities) {
+        long nodeCount = communities.size();
+        long partitionCount = partition.nodeCount();
+        var sortedNodesByCommunity = HugeLongArray.newArray(partitionCount);
+        HugeLongArray communityCoordinateArray = HugeLongArray.newArray(nodeCount);
+
+        HugeLongArray indexMap = HugeLongArray.newArray(nodeCount);
+        
+        BitSet setCommunityCoordinates = new BitSet(nodeCount + 1);
+
+        long endNode = partition.startNode() + partition.nodeCount();
+        for (long nodeId = partition.startNode(); nodeId < endNode; ++nodeId) {
+            long communityId = communities.get(nodeId);
+            setCommunityCoordinates.getAndSet(communityId);
+            communityCoordinateArray.addTo(communityId, 1);
+        }
+
+
+        long nodeSum = 0;
+        long bitIndex = setCommunityCoordinates.nextSetBit(0);
+        long currentId = 0;
+        while (bitIndex != -1) {
+            indexMap.set(bitIndex, currentId);
+            nodeSum += communityCoordinateArray.get(bitIndex);
+            communityCoordinateArray.set(currentId++, nodeSum);
+            bitIndex = setCommunityCoordinates.nextSetBit(bitIndex + 1);
+        }
+
+        for (long nodeId = endNode - 1; nodeId >= partition.startNode(); --nodeId) {
+            long community = communities.get(nodeId);
+            long communityId = indexMap.get(community);
+            long coordinate = communityCoordinateArray.get(communityId) - 1;
+            sortedNodesByCommunity.set(coordinate, nodeId);
+            communityCoordinateArray.set(communityId, coordinate);
+        }
+        return sortedNodesByCommunity;
+
     }
 }
+
