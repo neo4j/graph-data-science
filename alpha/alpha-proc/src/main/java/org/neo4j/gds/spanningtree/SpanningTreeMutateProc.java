def01a7dee32fa0c11b97ff59cc9259786843e68
ioannispan <ioannis.panagiotas@neotechnology.com>
2022-11-10 07:33:51 +0100

    Add mutate mode

---
+++ alpha/alpha-proc/src/main/java/org/neo4j/gds/spanningtree/SpanningTreeMutateProc.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.spanningtree;
+
+import org.neo4j.gds.BaseProc;
+import org.neo4j.gds.core.write.RelationshipExporter;
+import org.neo4j.gds.core.write.RelationshipExporterBuilder;
+import org.neo4j.gds.executor.ExecutionContext;
+import org.neo4j.gds.executor.ImmutableExecutionContext;
+import org.neo4j.gds.executor.ProcedureExecutor;
+import org.neo4j.procedure.Context;
+import org.neo4j.procedure.Description;
+import org.neo4j.procedure.Name;
+import org.neo4j.procedure.Procedure;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+import static org.neo4j.procedure.Mode.READ;
+
+// TODO: Always undirected
+public class SpanningTreeMutateProc extends BaseProc {
+
+    static final String DESCRIPTION = SpanningTreeWriteProc.DESCRIPTION;
+    @Context
+    public RelationshipExporterBuilder<? extends RelationshipExporter> relationshipExporterBuilder;
+
+    @Procedure(value = "gds.alpha.spanningTree.mutate", mode = READ)
+    @Description(DESCRIPTION)
+    public Stream<MutateResult> spanningTree(
+        @Name(value = "graphName") String graphName,
+        @Name(value = "configuration", defaultValue = "{}") Map<String, Object> configuration
+    ) {
+        return new ProcedureExecutor<>(
+            new SpanningTreeMutateSpec(),
+            executionContext()
+        ).compute(graphName, configuration, true, true);
+    }
+
+    @Override
+    public ExecutionContext executionContext() {
+        return ImmutableExecutionContext
+            .builder()
+            .databaseService(databaseService)
+            .log(log)
+            .procedureTransaction(procedureTransaction)
+            .transaction(transaction)
+            .callContext(callContext)
+            .userLogRegistryFactory(userLogRegistryFactory)
+            .taskRegistryFactory(taskRegistryFactory)
+            .username(username())
+            .relationshipExporterBuilder(relationshipExporterBuilder)
+            .build();
+    }
+
+}
