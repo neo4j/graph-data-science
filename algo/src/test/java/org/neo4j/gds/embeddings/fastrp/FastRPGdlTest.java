3b119017a011b686c6532b624ec3791d99cee0e2
Ertjon Mecka <ertjonmecka@gmail.com>
2023-01-03 20:21:21 +0100

    adapted shouldSwapInitialRandomVectors test & shouldAverageNeighbors test

--- algo/src/test/java/org/neo4j/gds/embeddings/fastrp/FastRPGdlTest.java
+++ algo/src/test/java/org/neo4j/gds/embeddings/fastrp/FastRPGdlTest.java
@@ -19,55 +19,75 @@
  */
 package org.neo4j.gds.embeddings.fastrp;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.neo4j.gds.ml.core.tensor.operations.FloatVectorOperations.l2Normalize;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.neo4j.gds.NodeLabel;
+import org.neo4j.gds.PropertyMapping;
+import org.neo4j.gds.RelationshipType;
+import org.neo4j.gds.StoreLoaderBuilder;
 import org.neo4j.gds.api.Graph;
+import org.neo4j.gds.api.GraphStore;
+import org.neo4j.gds.core.GraphLoader;
 import org.neo4j.gds.core.utils.paged.HugeObjectArray;
 import org.neo4j.gds.core.utils.progress.tasks.ProgressTracker;
 import org.neo4j.gds.extension.GdlExtension;
 import org.neo4j.gds.extension.GdlGraph;
 import org.neo4j.gds.extension.Inject;
 import org.neo4j.gds.ml.core.features.FeatureExtraction;
-
-import java.util.List;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
+import org.neo4j.gds.ml.core.features.FeatureExtractor;
 
 @GdlExtension
 public class FastRPGdlTest {
 
+    private static final int DEFAULT_EMBEDDING_DIMENSION = 128;
+    private static final FastRPBaseConfig DEFAULT_CONFIG = FastRPBaseConfig.builder()
+            .embeddingDimension(DEFAULT_EMBEDDING_DIMENSION)
+            .propertyRatio(0.5)
+            .featureProperties(List.of("f1", "f2", "f3"))
+            .addIterationWeight(1.0D)
+            .randomSeed(42L)
+            .build();
 
     @GdlGraph(graphNamePrefix = "array")
     private static final String X =
-        "CREATE" +
-        "  (a:N {f: [0.4, 1.3, 1.4]})" +
-        ", (b:N {f: [2.1, 0.5, 1.8]})" +
-        ", (c:N {f: [-0.3, 0.8, 2.8]})" +
-        ", (a)-[:REL {weight: 2.0}]->(b)" +
-        ", (b)-[:REL {weight: 1.0}]->(a)" +
-        ", (a)-[:REL {weight: 1.0}]->(c)" +
-        ", (c)-[:REL {weight: 1.0}]->(a)" +
-        ", (b)-[:REL {weight: 1.0}]->(c)" +
-        ", (c)-[:REL {weight: 1.0}]->(b)";
+            "CREATE" +
+                    "  (a:Node1 {f: [0.4, 1.3, 1.4]})" +
+                    ", (b:Node1 {f: [2.1, 0.5, 1.8]})" +
+                    ", (c:Node2 {f: [-0.3, 0.8, 2.8]})" +
+                    ", (a)-[:REL {weight: 2.0}]->(b)" +
+                    ", (b)-[:REL {weight: 1.0}]->(a)" +
+                    ", (a)-[:REL {weight: 1.0}]->(c)" +
+                    ", (c)-[:REL {weight: 1.0}]->(a)" +
+                    ", (b)-[:REL {weight: 1.0}]->(c)" +
+                    ", (c)-[:REL {weight: 1.0}]->(b)";
 
     @GdlGraph(graphNamePrefix = "scalar")
     private static final String Y =
-        "CREATE" +
-        "  (a:N {f1: 0.4, f2: 1.3, f3: 1.4})" +
-        ", (b:N {f1: 2.1, f2: 0.5, f3: 1.8})" +
-        ", (c:N {f1: -0.3, f2: 0.8, f3: 2.8})" +
-        ", (a)-[:REL {weight: 2.0}]->(b)" +
-        ", (b)-[:REL {weight: 1.0}]->(a)" +
-        ", (a)-[:REL {weight: 1.0}]->(c)" +
-        ", (c)-[:REL {weight: 1.0}]->(a)" +
-        ", (b)-[:REL {weight: 1.0}]->(c)" +
-        ", (c)-[:REL {weight: 1.0}]->(b)";
+            "CREATE" +
+                    "  (a:Node1 {f1: 0.4, f2: 1.3, f3: 1.4})" +
+                    ", (b:Node1 {f1: 2.1, f2: 0.5, f3: 1.8})" +
+                    ", (c:Node2 {f1: -0.3, f2: 0.8, f3: 2.8})" +
+                    ", (a)-[:REL {weight: 2.0}]->(b)" +
+                    ", (b)-[:REL {weight: 1.0}]->(a)" +
+                    ", (a)-[:REL {weight: 1.0}]->(c)" +
+                    ", (c)-[:REL {weight: 1.0}]->(a)" +
+                    ", (b)-[:REL {weight: 1.0}]->(c)" +
+                    ", (c)-[:REL {weight: 1.0}]->(b)";
 
     @Inject
     Graph scalarGraph;
 
     @Inject
     Graph arrayGraph;
+
+    @Inject
+    GraphStore scalarGraphStore;
 
     @Test
     void shouldYieldSameResultsForScalarAndArrayProperties() {
@@ -81,20 +101,78 @@ public class FastRPGdlTest {
         }
     }
 
-    private HugeObjectArray<float[]> embeddings(Graph graph, List<String> properties) {
-        var arrayConfig = ImmutableFastRPStreamConfig.builder()
-            .addAllFeatureProperties(properties)
-            .embeddingDimension(64)
-            .propertyRatio(0.5)
-            .build();
+    @Test
+    void shouldSwapInitialRandomVectors() {
+        final var graph = scalarGraphStore.getGraph(
+                NodeLabel.of("Node1"),
+                RelationshipType.of("REL"),
+                Optional.empty()
+        );
 
+        FastRP fastRP = new FastRP(
+                graph,
+                DEFAULT_CONFIG,
+                defaultFeatureExtractors(graph),
+                ProgressTracker.NULL_TRACKER
+        );
+
+        fastRP.initDegreePartition();
+        fastRP.initPropertyVectors();
+        fastRP.initRandomVectors();
+        HugeObjectArray<float[]> randomVectors = HugeObjectArray.newArray(float[].class, 2);
+        fastRP.currentEmbedding(-1).copyTo(randomVectors, 2);
+        fastRP.propagateEmbeddings();
+        HugeObjectArray<float[]> embeddings = fastRP.embeddings();
+
+        float[] expected = randomVectors.get(1);
+        l2Normalize(expected);
+
+        assertThat(embeddings.get(0)).isEqualTo(expected);
+    }
+
+    @Test
+    void shouldAverageNeighbors() {
+        final var graph = scalarGraphStore.getGraph(
+                List.of(NodeLabel.of("Node1"), NodeLabel.of("Node2")),
+                List.of(RelationshipType.of("REL")),
+                Optional.empty()
+        );
+
+        FastRP fastRP = new FastRP(
+                graph,
+                DEFAULT_CONFIG,
+                defaultFeatureExtractors(graph),
+                ProgressTracker.NULL_TRACKER
+        );
+
+        fastRP.initDegreePartition();
+        fastRP.initPropertyVectors();
+        fastRP.initRandomVectors();
+        HugeObjectArray<float[]> randomVectors = HugeObjectArray.newArray(float[].class, 3);
+        fastRP.currentEmbedding(-1).copyTo(randomVectors, 3);
+        fastRP.propagateEmbeddings();
+        HugeObjectArray<float[]> embeddings = fastRP.embeddings();
+
+        float[] expected = new float[DEFAULT_EMBEDDING_DIMENSION];
+        for (int i = 0; i < DEFAULT_EMBEDDING_DIMENSION; i++) {
+            expected[i] = (randomVectors.get(1)[i] + randomVectors.get(2)[i]) / 2.0f;
+        }
+        l2Normalize(expected);
+
+        assertThat(embeddings.get(0)).containsExactly(expected);
+    }
+
+    private HugeObjectArray<float[]> embeddings(Graph graph, List<String> properties) {
         var fastRPArray = new FastRP(
-            graph,
-            arrayConfig,
-            FeatureExtraction.propertyExtractors(graph, properties),
-            ProgressTracker.NULL_TRACKER,
-            Optional.of(123L)
+                graph,
+                DEFAULT_CONFIG,
+                FeatureExtraction.propertyExtractors(graph, properties),
+                ProgressTracker.NULL_TRACKER
         );
         return fastRPArray.compute().embeddings();
+    }
+
+    private List<FeatureExtractor> defaultFeatureExtractors(Graph graph) {
+        return FeatureExtraction.propertyExtractors(graph, DEFAULT_CONFIG.featureProperties());
     }
 }
