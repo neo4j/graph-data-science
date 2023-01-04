d54b0257205f9c5f50897ed4e248d368005d8b76
Sören Reichardt <soren.reichardt@neotechnology.com>
2023-01-04 10:38:03 +0100

    Block write-back when executed on composite db

---
+++ cypher-aggregation/src/main/java/org/neo4j/gds/projection/DatabaseTopologyHelper.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.projection;
+
+import org.neo4j.dbms.api.DatabaseManagementService;
+import org.neo4j.dbms.systemgraph.CommunityTopologyGraphDbmsModel;
+import org.neo4j.gds.compat.GraphDatabaseApiProxy;
+import org.neo4j.gds.core.Settings;
+import org.neo4j.graphdb.GraphDatabaseService;
+import org.neo4j.kernel.database.DatabaseReference;
+
+import java.util.stream.Collectors;
+
+public final class DatabaseTopologyHelper {
+
+    private DatabaseTopologyHelper() {}
+
+    public static boolean isCompositeDatabase(GraphDatabaseService graphDatabaseService) {
+        var dbms = GraphDatabaseApiProxy.resolveDependency(graphDatabaseService, DatabaseManagementService.class);
+        var systemDb = dbms.database(Settings.systemDatabaseName());
+        try (var tx = systemDb.beginTx()) {
+            var databaseTopology = new CommunityTopologyGraphDbmsModel(tx);
+            var compositeDatabases = databaseTopology.getAllCompositeDatabaseReferences()
+                .stream()
+                .map(DatabaseReference.Composite::databaseId)
+                .collect(Collectors.toSet());
+
+            return compositeDatabases.contains(GraphDatabaseApiProxy.databaseId(graphDatabaseService));
+        }
+    }
+}
