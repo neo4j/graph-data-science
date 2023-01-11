9ddb06cf62963f3ca2dbe3455e498c08bf415421
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2023-01-11 13:51:11 +0100

    Add tests to show two problems with KSpanningTree
    Co-Authored-By: Ioannis Panagiotas <ioannis.panagiotas@neotechnology.com>

--- alpha/alpha-algo/src/test/java/org/neo4j/gds/impl/spanningtree/KSpanningTreeTest.java
+++ alpha/alpha-algo/src/test/java/org/neo4j/gds/impl/spanningtree/KSpanningTreeTest.java
@@ -19,6 +19,7 @@
  */
 package org.neo4j.gds.impl.spanningtree;
 
+import org.apache.commons.lang3.mutable.MutableLong;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
@@ -29,9 +30,13 @@ import org.neo4j.gds.extension.GdlExtension;
 import org.neo4j.gds.extension.GdlGraph;
 import org.neo4j.gds.extension.IdFunction;
 import org.neo4j.gds.extension.Inject;
+import org.neo4j.gds.gdl.GdlFactory;
 import org.neo4j.gds.spanningtree.Prim;
 import org.neo4j.gds.spanningtree.SpanningTree;
 
+import java.util.HashSet;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
@@ -123,5 +128,79 @@ class KSpanningTreeTest {
             .compute();
 
         assertEquals(spanningTree, otherSpanningTree);
+    }
+
+    @Test
+    void shouldProduceSingleConnectedTree() {
+        var factory = GdlFactory.of("CREATE" +
+                                    "  (a:Node)" +
+                                    ", (b:Node)" +
+                                    ", (c:Node)" +
+                                    ", (d:Node)" +
+                                    ", (e:Node)" +
+                                    ", (a)-[:TYPE {cost: 1.0}]->(b)" +
+                                    ", (b)-[:TYPE {cost: 20.0}]->(c)" +
+                                    ", (c)-[:TYPE {cost: 30.0}]->(d)" +
+                                    ", (d)-[:TYPE {cost: 1.0}]->(e)"
+        );
+        var graph = factory.build().getUnion();
+        var startNode = factory.nodeId("a");
+
+        var k = 3;
+        var spanningTree = new KSpanningTree(
+            graph,
+            Prim.MIN_OPERATOR,
+            startNode,
+            k,
+            ProgressTracker.NULL_TRACKER
+        ).compute();
+
+        // if there are more than k nodes then there is more than one root
+        // meaning there is more than one tree (or the tree is broken)
+        var nodesInTree = new HashSet<Long>();
+        spanningTree.forEach((s, t, __) -> {
+            nodesInTree.add(s);
+            nodesInTree.add(t);
+            return true;
+        });
+
+        assertThat(nodesInTree.size()).isEqualTo(k);
+    }
+
+    @Test
+    void shouldProduceSingleTreeWithKMinusOneEdges() {
+        var factory = GdlFactory.of("CREATE" +
+                                    "  (a:Node)" +
+                                    ", (b:Node)" +
+                                    ", (c:Node)" +
+                                    ", (d:Node)" +
+                                    ", (e:Node)" +
+                                    ", (f:Node)" +
+                                    ", (a)-[:TYPE {cost: 1.0}]->(b)" +
+                                    ", (b)-[:TYPE {cost: 20.0}]->(c)" +
+                                    ", (c)-[:TYPE {cost: 30.0}]->(d)" +
+                                    ", (d)-[:TYPE {cost: 1.0}]->(e)" +
+                                    ", (e)-[:TYPE {cost: 1.0}]->(f)"
+        );
+        var graph = factory.build().getUnion();
+        var startNode = factory.nodeId("a");
+
+        var k = 2;
+
+        var spanningTree = new KSpanningTree(
+            graph,
+            Prim.MIN_OPERATOR,
+            startNode,
+            2,
+            ProgressTracker.NULL_TRACKER
+        ).compute();
+
+        var counter = new MutableLong(0);
+        spanningTree.forEach((__, ___, ____) -> {
+            counter.add(1);
+            return true;
+        });
+
+        assertThat(counter.getValue()).isEqualTo(k - 1);
     }
 }
