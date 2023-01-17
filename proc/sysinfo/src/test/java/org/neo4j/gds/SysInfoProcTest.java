e43eed7adc23a8c75378dce08ce5152749c63fe9
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2023-01-17 14:23:54 +0100

    Fix test

--- proc/sysinfo/src/test/java/org/neo4j/gds/SysInfoProcTest.java
+++ proc/sysinfo/src/test/java/org/neo4j/gds/SysInfoProcTest.java
@@ -66,7 +66,12 @@ class SysInfoProcTest extends BaseProcTest {
         "Neo4j 5.3",
         "Neo4j 5.3 (placeholder)",
         "Neo4j Settings 5.3",
-        "Neo4j Settings 5.3 (placeholder)"
+        "Neo4j Settings 5.3 (placeholder)",
+
+        "Neo4j 5.4",
+        "Neo4j 5.4 (placeholder)",
+        "Neo4j Settings 5.4",
+        "Neo4j Settings 5.4 (placeholder)"
     );
 
     @BeforeEach
@@ -145,6 +150,7 @@ class SysInfoProcTest extends BaseProcTest {
                     "Neo4j 5.3 (placeholder)",
                     "Neo4j 5.3"
                 );
+                break;
             case V_5_4:
                 expectedCompatibilities = Set.of(
                     "Neo4j Settings 5.4 (placeholder)",
