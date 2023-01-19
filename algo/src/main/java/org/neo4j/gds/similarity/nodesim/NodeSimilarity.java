3c92ada59e949258910cdf93c1e14386372546f8
Florentin Dörre <florentin.dorre@neotechnology.com>
2023-01-19 10:43:57 +0100

    Deduplicate progress logging in prepare

--- algo/src/main/java/org/neo4j/gds/similarity/nodesim/NodeSimilarity.java
+++ algo/src/main/java/org/neo4j/gds/similarity/nodesim/NodeSimilarity.java
@@ -207,6 +207,7 @@ public class NodeSimilarity extends Algorithm<NodeSimilarityResult> {
             degreeComputer.reset();
             vectorComputer.reset(degree);
 
+            progressTracker.logProgress(graph.degree(node));
             if (degree >= config.degreeCutoff()) {
                 if (sourceNodeFilter.test(node)) {
                     sourceNodes.set(node);
@@ -216,7 +217,6 @@ public class NodeSimilarity extends Algorithm<NodeSimilarityResult> {
                 }
 
                 // TODO: we don't need to do the rest of the prepare for a node that isn't going to be used in the computation
-                progressTracker.logProgress(graph.degree(node));
                 vectorComputer.forEachRelationship(node);
                 if (weighted) {
                     weights.set(node, vectorComputer.getWeights());
@@ -226,8 +226,6 @@ public class NodeSimilarity extends Algorithm<NodeSimilarityResult> {
                 }
                 return vectorComputer.targetIds.buffer;
             }
-
-            progressTracker.logProgress(graph.degree(node));
             return null;
         });
         nodesToCompare = sourceNodes.cardinality();
