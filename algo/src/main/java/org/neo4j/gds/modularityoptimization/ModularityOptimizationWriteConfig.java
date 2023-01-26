24be1b32258f1c3a07f617a71e0971c2d3b2636d
Ertjon Mecka <ertjonmecka@gmail.com>
2023-01-27 00:34:27 +0100

    added minCommunitySize to Modularity Optimization

--- algo/src/main/java/org/neo4j/gds/modularityoptimization/ModularityOptimizationWriteConfig.java
+++ algo/src/main/java/org/neo4j/gds/modularityoptimization/ModularityOptimizationWriteConfig.java
@@ -21,13 +21,14 @@ package org.neo4j.gds.modularityoptimization;
 
 import org.neo4j.gds.annotation.Configuration;
 import org.neo4j.gds.annotation.ValueClass;
+import org.neo4j.gds.config.CommunitySizeConfig;
 import org.neo4j.gds.config.WritePropertyConfig;
 import org.neo4j.gds.core.CypherMapWrapper;
 
 @Configuration
 @ValueClass
 @SuppressWarnings("immutables:subtype")
-public interface ModularityOptimizationWriteConfig extends ModularityOptimizationConfig, WritePropertyConfig {
+public interface ModularityOptimizationWriteConfig extends ModularityOptimizationConfig, WritePropertyConfig, CommunitySizeConfig {
 
     static ModularityOptimizationWriteConfig of(CypherMapWrapper config) {
         return new ModularityOptimizationWriteConfigImpl(config);
