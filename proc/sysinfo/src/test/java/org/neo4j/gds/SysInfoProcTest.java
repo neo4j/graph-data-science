ccd3af395fafe5123517ecc50e12dca69e131b23
Jonatan Jäderberg <jonatan.jaderberg@neotechnology.com>
2023-02-07 15:49:37 +0100

    Fix tests

--- proc/sysinfo/src/test/java/org/neo4j/gds/SysInfoProcTest.java
+++ proc/sysinfo/src/test/java/org/neo4j/gds/SysInfoProcTest.java
@@ -71,6 +71,11 @@ class SysInfoProcTest extends BaseProcTest {
         "Neo4j Settings 5.4",
         "Neo4j Settings 5.4 (placeholder)",
 
+        "Neo4j 5.5",
+        "Neo4j 5.5 (placeholder)",
+        "Neo4j Settings 5.5",
+        "Neo4j Settings 5.5 (placeholder)",
+
         "Neo4j DEV",
         "Neo4j DEV (placeholder)",
         "Neo4j Settings DEV",
