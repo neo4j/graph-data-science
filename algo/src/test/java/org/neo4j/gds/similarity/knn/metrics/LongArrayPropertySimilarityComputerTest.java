9ac61e3fec87f472f43bcbb3b1ffca232beba163
Florentin Dörre <florentin.dorre@neotechnology.com>
2023-03-02 15:37:03 +0100

    Prove bug in current size() usage

---
+++ algo/src/test/java/org/neo4j/gds/similarity/knn/metrics/LongArrayPropertySimilarityComputerTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.similarity.knn.metrics;
+
+import org.junit.jupiter.api.Test;
+import org.neo4j.gds.api.properties.nodes.LongArrayNodePropertyValues;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LongArrayPropertySimilarityComputerTest {
+
+    @Test
+    void usingSparseProperties() {
+        long[][] inputValues = {{42, 24}, null, {84, 48}};
+
+        var sparseProperties = new LongArrayNodePropertyValues() {
+            @Override
+            public long[] longArrayValue(long nodeId) {
+                return inputValues[(int) nodeId];
+            }
+
+            @Override
+            public long valuesStored() {
+                return 2;
+            }
+        };
+
+        var sortedValues = new LongArrayPropertySimilarityComputer.SortedLongArrayPropertyValues(sparseProperties);
+
+        assertThat(sortedValues.longArrayValue(0)).containsExactly(24, 42);
+        assertThat(sortedValues.longArrayValue(1)).isNull();
+        assertThat(sortedValues.longArrayValue(2)).containsExactly(48, 84);
+    }
+
+}
--- proc/similarity/src/test/java/org/neo4j/gds/similarity/knn/KnnStreamProcTest.java
+++ proc/similarity/src/test/java/org/neo4j/gds/similarity/knn/KnnStreamProcTest.java
@@ -94,4 +94,32 @@ class KnnStreamProcTest extends KnnProcTest<KnnStreamConfig> {
             Map.of("node1", 7L, "node2", 6L, "similarity", 1.0)
         ));
     }
+
+    @Test
+    void computeOverSparseNodeProperties() {
+        String nodeCreateQuery =
+            "CREATE " +
+            "  (alice:Person {grades: [24, 4]})" +
+            " ,(carol:Person)" +
+            " ,(eve:Person)" +
+            " ,(bob:Foo {grades: [24, 4, 42]})";
+
+        runQuery(nodeCreateQuery);
+
+        String createQuery = "CALL gds.graph.project('graph', " +
+                             "'Person', '*', {nodeProperties: {grades: {defaultValue: [1, 1]}}})";
+        runQuery(createQuery);
+
+        String algoQuery = GdsCypher.call("graph")
+            .algo("gds.knn")
+            .streamMode()
+            .addParameter("nodeLabels", List.of("Person"))
+            .addParameter("nodeProperties", List.of("grades"))
+            .yields("node1", "node2", "similarity");
+
+        assertCypherResult(algoQuery, List.of(
+            Map.of("node1", 6L, "node2", 7L, "similarity", 1.0),
+            Map.of("node1", 7L, "node2", 6L, "similarity", 1.0)
+        ));
+    }
 }
