8ea03cf36255f93370963c868e59140d96b4d559
Florentin Dörre <florentin.dorre@neotechnology.com>
2023-03-06 17:06:17 +0100

    Move IdentityPropertyValues to test-utils
    Its only used in test, so move away from core

--- test-utils/src/main/java/org/neo4j/gds/nodeproperties/IdentityPropertyValues.java
+++ test-utils/src/main/java/org/neo4j/gds/nodeproperties/IdentityPropertyValues.java
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.neo4j.gds.core.utils;
+package org.neo4j.gds.nodeproperties;
 
 import org.neo4j.gds.api.properties.nodes.LongNodePropertyValues;
 
