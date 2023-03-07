e355a7933f33ef20c80d675d3bd6fb0a2fe59e20
Florentin Dörre <florentin.dorre@neotechnology.com>
2023-03-07 10:24:00 +0100

    Use capacity for default value computation
    As the size is now the nodeCount and not the number of values set.

--- core/src/main/java/org/neo4j/gds/core/loading/nodeproperties/LongNodePropertiesBuilder.java
+++ core/src/main/java/org/neo4j/gds/core/loading/nodeproperties/LongNodePropertiesBuilder.java
@@ -128,7 +128,7 @@ public class LongNodePropertiesBuilder implements InnerNodePropertiesBuilder {
 
         var propertyValues = propertiesByMappedIdsBuilder.build();
 
-        var maybeMaxValue = size > 0
+        var maybeMaxValue = propertyValues.capacity() > 0
             ? OptionalLong.of((long) MAX_VALUE.getVolatile(LongNodePropertiesBuilder.this))
             : OptionalLong.empty();
 
