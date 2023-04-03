059464ece857fc7510583b3d611aff6714c2d35f
ioannispan <ioannis.panagiotas@neotechnology.com>
2023-04-03 11:57:13 +0200

    Port HashGNN Mutate to Spec

---
+++ proc/embeddings/src/main/java/org/neo4j/gds/embeddings/hashgnn/MutateResult.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.embeddings.hashgnn;
+
+import org.neo4j.gds.result.AbstractResultBuilder;
+
+import java.util.Map;
+
+@SuppressWarnings("unused")
+public final class MutateResult  {
+
+    public final long nodePropertiesWritten;
+    public final long mutateMillis;
+    public final long nodeCount;
+    public final long preProcessingMillis;
+    public final long computeMillis;
+    public final Map<String, Object> configuration;
+
+    MutateResult(
+        long nodeCount,
+        long nodePropertiesWritten,
+        long preProcessingMillis,
+        long computeMillis,
+        long mutateMillis,
+        Map<String, Object> config
+    ) {
+        this.nodeCount = nodeCount;
+        this.nodePropertiesWritten = nodePropertiesWritten;
+        this.preProcessingMillis = preProcessingMillis;
+        this.computeMillis = computeMillis;
+        this.mutateMillis = mutateMillis;
+        this.configuration = config;
+    }
+
+    static final class Builder extends AbstractResultBuilder<MutateResult> {
+
+        @Override
+        public MutateResult build() {
+            return new MutateResult(
+                nodeCount,
+                nodePropertiesWritten,
+                preProcessingMillis,
+                computeMillis,
+                mutateMillis,
+                config.toMap()
+            );
+        }
+    }
+}
--- proc/embeddings/src/test/java/org/neo4j/gds/embeddings/hashgnn/HashGNNMutateProcTest.java
+++ proc/embeddings/src/test/java/org/neo4j/gds/embeddings/hashgnn/HashGNNMutateProcTest.java
@@ -19,51 +19,102 @@
  */
 package org.neo4j.gds.embeddings.hashgnn;
 
-import org.neo4j.gds.AlgoBaseProc;
-import org.neo4j.gds.MutateNodePropertyTest;
-import org.neo4j.gds.api.nodeproperties.ValueType;
-import org.neo4j.gds.core.CypherMapWrapper;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.neo4j.gds.BaseProcTest;
+import org.neo4j.gds.GdsCypher;
+import org.neo4j.gds.api.DatabaseId;
+import org.neo4j.gds.api.Graph;
+import org.neo4j.gds.catalog.GraphProjectProc;
+import org.neo4j.gds.core.loading.GraphStoreCatalog;
+import org.neo4j.gds.extension.Neo4jGraph;
+import org.neo4j.gds.extension.TestGraph;
 
-class HashGNNMutateProcTest extends HashGNNProcTest<HashGNNMutateConfig> implements
-    MutateNodePropertyTest<HashGNN, HashGNNMutateConfig, HashGNNResult> {
+import java.util.List;
+import java.util.Map;
 
-    @Override
-    public CypherMapWrapper createMinimalConfig(CypherMapWrapper userInput) {
-        var minimalConfig = super.createMinimalConfig(userInput);
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.InstanceOfAssertFactories.LONG;
+import static org.neo4j.gds.TestSupport.assertGraphEquals;
+import static org.neo4j.gds.TestSupport.fromGdl;
 
-        if (!minimalConfig.containsKey("mutateProperty")) {
-            return minimalConfig.withString("mutateProperty", "embedding");
-        }
-        return minimalConfig;
-    }
+class HashGNNMutateProcTest extends BaseProcTest {
 
-    @Override
-    public Class<? extends AlgoBaseProc<HashGNN, HashGNNResult, HashGNNMutateConfig, ?>> getProcedureClazz() {
-        return HashGNNMutateProc.class;
-    }
-
-    @Override
-    public HashGNNMutateConfig createConfig(CypherMapWrapper mapWrapper) {
-        return HashGNNMutateConfig.of(mapWrapper);
-    }
-
-    @Override
-    public String mutateProperty() {
-        return "embedding";
-    }
-
-    @Override
-    public ValueType mutatePropertyType() {
-        return ValueType.DOUBLE_ARRAY;
-    }
-
-    @Override
-    public String expectedMutatedGraph() {
-        return "CREATE" +
-        "  (a {f1: 1, f2: [0.0, 0.0], embedding: [1.0, 0.0, 0.0]})" +
-        ", (b {f1: 0, f2: [1.0, 0.0], embedding: [1.0, 0.0, 1.0]})" +
-        ", (c {f1: 0, f2: [0.0, 1.0], embedding: [0.0, 0.0, 1.0]})" +
+    @Neo4jGraph
+    private static final String DB_CYPHER =
+        "CREATE" +
+        "  (a:N {f1: 1, f2: [0.0, 0.0]})" +
+        ", (b:N {f1: 0, f2: [1.0, 0.0]})" +
+        ", (c:N {f1: 0, f2: [0.0, 1.0]})" +
         ", (b)-[:R1]->(a)" +
         ", (b)-[:R2]->(c)";
+
+    static String expectedMutatedGraph = "CREATE" +
+                                         "  (a:N {f1: 1, f2: [0.0, 0.0], embedding: [1.0, 0.0, 0.0]})" +
+                                         ", (b:N {f1: 0, f2: [1.0, 0.0], embedding: [1.0, 0.0, 1.0]})" +
+                                         ", (c:N {f1: 0, f2: [0.0, 1.0], embedding: [0.0, 0.0, 1.0]})" +
+                                         ", (b)-[:R1]->(a)" +
+                                         ", (b)-[:R2]->(c)";
+
+    @BeforeEach
+    void setupGraph() throws Exception {
+        registerProcedures(
+            HashGNNMutateProc.class,
+            GraphProjectProc.class
+        );
+
+        String graphCreateQuery = GdsCypher.call("graph")
+            .graphProject()
+            .withNodeLabel("N")
+            .withNodeProperty("f1")
+            .withNodeProperty("f2")
+            .withRelationshipType("R1")
+            .withRelationshipType("R2")
+            .yields();
+        runQuery(graphCreateQuery);
     }
+
+    @Test
+    void shouldMutate() {
+        var query = GdsCypher.call("graph").algo("gds.beta.hashgnn")
+            .mutateMode().addParameter("heterogeneous", true)
+            .addParameter("iterations", 2)
+            .addParameter("embeddingDensity", 2)
+            .addParameter("randomSeed", 42L)
+            .addParameter("featureProperties", List.of("f1", "f2"))
+            .addParameter("mutateProperty", "embedding")
+            .yields();
+
+        var rowCount = runQueryWithRowConsumer(query, row -> {
+            assertThat(row.getNumber("preProcessingMillis"))
+                .asInstanceOf(LONG)
+                .isGreaterThan(-1L);
+
+            assertThat(row.getNumber("computeMillis"))
+                .asInstanceOf(LONG)
+                .isGreaterThan(-1L);
+
+            assertThat(row.getNumber("mutateMillis"))
+                .asInstanceOf(LONG)
+                .isGreaterThan(-1L);
+
+            assertThat(row.getNumber("nodePropertiesWritten"))
+                .asInstanceOf(LONG)
+                .isEqualTo(3L);
+
+            assertThat(row.getNumber("nodeCount"))
+                .asInstanceOf(LONG)
+                .isEqualTo(3L);
+
+            assertThat(row.get("configuration"))
+                .isInstanceOf(Map.class);
+        });
+
+        assertThat(rowCount).isEqualTo(1L);
+
+        Graph mutatedGraph = GraphStoreCatalog.get(getUsername(), DatabaseId.of(db), "graph").graphStore().getUnion();
+        TestGraph expected = fromGdl(expectedMutatedGraph);
+        assertGraphEquals(expected, mutatedGraph);
+    }
+
 }
