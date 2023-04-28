cfa415fb25409b86be071f1da3ab3b1970f588b0
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2023-04-28 05:54:14 +0100

    Test un/registering tasks where it belongs

--- executor/src/test/java/org/neo4j/gds/executor/TestMutateSpec.java
+++ executor/src/test/java/org/neo4j/gds/executor/TestMutateSpec.java
@@ -53,6 +53,7 @@ public class TestMutateSpec implements AlgorithmSpec<TestAlgorithm, TestAlgorith
             .withMutateMillis(42)
             .withWriteMillis(42)
             .withNodePropertiesWritten(42)
+            .withConfig(computationResult.config())
             .build();
     }
 }
