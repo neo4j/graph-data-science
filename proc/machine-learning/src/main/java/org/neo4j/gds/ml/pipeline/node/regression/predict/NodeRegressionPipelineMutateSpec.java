8eb5b056a3692645be035fc696aa82d3e54e5f59
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2023-05-05 06:16:37 +0100

    Migrate NodeRegressionPipelineStreamProc to specification

--- proc/machine-learning/src/main/java/org/neo4j/gds/ml/pipeline/node/regression/predict/NodeRegressionPipelineMutateSpec.java
+++ proc/machine-learning/src/main/java/org/neo4j/gds/ml/pipeline/node/regression/predict/NodeRegressionPipelineMutateSpec.java
@@ -21,7 +21,6 @@ package org.neo4j.gds.ml.pipeline.node.regression.predict;
 
 import org.neo4j.gds.MutatePropertyComputationResultConsumer;
 import org.neo4j.gds.api.properties.nodes.NodePropertyValues;
-import org.neo4j.gds.core.model.Model;
 import org.neo4j.gds.core.utils.paged.HugeDoubleArray;
 import org.neo4j.gds.core.write.NodeProperty;
 import org.neo4j.gds.executor.AlgorithmSpec;
@@ -30,13 +29,10 @@ import org.neo4j.gds.executor.ComputationResultConsumer;
 import org.neo4j.gds.executor.ExecutionContext;
 import org.neo4j.gds.executor.GdsCallable;
 import org.neo4j.gds.executor.NewConfigFunction;
-import org.neo4j.gds.ml.models.BaseModelData;
 import org.neo4j.gds.ml.pipeline.node.PredictMutateResult;
-import org.neo4j.gds.ml.pipeline.nodePipeline.NodePropertyPipelineBaseTrainConfig;
 
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.stream.Stream;
 
 import static org.neo4j.gds.executor.ExecutionMode.MUTATE_NODE_PROPERTY;
@@ -72,32 +68,7 @@ public class NodeRegressionPipelineMutateSpec
 
     @Override
     public Map<String, Object> preProcessConfig(Map<String, Object> userInput, ExecutionContext executionContext) {
-        return Optional.ofNullable(userInput.get("modelName"))
-            .map(modelName -> {
-                var modelCatalog = executionContext.modelCatalog();
-                assert modelCatalog != null : "ModelCatalog should have been set in the ExecutionContext by this point!!!";
-
-                var trainedModel = modelCatalog.get(
-                    executionContext.username(),
-                    (String) modelName,
-                    BaseModelData.class,
-                    NodePropertyPipelineBaseTrainConfig.class,
-                    Model.CustomInfo.class
-                );
-
-                var combinedTargetNodeLabels = Optional.ofNullable(userInput.get("targetNodeLabels"))
-                    .map(targetNodeLabels -> (List<String>) targetNodeLabels)
-                    .orElseGet(() -> trainedModel.trainConfig().targetNodeLabels());
-
-                var combinedRelationshipTypes = Optional.ofNullable(userInput.get("relationshipTypes"))
-                    .map(relationshipTypes -> (List<String>) relationshipTypes)
-                    .orElseGet(() -> trainedModel.trainConfig().relationshipTypes());
-
-                userInput.put("targetNodeLabels", combinedTargetNodeLabels);
-                userInput.put("relationshipTypes", combinedRelationshipTypes);
-                return userInput;
-            })
-            .orElse(userInput);
+        return NodeRegressionPipelineCompanion.enhanceUserInput(userInput, executionContext);
     }
 
     @Override
