7e51b9942d26893f8493a64f887d2aac0e080aea
Paul Horn <paul.horn@neotechnology.com>
2023-05-23 17:19:40 +0200

    Grow slower at the beginning
    Co-Authored-By: Max Kießling <max.kiessling@neotechnology.com>

--- core/src/main/java/org/neo4j/gds/core/loading/ChunkedAdjacencyLists.java
+++ core/src/main/java/org/neo4j/gds/core/loading/ChunkedAdjacencyLists.java
@@ -172,10 +172,16 @@ public final class ChunkedAdjacencyLists {
 
     static final int[] NEXT_CHUNK_LENGTH = {
         32,
+        32,
+        64,
         64,
         128,
+        128,
+        256,
         256,
         512,
+        512,
+        1024,
         1024,
         2048,
         4096,
