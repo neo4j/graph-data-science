9fdbd3b83150dcbbbc30c868f4d461e10571588f
Mats Rydberg <mats@neo4j.org>
2023-06-01 11:10:46 +0200

    Remove javadoc param without description

--- core/src/main/java/org/neo4j/gds/api/properties/nodes/NodePropertyValues.java
+++ core/src/main/java/org/neo4j/gds/api/properties/nodes/NodePropertyValues.java
@@ -99,8 +99,6 @@ public interface NodePropertyValues extends PropertyValues {
     }
 
     /**
-     *
-     * @param nodeId
      * @return Returns whether the value is present. This is necessary as for primitive types, we do not have a `null` value.
      */
     default boolean hasValue(long nodeId) {
