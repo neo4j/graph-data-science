aebd26dc072ed0804252d8771dc91079f8eca84f
Ioannis Pan <74839024+IoannisPanagiotas@users.noreply.github.com>
2023-06-07 15:07:29 +0200

    Update SyntaxMode.java

--- doc-test-tools/src/main/java/org/neo4j/gds/doc/syntax/SyntaxMode.java
+++ doc-test-tools/src/main/java/org/neo4j/gds/doc/syntax/SyntaxMode.java
@@ -74,10 +74,8 @@ public enum SyntaxMode {
     SYS_INFO("debug-sysinfo-syntax", false),
     WRITE_NODE_LABEL("include-with-write-node-label", false),
     MUTATE_NODE_LABEL("include-with-mutate-node-label", false),
-    GRAPH_GENERATE("include-with-graph-generate"),
-    ;
-
-
+    GRAPH_GENERATE("include-with-graph-generate"),;
+    
     private final String mode;
     public final boolean hasParameters;
     public final CustomProcedure.Namespace namespace;
