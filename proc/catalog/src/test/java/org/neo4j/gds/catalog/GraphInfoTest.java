462eb02c5f572b7bce79d8163370aea9d21cd8ac
Lasse Westh-Nielsen <lassewesth@gmail.com>
2023-06-20 12:59:24 +0200

    moar test migration

--- proc/catalog/src/test/java/org/neo4j/gds/catalog/GraphInfoTest.java
+++ proc/catalog/src/test/java/org/neo4j/gds/catalog/GraphInfoTest.java
@@ -20,6 +20,7 @@
 package org.neo4j.gds.catalog;
 
 import org.assertj.core.api.InstanceOfAssertFactories;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -31,13 +32,19 @@ import org.neo4j.gds.beta.generator.PropertyProducer;
 import org.neo4j.gds.beta.generator.RandomGraphGeneratorBuilder;
 import org.neo4j.gds.beta.generator.RelationshipDistribution;
 import org.neo4j.gds.config.GraphProjectConfig;
+import org.neo4j.gds.config.GraphProjectFromStoreConfig;
 import org.neo4j.gds.config.ImmutableGraphProjectFromStoreConfig;
+import org.neo4j.gds.core.CypherMapWrapper;
 import org.neo4j.gds.core.loading.CSRGraphStoreUtil;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Map;
 import java.util.Optional;
 import java.util.function.BiFunction;
 import java.util.stream.Stream;
 
+import static java.util.Collections.emptyMap;
 import static org.assertj.core.api.Assertions.as;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.provider.Arguments.arguments;
@@ -46,7 +53,10 @@ final class GraphInfoTest {
 
     @ParameterizedTest(name = "{1}")
     @MethodSource("producers")
-    void shouldIncludeMemoryUsageUnlessRequested(PropertyProducer<?> producer, @SuppressWarnings("unused") String displayName) {
+    void shouldIncludeMemoryUsageUnlessRequested(
+        PropertyProducer<?> producer,
+        @SuppressWarnings("unused") String displayName
+    ) {
         assertThat(
             create(
                 producer,
@@ -107,5 +117,83 @@ final class GraphInfoTest {
         );
 
         return constructor.apply(storeConfig, graphStore);
+    }
+
+    /**
+     * What is this you ask?
+     * <p>
+     * Well we used to have test of the drop procedure that asserted about these things,
+     * and the marshalling code for {configuration, store}->graph info is a bit convoluted.
+     * So for an abundance of caution I captured this.
+     */
+    @Test
+    void shouldConstructFromStoreWithoutMemoryUsage() {
+        var creationTime = ZonedDateTime.of(1969, 7, 20, 20, 17, 40, 0, ZoneId.of("UTC"));
+        var modificationTime = ZonedDateTime.of(1963, 8, 28, 17, 0, 0, 0, ZoneId.of("GMT-5"));
+        var graphProjectConfig = GraphProjectFromStoreConfig.of(
+            "some user",
+            "some graph",
+            "A",
+            "REL",
+            CypherMapWrapper.create(
+                Map.of(
+                    "creationTime", creationTime,
+                    "jobId", "some job"
+                )
+            )
+        );
+        var graphStore = new DummyGraphStore(modificationTime);
+        var graphInfo = GraphInfo.withoutMemoryUsage(graphProjectConfig, graphStore);
+
+        assertThat(graphInfo.creationTime).isEqualTo(creationTime);
+        assertThat(graphInfo.configuration).containsExactlyInAnyOrderEntriesOf(
+            Map.of(
+                "creationTime", creationTime,
+                "jobId", "some job",
+                "logProgress", true,
+                "nodeProjection", Map.of(
+                    "A", Map.of(
+                        "label", "A",
+                        "properties", emptyMap()
+                    )
+                ),
+                "nodeProperties", emptyMap(),
+                "readConcurrency", 4,
+                "relationshipProjection", Map.of(
+                    "REL", Map.of(
+                        "type", "REL",
+                        "orientation", "NATURAL",
+                        "aggregation", "DEFAULT",
+                        "indexInverse", false,
+                        "properties", emptyMap()
+                    )
+                ),
+                "relationshipProperties", emptyMap(),
+                "sudo", false,
+                "validateRelationships", false
+            )
+        );
+        assertThat(graphInfo.database).isEqualTo("some database");
+        assertThat(graphInfo.density).isEqualTo(0.5);
+        assertThat(graphInfo.graphName).isEqualTo("some graph");
+        assertThat(graphInfo.memoryUsage).isEqualTo("");
+        assertThat(graphInfo.modificationTime).isEqualTo(modificationTime);
+        assertThat(graphInfo.nodeCount).isEqualTo(2L);
+        assertThat(graphInfo.relationshipCount).isEqualTo(1L);
+        assertThat(graphInfo.schema).containsExactlyInAnyOrderEntriesOf(
+            Map.of(
+                "nodes", Map.of("A", Map.of()),
+                "relationships", Map.of("REL", Map.of()),
+                "graphProperties", Map.of()
+            )
+        );
+        assertThat(graphInfo.schemaWithOrientation).containsExactlyInAnyOrderEntriesOf(
+            Map.of(
+                "nodes", Map.of("A", Map.of()),
+                "relationships", Map.of("REL", Map.of("direction", "DIRECTED", "properties", Map.of())),
+                "graphProperties", Map.of()
+            )
+        );
+        assertThat(graphInfo.sizeInBytes).isEqualTo(-1L);
     }
 }
