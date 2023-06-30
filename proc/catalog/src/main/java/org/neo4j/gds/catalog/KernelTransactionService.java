395f089cc6e5cf293f571bf9015b7dac2a676d2b
Mats Rydberg <mats@neo4j.org>
2023-06-30 15:41:54 +0200

    Get the kernel transaction in the same way as Neo4j context injection
    Co-authored-by: Florentin Dörre <florentin.dorre@neotechnology.com>
    Co-authored-by: Lasse Westh-Nielsen <lassewesth@gmail.com>

--- proc/catalog/src/main/java/org/neo4j/gds/catalog/KernelTransactionService.java
+++ proc/catalog/src/main/java/org/neo4j/gds/catalog/KernelTransactionService.java
@@ -19,10 +19,9 @@
  */
 package org.neo4j.gds.catalog;
 
-import org.neo4j.graphdb.Transaction;
+import org.neo4j.internal.kernel.api.exceptions.ProcedureException;
 import org.neo4j.kernel.api.KernelTransaction;
 import org.neo4j.kernel.api.procedure.Context;
-import org.neo4j.kernel.impl.coreapi.InternalTransaction;
 
 public class KernelTransactionService {
     private final Context context;
@@ -31,9 +30,17 @@ public class KernelTransactionService {
         this.context = context;
     }
 
-    public KernelTransaction getKernelTransaction() {
-        Transaction transaction = context.graphDatabaseAPI().beginTx();
-        InternalTransaction it = (InternalTransaction) transaction;
-        return it.kernelTransaction();
+    KernelTransaction getKernelTransaction() {
+        try {
+            // this is not opening something new that we should close
+            // it only fetches the current transaction
+            // compare to what Neo4j does in: https://github.com/neo-technology/neo4j/blame/5.7.0/public/community/neo4j/src/main/java/org/neo4j/graphdb/facade/DatabaseManagementServiceFactory.java#L368-L369
+            // newer Neo4j versions offer a more direct method: https://github.com/neo-technology/neo4j/blob/e6a228f60efac4fd2584f5ec00de0207aad944ff/public/community/neo4j/src/main/java/org/neo4j/graphdb/facade/DatabaseManagementServiceFactory.java#L356
+            // we should make sure we do this in a compatible way
+            return context.internalTransaction().kernelTransaction();
+        } catch (ProcedureException e) {
+            // Neo4j itself throws a different exception here; see https://github.com/neo-technology/neo4j/blob/340b40bb956d077b3127c8ba8eb33f2d288bc844/public/community/procedure/src/main/java/org/neo4j/procedure/impl/FieldSetter.java#L44-L49
+            throw new RuntimeException(e);
+        }
     }
 }
