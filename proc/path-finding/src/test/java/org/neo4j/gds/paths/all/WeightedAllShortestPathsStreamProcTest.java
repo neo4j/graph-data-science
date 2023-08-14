e8841a23acf60a048d6af8f2b57b5cc5f0dfe4f8
ioannispan <ioannis.panagiotas@neotechnology.com>
2023-08-14 17:16:53 +0200

    Rewrite tests

--- proc/path-finding/src/test/java/org/neo4j/gds/paths/all/WeightedAllShortestPathsStreamProcTest.java
+++ proc/path-finding/src/test/java/org/neo4j/gds/paths/all/WeightedAllShortestPathsStreamProcTest.java
@@ -19,21 +19,21 @@
  */
 package org.neo4j.gds.paths.all;
 
+import org.apache.commons.lang3.mutable.MutableDouble;
+import org.assertj.core.data.Offset;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.neo4j.gds.BaseProcTest;
 import org.neo4j.gds.GdsCypher;
 import org.neo4j.gds.Orientation;
 import org.neo4j.gds.catalog.GraphProjectProc;
-import org.neo4j.graphdb.Label;
+import org.neo4j.gds.extension.IdFunction;
+import org.neo4j.gds.extension.Inject;
+import org.neo4j.gds.extension.Neo4jGraph;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-import static org.mockito.Mockito.eq;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.neo4j.gds.compat.GraphDatabaseApiProxy.runInTransaction;
+import java.util.concurrent.atomic.LongAdder;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 /**         5     5      5
  *      (1)---(2)---(3)----.
@@ -46,6 +46,7 @@ import static org.neo4j.gds.compat.GraphDatabaseApiProxy.runInTransaction;
  */
 final class WeightedAllShortestPathsStreamProcTest extends BaseProcTest {
 
+    @Neo4jGraph(offsetIds = true)
     private static final String DB_CYPHER =
             "CREATE" +
             "  (s:Node {name: 's'})" +
@@ -80,72 +81,89 @@ final class WeightedAllShortestPathsStreamProcTest extends BaseProcTest {
     private static long startNodeId;
     private static long targetNodeId;
 
+    @Inject
+    IdFunction idFunction;
+
+
     @BeforeEach
     void setup() throws Exception {
-        runQuery(DB_CYPHER);
+
         registerProcedures(AllShortestPathsStreamProc.class, GraphProjectProc.class);
-        runInTransaction(db, tx -> {
-            startNodeId = tx.findNode(Label.label("Node"), "name", "s").getId();
-            targetNodeId = tx.findNode(Label.label("Node"), "name", "x").getId();
-        });
+
+        startNodeId = idFunction.of("s");
+        targetNodeId = idFunction.of("x");
     }
 
     @Test
     void testMSBFSASP() {
-        final Consumer consumer = mock(Consumer.class);
 
         loadCompleteGraph(DEFAULT_GRAPH_NAME);
+
         String query = GdsCypher
             .call(DEFAULT_GRAPH_NAME)
-            .algo("gds", "alpha", "allShortestPaths")
+            .algo("gds.allShortestPaths")
             .streamMode()
             .yields();
 
-        runQueryWithRowConsumer(query, row -> {
+        MutableDouble wantedDistance = new MutableDouble();
+        LongAdder timesAppeared = new LongAdder();
+        var rowCount = runQueryWithRowConsumer(query, row -> {
             final long source = row.getNumber("sourceNodeId").longValue();
             final long target = row.getNumber("targetNodeId").longValue();
             final double distance = row.getNumber("distance").doubleValue();
-            assertNotEquals(Double.POSITIVE_INFINITY, distance);
+            assertThat(distance).isNotEqualTo(Double.POSITIVE_INFINITY);
             if (source == target) {
-                assertEquals(0.0, distance, 0.1);
+                assertThat(distance).isCloseTo(0, Offset.offset(1e-3));
             }
-            consumer.test(source, target, distance);
+            if (source == startNodeId && target == targetNodeId) {
+                timesAppeared.increment();
+                wantedDistance.setValue(distance);
+            }
         });
 
+        //11 nodes: 110 pairs
+        assertThat(rowCount).isEqualTo(110);
         // 4 steps from start to end max
-        verify(consumer, times(1)).test(eq(startNodeId), eq(targetNodeId), eq(4.0));
-
+        assertThat(timesAppeared.longValue()).isEqualTo(1l);
+        assertThat(wantedDistance.doubleValue()).isEqualTo(4.0);
     }
 
     @Test
     void testMSBFSASPIncoming() {
-        final Consumer consumer = mock(Consumer.class);
 
         loadCompleteGraph(DEFAULT_GRAPH_NAME, Orientation.REVERSE);
         String query = GdsCypher
             .call(DEFAULT_GRAPH_NAME)
-            .algo("gds", "alpha", "allShortestPaths")
+            .algo("gds.allShortestPaths")
             .streamMode()
             .yields();
 
-        runQueryWithRowConsumer(query, row -> {
+        MutableDouble wantedDistance = new MutableDouble();
+        LongAdder timesAppeared = new LongAdder();
+        var rowCount = runQueryWithRowConsumer(query, row -> {
             final long source = row.getNumber("sourceNodeId").longValue();
             final long target = row.getNumber("targetNodeId").longValue();
             final double distance = row.getNumber("distance").doubleValue();
-            assertNotEquals(Double.POSITIVE_INFINITY, distance);
+            assertThat(distance).isNotEqualTo(Double.POSITIVE_INFINITY);
             if (source == target) {
-                assertEquals(0.0, distance, 0.1);
+                assertThat(distance).isCloseTo(0, Offset.offset(1e-3));
             }
-            consumer.test(source, target, distance);
+            if (source == targetNodeId && target == startNodeId) {
+                timesAppeared.increment();
+                wantedDistance.setValue(distance);
+            }
         });
 
+        //11 nodes: 110 pairs
+        assertThat(rowCount).isEqualTo(110);
         // 4 steps from start to end max
-        verify(consumer, times(1)).test(eq(targetNodeId), eq(startNodeId), eq(4.0));
+        assertThat(timesAppeared.longValue()).isEqualTo(1l);
+        assertThat(wantedDistance.doubleValue()).isEqualTo(4.0);
+
     }
 
     @Test
     void testWeightedASP() {
-        final Consumer consumer = mock(Consumer.class);
 
         var createQuery = GdsCypher
             .call(DEFAULT_GRAPH_NAME)
@@ -157,28 +175,33 @@ final class WeightedAllShortestPathsStreamProcTest extends BaseProcTest {
 
         String query = GdsCypher
             .call(DEFAULT_GRAPH_NAME)
-            .algo("gds", "alpha", "allShortestPaths")
+            .algo("gds.allShortestPaths")
             .streamMode()
             .addParameter("relationshipWeightProperty", "cost")
             .yields();
 
-        runQueryWithRowConsumer(query, row -> {
+        MutableDouble wantedDistance = new MutableDouble();
+        LongAdder timesAppeared = new LongAdder();
+        var rowCount = runQueryWithRowConsumer(query, row -> {
             final long source = row.getNumber("sourceNodeId").longValue();
             final long target = row.getNumber("targetNodeId").longValue();
             final double distance = row.getNumber("distance").doubleValue();
-            assertNotEquals(Double.POSITIVE_INFINITY, distance);
+            assertThat(distance).isNotEqualTo(Double.POSITIVE_INFINITY);
             if (source == target) {
-                assertEquals(0.0, distance, 0.1);
+                assertThat(distance).isCloseTo(0, Offset.offset(1e-3));
             }
-            assertNotEquals(Double.POSITIVE_INFINITY, distance);
-            consumer.test(source, target, distance);
+            if (source == startNodeId && target == targetNodeId) {
+                timesAppeared.increment();
+                wantedDistance.setValue(distance);
+            }
         });
 
-        verify(consumer, times(1)).test(eq(startNodeId), eq(targetNodeId), eq(8.0));
+        //11 nodes: 110 pairs + 11 paths to itself
+        assertThat(rowCount).isEqualTo(121);
+        // 4 steps from start to end max
+        assertThat(timesAppeared.longValue()).isEqualTo(1l);
+        assertThat(wantedDistance.doubleValue()).isEqualTo(8.0);
 
     }
 
-    private interface Consumer {
-        void test(long source, long target, double distance);
-    }
 }
