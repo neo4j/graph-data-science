76c4c6244b4746d95dcb612977f109ec69a94626
ioannispan <ioannis.panagiotas@neotechnology.com>
2023-10-11 13:48:26 +0200

    Add estimate

--- proc/similarity/src/main/java/org/neo4j/gds/similarity/nodesim/NodeSimilarityStatsProc.java
+++ proc/similarity/src/main/java/org/neo4j/gds/similarity/nodesim/NodeSimilarityStatsProc.java
@@ -20,7 +20,6 @@
 package org.neo4j.gds.similarity.nodesim;
 
 import org.neo4j.gds.BaseProc;
-import org.neo4j.gds.executor.MemoryEstimationExecutor;
 import org.neo4j.gds.procedures.GraphDataScience;
 import org.neo4j.gds.procedures.similarity.SimilarityStatsResult;
 import org.neo4j.gds.results.MemoryEstimateResult;
@@ -53,10 +52,6 @@ public class NodeSimilarityStatsProc extends BaseProc {
         @Name(value = "graphNameOrConfiguration") Object graphNameOrConfiguration,
         @Name(value = "algoConfiguration") Map<String, Object> algoConfiguration
     ) {
-        return new MemoryEstimationExecutor<>(
-            new NodeSimilarityStatsSpecification(),
-            executionContext(),
-            transactionContext()
-        ).computeEstimate(graphNameOrConfiguration, algoConfiguration);
+        return facade.similarity().nodeSimilarityEstimateStats(graphNameOrConfiguration, algoConfiguration);
     }
 }
