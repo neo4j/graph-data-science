a72092b5d511357530de8cc1964946e5b0012617
ioannispan <ioannis.panagiotas@neotechnology.com>
2023-11-16 12:08:03 +0100

    Add Projection metric registrar
    Co-authored-by: Veselin Nikolov <veselin.nikolov@neotechnology.com>

---
+++ metrics-api/src/test/java/org/neo4j/gds/projections/ProjectionMetricsServiceTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.projections;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+class ProjectionMetricsServiceTest {
+
+    @Test
+    void shouldCreateNativeProjectionMetric() {
+        // given
+        var registrarMock = mock(ProjectionMetricRegistrar.class);
+        var metricsService = new ProjectionMetricsService(registrarMock);
+
+        // when
+        metricsService.createNative();
+
+        // then
+        verify(registrarMock, times(1)).create("native");
+        verifyNoMoreInteractions(registrarMock);
+    }
+
+    @Test
+    void shouldCreateCypherProjectionMetric() {
+        // given
+        var registrarMock = mock(ProjectionMetricRegistrar.class);
+        var metricsService = new ProjectionMetricsService(registrarMock);
+
+        // when
+        metricsService.createCypher();
+
+        // then
+        verify(registrarMock, times(1)).create("cypher");
+        verifyNoMoreInteractions(registrarMock);
+    }
+
+    @Test
+    void shouldCreateCypherV2ProjectionMetric() {
+        // given
+        var registrarMock = mock(ProjectionMetricRegistrar.class);
+        var metricsService = new ProjectionMetricsService(registrarMock);
+
+        // when
+        metricsService.createCypherV2();
+
+        // then
+        verify(registrarMock, times(1)).create("cypherV2");
+        verifyNoMoreInteractions(registrarMock);
+    }
+
+
+
+}
