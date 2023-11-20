5f480e167f1c7ecf89a42d0dd3e2f29ec8fcf42f
Florentin Dörre <florentin.dorre@neo4j.com>
2023-11-20 14:15:06 +0100

    Revert expected Neo4j 5.14 in test
    There is no 5.14 compat yet

--- neo4j-adapter/src/test/java/org/neo4j/gds/compat/Neo4jVersionTest.java
+++ neo4j-adapter/src/test/java/org/neo4j/gds/compat/Neo4jVersionTest.java
@@ -48,7 +48,7 @@ class Neo4jVersionTest {
         "5.11.0, V_5_11",
         "5.12.0, V_5_12",
         "5.13.0, V_5_13",
-        "5.14.0, V_5_14",
+        "5.14.0, V_Dev",
         "5.15.0, V_Dev",
     })
     void testParse(String input, Neo4jVersion expected) {
