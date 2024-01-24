62bc655e651290b46d2bc29e2b360d3b87307b7c
Jonatan Jäderberg <jonatan.jaderberg@neo4j.com>
2024-01-24 10:53:55 +0100

    Remove some unused code

--- proc/community/src/main/java/org/neo4j/gds/triangle/LocalClusteringCoefficientCompanion.java
+++ proc/community/src/main/java/org/neo4j/gds/triangle/LocalClusteringCoefficientCompanion.java
@@ -19,70 +19,9 @@
  */
 package org.neo4j.gds.triangle;
 
-import org.neo4j.gds.api.ProcedureReturnColumns;
-import org.neo4j.gds.api.properties.nodes.EmptyDoubleNodePropertyValues;
-import org.neo4j.gds.api.properties.nodes.NodePropertyValues;
-import org.neo4j.gds.api.properties.nodes.NodePropertyValuesAdapter;
-import org.neo4j.gds.collections.ha.HugeDoubleArray;
-import org.neo4j.gds.executor.ComputationResult;
-import org.neo4j.gds.result.AbstractCommunityResultBuilder;
-import org.neo4j.gds.result.AbstractResultBuilder;
-
 final class LocalClusteringCoefficientCompanion {
 
     static final String DESCRIPTION = "The local clustering coefficient is a metric quantifying how connected the neighborhood of a node is.";
 
-
     private LocalClusteringCoefficientCompanion() {}
-
-    static <CONFIG extends LocalClusteringCoefficientBaseConfig> NodePropertyValues nodeProperties(
-        ComputationResult<LocalClusteringCoefficient, LocalClusteringCoefficientResult, CONFIG> computeResult
-    ) {
-        return computeResult.result()
-            .map(LocalClusteringCoefficientResult::localClusteringCoefficients)
-            .map(NodePropertyValuesAdapter::adapt)
-            .orElse(EmptyDoubleNodePropertyValues.INSTANCE);
-    }
-
-    static <PROC_RESULT, CONFIG extends LocalClusteringCoefficientBaseConfig> AbstractResultBuilder<PROC_RESULT> resultBuilder(
-        ResultBuilder<PROC_RESULT> procResultBuilder,
-        ComputationResult<LocalClusteringCoefficient, LocalClusteringCoefficientResult, CONFIG> computeResult
-    ) {
-        var result = computeResult.result().orElse(EmptyResult.EMPTY_RESULT);
-
-        return procResultBuilder
-            .withAverageClusteringCoefficient(result.averageClusteringCoefficient());
-    }
-
-    abstract static class ResultBuilder<PROC_RESULT> extends AbstractCommunityResultBuilder<PROC_RESULT> {
-
-        double averageClusteringCoefficient = 0;
-
-        ResultBuilder(ProcedureReturnColumns returnColumns, int concurrency) {
-            super(returnColumns, concurrency);
-        }
-
-        ResultBuilder<PROC_RESULT> withAverageClusteringCoefficient(double averageClusteringCoefficient) {
-            this.averageClusteringCoefficient = averageClusteringCoefficient;
-            return this;
-        }
-    }
-
-    private static final class EmptyResult implements LocalClusteringCoefficientResult {
-
-        static final EmptyResult EMPTY_RESULT = new EmptyResult();
-
-        private EmptyResult() {}
-
-        @Override
-        public HugeDoubleArray localClusteringCoefficients() {
-            return HugeDoubleArray.newArray(0);
-        }
-
-        @Override
-        public double averageClusteringCoefficient() {
-            return 0;
-        }
-    }
-
 }
