748d7e4cfcde7c5718c827178a57b6ec88914ba3
Sören Reichardt <soren.reichardt@neo4j.com>
2024-03-27 15:21:20 +0100

    Add integration tests for node property write-back

--- core/src/main/java/org/neo4j/gds/config/WriteConfig.java
+++ core/src/main/java/org/neo4j/gds/config/WriteConfig.java
@@ -59,7 +59,6 @@ public interface WriteConfig extends ConcurrencyConfig {
     @Configuration.ToMapValue(value = "org.neo4j.gds.config.ArrowConnectionInfo#toMap")
     Optional<ArrowConnectionInfo> arrowConnectionInfo();
 
-    @Configuration.Ignore
     @Value.Default
     @Value.Auxiliary
     default boolean writeToResultStore() {
