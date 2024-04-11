e3d53ff06a393df05a1b18ff4fbc2057c250539b
Nicola Vitucci <nicola.vitucci@neo4j.com>
2024-04-11 15:54:19 +0100

    Test with multiple algorithms

--- doc/configs/process_conf.py
+++ doc/configs/process_conf.py
@@ -1,5 +1,5 @@
 import json
-import pathlib
+from pathlib import Path
 
 BASE_LINK = "xref:common-usage/running-algos.adoc#"
 LINKS = {
@@ -18,25 +18,28 @@ LINKS = {
 }
 
 conf_filename = "algorithms-conf.json"
-adoc_root = pathlib.Path("..") / "modules" / "ROOT" / "partials"
+adoc_root = Path("..") / "modules" / "ROOT" / "partials"
 
 with open(conf_filename) as conf_file:
     conf_json = json.load(conf_file)
 
     for algo in conf_json["algorithms"]:
-        if algo["name"] == "Article Rank":
-            adoc_filename = adoc_root / algo["page_path"] / "specific-configuration.adoc"
+        adoc_filename = adoc_root / algo["page_path"] / "specific-configuration.adoc"
 
-            with open(adoc_filename, "w") as adoc_file:
-              adoc_file.write("// DO NOT EDIT: File generated automatically\n")
+        if not Path(adoc_filename).exists():
+            print(f"File '{adoc_filename}' skipped")
+            continue
+
+        with open(adoc_filename, "w") as adoc_file:
+            adoc_file.write("// DO NOT EDIT: File generated automatically\n")
+            
+            for conf in algo["config"]:
+                name, type_, default, optional, description = conf["name"], conf["type"], conf["default"], conf["optional"], conf["description"]
+                if name in LINKS:
+                    name = BASE_LINK + LINKS[name] + f"[{name}]"
+                type_ = " or ".join(type_) if isinstance(type_, list) else type_
+                optional = "yes" if optional else "no"
+                default = "null" if default is None else default
                 
-              for conf in algo["config"]:
-                  name, type_, default, optional, description = conf["name"], conf["type"], conf["default"], conf["optional"], conf["description"]
-                  if name in LINKS:
-                      name = BASE_LINK + LINKS[name] + f"[{name}]"
-                  type_ = " or ".join(type_) if isinstance(type_, list) else type_
-                  optional = "yes" if optional else "no"
-                  default = "null" if default is None else default
-                  
-                  line = f"| {name} | {type_} | {default} | {optional} | {description}"
-                  adoc_file.write(line + "\n")
+                line = f"| {name} | {type_} | {default} | {optional} | {description}"
+                adoc_file.write(line + "\n")
