4a7c6a3a92ee66fd95ea6f737cbc16373beb1626
Nicola Vitucci <nicola.vitucci@neo4j.com>
2024-04-11 11:17:43 +0100

    Update JSON with page path

--- doc/configs/process_json.py
+++ doc/configs/process_json.py
@@ -19,20 +19,22 @@ LINKS = {
 
 # TODO: expand to all configs
 algorithm = "article-rank"
-
 conf_filename = pathlib.Path("json") / f"{algorithm}.json"
-adoc_filename = pathlib.Path("..") / "modules" / "ROOT" / "partials" / "algorithms" / algorithm / "specific-configuration.adoc"
+adoc_root = pathlib.Path("..") / "modules" / "ROOT" / "partials"
 
-with open(conf_filename) as conf_file, open(adoc_filename, "w") as adoc_file:
+with open(conf_filename) as conf_file:
     conf_json = json.load(conf_file)
-    adoc_file.write("// DO NOT EDIT: File generated automatically\n")
-      
-    for conf in conf_json["config"]:
-        name, type_, default, optional, description = conf["name"], conf["type"], conf["default"], conf["optional"], conf["description"]
-        if name in LINKS:
-            name = BASE_LINK + LINKS[name] + f"[{name}]"
-        type_ = " or ".join(type_) if isinstance(type_, list) else type_
-        optional = "yes" if optional else "no"
+    adoc_filename = adoc_root / conf_json["page_path"] / "specific-configuration.adoc"
+
+    with open(adoc_filename, "w") as adoc_file:
+      adoc_file.write("// DO NOT EDIT: File generated automatically\n")
         
-        line = f"| {name} | {type_} | {default} | {optional} | {description}"
-        adoc_file.write(line + "\n")
+      for conf in conf_json["config"]:
+          name, type_, default, optional, description = conf["name"], conf["type"], conf["default"], conf["optional"], conf["description"]
+          if name in LINKS:
+              name = BASE_LINK + LINKS[name] + f"[{name}]"
+          type_ = " or ".join(type_) if isinstance(type_, list) else type_
+          optional = "yes" if optional else "no"
+          
+          line = f"| {name} | {type_} | {default} | {optional} | {description}"
+          adoc_file.write(line + "\n")
