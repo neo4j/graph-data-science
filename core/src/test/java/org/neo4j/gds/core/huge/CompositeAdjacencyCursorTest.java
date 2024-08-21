18ebedb3d4226c0bdb3c05d291c38730811c00a7
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2024-08-21 08:43:33 +0100

    Move `core` db tests to integrationTest

--- core/src/test/java/org/neo4j/gds/core/huge/CompositeAdjacencyCursorTest.java
+++ core/src/test/java/org/neo4j/gds/core/huge/CompositeAdjacencyCursorTest.java
@@ -21,18 +21,17 @@ package org.neo4j.gds.core.huge;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.neo4j.gds.BaseTest;
+import org.neo4j.gds.api.Graph;
 import org.neo4j.gds.extension.GdlExtension;
 import org.neo4j.gds.extension.GdlGraph;
 import org.neo4j.gds.extension.Inject;
-import org.neo4j.gds.api.Graph;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @GdlExtension
-class CompositeAdjacencyCursorTest extends BaseTest {
+class CompositeAdjacencyCursorTest {
 
     @GdlGraph
     private static final String DB_CYPHER =
