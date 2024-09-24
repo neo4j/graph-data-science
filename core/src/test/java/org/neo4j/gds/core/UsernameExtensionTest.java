808824a86dd4879a440e61f8ad4e1c5630227b52
Paul Horn <paul.horn@neo4j.com>
2024-09-24 14:39:07 +0200

    We don't need this hacky piece of test anymore

--- core/src/test/java/org/neo4j/gds/core/UsernameExtensionTest.java
+++ core/src/test/java/org/neo4j/gds/core/UsernameExtensionTest.java
@@ -19,13 +19,11 @@
  */
 package org.neo4j.gds.core;
 
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 import org.neo4j.gds.annotation.SuppressForbidden;
 import org.neo4j.gds.compat.GraphDatabaseApiProxy;
 import org.neo4j.gds.utils.CheckedRunnable;
 import org.neo4j.graphdb.Result;
-import org.neo4j.internal.unsafe.UnsafeUtil;
 import org.neo4j.procedure.Context;
 import org.neo4j.procedure.Procedure;
 import org.neo4j.test.TestDatabaseManagementServiceBuilder;
@@ -74,51 +72,6 @@ class UsernameExtensionTest {
             dbms.shutdown();
         };
         assertThat(runCapturingStdOut(runnable)).isEmpty();
-    }
-
-    @Test
-    void shouldInvokeCompatLayerWhenTheUsernameIsInteractedWith() throws Exception {
-        // This test checks that we initialize the Neo4jProxy in the procedure invocation
-        // Since we have no control over test execution, it might be that the class is already initialized.
-        // There is also no good way of checking if a class is initialized without also initializing it.
-        // So we will do the not-so-good way for now
-
-        // UnsafeUtil does not expose the actual unsafe (for good reasons)
-        var theUnsafeField = UnsafeUtil.class.getDeclaredField("unsafe");
-        // This might trigger a warning of illegal reflective access
-        theUnsafeField.setAccessible(true);
-        var theUnsafe = (sun.misc.Unsafe) theUnsafeField.get(null);
-
-        Assumptions.assumeFalse(theUnsafe == null, "There is no Unsafe available on this platform, skipping test");
-
-        Class<?> neoProxyClass = Class.forName(
-            "org.neo4j.gds.compat.Neo4jProxy",
-            /* initialize class = */ false,
-            getClass().getClassLoader()
-        );
-
-        // The unsafe method is (other than being unsafe) deprecated since JDK15 and scheduled for removal with no replacement.
-        // The implementation is apparently not thread-safe and one should use `ensureInitialized` in the
-        // MethodHandles API, but that does not give us what we want.
-        Assumptions.assumeTrue(
-            theUnsafe.shouldBeInitialized(neoProxyClass),
-            "The Neo4jProxy is already initialized, this test cannot succeed"
-        );
-
-        CheckedRunnable<Exception> runnable = () -> {
-            var dbms = new TestDatabaseManagementServiceBuilder()
-                .impermanent()
-                .noOpSystemGraphInitializer()
-                // make sure that we 1) have our extension under test and 2) have it only once
-                .removeExtensions(ex -> ex instanceof UsernameExtension)
-                .addExtension(new UsernameExtension())
-                .build();
-            var db = dbms.database(DEFAULT_DATABASE_NAME);
-            GraphDatabaseApiProxy.registerProcedures(db, UsernameProc.class);
-            var ignored = db.executeTransactionally("CALL gds.test.username()", Map.of(), Result::resultAsString);
-            dbms.shutdown();
-        };
-        assertThat(runCapturingStdOut(runnable)).contains(" Loaded compatibility layer:");
     }
 
     @SuppressForbidden(reason = "System.out is used")
