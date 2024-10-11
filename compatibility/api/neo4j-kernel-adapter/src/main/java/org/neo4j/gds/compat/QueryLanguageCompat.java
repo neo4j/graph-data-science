0862a4f29668361b99f8550bbd2ba2335f31017b
Paul Horn <paul.horn@neo4j.com>
2024-10-11 14:21:38 +0200

    Mark QueryLanguageCompat as CompatSince

--- compatibility/api/neo4j-kernel-adapter/src/main/java/org/neo4j/gds/compat/QueryLanguageCompat.java
+++ compatibility/api/neo4j-kernel-adapter/src/main/java/org/neo4j/gds/compat/QueryLanguageCompat.java
@@ -30,6 +30,7 @@ import java.lang.invoke.MethodType;
 import java.util.EnumMap;
 import java.util.stream.Stream;
 
+@CompatSince(minor = 25)
 public final class QueryLanguageCompat {
 
     private QueryLanguageCompat() {}
