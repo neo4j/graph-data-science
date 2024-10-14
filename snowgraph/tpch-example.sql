ddebccdf302b9fa5d540823da822167a41218431
Martin Junghanns <m.junghanns@mailbox.org>
2024-10-14 16:22:12 +0200

    Describe data preparation

--- snowgraph/tpch-example.sql
+++ snowgraph/tpch-example.sql
@@ -24,85 +24,95 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 -- | Basket analysis example on TPC-H data  |
 -- *========================================*
 
--- Assumptions:
+-- In this example, we will use Neo4j Graph Data Science (GDS) for Snowflake to perform basket analysis on the TPC-H dataset.
+-- For that we will look at parts that are often ordered together and use the node similarity algorithm to find similar parts.
+-- We will then write the results back to Snowflake and query the results.
+--
+-- For this example to run, we make the following assumptions:
+--
 -- * Neo4j Graph Data Science application is installed correctly and called "Neo4j_GDS"
 -- * The current role can create databases and schemas, compute pools and warehouses
 -- * The current role has granted the application role Neo4j_GDS.app_user
+-- * The current role has access to the Snowflake sample data set (database "snowflake_sample_data")
+--
+-- The example is split into three parts:
+-- 1. Data preparation,
+-- 2. Application setup, and
+-- 3. Graph analysis.
 
+-- ==================================================
+-- 1. Data preparation
+-- ==================================================
 
--- Create a database which we will use to prepare data for GDS
--- and also to write back graph algorithm results.
+-- Create a database which we will use to prepare data for GDS.
 CREATE DATABASE IF NOT EXISTS tpch_example;
 CREATE SCHEMA IF NOT EXISTS tpch_example.gds;
 USE SCHEMA tpch_example.gds;
 
+-- GDS expects the data to be in a specific format: a table/view for nodes and a table/view for relationships.
+-- In addition, GDS requires node identifiers to be globally unique integers.
+--
+-- For our analysis, the nodes will be parts and the orders in which they appeared.
+-- The relationships will be the line items linking a part to an order.
+--
+-- We start by creating the node view for our graph.
+-- First we need to map the primary keys for parts and orders to globally unique node ids.
 
--- ====
--- Create mapping tables for each table we want to project as nodes.
--- We map the primary keys of each table to a globally unique id.
--- ====
+-- We use a sequence to generate globally unique node identifiers.
+CREATE OR REPLACE SEQUENCE global_id START = 0 INCREMENT = 1;
 
--- We use a sequence to generate globally unqiue node ids
-CREATE OR REPLACE SEQUENCE seq1 START = 0 INCREMENT = 1;
-
--- Map part keys to globally unique node ids.
--- We create tables to persist the generated ids.
+-- We create two mapping tables, one for parts and one for orders.
+-- This is necessary because the primary key sets for both tables might overlap.
 CREATE OR REPLACE TABLE node_mapping_parts(gdsId, p_partkey) AS
-    SELECT seq1.nextval, p_partkey
+    SELECT global_id.nextval, p_partkey
     FROM snowflake_sample_data.tpch_sf1.part;
--- map order keys to globally unique node ids
 CREATE OR REPLACE TABLE node_mapping_orders(gdsId, o_orderkey) AS
-    SELECT seq1.nextval, o_orderkey
+    SELECT global_id.nextval, o_orderkey
     FROM snowflake_sample_data.tpch_sf1.orders;
 
---
--- Build view using the data we want to project as relationships.
--- We join in the mapping information we created beforehand.
---
+-- Next, we can create the final node view that we use for our graph projection.
+-- Note, that the view must contain a column named "nodeId" to be recognized by GDS.
+-- Any additional column will be used as node property, but we don't need that for this example.
+CREATE OR REPLACE VIEW nodes(nodeId) AS
+    SELECT nmp.gdsId
+    UNION
+    SELECT nmo.gdsId;
 
--- map (:Part)-[:IN]->(:Order)
+-- Let's quickly verify the cardinality of our views.
+-- As it is the union of parts and orders, we expect 1,700,000 rows.
+SELECT count(*) FROM nodes;
+
+-- We can now create the relationship view.
+-- As mentioned earlier, we will use the line items to create relationships between parts and orders.
+-- We join the line items with parts and orders to get the source and target nodes for our relationships.
+-- We also join the mapping tables to get the globally unique node ids.
+-- Note, that the view must contain columns named "sourceNodeId" and "targetNodeId" to be recognized by GDS.
+-- Any additional column will be used as relationship property, but we don't need that for this example.
 CREATE OR REPLACE VIEW relationships(sourceNodeId, targetNodeId) AS
     SELECT 
         nmp.gdsId AS sourceNodeId, 
         nmo.gdsId AS targetNodeId
-    FROM snowflake_sample_data.tpch_sf1.part p 
+    FROM snowflake_sample_data.tpch_sf1.part p
+        -- The first two joins build the relationships between parts and orders
         JOIN snowflake_sample_data.tpch_sf1.lineitem l
           ON p.p_partkey = l.l_partkey
         JOIN snowflake_sample_data.tpch_sf1.orders o
           ON o.o_orderkey = l.l_orderkey
-        -- we join in the globally unique node ids to
-        -- match the node table
+        -- The second two joins map the primary keys to globally unique node ids
         JOIN node_mapping_parts nmp
           ON nmp.p_partkey = p.p_partkey
         JOIN node_mapping_orders nmo
           ON nmo.o_orderkey = o.o_orderkey;
 
---
--- Build a node view that contains all nodes that have outgoing or
--- incoming relationships.
---
-        
-CREATE OR REPLACE VIEW nodes(nodeId) AS 
-    SELECT nmp.gdsId 
-    FROM node_mapping_parts nmp
-        JOIN relationships r 
-          ON nmp.gdsId = r.sourceNodeId
-    UNION
-    SELECT nmo.gdsId 
-    FROM node_mapping_orders nmo
-        JOIN relationships r 
-          ON nmo.gdsId = r.targetNodeId;
-
--- Verify the cardinality of our views
-
--- Node view: 1,700,000 rows
-SELECT count(*) FROM nodes;
--- Relationship view: 6,001,215 rows
+-- Let's quickly verify the cardinality of our relationship view.
+-- As it is the join of parts, line items, and orders, we expect 6,001,215 rows.
 SELECT count(*) FROM relationships;
 
+-- We have now prepared the data for GDS.
 
 -- ==================================================
-
+-- 2. Application setup
+-- ==================================================
 
 -- ====
 -- Setup compute pool and application
@@ -133,13 +143,9 @@ GRANT SELECT ON ALL VIEWS  IN SCHEMA tpch_example.gds TO APPLICATION Neo4j_GDS;
 GRANT CREATE TABLE ON SCHEMA tpch_example.gds TO APPLICATION Neo4j_GDS;
 
 
-
 -- ==================================================
-
-
--- ====
--- Use GDS for basket analysis.
--- ====
+-- 3. Graph analysis
+-- ==================================================
 
 -- Create a new GDS session
 CALL gds.create_session('Neo4j_GDS_pool', 'Neo4j_GDS_warehouse');
@@ -165,6 +171,10 @@ SELECT gds.write_relationships('parts_in_orders', {
     'relationshipProperty': 'similarity',
     'table':                'tpch_example.gds.part_similar_to_part'
 });
+
+-- ==================================================
+-- 3. result analysis
+-- ==================================================
 
 -- Grant select privilege to your role on the new table create by GDS
 
