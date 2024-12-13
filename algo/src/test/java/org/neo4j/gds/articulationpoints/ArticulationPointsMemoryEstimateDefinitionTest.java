944803b10dc678441039e58ba68d6d1595025dc7
ioannispan <ioannis.panagiotas@neotechnology.com>
2024-12-13 13:46:37 +0100

    Remove the `children` array to trim some memory

--- algo/src/test/java/org/neo4j/gds/articulationpoints/ArticulationPointsMemoryEstimateDefinitionTest.java
+++ algo/src/test/java/org/neo4j/gds/articulationpoints/ArticulationPointsMemoryEstimateDefinitionTest.java
@@ -30,6 +30,6 @@ class ArticulationPointsMemoryEstimateDefinitionTest {
 
         MemoryEstimationAssert.assertThat(memoryEstimation)
             .memoryRange(100, 6000, new Concurrency(1))
-            .hasSameMinAndMaxEqualTo(218760);
+            .hasSameMinAndMaxEqualTo(217912);
     }
 }
