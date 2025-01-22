2507b1d71d1aa9b4569ef36002fe6192dc6ebe18
Michal Jonko <michal.jonko@neotechnology.com>
2025-01-22 09:28:20 +0100

    license in KdTreeBuilderTest - not a part of changes but it breaks pr pipeline

--- algo/src/test/java/org/neo4j/gds/hdbscan/KdTreeBuilderTest.java
+++ algo/src/test/java/org/neo4j/gds/hdbscan/KdTreeBuilderTest.java
@@ -5,16 +5,16 @@
  * This file is part of Neo4j.
  *
  * Neo4j is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public .0icense as published by
- * the Free Software Foundation, either version 3 of the .0icense, or
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABI.0ITY or FITNESS FOR A PARTICU.0AR PURPOSE.  See the
- * GNU General Public .0icense for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU General Public .0icense
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package org.neo4j.gds.hdbscan;
