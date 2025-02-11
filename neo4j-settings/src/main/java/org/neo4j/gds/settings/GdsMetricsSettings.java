f5417bd3e7e7ff85dd241912a56c3184868343fd
Florentin Dörre <florentin@owitsch.de>
2025-02-11 14:22:16 +0100

    Initialize metrics lazily (#10133)
    * Initialize metrics lazily
    
    Prepare for using non-default CollectorRegistry for Neo4j plugin setup.
    
    * Register algo metrics in registry (also non-default)
    
    Co-authored-by: Max Kießling <max.kiessling@neo4j.com>
    
    * Improve error details for flaky test
    
    ---------
    
    Co-authored-by: Max Kießling <max.kiessling@neo4j.com>

--- neo4j-settings/src/main/java/org/neo4j/gds/settings/GdsMetricsSettings.java
+++ neo4j-settings/src/main/java/org/neo4j/gds/settings/GdsMetricsSettings.java
@@ -19,23 +19,23 @@
  */
 package org.neo4j.gds.settings;
 
+import org.neo4j.annotations.service.ServiceProvider;
+import org.neo4j.configuration.Description;
+import org.neo4j.configuration.SettingImpl;
+import org.neo4j.configuration.SettingsDeclaration;
 import org.neo4j.graphdb.config.Setting;
 
-import java.nio.file.Path;
+import static org.neo4j.configuration.SettingValueParsers.BOOL;
 
-public final class GdsSettings {
+@ServiceProvider
+public class GdsMetricsSettings implements SettingsDeclaration {
 
-    private GdsSettings() {}
+    // Replacement for the prometheus server from GDS (used for Sessions)
+    @Description("Use GDS-only prometeheus server instead of relying on DBMS")
+    public static final Setting<Boolean> gds_metrics_server_enabled = SettingImpl.newBuilder(
+            "internal.gds.custom_metrics_server", BOOL, Boolean.FALSE)
+        .immutable()
+        .internal()
+        .build();
 
-    public static Setting<Path> exportLocation() {
-        return GraphStoreExportSettings.export_location_setting;
-    }
-
-    public static Setting<Boolean> validateUsingMaxMemoryEstimation() {
-        return MemoryEstimationSettings.validate_using_max_memory_estimation;
-    }
-
-    public static Setting<Boolean> useGdsMetricsServer() {
-        return GdsMetricsSettings.gds_metrics_server_enabled;
-    }
 }
