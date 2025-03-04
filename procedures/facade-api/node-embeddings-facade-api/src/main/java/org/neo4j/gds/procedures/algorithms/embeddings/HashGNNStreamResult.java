581aa3ec8a0e11b864266a5bae015802ffa04420
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2025-03-04 15:39:39 +0000

    Fix code style

--- procedures/facade-api/node-embeddings-facade-api/src/main/java/org/neo4j/gds/procedures/algorithms/embeddings/HashGNNStreamResult.java
+++ procedures/facade-api/node-embeddings-facade-api/src/main/java/org/neo4j/gds/procedures/algorithms/embeddings/HashGNNStreamResult.java
@@ -23,9 +23,9 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public record HashGNNStreamResult( long nodeId, List<Double> embedding){
+public record HashGNNStreamResult(long nodeId, List<Double> embedding) {
 
-    static  HashGNNStreamResult create (long nodeId, double[] embeddings) {
-        return  new HashGNNStreamResult(nodeId,Arrays.stream(embeddings).boxed().collect(Collectors.toList()));
+    static HashGNNStreamResult create(long nodeId, double[] embeddings) {
+        return new HashGNNStreamResult(nodeId, Arrays.stream(embeddings).boxed().collect(Collectors.toList()));
     }
 }
