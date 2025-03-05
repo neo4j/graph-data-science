44687035c4d76c9c7bb39fbdbde36fc79bc20e8e
Florentin Dörre <florentin.dorre@neo4j.com>
2025-03-05 12:33:40 +0100

    Extend existing list reader
    moving it into a common package

--- procedures/facade-api/node-embeddings-facade-api/src/main/java/org/neo4j/gds/procedures/algorithms/embeddings/DefaultNodeEmbeddingsStreamResult.java
+++ procedures/facade-api/node-embeddings-facade-api/src/main/java/org/neo4j/gds/procedures/algorithms/embeddings/DefaultNodeEmbeddingsStreamResult.java
@@ -31,7 +31,7 @@ public record DefaultNodeEmbeddingsStreamResult(long nodeId, List<Double> embedd
             Collectors.toList()));
     }
 
-    static DefaultNodeEmbeddingsStreamResult create(long nodeId, float[] embeddingAsArray) {
+    public static DefaultNodeEmbeddingsStreamResult create(long nodeId, float[] embeddingAsArray) {
         var embedding = new ArrayList<Double>(embeddingAsArray.length);
         for (var f : embeddingAsArray) {
             embedding.add((double) f);
