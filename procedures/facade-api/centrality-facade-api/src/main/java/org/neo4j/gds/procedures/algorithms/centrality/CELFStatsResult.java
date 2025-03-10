179d8f98410159c22a8890201f90adade705aef7
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2025-03-10 12:09:55 +0000

    Convert CELFStatsResult to record

--- procedures/facade-api/centrality-facade-api/src/main/java/org/neo4j/gds/procedures/algorithms/centrality/CELFStatsResult.java
+++ procedures/facade-api/centrality-facade-api/src/main/java/org/neo4j/gds/procedures/algorithms/centrality/CELFStatsResult.java
@@ -20,42 +20,17 @@
 package org.neo4j.gds.procedures.algorithms.centrality;
 
 import org.neo4j.gds.applications.algorithms.machinery.AlgorithmProcessingTimings;
-import org.neo4j.gds.result.AbstractResultBuilder;
 
 import java.util.Map;
 
-public final class CELFStatsResult {
-    public final long computeMillis;
-    public final double totalSpread;
-    public final long nodeCount;
-    public final Map<String, Object> configuration;
-
-
-    public CELFStatsResult(long computeMillis, double totalSpread, long nodeCount, Map<String, Object> configuration) {
-        this.computeMillis = computeMillis;
-        this.totalSpread = totalSpread;
-        this.nodeCount = nodeCount;
-        this.configuration = configuration;
-    }
-
-    public static Builder builder() {
-        return new Builder();
-    }
+public record CELFStatsResult(
+    long computeMillis,
+    double totalSpread,
+    long nodeCount,
+    Map<String, Object> configuration
+) {
 
     static CELFStatsResult emptyFrom(AlgorithmProcessingTimings timings, Map<String, Object> configurationMap) {
         return new CELFStatsResult(timings.computeMillis, 0, 0, configurationMap);
-    }
-
-    public static class Builder extends AbstractResultBuilder<CELFStatsResult> {
-        private double totalSpread;
-
-        public Builder withTotalSpread(double totalSpread) {
-            this.totalSpread = totalSpread;
-            return this;
-        }
-
-        public CELFStatsResult build() {
-            return new CELFStatsResult(computeMillis, totalSpread, nodeCount, config.toMap());
-        }
     }
 }
