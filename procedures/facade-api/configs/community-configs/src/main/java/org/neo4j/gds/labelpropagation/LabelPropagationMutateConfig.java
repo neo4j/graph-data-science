fa83c2d0c76780c81b8830168cb588a87c16664f
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2025-03-11 10:02:30 +0000

    Add Remote LabelPropagation Mutate

--- procedures/facade-api/configs/community-configs/src/main/java/org/neo4j/gds/labelpropagation/LabelPropagationMutateConfig.java
+++ procedures/facade-api/configs/community-configs/src/main/java/org/neo4j/gds/labelpropagation/LabelPropagationMutateConfig.java
@@ -23,10 +23,16 @@ import org.neo4j.gds.annotation.Configuration;
 import org.neo4j.gds.config.MutateNodePropertyConfig;
 import org.neo4j.gds.core.CypherMapWrapper;
 
+import java.util.Map;
+
 @Configuration
 public interface LabelPropagationMutateConfig extends LabelPropagationBaseConfig, MutateNodePropertyConfig {
 
     static LabelPropagationMutateConfig of(CypherMapWrapper userInput) {
         return new LabelPropagationMutateConfigImpl(userInput);
+    }
+
+    static LabelPropagationMutateConfig of(Map<String, Object> userInput) {
+        return of(CypherMapWrapper.create(userInput));
     }
 }
