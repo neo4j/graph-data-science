2b7134c3bbf2a4a639f6b81d99de58a00c4f2641
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2025-03-11 11:17:46 +0000

    Add Remote TriangleCount Mutate

--- procedures/facade-api/configs/community-configs/src/main/java/org/neo4j/gds/triangle/TriangleCountMutateConfig.java
+++ procedures/facade-api/configs/community-configs/src/main/java/org/neo4j/gds/triangle/TriangleCountMutateConfig.java
@@ -23,10 +23,16 @@ import org.neo4j.gds.annotation.Configuration;
 import org.neo4j.gds.config.MutateNodePropertyConfig;
 import org.neo4j.gds.core.CypherMapWrapper;
 
+import java.util.Map;
+
 @Configuration
 public interface TriangleCountMutateConfig extends TriangleCountBaseConfig, MutateNodePropertyConfig {
 
     static TriangleCountMutateConfig of(CypherMapWrapper userInput) {
         return new TriangleCountMutateConfigImpl(userInput);
+    }
+
+    static TriangleCountMutateConfig of(Map<String, Object> userInput) {
+        return of(CypherMapWrapper.create(userInput));
     }
 }
