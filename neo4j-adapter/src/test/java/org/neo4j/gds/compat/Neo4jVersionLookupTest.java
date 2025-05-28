a803a9e87938ead1f7e5088b9645cc9f991ca9d0
Brian Shi <brian.shi@neotechnology.com>
2025-05-28 11:03:06 +0100

    Update Neo4j 5.26.7

--- neo4j-adapter/src/test/java/org/neo4j/gds/compat/Neo4jVersionLookupTest.java
+++ neo4j-adapter/src/test/java/org/neo4j/gds/compat/Neo4jVersionLookupTest.java
@@ -35,7 +35,7 @@ class Neo4jVersionLookupTest {
 
     @Test
     void testParseV5() {
-        assertThat(Neo4jVersionLookup.parse("5.26.6", "5.26.6"))
+        assertThat(Neo4jVersionLookup.parse("5.26.7", "5.26.7"))
             .returns(true, v -> v.matches(5, 26));
     }
 
