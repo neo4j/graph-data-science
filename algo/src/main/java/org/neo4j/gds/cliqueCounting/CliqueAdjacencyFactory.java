373818278ea65959eded6086d7951d8bc59ce020
Alfred Clemedtson <alfred.clemedtson@neo4j.com>
2025-07-04 18:10:17 +0200

    Replace with cursors
    Co-authored-by: Ioannis Panagiotas<ioannis.panagiotas@neotechnology.com>

---
+++ algo/src/main/java/org/neo4j/gds/cliqueCounting/CliqueAdjacencyFactory.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.cliqueCounting;
+
+import org.neo4j.gds.api.Graph;
+import org.neo4j.gds.cliqueCounting.intersect.CliqueAdjacency;
+import org.neo4j.gds.cliqueCounting.intersect.HugeGraphCliqueIntersect;
+import org.neo4j.gds.cliqueCounting.intersect.NodeFilteredCliqueIntersect;
+import org.neo4j.gds.cliqueCounting.intersect.UnionGraphCliqueIntersect;
+import org.neo4j.gds.core.huge.HugeGraph;
+import org.neo4j.gds.core.huge.NodeFilteredGraph;
+import org.neo4j.gds.core.huge.UnionGraph;
+
+public final class CliqueAdjacencyFactory {
+
+    private CliqueAdjacencyFactory() {}
+
+    static CliqueAdjacency createCliqueAdjacency(Graph graph) {
+        switch (graph) {
+            case HugeGraph hugeGraph -> {
+                return new HugeGraphCliqueIntersect(hugeGraph.relationshipTopology().adjacencyList());
+            }
+            case UnionGraph unionGraph -> {
+                return new UnionGraphCliqueIntersect(i -> i, unionGraph.relationshipTopology());
+            }
+            case NodeFilteredGraph nodeFilteredGraph -> {
+                var innerGraph = nodeFilteredGraph.graph();
+                var innerAdjacency = createCliqueAdjacency(innerGraph);
+                return new NodeFilteredCliqueIntersect(nodeFilteredGraph, innerAdjacency);
+            }
+            default -> throw new RuntimeException("Unsupported graph type: " + graph.getClass());
+        }
+
+    }
+}
