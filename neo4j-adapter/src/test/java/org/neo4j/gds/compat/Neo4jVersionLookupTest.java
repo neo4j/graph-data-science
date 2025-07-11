6875ee6a1ca9807d5226dfc5ebc3a947d55027b4
Brian Shi <brian.shi@neotechnology.com>
2025-07-11 10:29:26 +0100

    Update Neo4j 5.26.9 for GDS 2.13

--- neo4j-adapter/src/test/java/org/neo4j/gds/compat/Neo4jVersionLookupTest.java
+++ neo4j-adapter/src/test/java/org/neo4j/gds/compat/Neo4jVersionLookupTest.java
@@ -32,7 +32,7 @@ class Neo4jVersionLookupTest {
 
     @Test
     void testParse() {
-        assertThat(Neo4jVersionLookup.parse("5.26.8", "5.26.8"))
+        assertThat(Neo4jVersionLookup.parse("5.26.9", "5.26.9"))
             .returns(new Neo4jVersion.MajorMinor(5, 26), Neo4jVersion::semanticVersion)
             .returns(true, Neo4jVersion::isSupported);
     }
