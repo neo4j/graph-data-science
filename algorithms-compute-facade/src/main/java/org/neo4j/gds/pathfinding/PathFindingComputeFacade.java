22fdf4480eca62203d2515739240f9fedf6184c9
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2025-07-17 13:02:54 +0100

    Bootstrap the unimplemented methods

--- algorithms-compute-facade/src/main/java/org/neo4j/gds/pathfinding/PathFindingComputeFacade.java
+++ algorithms-compute-facade/src/main/java/org/neo4j/gds/pathfinding/PathFindingComputeFacade.java
@@ -228,59 +228,129 @@ public class PathFindingComputeFacade {
     }
 
     CompletableFuture<PathFindingResult> deltaStepping() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<HugeLongArray> depthFirstSearch() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<SpanningTree> kSpanningTree() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<PathFindingResult> longestPath() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<Stream<long[]>> randomWalk() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<HugeAtomicLongArray> randomWalkCountingNodeVisits() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<PrizeSteinerTreeResult> pcst() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<PathFindingResult> singlePairShortestPathAStar() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<PathFindingResult> singlePairShortestPathDijkstra() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<PathFindingResult> singlePairShortestPathYens() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<PathFindingResult> singleSourceShortestPathDijkstra() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<SpanningTree> spanningTree() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<SteinerTreeResult> steinerTree() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
     CompletableFuture<TopologicalSortResult> topologicalSort() {
-        throw new RuntimeException("Not yet implemented");
+        // Create ProgressTracker
+        // Fetch the Graph the algorithm will operate on
+        // Create the algorithm
+        // Submit the algorithm for async computation
+
+        return CompletableFuture.failedFuture(new RuntimeException("Not yet implemented"));
     }
 
 }
