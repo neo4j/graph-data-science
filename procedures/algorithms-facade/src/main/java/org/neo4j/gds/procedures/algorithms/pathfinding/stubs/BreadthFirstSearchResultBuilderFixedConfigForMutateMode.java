e8d3159094b7f4d375453726bdb53e2884e508f2
Jacob Sznajdman <breakanalysis@gmail.com>
2025-07-31 17:26:29 +0200

    Apply review comments and bugfixes for BFS / Snowflake
    Fixed bug of incorrect mapping of nodeids and configs in result
    Finished the bfs integration test
    
    Co-Authored-By: Martin Junghanns <martin.junghanns@neo4j.com>

---
+++ procedures/algorithms-facade/src/main/java/org/neo4j/gds/procedures/algorithms/pathfinding/stubs/BreadthFirstSearchResultBuilderFixedConfigForMutateMode.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.procedures.algorithms.pathfinding.stubs;
+
+import org.neo4j.gds.api.Graph;
+import org.neo4j.gds.applications.algorithms.machinery.AlgorithmProcessingTimings;
+import org.neo4j.gds.applications.algorithms.machinery.ResultBuilder;
+import org.neo4j.gds.applications.algorithms.metadata.RelationshipsWritten;
+import org.neo4j.gds.collections.ha.HugeLongArray;
+import org.neo4j.gds.paths.traverse.BfsMutateConfig;
+import org.neo4j.gds.procedures.algorithms.pathfinding.PathFindingMutateResult;
+
+import java.util.Map;
+import java.util.Optional;
+
+public class BreadthFirstSearchResultBuilderFixedConfigForMutateMode implements ResultBuilder<BfsMutateConfig, HugeLongArray, PathFindingMutateResult, RelationshipsWritten> {
+    private final Map<String, Object> originalConfigMap;
+
+    public BreadthFirstSearchResultBuilderFixedConfigForMutateMode(Map<String, Object> originalConfigMap) {this.originalConfigMap = originalConfigMap;}
+
+    @Override
+    public PathFindingMutateResult build(
+        Graph graph,
+        BfsMutateConfig configuration,
+        Optional<HugeLongArray> result,
+        AlgorithmProcessingTimings timings,
+        Optional<RelationshipsWritten> metadata
+    ) {
+        return PathFindingMutateResult.create(
+            timings,
+            metadata,
+            originalConfigMap
+        );
+    }
+}
