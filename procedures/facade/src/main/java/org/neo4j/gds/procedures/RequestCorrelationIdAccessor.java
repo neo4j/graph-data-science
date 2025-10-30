2ffdc7b3e711a9eb39af46fc7eefe6953813a5d4
Lasse Westh-Nielsen <lassewesth@gmail.com>
2025-10-30 13:21:55 +0100

    fix test

---
+++ procedures/facade/src/main/java/org/neo4j/gds/procedures/RequestCorrelationIdAccessor.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.procedures;
+
+import org.neo4j.gds.core.RequestCorrelationId;
+import org.neo4j.gds.integration.Neo4jPoweredRequestCorrelationId;
+import org.neo4j.internal.kernel.api.exceptions.ProcedureException;
+import org.neo4j.kernel.api.procedure.Context;
+
+public class RequestCorrelationIdAccessor {
+    /**
+     * We use the transaction sequence number not because we particularly want to, but because Neo4j has limitations.
+     * The transaction id seems to not be available; and they do not offer a first class request id concept.
+     * Here is to them improving that in future.
+     */
+    public RequestCorrelationId getRequestCorrelationId(Context context) {
+        try {
+            @SuppressWarnings("resource")
+            var transactionSequenceNumber = context.kernelTransaction().getTransactionSequenceNumber();
+
+            return Neo4jPoweredRequestCorrelationId.create(transactionSequenceNumber);
+        } catch (ProcedureException e) {
+            // this must be a programmer error
+            throw new IllegalStateException("Unable to construct GDS", e);
+        }
+    }
+}
