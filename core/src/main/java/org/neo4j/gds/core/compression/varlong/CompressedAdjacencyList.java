f8bebca1eaa34599290202125511a53b16a12f10
Martin Junghanns <m.junghanns@mailbox.org>
2025-12-18 08:31:24 +0100

    Fix imports after rebase

--- core/src/main/java/org/neo4j/gds/core/compression/varlong/CompressedAdjacencyList.java
+++ core/src/main/java/org/neo4j/gds/core/compression/varlong/CompressedAdjacencyList.java
@@ -27,7 +27,6 @@ import org.neo4j.gds.api.AdjacencyList;
 import org.neo4j.gds.collections.PageUtil;
 import org.neo4j.gds.collections.ha.HugeIntArray;
 import org.neo4j.gds.collections.ha.HugeLongArray;
-import org.neo4j.gds.core.compression.MemoryInfo;
 import org.neo4j.gds.core.compression.common.BumpAllocator;
 import org.neo4j.gds.core.loading.MutableIntValue;
 import org.neo4j.gds.mem.Estimate;
