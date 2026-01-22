b5927643c1d3faa6a5bc633e4f663225fb53601c
Lasse Westh-Nielsen <lassewesth@gmail.com>
2026-01-22 13:57:57 +0100

    retiring proc the module

--- proc/testing/src/test/java/org/neo4j/gds/testing/GraphMutateProcIntegrationTest.java
+++ proc/testing/src/test/java/org/neo4j/gds/testing/GraphMutateProcIntegrationTest.java
@@ -20,7 +20,6 @@
 package org.neo4j.gds.testing;
 
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.neo4j.gds.BaseProcTest;
