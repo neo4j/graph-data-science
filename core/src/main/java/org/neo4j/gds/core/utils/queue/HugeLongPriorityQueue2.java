7400afdf6a65210b42030ada7e57e207913d9107
Martin Junghanns <m.junghanns@mailbox.org>
2026-06-08 09:02:33 +0200

    Add 4-ary heap based priority queue
    Benchmark results compared to the binary heap
    
      ┌───────────┬─────────────────────┬──────────────────┬─────────────────┐
      │ Operation │ Binary heap (2-ary) │    4-ary heap    │ 4-ary vs binary │
      ├───────────┼─────────────────────┼──────────────────┼─────────────────┤
      │ add       │ 193.66 ± 8.34       │ 146.33 ± 0.54    │ −24.4% (faster) │
      ├───────────┼─────────────────────┼──────────────────┼─────────────────┤
      │ update    │ 475.28 ± 18.93      │ 310.72 ± 9.18    │ −34.6% (faster) │
      ├───────────┼─────────────────────┼──────────────────┼─────────────────┤
      │ pop       │ 10621.03 ± 111.58   │ 11913.29 ± 80.45 │ +12.2% (slower) │
      └───────────┴─────────────────────┴──────────────────┴─────────────────┘
    
    The pop operation becomes slower because we do 4 lessThan operations per level,
    which results in 4 random accesses as the costs are indexed by node id, not
    by heap position.
    
      ┌──────────────────────────────────────┬─────────┬───────┐
      │                Method                │ Samples │   %   │
      ├──────────────────────────────────────┼─────────┼───────┤
      │ HugeDoubleArray.get                  │     559 │ 45.4% │
      ├──────────────────────────────────────┼─────────┼───────┤
      │ HugeLongArray.get                    │     359 │ 29.1% │
      ├──────────────────────────────────────┼─────────┼───────┤
      │ HugeLongPriorityQueue2.smallestChild │     170 │ 13.8% │
      ├──────────────────────────────────────┼─────────┼───────┤
      │ HugeLongArray.set                    │      95 │  7.7% │
      ├──────────────────────────────────────┼─────────┼───────┤
      │ HugeLongPriorityQueue2.downHeap      │      25 │  2.0% │
      └──────────────────────────────────────┴─────────┴───────┘

---
+++ core/src/main/java/org/neo4j/gds/core/utils/queue/HugeLongPriorityQueue2.java
@@ -0,0 +1,344 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.core.utils.queue;
+
+import org.neo4j.gds.collections.ha.HugeDoubleArray;
+import org.neo4j.gds.collections.ha.HugeLongArray;
+import org.neo4j.gds.collections.primitive.PrimitiveLongIterable;
+import org.neo4j.gds.mem.MemoryEstimation;
+import org.neo4j.gds.mem.MemoryEstimations;
+
+import java.util.PrimitiveIterator;
+
+/**
+ * A PriorityQueue specialized for longs that maintains a partial ordering of
+ * its elements such that the smallest value can always be found in constant time.
+ * The definition of what <i>small</i> means is up to the implementing subclass.
+ * <p>
+ * Put()'s and pop()'s require log(size) time but the remove() cost implemented here is linear.
+ * <p>
+ * <b>NOTE</b>: Iteration order is not specified.
+ *
+ * Implementation has been copied from https://issues.apache.org/jira/browse/SOLR-2092
+ * and slightly adapted to our needs.
+ */
+public abstract class HugeLongPriorityQueue2 implements PrimitiveLongIterable {
+
+
+    public static MemoryEstimation memoryEstimation() {
+        return MemoryEstimations.builder(HugeLongPriorityQueue2.class)
+            .perNode("heap", HugeLongArray::memoryEstimation)
+            .perNode("costs", HugeDoubleArray::memoryEstimation)
+            .perNode("inverted index", HugeLongArray::memoryEstimation)
+            .build();
+    }
+
+    private final long capacity;
+
+    private HugeLongArray heap;
+    private HugeLongArray mapIndexTo;
+    private long size = 0;
+
+    protected HugeDoubleArray costValues;
+
+    /**
+     * Creates a new priority queue with the given capacity.
+     * The size is fixed, the queue cannot shrink or grow.
+     */
+    protected HugeLongPriorityQueue2(long capacity) {
+        long heapSize;
+        if (0 == capacity) {
+            // We allocate 1 extra to avoid if statement in top()
+            heapSize = 2;
+        } else {
+            // NOTE: we add +1 because all access to heap is
+            // 1-based not 0-based.  heap[0] is unused.
+            heapSize = capacity + 1;
+        }
+        this.capacity = capacity;
+        this.heap = HugeLongArray.newArray(heapSize);
+        this.mapIndexTo = HugeLongArray.newArray(heapSize);
+        this.costValues = HugeDoubleArray.newArray(capacity);
+    }
+
+    /**
+     * Adds the element at the specified position in the heap array
+     */
+    private void placeElement(long position, long element) {
+        heap.set(position, element);
+        mapIndexTo.set(element, position);
+    }
+
+    /**
+     * Adds an element associated with a cost to the queue in log(size) time.
+     */
+    public void add(long element, double cost) {
+        assert element < capacity;
+        addCost(element, cost);
+        size++;
+        placeElement(size, element);
+        upHeap(size);
+    }
+
+    /**
+     * Adds an element associated with a cost to the queue in log(size) time.
+     * If the element was already in the queue, it's cost are updated and the
+     * heap is reordered in log(size) time.
+     */
+    public void set(long element, double cost) {
+        assert element < capacity;
+        if (addCost(element, cost)) {
+            update(element);
+        } else {
+            size++;
+            placeElement(size, element);
+            upHeap(size);
+        }
+    }
+
+    /**
+     * Returns the cost associated with the given element.
+     * If the element has been popped from the queue, its
+     * latest cost value is being returned.
+     *
+     * @return The double cost value for the element. 0.0D if the element is not found.
+     */
+    public double cost(long element) {
+        return costValues.get(element);
+    }
+
+    /**
+     * Returns true, iff the element is contained in the queue.
+     */
+    public boolean containsElement(long element) {
+        return mapIndexTo.get(element) > 0;
+    }
+
+    /**
+     * Returns the element with the minimum cost from the queue in constant time.
+     */
+    public long top() {
+
+        if (isEmpty()) {
+            throw new IndexOutOfBoundsException("Priority Queue is empty");
+        }
+        return heap.get(1);
+    }
+
+    /**
+     * Removes and returns the element with the minimum cost from the queue in log(size) time.
+     */
+    public long pop() {
+        if (size > 0) {
+            long result = heap.get(1);    // save first value
+            placeElement(1, heap.get(size));    // move last to first
+            size--;
+            downHeap(1);           // adjust heap
+            removeCost(result);
+            return result;
+        } else {
+            return -1;
+        }
+    }
+
+    /**
+     * Returns the number of elements currently stored in the queue.
+     */
+    public long size() {
+        return size;
+    }
+
+    /**
+     * Removes all entries from the queue, releases all buffers.
+     * The queue can no longer be used afterwards.
+     */
+    public void release() {
+        size = 0;
+        heap = null;
+        mapIndexTo = null;
+        costValues.release();
+    }
+
+    /**
+     * Defines the ordering of the queue.
+     * Returns true iff {@code a} is strictly less than {@code b}.
+     * <p>
+     * The default behavior assumes a min queue, where the value with smallest cost is on top.
+     * To implement a max queue, return {@code b < a}.
+     * The resulting order is not stable.
+     */
+    protected abstract boolean lessThan(long a, long b);
+
+    /**
+     * Adds the given element to the queue.
+     * If the element already exists, it's cost is overridden.
+     *
+     * @return true, if the element already existed, false otherwise.
+     */
+    private boolean addCost(long element, double cost) {
+        boolean elementExists = mapIndexTo.get(element) > 0;
+        costValues.set(element, cost);
+        return elementExists;
+    }
+
+    /**
+     * @return true iff there are currently no elements stored in the queue.
+     */
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    /**
+     * Removes all entries from the queue.
+     */
+    public void clear() {
+        size = 0;
+        mapIndexTo.fill(0L);
+    }
+
+     long findElementPosition(long element) {
+         return mapIndexTo.get(element);
+
+     }
+
+    private boolean upHeap(long origPos) {
+        long i = origPos;
+        // save bottom node
+        long node = heap.get(i);
+        // find parent of current node in a 4-ary heap: (i + 2) / 4
+        long j = (i + 2) >>> 2;
+        while (j > 0 && lessThan(node, heap.get(j))) {
+            // shift parents down
+            placeElement(i, heap.get(j));
+            i = j;
+            // find new parent of swapped node
+            j = (j + 2) >>> 2;
+        }
+        // install saved node
+        placeElement(i, node);
+        return i != origPos;
+    }
+
+    private void downHeap(long i) {
+        // save top node
+        long node = heap.get(i);
+        // find first of up to four children: 4i - 2
+        long firstChild = (i << 2) - 2;
+        long smallest = smallestChild(firstChild);
+        while (smallest <= size && lessThan(heap.get(smallest), node)) {
+            // shift up smallest child
+            placeElement(i, heap.get(smallest));
+            i = smallest;
+            // find smallest child of swapped node
+            firstChild = (i << 2) - 2;
+            smallest = smallestChild(firstChild);
+        }
+        // install saved node
+        placeElement(i, node);
+    }
+
+    /**
+     * Returns the index of the smallest among the up to four children
+     * starting at {@code firstChild}. If no children exist, the returned
+     * index is greater than {@code size}.
+     */
+    private long smallestChild(long firstChild) {
+        long smallest = firstChild;
+        long last = firstChild + 3;
+        if (last > size) {
+            last = size;
+        }
+        for (long k = firstChild + 1; k <= last; k++) {
+            if (lessThan(heap.get(k), heap.get(smallest))) {
+                smallest = k;
+            }
+        }
+        return smallest;
+    }
+
+    private void update(long element) {
+        long pos = findElementPosition(element);
+        if (pos != 0) {
+            if (!upHeap(pos) && pos < size) {
+                downHeap(pos);
+            }
+        }
+    }
+
+    private void removeCost(long element) {
+        mapIndexTo.set(element, 0);
+    }
+
+    @Override
+    public PrimitiveIterator.OfLong iterator() {
+        return new PrimitiveIterator.OfLong() {
+
+            int i = 1;
+
+            @Override
+            public boolean hasNext() {
+                return i <= size;
+            }
+
+            /**
+             * @throws ArrayIndexOutOfBoundsException when the iterator is exhausted.
+             */
+            @Override
+            public long nextLong() {
+                return heap.get(i++);
+            }
+        };
+    }
+
+    /**
+     * Returns a non growing min priority queue,
+     * i.e. the element with the lowest priority is always on top.
+     */
+    public static HugeLongPriorityQueue2 min(long capacity) {
+        return new HugeLongPriorityQueue2(capacity) {
+            @Override
+            protected boolean lessThan(long a, long b) {
+                return costValues.get(a) < costValues.get(b);
+            }
+        };
+    }
+
+    /**
+     * Returns a non growing max priority queue,
+     * i.e. the element with the highest priority is always on top.
+     */
+    public static HugeLongPriorityQueue2 max(long capacity) {
+        return new HugeLongPriorityQueue2(capacity) {
+            @Override
+            protected boolean lessThan(long a, long b) {
+                return costValues.get(a) > costValues.get(b);
+            }
+        };
+    }
+
+
+    /**
+     * Returns the element in the i-th position of the heap
+     */
+    public long getIth(int i) {
+        return heap.get(i + 1);
+    }
+
+}
---
+++ core/src/test/java/org/neo4j/gds/core/utils/queue/HugeLongPriority2QueueTest.java
@@ -0,0 +1,232 @@
+/*
+ * Copyright (c) "Neo4j"
+ * Neo4j Sweden AB [http://neo4j.com]
+ *
+ * This file is part of Neo4j.
+ *
+ * Neo4j is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.neo4j.gds.core.utils.queue;
+
+import io.qala.datagen.RandomShortApi;
+import org.eclipse.collections.api.tuple.Pair;
+import org.eclipse.collections.impl.tuple.Tuples;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.stream.Collectors;
+
+import static io.qala.datagen.RandomShortApi.integer;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
+class HugeLongPriority2QueueTest {
+
+    @Test
+    void testIsEmpty() {
+        var capacity = integer(10, 20);
+        var queue = HugeLongPriorityQueue2.min(capacity);
+        assertThat(queue.size()).isEqualTo(0L);
+        assertThatThrownBy(() -> queue.top()
+        ).hasMessageContaining("empty");
+    }
+
+    @Test
+    void testClear() {
+        var maxSize = integer(3, 10);
+        var queue = HugeLongPriorityQueue2.min(maxSize);
+        var count = integer(3, maxSize);
+        for (long element = 0; element < count; element++) {
+            queue.add(element, integer(1, 5));
+        }
+        assertThat(queue.size()).isEqualTo(count);
+        queue.clear();
+        assertThat(queue.size()).isEqualTo(0L);
+    }
+
+    @Test
+    void testAdd() {
+        var size = 50;
+        var count = integer(5, size);
+        var queue = HugeLongPriorityQueue2.min(size);
+        var minElement = -1L;
+        var minCost = Double.POSITIVE_INFINITY;
+
+        for (long key = 0; key < count; key++) {
+            double weight = exclusiveDouble(0D, 100D);
+            if (weight < minCost) {
+                minCost = weight;
+                minElement = key;
+            }
+            queue.add(key, weight);
+            assertThat(queue.top()).isEqualTo(minElement);
+        }
+    }
+
+    @Test
+    void testAddAndPop() {
+        var capacity = 50;
+        var queue = HugeLongPriorityQueue2.min(capacity);
+        var elements = new ArrayList<Pair<Long, Double>>();
+        var count = integer(5, capacity);
+        var minElement = -1L;
+        var minCost = Double.POSITIVE_INFINITY;
+
+        for (long element = 0; element < count; element++) {
+            var weight = exclusiveDouble(0D, 100D);
+            if (weight < minCost) {
+                minCost = weight;
+                minElement = element;
+            }
+            queue.add(element, weight);
+            assertThat(queue.top()).isEqualTo(minElement);
+            elements.add(Tuples.pair(element, weight));
+        }
+
+        // PQ isn't stable for duplicate elements, so we have to
+        // test those with non strict ordering requirements
+        var byCost = elements
+            .stream()
+            .collect(Collectors.groupingBy(
+                Pair::getTwo,
+                Collectors.mapping(Pair::getOne, Collectors.toSet())
+            ));
+        var costGroups = byCost
+            .keySet()
+            .stream()
+            .sorted()
+            .toList();
+
+        for (var cost : costGroups) {
+            var allowedElements = byCost.get(cost);
+            while (!allowedElements.isEmpty()) {
+                long item = queue.pop();
+                assertThat(item).isIn(allowedElements);
+                allowedElements.remove(item);
+            }
+        }
+
+        assertThat(queue.isEmpty()).isTrue();
+    }
+
+    @Test
+    void testUpdateDecreasing() {
+        var capacity = 50;
+        var queue = HugeLongPriorityQueue2.min(capacity);
+
+        var count = integer(5, capacity);
+        var minCost = Double.POSITIVE_INFINITY;
+        for (long element = 0; element < count; element++) {
+            double weight = exclusiveDouble(50D, 100D);
+            if (weight < minCost) {
+                minCost = weight;
+            }
+            queue.add(element, weight);
+        }
+
+        for (long element = count - 1; element >= 0; element--) {
+            minCost = Math.nextDown(minCost);
+            queue.set(element, minCost);
+            assertThat(element).isEqualTo(queue.top());
+        }
+    }
+
+    @Test
+    void testUpdateIncreasing() {
+        var capacity = 50;
+        var queue = HugeLongPriorityQueue2.min(capacity);
+        int count = integer(5, capacity);
+        double maxCost = Double.NEGATIVE_INFINITY;
+
+        for (long element = 0; element < count; element++) {
+            var weight = exclusiveDouble(50D, 100D);
+            if (weight > maxCost) {
+                maxCost = weight;
+            }
+            queue.add(element, weight);
+        }
+
+        var top = queue.top();
+        for (var element = count - 1; element >= 0; element--) {
+            if (element == top) {
+                continue;
+            }
+            maxCost = Math.nextUp(maxCost);
+            queue.set(element, maxCost);
+            assertThat(top).isEqualTo(queue.top());
+        }
+    }
+
+    @Test
+    void testAddAndSetDifferentPrio() {
+        HugeLongPriorityQueue2 priorityQueue = HugeLongPriorityQueue2.min(2);
+        priorityQueue.add(1, 1);
+        priorityQueue.set(1, 2);
+        assertThat(priorityQueue.size()).isEqualTo(1);
+    }
+
+    @Test
+    void testAddAndSetSamePrio() {
+        HugeLongPriorityQueue2 priorityQueue = HugeLongPriorityQueue2.min(2);
+        priorityQueue.add(1, 1);
+        priorityQueue.set(1, 1);
+        assertThat(priorityQueue.size()).isEqualTo(1);
+    }
+
+    @Test
+    void testSetZeroCost() {
+        HugeLongPriorityQueue2 priorityQueue = HugeLongPriorityQueue2.min(2);
+        priorityQueue.set(1, 0);
+        priorityQueue.set(1, 0);
+        assertThat(priorityQueue.size()).isEqualTo(1);
+    }
+
+    @Test
+    void testMapIndex() {
+        HugeLongPriorityQueue2 priorityQueue = HugeLongPriorityQueue2.min(11);
+        for (int i = 0; i < 10; ++i) {
+            priorityQueue.add(i, 10 - i);
+        }
+        for (int i = 0; i < 10; ++i) {
+            var ith = priorityQueue.getIth(i);
+            assertThat(priorityQueue.findElementPosition(ith)).isEqualTo(i + 1);
+        }
+        assertThat(priorityQueue.findElementPosition(10)).isEqualTo(0);
+    }
+
+    @Test
+    void shouldKeepCostAfterDeleting() {
+        //This test verifies that after deleting an element .cost returns its last value
+        //If you change HLPQ so that this test fails, please revert the changes in
+        //https://github.com/neo-technology/graph-analytics/pull/7360 to use the 'costToParent' array again.
+        HugeLongPriorityQueue2 priorityQueue = HugeLongPriorityQueue2.min(11);
+        for (int i = 0; i < 10; ++i) {
+            priorityQueue.add(i, 10 - i);
+        }
+        assertThat(priorityQueue.cost(9)).isEqualTo(1);
+        for (int i = 0; i < 10; ++i) {
+            priorityQueue.pop();
+        }
+        for (int i = 0; i < 9; ++i) {
+            priorityQueue.add(i, 10 - i);
+        }
+        assertThat(priorityQueue.containsElement(9)).isFalse();
+        assertThat(priorityQueue.cost(9)).isEqualTo(1);
+    }
+
+    private double exclusiveDouble(double exclusiveMin, double exclusiveMax) {
+        return RandomShortApi.Double(Math.nextUp(exclusiveMin), exclusiveMax);
+    }
+}
