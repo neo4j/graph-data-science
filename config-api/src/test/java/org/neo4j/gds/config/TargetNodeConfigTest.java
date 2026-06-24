d6e98e996e7070e3ab2b4891673ac54f38ff5ac8
Martin Junghanns <m.junghanns@mailbox.org>
2026-06-24 10:39:30 +0200

    Fix left over import changes

--- config-api/src/test/java/org/neo4j/gds/config/TargetNodeConfigTest.java
+++ config-api/src/test/java/org/neo4j/gds/config/TargetNodeConfigTest.java
@@ -22,7 +22,7 @@ package org.neo4j.gds.config;
 import org.junit.jupiter.api.Test;
 import org.neo4j.gds.NodeLabel;
 import org.neo4j.gds.api.GraphStore;
-import org.neo4j.gds.api.IdMap;
+import org.neo4j.gds.api.nodes.IdMap;
 
 import java.util.List;
 
