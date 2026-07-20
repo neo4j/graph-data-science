75b242aa03b97f127d67f390ae764ec03c65a7a2
Martin Junghanns <m.junghanns@mailbox.org>
2026-07-20 11:48:45 +0200

    Revert enabling jol.magicFieldOffset
    Co-Authored-By: Jakob Hok <jakob.hok@neo4j.com>

--- memory-usage/src/main/java/org/neo4j/gds/mem/MemoryUsage.java
+++ memory-usage/src/main/java/org/neo4j/gds/mem/MemoryUsage.java
@@ -61,7 +61,6 @@ public final class MemoryUsage {
         @SuppressForbidden(reason = "we want to use system.out here")
         private static boolean isVmInfoAvailable() {
             macWorkaround();
-            javaRecordWorkaround();
 
             var sysOut = System.out;
             try {
@@ -90,16 +89,6 @@ public final class MemoryUsage {
             if (System.getProperty("os.name").contains("Mac")) {
                 System.setProperty("jol.skipHotspotSAAttach", "true");
             }
-        }
-
-        /**
-         * Workaround to make JOL work with Java records. Not ideal according to documentation:
-         * <a href="https://github.com/openjdk/jol/blob/db2cc744485031c8149991a004ab02f2ea9b24dd/jol-core/src/main/java/org/openjdk/jol/vm/HotspotUnsafe.java#L50">
-         *     HotspotUnsafe.java
-         * </a>
-         */
-        private static void javaRecordWorkaround() {
-            System.setProperty("jol.magicFieldOffset", "true");
         }
 
         private static final class NullOutputStream extends OutputStream {
