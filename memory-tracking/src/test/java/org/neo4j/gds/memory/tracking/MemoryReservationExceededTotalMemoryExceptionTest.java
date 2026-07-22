98c2d82c9a496cbd11233d72c41cf893e9162bb1
Veselin Nikolov <veselin.nikolov@neotechnology.com>
2026-07-22 09:31:15 +0100

    Implement getMessage for Memory Exception
    Co-authored-by: Ioannis Panagiotas <ioannis.panagiotas@neotechnology.com>

--- memory-tracking/src/test/java/org/neo4j/gds/memory/tracking/MemoryReservationExceededTotalMemoryExceptionTest.java
+++ memory-tracking/src/test/java/org/neo4j/gds/memory/tracking/MemoryReservationExceededTotalMemoryExceptionTest.java
@@ -19,27 +19,17 @@
  */
 package org.neo4j.gds.memory.tracking;
 
-import static org.neo4j.gds.utils.StringFormatting.formatWithLocale;
+import org.junit.jupiter.api.Test;
 
-public class MemoryReservationExceededTotalMemoryException extends MemoryGuardException {
+import static org.assertj.core.api.Assertions.assertThat;
 
-    private final long bytesRequired;
-    private final long bytesAvailable;
+class MemoryReservationExceededTotalMemoryExceptionTest {
 
-    public MemoryReservationExceededTotalMemoryException(long bytesRequired, long bytesAvailable) {
-        this.bytesRequired = bytesRequired;
-        this.bytesAvailable = bytesAvailable;
+    @Test
+    void shouldReturnProperMessage() {
+        var memoryException = new MemoryReservationExceededTotalMemoryException(4, 3);
+
+        assertThat(memoryException).hasMessage("Required memory 4 bytes exceeds the available 3 bytes.");
     }
 
-    public long bytesRequired() {
-        return bytesRequired;
-    }
-    public long bytesAvailable() {
-        return bytesAvailable;
-    }
-
-    @Override
-    public String getMessage() {
-        return formatWithLocale("Required memory %s bytes exceeds the available %s bytes.", bytesRequired, bytesAvailable);
-    }
 }
